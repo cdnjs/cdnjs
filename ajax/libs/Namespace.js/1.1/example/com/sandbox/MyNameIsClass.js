fd54736a5f9ecc13d6c12b79973412b7500eaed1
Freddy Knuth <halkon@gmail.com>
2012-05-09 13:38:39 -0500

    Removed unnecessary files

--- ajax/libs/Namespace.js/1.1/example/com/sandbox/MyNameIsClass.js
+++
@@ -1,9 +0,0 @@
-
-Namespace('com.sandbox', {
-
-	MyNameIsClass: function(name) {
-		com.sandbox.Console.log('my name is ' + name);
-		return {};
-	}
-
-});
--- ajax/libs/Namespace.js/1.1/example/com/sandbox/SayHiClass.js
+++
@@ -1,9 +0,0 @@
-
-Namespace('com.sandbox', {
-
-	SayHiClass: function() {
-		com.sandbox.Console.log('hi');
-		return {};
-	}
-
-});
