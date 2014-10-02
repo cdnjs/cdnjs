39ee3392a3df81ad3da636b0cd2a42323b65c160
Peter Dave Hello <hsu@peterdavehello.org>
2014-10-02 15:50:05 +0800

    add bootstrap-select v1.6.3

---
+++ ajax/libs/bootstrap-select/1.6.3/js/i18n/defaults-ro_RO.js
@@ -0,0 +1,15 @@
+/*!
+ * Bootstrap-select v1.6.2 (http://silviomoreto.github.io/bootstrap-select/)
+ *
+ * Copyright 2013-2014 bootstrap-select
+ * Licensed under MIT (https://github.com/silviomoreto/bootstrap-select/blob/master/LICENSE)
+ */
+(function ($) {
+  $.fn.selectpicker.defaults = {
+    noneSelectedText: 'Nu a fost selectat nimic',
+    noneResultsText: 'Nu exista niciun rezultat',
+    countSelectedText: '{0} din {1} selectat(e)',
+    maxOptionsText: ['Limita a fost atinsa ({n} {var} max)', 'Limita de grup a fost atinsa ({n} {var} max)', ['iteme', 'item']],
+    multipleSeparator: ', '
+  };
+}(jQuery));
