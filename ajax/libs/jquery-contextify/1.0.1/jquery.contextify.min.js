a7f3e70e66170a8418927e564bbee4e6fb97790e
Adam Bouqdib <adam@abemedia.co.uk>
2014-10-21 19:47:58 +0100

    Update jquery.contextify.min.js

--- ajax/libs/jquery-contextify/1.0.1/jquery.contextify.min.js
+++ ajax/libs/jquery-contextify/1.0.1/jquery.contextify.min.js
@@ -1,5 +1,5 @@
 /*!
-* jQuery Smoove v1.0.1 (http://contextify.donlabs.com)
+* jQuery Contextify v1.0.1 (http://contextify.donlabs.com)
 * Copyright (c) 2014 Adam Bouqdib
 * Licensed under GPL-2.0 (http://abemedia.co.uk/license) 
 */
