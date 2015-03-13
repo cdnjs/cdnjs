54076469261233045d90abf47309a469299c7d1b
David Tesler <david@tesler.com>
2015-03-13 11:36:37 -0700

    Added cleverbot.io.js

---
+++ ajax/libs/cleverbot.io/1.0/cleverbot.io.js
@@ -0,0 +1,55 @@
+if (!window.jQuery) {
+    var script = document.createElement('script');
+    script.type = "text/javascript";
+    script.src = "https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js";
+    document.getElementsByTagName('head')[0].appendChild(script);
+}
+const cleverbotIO = function (user, key) {
+    this.base_url = "https://cleverbot.io/1,0/";
+    this.user = user;
+    this.key = key;
+	this.setNick = function (nick) {
+		this.nick = nick;
+	}
+    this.create = function (callback) {
+        $.post(this.base_url + "create", {
+            user: this.user,
+            key: this.key,
+            nick: this.nick
+        }, function (data) {
+            data = JSON.parse(data);
+            if (data.status == "success") {
+                this.nick = data.nick;
+                callback(false, this.nick);
+            }
+            if (data.status == "Error: reference name already exists") {
+                callback(false, this.nick);
+            }
+            else {
+                console.log(data.status);
+                callback(true, data.status);
+            }
+        });
+    }
+    this.ask = function (input, callback) {
+        $.post(this.base_url + "ask", {
+            user: this.user,
+            key: this.key,
+            nick: this.nick,
+            text: input
+        }, function (data) {
+            data = JSON.parse(data);
+            if (data.status == "success") {
+                callback(false, data.response);
+            }
+            else {
+                console.log(data.status);
+                callback(true, data.status);
+            }
+        });
+    }
+
+}
+if (typeof cleverbot == "undefined") {
+    var cleverbot = cleverbotIO;
+}
