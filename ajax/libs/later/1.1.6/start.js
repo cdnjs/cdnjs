1a7e7106448776442186d896b46fe86be27996ee
Mathias Nestler <m@mathiasnestler.de>
2015-06-09 00:44:35 +0200

    Added later@1.1.6

---
+++ ajax/libs/later/1.1.6/start.js
@@ -0,0 +1,9 @@
+/**
+* Later.js 0.1.0
+* (c) 2013 Bill, BunKat LLC.
+* Later is freely distributable under the MIT license.
+* For all details and documentation:
+*     http://bunkat.github.com/later
+*/
+later = (function() {
+  var later = {version: "1.1.6"}; // semver
