ec84135d2801c9fb5320fbd7a51761553b32dfe1
Bernardo Silva <benny.stuff@gmail.com>
2015-10-31 18:17:55 +0000

    This fixes #5743 Add angular-truncate v2 initial versions.

---
+++ ajax/libs/angular-truncate-2/0.4.0/angular-truncate-2.js
@@ -0,0 +1,57 @@
+/**
+ * Angular Truncate 2 - Ellipsis for your templates
+ * @version v0.4.0 - 2015-10-31
+ * @link https://github.com/BernardoSilva/angular-truncate-2
+ * @author Bernardo Silva
+ * @license MIT License, http://www.opensource.org/licenses/MIT
+ */
+angular.module('truncate', [])
+    .filter('characters', function () {
+        return function (input, chars, breakOnWord) {
+            if (isNaN(chars)) return input;
+            if (chars <= 0) return '';
+            if (input && input.length > chars) {
+                input = input.substring(0, chars);
+
+                if (!breakOnWord) {
+                    var lastspace = input.lastIndexOf(' ');
+                    //get last space
+                    if (lastspace !== -1) {
+                        input = input.substr(0, lastspace);
+                    }
+                }else{
+                    while(input.charAt(input.length-1) === ' '){
+                        input = input.substr(0, input.length -1);
+                    }
+                }
+                return input + '…';
+            }
+            return input;
+        };
+    })
+    .filter('splitcharacters', function() {
+        return function (input, chars) {
+            if (isNaN(chars)) return input;
+            if (chars <= 0) return '';
+            if (input && input.length > chars) {
+                var prefix = input.substring(0, chars/2);
+                var postfix = input.substring(input.length-chars/2, input.length);
+                return prefix + '...' + postfix;
+            }
+            return input;
+        };
+    })
+    .filter('words', function () {
+        return function (input, words, postfix) {
+            if (isNaN(words)) return input;
+            if (words <= 0) return '';
+            if (typeof postfix === 'undefined') postfix = '…';
+            if (input) {
+                var inputWords = input.split(/\s+/);
+                if (inputWords.length > words) {
+                    input = inputWords.slice(0, words).join(' ') + postfix;
+                }
+            }
+            return input;
+        };
+    });
