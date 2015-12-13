d4505f9b89a4da58598ca91f76d60cd309a8000e
Evrim Çabuk <ecabuk@Evrims-MacBook-Pro-2.local>
2015-12-13 02:42:13 +0200

    Added jquery-ui-timepicker-addon@1.6.1

---
+++ ajax/libs/jquery-ui-timepicker-addon/1.6.1/i18n/jquery-ui-timepicker-no.js
@@ -0,0 +1,22 @@
+/* Norwegian translation for the jQuery Timepicker Addon */
+/* Written by Morten Hauan (http://hauan.me) */
+(function($) {
+	$.timepicker.regional['no'] = {
+		timeOnlyTitle: 'Velg tid',
+		timeText: 'Tid',
+		hourText: 'Time',
+		minuteText: 'Minutt',
+		secondText: 'Sekund',
+		millisecText: 'Millisekund',
+		microsecText: 'mikrosekund',
+		timezoneText: 'Tidssone',
+		currentText: 'Nå',
+		closeText: 'Lukk',
+		timeFormat: 'HH:mm',
+		timeSuffix: '',
+		amNames: ['am', 'AM', 'A'],
+		pmNames: ['pm', 'PM', 'P'],
+		isRTL: false
+	};
+	$.timepicker.setDefaults($.timepicker.regional['no']);
+})(jQuery);
