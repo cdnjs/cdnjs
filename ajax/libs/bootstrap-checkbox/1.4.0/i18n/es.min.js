e2964ab5df7f1bcd6b4d8f974fda83c230636f70
nolsherry <lovingkpp@gmail.com>
2016-04-16 18:07:00 +0800

    add bootstrap-checkbox v1.4.0 w/ auto-update
    close #7560, cc @vsn4ik

---
+++ ajax/libs/bootstrap-checkbox/1.4.0/i18n/es.min.js
@@ -0,0 +1,7 @@
+/*!
+ * Bootstrap-checkbox v1.4.0 (https://vsn4ik.github.io/bootstrap-checkbox/)
+ * Copyright 2013-2016 Vasily A. (https://github.com/vsn4ik)
+ * Licensed under the MIT license
+ */
+
+"use strict";!function(a){a.extend(a.fn.checkboxpicker.defaults,{offLabel:"No",onLabel:"Si",warningMessage:"Por favor, no utilice Bootstrap-checkbox para elementos tipo label."})}(jQuery);
---
+++ ajax/libs/bootstrap-checkbox/1.4.0/i18n/pt.min.js
@@ -0,0 +1,7 @@
+/*!
+ * Bootstrap-checkbox v1.4.0 (https://vsn4ik.github.io/bootstrap-checkbox/)
+ * Copyright 2013-2016 Vasily A. (https://github.com/vsn4ik)
+ * Licensed under the MIT license
+ */
+
+"use strict";!function(a){a.extend(a.fn.checkboxpicker.defaults,{offLabel:"Não",onLabel:"Sim",warningMessage:"Elementos do tipo label não são suportados dentro do Bootstrap-checkbox."})}(jQuery);
