77c54b37e3b0ef1d1cac820a3eca0f9f83a83038
PeterBot <PeterBot@users.noreply.github.com>
2017-07-05 02:01:00 +0800

    Add jquery.tablesorter v2.28.15

---
+++ ajax/libs/jquery.tablesorter/2.28.15/js/parsers/parser-image.min.js
@@ -0,0 +1,2 @@
+/*! Parser: image - new 7/17/2014 (v2.17.5) */
+!function(t){"use strict";t.tablesorter.addParser({id:"image",is:function(){return!1},format:function(r,e,i){return t(i).find("img").attr(e.config.imgAttr||"alt")||r},parsed:!0,type:"text"})}(jQuery);
