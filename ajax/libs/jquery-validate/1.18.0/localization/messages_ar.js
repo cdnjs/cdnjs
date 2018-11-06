a085763946c82936708613625f6c0798023d0c0c
Brahim Arkni <brahim.arkni@gmail.com>
2018-11-06 12:51:26 +0000

    Fix messages_ar.js file of jquery-validate@1.18.0
    Ref https://github.com/cdnjs/cdnjs/issues/13032#issuecomment-435388465

--- ajax/libs/jquery-validate/1.18.0/localization/messages_ar.js
+++ ajax/libs/jquery-validate/1.18.0/localization/messages_ar.js
@@ -1,3 +1,13 @@
+(function( factory ) {
+	if ( typeof define === "function" && define.amd ) {
+		define( ["jquery", "../jquery.validate"], factory );
+	} else if (typeof module === "object" && module.exports) {
+		module.exports = factory( require( "jquery" ) );
+	} else {
+		factory( jQuery );
+	}
+}(function( $ ) {
+
 /*
  * Translated default messages for the jQuery validation plugin.
  * Locale: AR (Arabic; العربية)
@@ -17,11 +27,6 @@ $.extend( $.validator.messages, {
 	maxlength: $.validator.format( "الحد الأقصى لعدد الحروف هو {0}" ),
 	minlength: $.validator.format( "الحد الأدنى لعدد الحروف هو {0}" ),
 	rangelength: $.validator.format( "عدد الحروف يجب أن يكون بين {0} و {1}" ),
-	range: $.validator.format( "رجاء إدخال عدد قيمته بين {0} و {1}" ),
-	max: $.validator.format( "رجاء إدخال عدد أقل من أو يساوي {0}" ),
-	min: $.validator.format( "رجاء إدخال عدد أكبر من أو يساوي {0}" )
-} );
-ين {0} و {1}" ),
 	range: $.validator.format( "رجاء إدخال عدد قيمته بين {0} و {1}" ),
 	max: $.validator.format( "رجاء إدخال عدد أقل من أو يساوي {0}" ),
 	min: $.validator.format( "رجاء إدخال عدد أكبر من أو يساوي {0}" )
