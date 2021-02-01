9434c1bbecd32603ae573eae84db461991f0bf8a
robocdnjs <cdnjs-github@cloudflare.com>
2021-02-01 18:19:11 +0000

    Add fasy (9.0.0)

---
+++ ajax/libs/fasy/9.0.0/esm/index.mjs
@@ -0,0 +1,5 @@
+/*! Fasy: index.mjs
+    v9.0.0 (c) 2021 Kyle Simpson
+    MIT License: http://getify.mit-license.org
+*/
+import concurrent from"./concurrent.mjs";import serial from"./serial.mjs";import transducers from"./transducers.mjs";export default{concurrent:concurrent,serial:serial,transducers:transducers};export{concurrent};export{serial};export{transducers};
