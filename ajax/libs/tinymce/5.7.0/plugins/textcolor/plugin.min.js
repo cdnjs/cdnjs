71fcc012e060ea4f5a7d2e451acde49c6efb80f0
robocdnjs <cdnjs-github@cloudflare.com>
2021-02-10 05:59:03 +0000

    Add tinymce (5.7.0)

---
+++ ajax/libs/tinymce/5.7.0/plugins/textcolor/plugin.min.js
@@ -0,0 +1,9 @@
+/**
+ * Copyright (c) Tiny Technologies, Inc. All rights reserved.
+ * Licensed under the LGPL or a commercial license.
+ * For LGPL see License.txt in the project root for license information.
+ * For commercial licenses see https://www.tiny.cloud/
+ *
+ * Version: 5.7.0 (2021-02-10)
+ */
+!function(){"use strict";tinymce.util.Tools.resolve("tinymce.PluginManager").add("textcolor",function(){console.warn("Text color plugin is now built in to the core editor, please remove it from your editor configuration")})}();
