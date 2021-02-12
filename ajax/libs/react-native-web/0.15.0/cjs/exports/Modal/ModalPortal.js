121055c2482b4d106cf029bd66a7bb0b29200fb1
robocdnjs <cdnjs-github@cloudflare.com>
2021-02-12 21:31:57 +0000

    Add react-native-web (0.15.0)

---
+++ ajax/libs/react-native-web/0.15.0/cjs/exports/Modal/ModalPortal.js
@@ -0,0 +1,53 @@
+"use strict";
+
+exports.__esModule = true;
+exports.default = void 0;
+
+var _react = require("react");
+
+var _ExecutionEnvironment = require("fbjs/lib/ExecutionEnvironment");
+
+var _reactDom = _interopRequireDefault(require("react-dom"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+/**
+ * Copyright (c) Nicolas Gallagher.
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * 
+ */
+function ModalPortal(props) {
+  var children = props.children;
+  var elementRef = (0, _react.useRef)(null);
+
+  if (_ExecutionEnvironment.canUseDOM && !elementRef.current) {
+    var element = document.createElement('div');
+
+    if (element && document.body) {
+      document.body.appendChild(element);
+      elementRef.current = element;
+    }
+  }
+
+  (0, _react.useEffect)(function () {
+    if (_ExecutionEnvironment.canUseDOM) {
+      return function () {
+        if (document.body && elementRef.current) {
+          document.body.removeChild(elementRef.current);
+          elementRef.current = null;
+        }
+      };
+    }
+  }, []);
+  return elementRef.current && _ExecutionEnvironment.canUseDOM ?
+  /*#__PURE__*/
+  _reactDom.default.createPortal(children, elementRef.current) : null;
+}
+
+var _default = ModalPortal;
+exports.default = _default;
+module.exports = exports.default;
---
+++ ajax/libs/react-native-web/0.15.0/cjs/vendor/react-native/NativeEventEmitter/index.js
@@ -0,0 +1,74 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @format
+ * 
+ */
+'use strict';
+
+exports.__esModule = true;
+exports.default = void 0;
+
+var _EventEmitter2 = _interopRequireDefault(require("../emitter/EventEmitter"));
+
+var _RCTDeviceEventEmitter = _interopRequireDefault(require("./RCTDeviceEventEmitter"));
+
+var _invariant = _interopRequireDefault(require("fbjs/lib/invariant"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _inheritsLoose(subClass, superClass) { subClass.prototype = Object.create(superClass.prototype); subClass.prototype.constructor = subClass; _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+/**
+ * Abstract base class for implementing event-emitting modules. This implements
+ * a subset of the standard EventEmitter node module API.
+ */
+var NativeEventEmitter =
+/*#__PURE__*/
+function (_EventEmitter) {
+  _inheritsLoose(NativeEventEmitter, _EventEmitter);
+
+  function NativeEventEmitter(nativeModule) {
+    return _EventEmitter.call(this, _RCTDeviceEventEmitter.default.sharedSubscriber) || this;
+  }
+
+  var _proto = NativeEventEmitter.prototype;
+
+  _proto.addListener = function addListener(eventType, listener, context) {
+    if (this._nativeModule != null) {
+      this._nativeModule.addListener(eventType);
+    }
+
+    return _EventEmitter.prototype.addListener.call(this, eventType, listener, context);
+  };
+
+  _proto.removeAllListeners = function removeAllListeners(eventType) {
+    (0, _invariant.default)(eventType, 'eventType argument is required.');
+    var count = this.listeners(eventType).length;
+
+    if (this._nativeModule != null) {
+      this._nativeModule.removeListeners(count);
+    }
+
+    _EventEmitter.prototype.removeAllListeners.call(this, eventType);
+  };
+
+  _proto.removeSubscription = function removeSubscription(subscription) {
+    if (this._nativeModule != null) {
+      this._nativeModule.removeListeners(1);
+    }
+
+    _EventEmitter.prototype.removeSubscription.call(this, subscription);
+  };
+
+  return NativeEventEmitter;
+}(_EventEmitter2.default);
+
+var _default = NativeEventEmitter;
+exports.default = _default;
+module.exports = exports.default;
