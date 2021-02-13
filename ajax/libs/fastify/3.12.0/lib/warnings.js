1565519db36f777e4b18263f7578ed479cc46aec
robocdnjs <cdnjs-github@cloudflare.com>
2021-02-13 13:22:06 +0000

    Add fastify (3.12.0)

---
+++ ajax/libs/fastify/3.12.0/lib/warnings.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const warning = require('fastify-warning')()
+
+/**
+ * Deprecation codes:
+ *   - FSTDEP001
+ *   - FSTDEP002
+ *   - FSTDEP003
+ *   - FSTDEP004
+ *   - FSTDEP005
+ */
+
+warning.create('FastifyDeprecation', 'FSTDEP001', 'You are accessing the Node.js core request object via "request.req", Use "request.raw" instead.')
+
+warning.create('FastifyDeprecation', 'FSTDEP002', 'You are accessing the Node.js core response object via "reply.res", Use "reply.raw" instead.')
+
+warning.create('FastifyDeprecation', 'FSTDEP003', 'You are using the legacy Content Type Parser function signature. Use the one suggested in the documentation instead.')
+
+warning.create('FastifyDeprecation', 'FSTDEP004', 'You are using the legacy preParsing hook signature. Use the one suggested in the documentation instead.')
+
+warning.create('FastifyDeprecation', 'FSTDEP005', 'You are accessing the deprecated "request.connection" property. Use "request.socket" instead.')
+
+warning.create('FastifyDeprecation', 'FSTDEP006', 'You are decorating Request/Reply with a reference type. This reference is shared amongst all requests. Use onRequest hook instead. Property: %s')
+
+warning.create('FastifyDeprecation', 'FSTDEP007', 'You are trying to set a HEAD route using "exposeHeadRoute" route flag when a sibling route is already set. See documentation for more info.')
+
+module.exports = warning
