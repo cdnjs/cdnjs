1ceb6e8c508e5b44e1b46d0d4a3af3f8483edd63
robocdnjs <cdnjs-github@cloudflare.com>
2021-02-14 00:51:19 +0000

    Add react-day-picker (8.0.0-beta.3)

---
+++ ajax/libs/react-day-picker/8.0.0-beta.3/index.js
@@ -0,0 +1,4 @@
+export * from './components';
+export * from './types';
+export * from './hooks';
+//# sourceMappingURL=index.js.map
---
+++ ajax/libs/react-day-picker/8.0.0-beta.3/index.min.js
@@ -0,0 +1,1 @@
+export*from"./components";export*from"./types";export*from"./hooks";
