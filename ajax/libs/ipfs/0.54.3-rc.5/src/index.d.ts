f2eb3dff3e785bdb34bdaafe85408d674d927f70
robocdnjs <cdnjs-github@cloudflare.com>
2021-02-22 16:43:39 +0000

    Add ipfs (0.54.3-rc.5)

---
+++ ajax/libs/ipfs/0.54.3-rc.5/src/index.d.ts
@@ -0,0 +1,17 @@
+declare const _exports: {
+    multihash: typeof import("multihashes/src/");
+    create: typeof import("ipfs-core/src/components").create;
+    crypto: typeof import("libp2p-crypto");
+    isIPFS: any;
+    CID: typeof import("cids");
+    multiaddr: typeof import("multiaddr");
+    multibase: any;
+    multihashing: typeof import("multihashing-async");
+    multicodec: any;
+    PeerId: typeof import("peer-id");
+    globSource: typeof import("ipfs-utils/src/files/glob-source");
+    urlSource: typeof import("ipfs-utils/src/files/url-source");
+};
+export = _exports;
+export type IPFS = import("ipfs-core/src/components");
+//# sourceMappingURL=index.d.ts.map
