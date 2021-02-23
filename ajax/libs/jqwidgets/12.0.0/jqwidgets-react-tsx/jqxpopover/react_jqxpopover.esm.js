edf3081730b9ebfae474210b0967b1c6b1b87495
robocdnjs <cdnjs-github@cloudflare.com>
2021-02-23 16:52:58 +0000

    Add jqwidgets (12.0.0)

---
+++ ajax/libs/jqwidgets/12.0.0/jqwidgets-react-tsx/jqxpopover/react_jqxpopover.esm.js
@@ -0,0 +1,117 @@
+import * as jqxcore from '../../jqwidgets/jqxcore';
+import * as jqxpopover from '../../jqwidgets/jqxpopover';
+import { createElement, PureComponent } from 'react';
+
+/*! *****************************************************************************
+Copyright (c) Microsoft Corporation. All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+this file except in compliance with the License. You may obtain a copy of the
+License at http://www.apache.org/licenses/LICENSE-2.0
+
+THIS CODE IS PROVIDED ON AN *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED
+WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+MERCHANTABLITY OR NON-INFRINGEMENT.
+
+See the Apache Version 2.0 License for specific language governing permissions
+and limitations under the License.
+***************************************************************************** */
+/* global Reflect, Promise */
+
+var extendStatics = function(d, b) {
+    extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return extendStatics(d, b);
+};
+
+function __extends(d, b) {
+    extendStatics(d, b);
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+}
+
+var JqxPopover = /** @class */ (function (_super) {
+    __extends(JqxPopover, _super);
+    function JqxPopover(props) {
+        var _this = _super.call(this, props) || this;
+        /* tslint:disable:variable-name */
+        _this._jqx = JQXLite;
+        _this._id = 'JqxPopover' + _this._jqx.generateID();
+        _this._componentSelector = '#' + _this._id;
+        _this.state = { lastProps: props };
+        return _this;
+    }
+    JqxPopover.getDerivedStateFromProps = function (props, state) {
+        if (!Object.is) {
+            Object.is = function (x, y) {
+                if (x === y) {
+                    return x !== 0 || 1 / x === 1 / y;
+                }
+                else {
+                    return x !== x && y !== y;
+                }
+            };
+        }
+        var areEqual = Object.is(props, state.lastProps);
+        if (!areEqual) {
+            var newState = { lastProps: props };
+            return newState;
+        }
+        return null;
+    };
+    JqxPopover.prototype.componentDidMount = function () {
+        var widgetOptions = this._manageProps();
+        this._jqx(this._componentSelector).jqxPopover(widgetOptions);
+        this._wireEvents();
+    };
+    JqxPopover.prototype.componentDidUpdate = function () {
+        var widgetOptions = this._manageProps();
+        this.setOptions(widgetOptions);
+        this._wireEvents();
+    };
+    JqxPopover.prototype.render = function () {
+        return (createElement("div", { id: this._id, className: this.props.className, style: this.props.style }, this.props.children));
+    };
+    JqxPopover.prototype.setOptions = function (options) {
+        this._jqx(this._componentSelector).jqxPopover(options);
+    };
+    JqxPopover.prototype.getOptions = function (option) {
+        return this._jqx(this._componentSelector).jqxPopover(option);
+    };
+    JqxPopover.prototype.close = function () {
+        this._jqx(this._componentSelector).jqxPopover('close');
+    };
+    JqxPopover.prototype.destroy = function () {
+        this._jqx(this._componentSelector).jqxPopover('destroy');
+    };
+    JqxPopover.prototype.open = function () {
+        this._jqx(this._componentSelector).jqxPopover('open');
+    };
+    JqxPopover.prototype._manageProps = function () {
+        var widgetProps = ['arrowOffsetValue', 'animationOpenDelay', 'animationCloseDelay', 'autoClose', 'animationType', 'height', 'initContent', 'isModal', 'offset', 'position', 'rtl', 'selector', 'showArrow', 'showCloseButton', 'width', 'title', 'theme'];
+        var options = {};
+        for (var prop in this.props) {
+            if (widgetProps.indexOf(prop) !== -1) {
+                options[prop] = this.props[prop];
+            }
+        }
+        return options;
+    };
+    JqxPopover.prototype._wireEvents = function () {
+        for (var prop in this.props) {
+            if (prop.indexOf('on') === 0) {
+                var originalEventName = prop.slice(2);
+                originalEventName = originalEventName.charAt(0).toLowerCase() + originalEventName.slice(1);
+                this._jqx(this._componentSelector).off(originalEventName);
+                this._jqx(this._componentSelector).on(originalEventName, this.props[prop]);
+            }
+        }
+    };
+    return JqxPopover;
+}(PureComponent));
+var jqx = window.jqx;
+var JQXLite = window.JQXLite;
+
+export default JqxPopover;
+export { jqx, JQXLite };
---
+++ ajax/libs/jqwidgets/12.0.0/jqwidgets-vue/vue_jqxdropdownbutton.vue
@@ -0,0 +1,400 @@
+<template>
+    <div v-bind:id="id">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    import '../jqwidgets/jqxcore.js';
+    import '../jqwidgets/jqxbuttons.js';
+    import '../jqwidgets/jqxdropdownbutton.js';
+
+    export default {
+        props: {
+            animationType: String,
+            arrowSize: Number,
+            autoOpen: Boolean,
+            closeDelay: Number,
+            disabled: Boolean,
+            dropDownHorizontalAlignment: String,
+            dropDownVerticalAlignment: String,
+            dropDownWidth: [String, Number],
+            enableBrowserBoundsDetection: Boolean,
+            height: [Number, String],
+            initContent: Function,
+            openDelay: Number,
+            popupZIndex: Number,
+            rtl: Boolean,
+            template: String,
+            theme: String,
+            width: [Number, String],
+            autoCreate: {
+                default: true,
+                type: Boolean
+            }
+        },
+        created: function () {
+            this.id = 'jqxDropDownButton' + JQXLite.generateID();
+            this.componentSelector = '#' + this.id;
+        },
+        mounted: function () {
+            if (this.autoCreate) this.__createComponent__();
+        },
+        methods: {
+            createComponent: function (options) {
+                if (!this.autoCreate) this.__createComponent__(options)
+                else console.warn('Component is already created! If you want to use createComponent, please set "autoCreate" property to "false".');
+            },
+            setOptions: function (options) {
+                JQXLite(this.componentSelector).jqxDropDownButton(options);
+            },
+            getOptions: function () {
+                const usedProps = Object.keys(this.__manageProps__());
+                const resultToReturn = {};
+                for (let i = 0; i < usedProps.length; i++) {
+                    resultToReturn[usedProps[i]] = JQXLite(this.componentSelector).jqxDropDownButton(usedProps[i]);
+                }
+                return resultToReturn;
+            },
+            close: function() {
+                JQXLite(this.componentSelector).jqxDropDownButton('close');  
+            },
+            destroy: function() {
+                JQXLite(this.componentSelector).jqxDropDownButton('destroy');  
+            },
+            focus: function() {
+                JQXLite(this.componentSelector).jqxDropDownButton('focus');  
+            },
+            getContent: function() {
+                return JQXLite(this.componentSelector).jqxDropDownButton('getContent');  
+            },
+            isOpened: function() {
+                return JQXLite(this.componentSelector).jqxDropDownButton('isOpened');  
+            },
+            open: function() {
+                JQXLite(this.componentSelector).jqxDropDownButton('open');  
+            },
+            setContent: function(content) {
+                JQXLite(this.componentSelector).jqxDropDownButton('setContent', content);  
+            },
+            _animationType: function(arg) {
+                if (arg !== undefined) {
+                    JQXLite(this.componentSelector).jqxDropDownButton('animationType', arg)
+                } else {
+                    return JQXLite(this.componentSelector).jqxDropDownButton('animationType');
+                }
+            },
+            _arrowSize: function(arg) {
+                if (arg !== undefined) {
+                    JQXLite(this.componentSelector).jqxDropDownButton('arrowSize', arg)
+                } else {
+                    return JQXLite(this.componentSelector).jqxDropDownButton('arrowSize');
+                }
+            },
+            _autoOpen: function(arg) {
+                if (arg !== undefined) {
+                    JQXLite(this.componentSelector).jqxDropDownButton('autoOpen', arg)
+                } else {
+                    return JQXLite(this.componentSelector).jqxDropDownButton('autoOpen');
+                }
+            },
+            _closeDelay: function(arg) {
+                if (arg !== undefined) {
+                    JQXLite(this.componentSelector).jqxDropDownButton('closeDelay', arg)
+                } else {
+                    return JQXLite(this.componentSelector).jqxDropDownButton('closeDelay');
+                }
+            },
+            _disabled: function(arg) {
+                if (arg !== undefined) {
+                    JQXLite(this.componentSelector).jqxDropDownButton('disabled', arg)
+                } else {
+                    return JQXLite(this.componentSelector).jqxDropDownButton('disabled');
+                }
+            },
+            _dropDownHorizontalAlignment: function(arg) {
+                if (arg !== undefined) {
+                    JQXLite(this.componentSelector).jqxDropDownButton('dropDownHorizontalAlignment', arg)
+                } else {
+                    return JQXLite(this.componentSelector).jqxDropDownButton('dropDownHorizontalAlignment');
+                }
+            },
+            _dropDownVerticalAlignment: function(arg) {
+                if (arg !== undefined) {
+                    JQXLite(this.componentSelector).jqxDropDownButton('dropDownVerticalAlignment', arg)
+                } else {
+                    return JQXLite(this.componentSelector).jqxDropDownButton('dropDownVerticalAlignment');
+                }
+            },
+            _dropDownWidth: function(arg) {
+                if (arg !== undefined) {
+                    JQXLite(this.componentSelector).jqxDropDownButton('dropDownWidth', arg)
+                } else {
+                    return JQXLite(this.componentSelector).jqxDropDownButton('dropDownWidth');
+                }
+            },
+            _enableBrowserBoundsDetection: function(arg) {
+                if (arg !== undefined) {
+                    JQXLite(this.componentSelector).jqxDropDownButton('enableBrowserBoundsDetection', arg)
+                } else {
+                    return JQXLite(this.componentSelector).jqxDropDownButton('enableBrowserBoundsDetection');
+                }
+            },
+            _height: function(arg) {
+                if (arg !== undefined) {
+                    JQXLite(this.componentSelector).jqxDropDownButton('height', arg)
+                } else {
+                    return JQXLite(this.componentSelector).jqxDropDownButton('height');
+                }
+            },
+            _initContent: function(arg) {
+                if (arg !== undefined) {
+                    JQXLite(this.componentSelector).jqxDropDownButton('initContent', arg)
+                } else {
+                    return JQXLite(this.componentSelector).jqxDropDownButton('initContent');
+                }
+            },
+            _openDelay: function(arg) {
+                if (arg !== undefined) {
+                    JQXLite(this.componentSelector).jqxDropDownButton('openDelay', arg)
+                } else {
+                    return JQXLite(this.componentSelector).jqxDropDownButton('openDelay');
+                }
+            },
+            _popupZIndex: function(arg) {
+                if (arg !== undefined) {
+                    JQXLite(this.componentSelector).jqxDropDownButton('popupZIndex', arg)
+                } else {
+                    return JQXLite(this.componentSelector).jqxDropDownButton('popupZIndex');
+                }
+            },
+            _rtl: function(arg) {
+                if (arg !== undefined) {
+                    JQXLite(this.componentSelector).jqxDropDownButton('rtl', arg)
+                } else {
+                    return JQXLite(this.componentSelector).jqxDropDownButton('rtl');
+                }
+            },
+            _template: function(arg) {
+                if (arg !== undefined) {
+                    JQXLite(this.componentSelector).jqxDropDownButton('template', arg)
+                } else {
+                    return JQXLite(this.componentSelector).jqxDropDownButton('template');
+                }
+            },
+            _theme: function(arg) {
+                if (arg !== undefined) {
+                    JQXLite(this.componentSelector).jqxDropDownButton('theme', arg)
+                } else {
+                    return JQXLite(this.componentSelector).jqxDropDownButton('theme');
+                }
+            },
+            _width: function(arg) {
+                if (arg !== undefined) {
+                    JQXLite(this.componentSelector).jqxDropDownButton('width', arg)
+                } else {
+                    return JQXLite(this.componentSelector).jqxDropDownButton('width');
+                }
+            },
+            __createComponent__: function (options) {
+                let widgetOptions;
+                options ? widgetOptions = options : widgetOptions = this.__manageProps__();
+                JQXLite(this.componentSelector).jqxDropDownButton(widgetOptions);
+                this.__extendProps__();
+                this.__wireEvents__();
+            },
+            __manageProps__: function () {
+                const widgetProps = ['animationType','arrowSize','autoOpen','closeDelay','disabled','dropDownHorizontalAlignment','dropDownVerticalAlignment','dropDownWidth','enableBrowserBoundsDetection','height','initContent','openDelay','popupZIndex','rtl','template','theme','width'];
+                const componentProps = this.$options.propsData;
+                let options = {};
+
+                for (let prop in componentProps) {
+                    if (widgetProps.indexOf(prop) !== -1) {
+                        options[prop] = componentProps[prop];
+                    }
+                }
+                return options;
+            },
+            __extendProps__: function () {
+                const that = this;
+
+                Object.defineProperty(that, 'animationType', {
+                    get: function() {
+                        return that._animationType();
+                    },
+                    set: function(newValue) {
+                        that._animationType(newValue);
+                    },
+                    enumerable: true,
+                    configurable: true
+                });
+                Object.defineProperty(that, 'arrowSize', {
+                    get: function() {
+                        return that._arrowSize();
+                    },
+                    set: function(newValue) {
+                        that._arrowSize(newValue);
+                    },
+                    enumerable: true,
+                    configurable: true
+                });
+                Object.defineProperty(that, 'autoOpen', {
+                    get: function() {
+                        return that._autoOpen();
+                    },
+                    set: function(newValue) {
+                        that._autoOpen(newValue);
+                    },
+                    enumerable: true,
+                    configurable: true
+                });
+                Object.defineProperty(that, 'closeDelay', {
+                    get: function() {
+                        return that._closeDelay();
+                    },
+                    set: function(newValue) {
+                        that._closeDelay(newValue);
+                    },
+                    enumerable: true,
+                    configurable: true
+                });
+                Object.defineProperty(that, 'disabled', {
+                    get: function() {
+                        return that._disabled();
+                    },
+                    set: function(newValue) {
+                        that._disabled(newValue);
+                    },
+                    enumerable: true,
+                    configurable: true
+                });
+                Object.defineProperty(that, 'dropDownHorizontalAlignment', {
+                    get: function() {
+                        return that._dropDownHorizontalAlignment();
+                    },
+                    set: function(newValue) {
+                        that._dropDownHorizontalAlignment(newValue);
+                    },
+                    enumerable: true,
+                    configurable: true
+                });
+                Object.defineProperty(that, 'dropDownVerticalAlignment', {
+                    get: function() {
+                        return that._dropDownVerticalAlignment();
+                    },
+                    set: function(newValue) {
+                        that._dropDownVerticalAlignment(newValue);
+                    },
+                    enumerable: true,
+                    configurable: true
+                });
+                Object.defineProperty(that, 'dropDownWidth', {
+                    get: function() {
+                        return that._dropDownWidth();
+                    },
+                    set: function(newValue) {
+                        that._dropDownWidth(newValue);
+                    },
+                    enumerable: true,
+                    configurable: true
+                });
+                Object.defineProperty(that, 'enableBrowserBoundsDetection', {
+                    get: function() {
+                        return that._enableBrowserBoundsDetection();
+                    },
+                    set: function(newValue) {
+                        that._enableBrowserBoundsDetection(newValue);
+                    },
+                    enumerable: true,
+                    configurable: true
+                });
+                Object.defineProperty(that, 'height', {
+                    get: function() {
+                        return that._height();
+                    },
+                    set: function(newValue) {
+                        that._height(newValue);
+                    },
+                    enumerable: true,
+                    configurable: true
+                });
+                Object.defineProperty(that, 'initContent', {
+                    get: function() {
+                        return that._initContent();
+                    },
+                    set: function(newValue) {
+                        that._initContent(newValue);
+                    },
+                    enumerable: true,
+                    configurable: true
+                });
+                Object.defineProperty(that, 'openDelay', {
+                    get: function() {
+                        return that._openDelay();
+                    },
+                    set: function(newValue) {
+                        that._openDelay(newValue);
+                    },
+                    enumerable: true,
+                    configurable: true
+                });
+                Object.defineProperty(that, 'popupZIndex', {
+                    get: function() {
+                        return that._popupZIndex();
+                    },
+                    set: function(newValue) {
+                        that._popupZIndex(newValue);
+                    },
+                    enumerable: true,
+                    configurable: true
+                });
+                Object.defineProperty(that, 'rtl', {
+                    get: function() {
+                        return that._rtl();
+                    },
+                    set: function(newValue) {
+                        that._rtl(newValue);
+                    },
+                    enumerable: true,
+                    configurable: true
+                });
+                Object.defineProperty(that, 'template', {
+                    get: function() {
+                        return that._template();
+                    },
+                    set: function(newValue) {
+                        that._template(newValue);
+                    },
+                    enumerable: true,
+                    configurable: true
+                });
+                Object.defineProperty(that, 'theme', {
+                    get: function() {
+                        return that._theme();
+                    },
+                    set: function(newValue) {
+                        that._theme(newValue);
+                    },
+                    enumerable: true,
+                    configurable: true
+                });
+                Object.defineProperty(that, 'width', {
+                    get: function() {
+                        return that._width();
+                    },
+                    set: function(newValue) {
+                        that._width(newValue);
+                    },
+                    enumerable: true,
+                    configurable: true
+                });
+            },
+            __wireEvents__: function () {
+                const that = this;
+
+                JQXLite(this.componentSelector).on('close', function (event) { that.$emit('close', event); });
+                JQXLite(this.componentSelector).on('open', function (event) { that.$emit('open', event); });
+            }
+        }
+    }
+</script>
