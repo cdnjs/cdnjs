fd2d6ad0ce544de4c385174a2b97731deeadfe5b
robocdnjs <cdnjs-github@cloudflare.com>
2021-02-24 23:26:28 +0000

    Add sceditor (3.0.0)

---
+++ ajax/libs/sceditor/3.0.0/plugins/plaintext.js
@@ -0,0 +1,3 @@
+/* SCEditor v3.0.0 | (C) 2017, Sam Clarke | sceditor.com/license */
+
+!function(t){"use strict";var i=t.utils.extend;t.plugins.plaintext=function(){var e=!0;this.init=function(){var t=this.commands,n=this.opts;n&&n.plaintext&&n.plaintext.addButton&&(e=n.plaintext.enabled,t.pastetext=i(t.pastetext||{},{state:function(){return e?1:0},exec:function(){e=!e}}))},this.signalPasteRaw=function(t){var n;e&&(t.html&&!t.text&&((n=document.createElement("div")).innerHTML=t.html,t.text=n.innerText),t.html=null)}}}(sceditor);
