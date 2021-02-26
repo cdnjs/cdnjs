8bf2eed8728bf4bbf7ec1f24869186322c791e6b
robocdnjs <cdnjs-github@cloudflare.com>
2021-02-26 21:46:06 +0000

    Add popper.js (2.8.6)

---
+++ ajax/libs/popper.js/2.8.6/esm/dom-utils/getWindow.js
@@ -0,0 +1,15 @@
+/*:: import type { Window } from '../types'; */
+
+/*:: declare function getWindow(node: Node | Window): Window; */
+export default function getWindow(node) {
+  if (node == null) {
+    return window;
+  }
+
+  if (node.toString() !== '[object Window]') {
+    var ownerDocument = node.ownerDocument;
+    return ownerDocument ? ownerDocument.defaultView || window : window;
+  }
+
+  return node;
+}
