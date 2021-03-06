93a6663c2f48ab3b682642fb34a3a185bd6b47e7
robocdnjs <cdnjs-github@cloudflare.com>
2021-03-06 19:59:17 +0000

    Add tarteaucitronjs (1.8.5)

---
+++ ajax/libs/tarteaucitronjs/1.8.5/lang/tarteaucitron.no.js
@@ -0,0 +1,80 @@
+/*global tarteaucitron */
+tarteaucitron.lang = {
+	"middleBarHead" : "☝ 🍪",
+	"adblock"       : "Hei! Dette nettstedet er gjennomsiktig og lar deg kontrollere hvilke tredjeparts tjenester du vil tillate.",
+	"adblock_call"  : "For å gjøre endringer, vær så snill å deaktivere annonse-blokkering.",
+	"reload"        : "Oppdater side",
+
+	"alertBigScroll" : "Ved å fortsette å scrolle,",
+	"alertBigClick"  : "Dersom du fortsetter å bruke dette nettstedet,",
+	"alertBig"       : "tillater du alle tredjeparts tjenester",
+
+	"alertBigPrivacy" : "Dette nettstedet bruker informasjonskapsler og gir deg kontroll over hva du vil aktivere",
+	"alertSmall"      : "Administrer tjenester",
+	"personalize"     : "Personaliser",
+	"acceptAll"       : "OK, aksepter alt",
+	"close"           : "Steng",
+
+	"privacyUrl" : "Personvernregler",
+
+	"all" : "Preferanse for alle tjenester",
+
+	"info"             : "Beskytt ditt personvern",
+	"disclaimer"       : "Ved å tillate disse tredjepartstjenestene godtar du informasjonskapslene deres og bruken av sporingsteknologier som er nødvendige for at de skal fungere korrekt.",
+	"allow"            : "Tillat",
+	"deny"             : "Ikke tillat",
+	"noCookie"         : "Denne tjenesten bruker ikke informasjonskapsel.",
+	"useCookie"        : "Denne tjenesten kan installeres",
+	"useCookieCurrent" : "Denne tjenesten er installert",
+	"useNoCookie"      : "TDenne tjenesten har ikke installert noen informasjonskapsel.",
+	"more"             : "Les mer",
+	"source"           : "Se den offisielle nettsiden",
+	"credit"           : "Informasjonskapsler styres av tarteaucitron.js",
+	"noServices"       : "Dette nettstedet bruker ingen informasjonskapsler som krever ditt samtykke.",
+
+	"toggleInfoBox" : "Vis / skjul informasjon om lagring av informasjonskapsler",
+	"title"         : "Panel for informasjonskapsler",
+	"cookieDetail"  : "Informasjon om informasjonskapsler for",
+	"ourSite"       : "på nettstedet vårt",
+	"newWindow"     : "(nytt vindu)",
+	"allowAll"      : "Tillat alle informasjonskapsler",
+	"denyAll"       : "Nekt alle informasjonskapsler",
+
+	"fallback" : "er skrudd av.",
+
+	"ads"      : {
+		"title"   : "Annonsenettverk",
+		"details" : "Annonsenettverket kan generere inntekter ved å selge reklameplass på nettstedet."
+	},
+	"analytic" : {
+		"title"   : "Målgruppe målinger",
+		"details" : "Målgruppens målingstjenester ble brukt til å generere nyttig informasjon for å forbedre nettstedet."
+	},
+	"social"   : {
+		"title"   : "Sosiale nettverk",
+		"details" : "Sosiale nettverk kan forbedre brukervennligheten til nettstedet og bidra til å markedsføre det."
+	},
+	"video"    : {
+		"title"   : "Video",
+		"details" : "Videodelingstjenester hjelper til med å legge til rik media på nettstedet og øke synligheten."
+	},
+	"comment"  : {
+		"title"   : "Kommentarer",
+		"details" : "Kommentaradministratorer legger til rette for arkivering av kommentarer og bekjemper spam."
+	},
+	"support"  : {
+		"title"   : "Brukerstøtte",
+		"details" : "Brukerstøtte lar deg komme i kontakt med nettstedsteamet og bidra til å forbedre nettstedet."
+	},
+	"api"      : {
+		"title"   : "API-er",
+		"details" : "API-er brukes til å laste inn skript: geolokalisering, søkemotorer, oversettelser, ..."
+	},
+	"other"    : {
+		"title"   : "Annet",
+		"details" : "Tjenester for å vise innhold på nettet."
+	},
+
+	"mandatoryTitle" : "Obligatoriske informasjonskapsler",
+	"mandatoryText"  : "Dette nettstedet bruker obligatoriske informasjonskapsler som er nødvendige for at nettstedet skal fungere som det skal. Disse kan ikke deaktiveres."
+};
---
+++ ajax/libs/tarteaucitronjs/1.8.5/lang/tarteaucitron.oc.js
@@ -0,0 +1,81 @@
+/*global tarteaucitron */
+tarteaucitron.lang = {
+
+    "middleBarHead": "☝ 🍪",
+    "adblock": "Bonjorn! Aqueste site jòga la transparéncia e vos dòna la possibilitat de causir los servicis tèrces a activar.",
+    "adblock_call": "Mercés de desactivar vòstre adblocker per començar la personalizacion.",
+    "reload": "Recargar la pagina",
+    
+    "alertBigScroll": "En contunhant de desfilar,",
+    "alertBigClick": "En seguissent vòstra navigacion,",
+    "alertBig": "acceptatz l'utilizacion de servicis tèrces que pòdon installar de cookies",
+    
+    "alertBigPrivacy": "Aqueste site utiliza de cookies e vos dòna lo contraròtle sus çò que volètz activar",
+    "alertSmall": "Gestion dels servicis",
+    "acceptAll": "OK, tot acceptar",
+    "personalize": "Personalizar",
+    "close": "Tampar",
+
+    "privacyUrl": "Politica de confidencialitat",
+
+    "all": "Preferéncias per totes los servicis",
+
+    "info": "Proteccion de vòstra vida privada",
+    "disclaimer": "En autorizant aquestes servicis tèrces, acceptatz lo depaus e la lectura de cookies e l'utilizacion de tecnologias de seguiment necessaris a lor bon foncionament.",
+    "allow": "Autorizar",
+    "deny": "Interdire",
+    "noCookie": "Aqueste servici daissa pas cap de cookies.",
+    "useCookie": "Aqueste servici pòt daissar",
+    "useCookieCurrent": "Aqueste servici a daissat",
+    "useNoCookie": "Aqueste servici a pas daissat cap de cookies.",
+    "more": "Ne saber mai",
+    "source": "Veire lo site oficial",
+    "credit": "Gestion dels cookies per tarteaucitron.js",
+    "noServices": "Aqueste site utiliza pas cap de cookies que demandan vòstre consentiment.",
+
+    "toggleInfoBox": "Mostrar/amagar las informacions sus l'emmagazinatge dels cookies",
+    "title": "Panèl de gestion dels cookies",
+    "cookieDetail": "Detalh dels cookies",
+    "ourSite": "sus nòstre site",
+    "newWindow": "(novèla fenèstra)",
+    "allowAll": "Autorizar totes los cookies",
+    "denyAll": "Interdire totes los cookies",
+    
+    "fallback": "es desactivat.",
+
+    "ads": {
+        "title": "Regias publicitàrias",
+        "details": "Las regias publicitàrias permeton de gerir de revenguts en comercialisant los espacis publicitaris del site."
+    },
+    "analytic": {
+        "title": "Mesura d'audiéncia",
+        "details": "Los servicis de mesura d'audiénce permeton de generar d'estatisticas de frequentacion utilas per melhorar lo site."
+    },
+    "social": {
+        "title": "Malhums socials",
+        "details": "Los malhums socials permeton de melhorar la convivéncia del site e d'ajudar sa promocion via los partatges."
+    },
+    "video": {
+        "title": "Vidèos",
+        "details": "Los servicis de partatge de vidèo permeton d'enriquir lo site de contengut multimèdia e aumentan sa visibilitat."
+    },
+    "comment": {
+        "title": "Comentaris",
+        "details": "Los gestionaris de comentaris facilitan lo depaus de vòstres comentaris e lutan contra los messatges indesirables."
+    },
+    "support": {
+        "title": "Assiténcia",
+        "details": "Los servicis d'assisténcia vos permeton de dintrar en contacte amb l'equipa del site e d'ajudar a son melhorament."
+    },
+    "api": {
+        "title": "APIs",
+        "details": "Las APIs permeton de cargar de scripts : geolocalizacion, motors de recèrca, traduccions, ..."
+    },
+    "other": {
+        "title": "Autre",
+        "details": "Servicis que cèrcan a afichar de contengut web."
+    },
+    
+    "mandatoryTitle": "Cookies necessaris",
+    "mandatoryText": "Aqueste site utiliza de cookies necessaris pel seu pròpri foncionament que pòdon pas èsser desactivats."
+};
