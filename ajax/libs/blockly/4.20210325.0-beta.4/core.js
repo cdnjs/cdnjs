32407c7c33f0eb19a5df74fe5d58e437c05ba1fb
robocdnjs <cdnjs-github@cloudflare.com>
2021-03-12 19:13:47 +0000

    Add blockly (4.20210325.0-beta.4)

---
+++ ajax/libs/blockly/4.20210325.0-beta.4/core.js
@@ -0,0 +1,40 @@
+/* eslint-disable */
+(function (Blockly){
+/**
+ * @license
+ * Copyright 2019 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @fileoverview Blockly core module for Node. It includes blockly-node.js
+ *               and adds a helper method for setting the locale.
+ */
+
+/* eslint-disable */
+'use strict';
+
+// Add a helper method to set the Blockly locale.
+Blockly.setLocale = function (locale) {
+  Blockly.Msg = Blockly.Msg || {};
+  Object.keys(locale).forEach(function (k) {
+    Blockly.Msg[k] = locale[k];
+  });
+};
+
+// Override textToDomDocument and provide Node.js alternatives to DOMParser and
+// XMLSerializer.
+if (typeof Blockly.utils.global.document !== 'object') {
+  var JSDOM = require('jsdom').JSDOM;
+  var dom = new JSDOM();
+  Blockly.utils.global.DOMParser = dom.window.DOMParser;
+  Blockly.utils.global.XMLSerializer = dom.window.XMLSerializer;
+  var doc = Blockly.utils.xml.textToDomDocument(
+    '<xml xmlns="https://developers.google.com/blockly/xml"></xml>');
+  Blockly.utils.xml.document = function() {
+    return doc;
+  };
+}
+
+module.exports = Blockly;
+})(require('./blockly')); 
