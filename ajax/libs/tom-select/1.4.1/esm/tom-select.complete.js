69167ab36bf7cb5a60ea79f5717597e571a9236e
robocdnjs <cdnjs-github@cloudflare.com>
2021-03-24 17:22:43 +0000

    Add tom-select (1.4.1)

---
+++ ajax/libs/tom-select/1.4.1/esm/tom-select.complete.js
@@ -0,0 +1,20 @@
+/**
+* Tom Select v1.4.1
+* Licensed under the Apache License, Version 2.0 (the "License");
+*/
+
+import TomSelect from './tom-select.js';
+export { default } from './tom-select.js';
+import './plugins/change_listener/plugin.js';
+import './plugins/checkbox_options/plugin.js';
+import './plugins/clear_button/plugin.js';
+import './plugins/drag_drop/plugin.js';
+import './plugins/dropdown_header/plugin.js';
+import './plugins/dropdown_input/plugin.js';
+import './plugins/input_autogrow/plugin.js';
+import './plugins/no_backspace_delete/plugin.js';
+import './plugins/no_active_items/plugin.js';
+import './plugins/optgroup_columns/plugin.js';
+import './plugins/remove_button/plugin.js';
+import './plugins/restore_on_backspace/plugin.js';
+//# sourceMappingURL=tom-select.complete.js.map
---
+++ ajax/libs/tom-select/1.4.1/types/contrib/highlight.d.ts
@@ -0,0 +1,13 @@
+/**
+ * highlight v3 | MIT license | Johann Burkard <jb@eaio.com>
+ * Highlights arbitrary terms in a node.
+ *
+ * - Modified by Marshal <beatgates@gmail.com> 2011-6-24 (added regex)
+ * - Modified by Brian Reavis <brian@thirdroute.com> 2012-8-27 (cleanup)
+ */
+export function highlight(element: any, pattern: any): void;
+/**
+ * removeHighlight fn copied from highlight v5 and
+ * edited to remove with(), pass js strict mode, and use without jquery
+ */
+export function removeHighlight(el: any): void;
