8a63a39a000193193401d4b6fb4a8942db2494c5
robocdnjs <cdnjs-github@cloudflare.com>
2021-03-24 17:22:10 +0000

    Add tom-select (1.4.0)

---
+++ ajax/libs/tom-select/1.4.0/types/tom-select.popular.d.ts
@@ -0,0 +1,6 @@
+import TomSelect from './tom-select';
+import './plugins/dropdown_input/plugin.js';
+import './plugins/no_backspace_delete/plugin.js';
+import './plugins/remove_button/plugin.js';
+import './plugins/restore_on_backspace/plugin.js';
+export default TomSelect;
