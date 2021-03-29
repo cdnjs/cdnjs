8fe8797974a5f5ca3c1e391fa9a48d6bf60e156c
robocdnjs <cdnjs-github@cloudflare.com>
2021-03-29 21:34:11 +0000

    Add react-native-web (0.15.3)

---
+++ ajax/libs/react-native-web/0.15.3/vendor/react-native/NativeEventEmitter/index.js
@@ -0,0 +1,65 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @format
+ * 
+ */
+'use strict';
+
+function _inheritsLoose(subClass, superClass) { subClass.prototype = Object.create(superClass.prototype); subClass.prototype.constructor = subClass; _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+import EventEmitter from '../emitter/EventEmitter';
+import RCTDeviceEventEmitter from './RCTDeviceEventEmitter';
+import invariant from 'fbjs/lib/invariant';
+
+/**
+ * Abstract base class for implementing event-emitting modules. This implements
+ * a subset of the standard EventEmitter node module API.
+ */
+var NativeEventEmitter =
+/*#__PURE__*/
+function (_EventEmitter) {
+  _inheritsLoose(NativeEventEmitter, _EventEmitter);
+
+  function NativeEventEmitter(nativeModule) {
+    return _EventEmitter.call(this, RCTDeviceEventEmitter.sharedSubscriber) || this;
+  }
+
+  var _proto = NativeEventEmitter.prototype;
+
+  _proto.addListener = function addListener(eventType, listener, context) {
+    if (this._nativeModule != null) {
+      this._nativeModule.addListener(eventType);
+    }
+
+    return _EventEmitter.prototype.addListener.call(this, eventType, listener, context);
+  };
+
+  _proto.removeAllListeners = function removeAllListeners(eventType) {
+    invariant(eventType, 'eventType argument is required.');
+    var count = this.listeners(eventType).length;
+
+    if (this._nativeModule != null) {
+      this._nativeModule.removeListeners(count);
+    }
+
+    _EventEmitter.prototype.removeAllListeners.call(this, eventType);
+  };
+
+  _proto.removeSubscription = function removeSubscription(subscription) {
+    if (this._nativeModule != null) {
+      this._nativeModule.removeListeners(1);
+    }
+
+    _EventEmitter.prototype.removeSubscription.call(this, subscription);
+  };
+
+  return NativeEventEmitter;
+}(EventEmitter);
+
+export default NativeEventEmitter;
