f7e58a41a25b6fb5b9250b2d79d2c55a10b89a78
robocdnjs <cdnjs-github@cloudflare.com>
2021-04-04 20:23:03 +0000

    Add highlight.js (10.7.2)

---
+++ ajax/libs/highlight.js/10.7.2/languages/python-repl.min.js
@@ -0,0 +1,4 @@
+hljs.registerLanguage("python-repl",(()=>{"use strict";return s=>({
+aliases:["pycon"],contains:[{className:"meta",starts:{end:/ |$/,starts:{end:"$",
+subLanguage:"python"}},variants:[{begin:/^>>>(?=[ ]|$)/},{
+begin:/^\.\.\.(?=[ ]|$)/}]}]})})());
