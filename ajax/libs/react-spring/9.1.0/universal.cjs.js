fbcc7eb793c4f1197cc454fbead4333668b6a706
robocdnjs <cdnjs-github@cloudflare.com>
2021-04-08 18:35:42 +0000

    Add react-spring (9.1.0)

---
+++ ajax/libs/react-spring/9.1.0/universal.cjs.js
@@ -0,0 +1,16 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+var core = require('@react-spring/core');
+
+
+
+Object.keys(core).forEach(function (k) {
+	if (k !== 'default') Object.defineProperty(exports, k, {
+		enumerable: true,
+		get: function () {
+			return core[k];
+		}
+	});
+});
