80887886135804ecd66d8bbcdef9fd5dc4c13799
robocdnjs <cdnjs-github@cloudflare.com>
2021-04-24 01:41:38 +0000

    Add otpauth (7.0.1)

---
+++ ajax/libs/otpauth/7.0.1/index.d.ts
@@ -0,0 +1,6 @@
+import { HOTP } from "./hotp";
+import { TOTP } from "./totp";
+import { URI } from "./uri";
+import { Secret } from "./secret";
+import { version } from "./version";
+export { HOTP, TOTP, URI, Secret, version };
---
+++ ajax/libs/otpauth/7.0.1/uri.d.ts
@@ -0,0 +1,20 @@
+/**
+ * HOTP/TOTP object/string conversion.
+ * {@link https://github.com/google/google-authenticator/wiki/Key-Uri-Format|Key URI Format}
+ */
+export class URI {
+    /**
+     * Parses a Google Authenticator key URI and returns an HOTP/TOTP object.
+     * @param {string} uri Google Authenticator Key URI.
+     * @returns {HOTP|TOTP} HOTP/TOTP object.
+     */
+    static parse(uri: string): HOTP | TOTP;
+    /**
+     * Converts an HOTP/TOTP object to a Google Authenticator key URI.
+     * @param {HOTP|TOTP} otp HOTP/TOTP object.
+     * @returns {string} Google Authenticator Key URI.
+     */
+    static stringify(otp: HOTP | TOTP): string;
+}
+import { HOTP } from "./hotp";
+import { TOTP } from "./totp";
