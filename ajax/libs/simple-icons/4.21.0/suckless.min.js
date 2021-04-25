9096e5195de87987d674e77136398685ffd99b6f
robocdnjs <cdnjs-github@cloudflare.com>
2021-04-25 09:26:30 +0000

    Add simple-icons (4.21.0)

---
+++ ajax/libs/simple-icons/4.21.0/suckless.min.js
@@ -0,0 +1,1 @@
+module.exports={title:"suckless",slug:"suckless",svg:'<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>suckless icon</title><path d="M0 4h24v4H4v2h20v10H0v-4h20v-2H0z"/></svg>',get path(){return this.svg.match(/<path\s+d="([^"]*)/)[1]},source:"https://suckless.org",hex:"1177AA",guidelines:void 0,license:void 0};
