adec5aefc69ed063721b250220341f3293789ddd
robocdnjs <cdnjs-github@cloudflare.com>
2021-05-19 17:43:12 +0000

    Add caf (13.1.1)

---
+++ ajax/libs/caf/13.1.1/esm/index.mjs
@@ -0,0 +1,5 @@
+/*! CAF: index.mjs
+	v13.1.1 (c) 2021 Kyle Simpson
+	MIT License: http://getify.mit-license.org
+*/
+export{default as CAF}from"./caf.mjs";export{default as CAG}from"./cag.mjs";export{default as CAFShared}from"./shared.mjs";
