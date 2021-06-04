8ac00a2212ce15bdfae672cfb1a99e99947afdc0
robocdnjs <cdnjs-github@cloudflare.com>
2021-06-04 14:26:01 +0000

    Add datatables.net-colreorder-dt (1.5.4)

---
+++ ajax/libs/datatables.net-colreorder-dt/1.5.4/colReorder.dataTables.min.js
@@ -0,0 +1,5 @@
+/*!
+ DataTables styling wrapper for ColReorder
+ ©2018 SpryMedia Ltd - datatables.net/license
+*/
+(function(c){"function"===typeof define&&define.amd?define(["jquery","datatables.net-dt","datatables.net-colreorder"],function(a){return c(a,window,document)}):"object"===typeof exports?module.exports=function(a,b){a||(a=window);if(!b||!b.fn.dataTable)b=require("datatables.net-dt")(a,b).$;b.fn.dataTable.ColReorder||require("datatables.net-colreorder")(a,b);return c(b,a,a.document)}:c(jQuery,window,document)})(function(c){return c.fn.dataTable});
