ea06433e02395eae81915f6ae38902b339493120
robocdnjs <cdnjs-github@cloudflare.com>
2021-06-04 14:20:39 +0000

    Add datatables.net-keytable-dt (2.6.2)

---
+++ ajax/libs/datatables.net-keytable-dt/2.6.2/keyTable.dataTables.js
@@ -0,0 +1,38 @@
+/*! DataTables styling wrapper for KeyTable
+ * ©2018 SpryMedia Ltd - datatables.net/license
+ */
+
+(function( factory ){
+	if ( typeof define === 'function' && define.amd ) {
+		// AMD
+		define( ['jquery', 'datatables.net-dt', 'datatables.net-keytable'], function ( $ ) {
+			return factory( $, window, document );
+		} );
+	}
+	else if ( typeof exports === 'object' ) {
+		// CommonJS
+		module.exports = function (root, $) {
+			if ( ! root ) {
+				root = window;
+			}
+
+			if ( ! $ || ! $.fn.dataTable ) {
+				$ = require('datatables.net-dt')(root, $).$;
+			}
+
+			if ( ! $.fn.dataTable.KeyTable ) {
+				require('datatables.net-keytable')(root, $);
+			}
+
+			return factory( $, root, root.document );
+		};
+	}
+	else {
+		// Browser
+		factory( jQuery, window, document );
+	}
+}(function( $, window, document, undefined ) {
+
+return $.fn.dataTable;
+
+}));
