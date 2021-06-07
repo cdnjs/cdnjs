7cb68f319ec1a273210109acc4622e8423b66f69
robocdnjs <cdnjs-github@cloudflare.com>
2021-06-07 15:25:16 +0000

    Add simple-icons (4.23.0)

---
+++ ajax/libs/simple-icons/4.23.0/bandcamp.js
@@ -0,0 +1,1 @@
+module.exports={title:"Bandcamp",slug:"bandcamp",svg:'<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Bandcamp icon</title><path d="M0 18.75l7.437-13.5H24l-7.438 13.5H0z"/></svg>',get path(){return this.svg.match(/<path\s+d="([^"]*)/)[1]},source:"https://bandcamp.com/buttons",hex:"408294",guidelines:void 0,license:void 0};
---
+++ ajax/libs/simple-icons/4.23.0/teratail.min.js
@@ -0,0 +1,1 @@
+module.exports={title:"teratail",slug:"teratail",svg:'<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>teratail icon</title><path d="M9.81.968h4.375L24 23.032h-5.107L12.121 6.605h-.198L5.148 23.03H0Z"/></svg>',get path(){return this.svg.match(/<path\s+d="([^"]*)/)[1]},source:"https://teratail.com/",hex:"F4C51C",guidelines:void 0,license:void 0};
