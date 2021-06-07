1606e38583d0e0b29bece681b9b7dd78af9d82e4
robocdnjs <cdnjs-github@cloudflare.com>
2021-06-07 14:32:15 +0000

    Add simple-icons (4.22.0)

---
+++ ajax/libs/simple-icons/4.22.0/delicious.min.js
@@ -0,0 +1,1 @@
+module.exports={title:"Delicious",slug:"delicious",svg:'<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Delicious icon</title><path d="M12 12H0v12h12V12zM24 0H12v12h12V0z"/></svg>',get path(){return this.svg.match(/<path\s+d="([^"]*)/)[1]},source:"http://del.icio.us/",hex:"0000FF",guidelines:void 0,license:void 0};
