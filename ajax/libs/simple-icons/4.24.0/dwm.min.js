03dd51ad942cb08aaa187792eba37cdec2e9fa1e
robocdnjs <cdnjs-github@cloudflare.com>
2021-06-07 16:25:43 +0000

    Add simple-icons (4.24.0)

---
+++ ajax/libs/simple-icons/4.24.0/dwm.min.js
@@ -0,0 +1,1 @@
+module.exports={title:"dwm",slug:"dwm",svg:'<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>dwm icon</title><path d="M0 11h6V7h2v8h2v-4h2v4h2v-4h10v6h-2v-4h-2v4h-2v-4h-2v4H2v-2h4v-2H2v4H0z"/></svg>',get path(){return this.svg.match(/<path\s+d="([^"]*)/)[1]},source:"https://dwm.suckless.org",hex:"1177AA",guidelines:void 0,license:void 0};
---
+++ ajax/libs/simple-icons/4.24.0/framer.min.js
@@ -0,0 +1,1 @@
+module.exports={title:"Framer",slug:"framer",svg:'<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Framer icon</title><path d="M4 0h16v8h-8zM4 8h8l8 8H4zM4 16h8v8z"/></svg>',get path(){return this.svg.match(/<path\s+d="([^"]*)/)[1]},source:"https://framer.com",hex:"0055FF",guidelines:void 0,license:void 0};
