a7a3a076e398594988b3ba56d6a55b4c36443e81
robocdnjs <cdnjs-github@cloudflare.com>
2021-06-07 20:32:22 +0000

    Add highlight.js (11.0.0-beta1)

---
+++ ajax/libs/highlight.js/11.0.0-beta1/languages/taggerscript.min.js
@@ -0,0 +1,6 @@
+hljs.registerLanguage("taggerscript",(()=>{"use strict";return e=>({
+name:"Tagger Script",contains:[{className:"comment",begin:/\$noop\(/,end:/\)/,
+contains:[{begin:/\\[()]/},{begin:/\(/,end:/\)/,contains:[{begin:/\\[()]/
+},"self"]}],relevance:10},{className:"keyword",begin:/\$[_a-zA-Z0-9]+(?=\()/},{
+className:"variable",begin:/%[_a-zA-Z0-9:]+%/},{className:"symbol",
+begin:/\\[\\nt$%,()]/},{className:"symbol",begin:/\\u[a-fA-F0-9]{4}/}]})})());
