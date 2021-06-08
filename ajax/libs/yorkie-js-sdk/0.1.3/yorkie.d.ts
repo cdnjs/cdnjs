6f6a2bd268e95dc855a9e351b9019c974f10cdc8
robocdnjs <cdnjs-github@cloudflare.com>
2021-06-08 20:07:53 +0000

    Add yorkie-js-sdk (0.1.3)

---
+++ ajax/libs/yorkie-js-sdk/0.1.3/yorkie.d.ts
@@ -0,0 +1,27 @@
+import Long from 'long';
+import { Client, ClientOptions, ClientEventType } from './core/client';
+import { Document, Indexable, DocEventType } from './document/document';
+export { Client, Document };
+export { TimeTicket } from './document/time/ticket';
+export { ActorID } from './document/time/actor_id';
+export { JSONElement } from './document/json/element';
+export { JSONObject } from './document/json/object';
+export { JSONArray } from './document/json/array';
+export { PlainText } from './document/json/plain_text';
+export { RichText } from './document/json/rich_text';
+export { Change, ChangeType } from './document/json/rga_tree_split';
+export declare type EventType = ClientEventType | DocEventType;
+/**
+ * The top-level yorkie namespace with additional properties.
+ *
+ * In production, this will be called exactly once and the result
+ * assigned to the `yorkie` global.
+ *
+ * e.g) `yorkie.createClient(...);`
+ */
+declare const yorkie: {
+    createClient(rpcAddr: string, opts?: ClientOptions | undefined): Client;
+    createDocument<T = Indexable>(collection: string, document: string): Document<T>;
+    Long: Long.LongConstructor;
+};
+export default yorkie;
