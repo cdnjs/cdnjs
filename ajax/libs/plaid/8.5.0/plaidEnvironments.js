17762512041ee1bab7eb245a0d9562bb46243a31
robocdnjs <cdnjs-github@cloudflare.com>
2021-06-08 12:04:39 +0000

    Add plaid (8.5.0)

---
+++ ajax/libs/plaid/8.5.0/plaidEnvironments.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var environments = {
+  production: 'https://production.plaid.com',
+  sandbox: 'https://sandbox.plaid.com',
+  development: 'https://development.plaid.com'
+};
+
+module.exports = environments;
