5afd5ca4f6c0e22cdd6e64e0ece7a030ed7f7150
robocdnjs <cdnjs-github@cloudflare.com>
2021-06-08 02:37:49 +0000

    Add react-imgix (9.1.3)

---
+++ ajax/libs/react-imgix/9.1.3/targetWidths.js
@@ -0,0 +1,15 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _jsCore = _interopRequireDefault(require("@imgix/js-core"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _default = _jsCore.default.targetWidths();
+
+exports.default = _default;
+//# sourceMappingURL=targetWidths.js.map
