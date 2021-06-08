7d611cbc0877a3ceaefaa800b2ed33885826c5bd
robocdnjs <cdnjs-github@cloudflare.com>
2021-06-08 01:34:04 +0000

    Add simple-icons (5.0.0)

---
+++ ajax/libs/simple-icons/5.0.0/plex.min.js
@@ -0,0 +1,1 @@
+module.exports={title:"Plex",slug:"plex",svg:'<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Plex</title><path d="M11.643 0H4.68l7.679 12L4.68 24h6.963l7.677-12-7.677-12"/></svg>',get path(){return this.svg.match(/<path\s+d="([^"]*)/)[1]},source:"https://brand.plex.tv/",hex:"E5A00D",guidelines:"https://brand.plex.tv/",license:void 0};
