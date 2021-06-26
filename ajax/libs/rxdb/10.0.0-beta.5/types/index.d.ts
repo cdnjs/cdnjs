94dc4ded156f0190c379f1568197c07338ea8548
robocdnjs <cdnjs-github@cloudflare.com>
2021-06-26 14:03:35 +0000

    Add rxdb (10.0.0-beta.5)

---
+++ ajax/libs/rxdb/10.0.0-beta.5/types/index.d.ts
@@ -0,0 +1,7 @@
+/**
+ * this is the default rxdb-export
+ * It has a batteries-included guarantee.
+ * It basically just rxdb-core with some default plugins
+ */
+export * from './core';
+export * from './plugins/pouchdb';
