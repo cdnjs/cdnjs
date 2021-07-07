0eaa3b7199c872a5b75b54f11be14960f726d84c
robocdnjs <cdnjs-github@cloudflare.com>
2021-07-07 08:04:16 +0000

    Add ckeditor5 (29.0.0)

---
+++ ajax/libs/ckeditor5/29.0.0/translations/oc.js
@@ -0,0 +1,1 @@
+!function(n){const i=n.oc=n.oc||{};i.dictionary=Object.assign(i.dictionary||{},{Bold:"Gras",Cancel:"Anullar",Italic:"Italica",Save:"Enregistrar","Show more items":""}),i.getPluralForm=function(n){return n>1}}(window.CKEDITOR_TRANSLATIONS||(window.CKEDITOR_TRANSLATIONS={}));
---
+++ ajax/libs/ckeditor5/29.0.0/translations/tt.min.js
@@ -0,0 +1,1 @@
+!function(t){const n=t.tt=t.tt||{};n.dictionary=Object.assign(n.dictionary||{},{Bold:"Калын",Cancel:"",Italic:"",Redo:"Кабатла",Save:"Сакла","Show more items":"",Undo:""}),n.getPluralForm=function(t){return 0}}(window.CKEDITOR_TRANSLATIONS||(window.CKEDITOR_TRANSLATIONS={}));
