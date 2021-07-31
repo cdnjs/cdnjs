7f109f5d9aa106cda3e58410d2460157444c9e61
robocdnjs <cdnjs-github@cloudflare.com>
2021-07-31 03:04:12 +0000

    Add firebase (9.0.0-beta.8)

---
+++ ajax/libs/firebase/9.0.0-beta.8/firebase-auth-cordova.js
@@ -0,0 +1,2 @@
+export*from"@firebase/auth-exp/cordova";
+//# sourceMappingURL=firebase-auth-cordova.js.map
---
+++ ajax/libs/firebase/9.0.0-beta.8/firebase-auth-react-native.js
@@ -0,0 +1,2 @@
+export*from"@firebase/auth-exp/react-native";
+//# sourceMappingURL=firebase-auth-react-native.js.map
