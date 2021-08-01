23cfbaa24c3b4d321282cb2c09387476b02c6c66
robocdnjs <cdnjs-github@cloudflare.com>
2021-08-01 22:05:23 +0000

    Add openplayerjs (2.8.0)

---
+++ ajax/libs/openplayerjs/2.8.0/types/interfaces/events-list.d.ts
@@ -0,0 +1,10 @@
+/**
+ * Event list
+ *
+ * @description An object that maps a list of events with callbacks.
+ * @interface EventsList
+ * @export
+ */
+export default interface EventsList {
+    [key: string]: any;
+}
---
+++ ajax/libs/openplayerjs/2.8.0/types/interfaces/source.d.ts
@@ -0,0 +1,11 @@
+/**
+ * Media source
+ *
+ * @description Object that contains the defaut elements for a media source.
+ * @interface Source
+ * @export
+ */
+export default interface Source {
+    src: string;
+    type: string;
+}
