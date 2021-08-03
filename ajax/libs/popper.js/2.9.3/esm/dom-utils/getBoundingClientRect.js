710a501054bdfdaf1d7df3011ae3ef31c568c026
robocdnjs <cdnjs-github@cloudflare.com>
2021-08-03 17:13:06 +0000

    Add popper.js (2.9.3)

---
+++ ajax/libs/popper.js/2.9.3/esm/dom-utils/getBoundingClientRect.js
@@ -0,0 +1,28 @@
+import { isHTMLElement } from "./instanceOf.js";
+var round = Math.round;
+export default function getBoundingClientRect(element, includeScale) {
+  if (includeScale === void 0) {
+    includeScale = false;
+  }
+
+  var rect = element.getBoundingClientRect();
+  var scaleX = 1;
+  var scaleY = 1;
+
+  if (isHTMLElement(element) && includeScale) {
+    // Fallback to 1 in case both values are `0`
+    scaleX = rect.width / element.offsetWidth || 1;
+    scaleY = rect.height / element.offsetHeight || 1;
+  }
+
+  return {
+    width: round(rect.width / scaleX),
+    height: round(rect.height / scaleY),
+    top: round(rect.top / scaleY),
+    right: round(rect.right / scaleX),
+    bottom: round(rect.bottom / scaleY),
+    left: round(rect.left / scaleX),
+    x: round(rect.left / scaleX),
+    y: round(rect.top / scaleY)
+  };
+}
