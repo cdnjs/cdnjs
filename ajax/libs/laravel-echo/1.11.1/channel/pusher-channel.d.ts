f4cd11c6e9e88e85c333f80838a323efa71af90b
robocdnjs <cdnjs-github@cloudflare.com>
2021-08-03 17:13:31 +0000

    Add laravel-echo (1.11.1)

---
+++ ajax/libs/laravel-echo/1.11.1/channel/pusher-channel.d.ts
@@ -0,0 +1,67 @@
+import { EventFormatter } from '../util';
+import { Channel } from './channel';
+/**
+ * This class represents a Pusher channel.
+ */
+export declare class PusherChannel extends Channel {
+    /**
+     * The Pusher client instance.
+     */
+    pusher: any;
+    /**
+     * The name of the channel.
+     */
+    name: any;
+    /**
+     * Channel options.
+     */
+    options: any;
+    /**
+     * The event formatter.
+     */
+    eventFormatter: EventFormatter;
+    /**
+     * The subscription of the channel.
+     */
+    subscription: any;
+    /**
+     * Create a new class instance.
+     */
+    constructor(pusher: any, name: any, options: any);
+    /**
+     * Subscribe to a Pusher channel.
+     */
+    subscribe(): any;
+    /**
+     * Unsubscribe from a Pusher channel.
+     */
+    unsubscribe(): void;
+    /**
+     * Listen for an event on the channel instance.
+     */
+    listen(event: string, callback: Function): PusherChannel;
+    /**
+     * Listen for all events on the channel instance.
+     */
+    listenToAll(callback: Function): PusherChannel;
+    /**
+     * Stop listening for an event on the channel instance.
+     */
+    stopListening(event: string, callback?: Function): PusherChannel;
+    /**
+     * Stop listening for all events on the channel instance.
+     */
+    stopListeningToAll(callback?: Function): PusherChannel;
+    /**
+     * Register a callback to be called anytime a subscription succeeds.
+     */
+    subscribed(callback: Function): PusherChannel;
+    /**
+     * Register a callback to be called anytime a subscription error occurs.
+     */
+    error(callback: Function): PusherChannel;
+    /**
+     * Bind a channel to an event.
+     */
+    on(event: string, callback: Function): PusherChannel;
+}
---
+++ ajax/libs/laravel-echo/1.11.1/connector/socketio-connector.d.ts
@@ -0,0 +1,57 @@
+import { Connector } from './connector';
+import { SocketIoChannel, SocketIoPrivateChannel, SocketIoPresenceChannel } from './../channel';
+/**
+ * This class creates a connnector to a Socket.io server.
+ */
+export declare class SocketIoConnector extends Connector {
+    /**
+     * The Socket.io connection instance.
+     */
+    socket: any;
+    /**
+     * All of the subscribed channel names.
+     */
+    channels: {
+        [name: string]: SocketIoChannel;
+    };
+    /**
+     * Create a fresh Socket.io connection.
+     */
+    connect(): void;
+    /**
+     * Get socket.io module from global scope or options.
+     */
+    getSocketIO(): any;
+    /**
+     * Listen for an event on a channel instance.
+     */
+    listen(name: string, event: string, callback: Function): SocketIoChannel;
+    /**
+     * Get a channel instance by name.
+     */
+    channel(name: string): SocketIoChannel;
+    /**
+     * Get a private channel instance by name.
+     */
+    privateChannel(name: string): SocketIoPrivateChannel;
+    /**
+     * Get a presence channel instance by name.
+     */
+    presenceChannel(name: string): SocketIoPresenceChannel;
+    /**
+     * Leave the given channel, as well as its private and presence variants.
+     */
+    leave(name: string): void;
+    /**
+     * Leave the given channel.
+     */
+    leaveChannel(name: string): void;
+    /**
+     * Get the socket ID for the connection.
+     */
+    socketId(): string;
+    /**
+     * Disconnect Socketio connection.
+     */
+    disconnect(): void;
+}
