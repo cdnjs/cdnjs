176aaa4931f005fe0b4fb0da7814b56c893dd409
robocdnjs <cdnjs-github@cloudflare.com>
2021-08-16 06:04:45 +0000

    Add typeit (8.0.0-rc.5)

---
+++ ajax/libs/typeit/8.0.0-rc.5/helpers/isInput.d.ts
@@ -0,0 +1,2 @@
+declare const _default: (el: HTMLElement) => boolean;
+export default _default;
---
+++ ajax/libs/typeit/8.0.0-rc.5/helpers/repositionCursor.d.ts
@@ -0,0 +1,2 @@
+declare const _default: (element: Node, allChars: any[], cursor: Node, cursorPosition: number) => void;
+export default _default;
