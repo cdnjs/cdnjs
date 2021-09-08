e0fafd35f7cf0e2f204be6cc6a68aac0b50aebbb
robocdnjs <cdnjs-github@cloudflare.com>
2021-09-08 11:04:09 +0000

    Add datatables.net-dt (1.1.1)

---
+++ ajax/libs/datatables.net-dt/1.1.1/js/dataTables.dataTables.min.js
@@ -0,0 +1,5 @@
+/*!
+ DataTables styling integration
+ ©2018 SpryMedia Ltd - datatables.net/license
+*/
+(function(c){"function"===typeof define&&define.amd?define(["jquery","datatables.net"],function(a){return c(a,window,document)}):"object"===typeof exports?module.exports=function(a,b){a||(a=window);b&&b.fn.dataTable||(b=require("datatables.net")(a,b).$);return c(b,a,a.document)}:c(jQuery,window,document)})(function(c,a,b,d){return c.fn.dataTable});
