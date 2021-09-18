9ef8c227bb72cfdcc7a4ccebb48383d3f9a94c49
robocdnjs <cdnjs-github@cloudflare.com>
2021-09-18 10:04:09 +0000

    Add human (2.2.2)

---
+++ ajax/libs/human/2.2.2/tfjs.version.js
@@ -0,0 +1,44 @@
+/*
+  Human
+  homepage: <https://github.com/vladmandic/human>
+  author: <https://github.com/vladmandic>'
+*/
+
+// node_modules/.pnpm/@tensorflow+tfjs@3.9.0_seedrandom@3.0.5/node_modules/@tensorflow/tfjs/package.json
+var version = "3.9.0";
+
+// node_modules/.pnpm/@tensorflow+tfjs-core@3.9.0/node_modules/@tensorflow/tfjs-core/package.json
+var version2 = "3.9.0";
+
+// node_modules/.pnpm/@tensorflow+tfjs-data@3.9.0_470c05e138890ee649df8440b2dfddf4/node_modules/@tensorflow/tfjs-data/package.json
+var version3 = "3.9.0";
+
+// node_modules/.pnpm/@tensorflow+tfjs-layers@3.9.0_@tensorflow+tfjs-core@3.9.0/node_modules/@tensorflow/tfjs-layers/package.json
+var version4 = "3.9.0";
+
+// node_modules/.pnpm/@tensorflow+tfjs-converter@3.9.0_@tensorflow+tfjs-core@3.9.0/node_modules/@tensorflow/tfjs-converter/package.json
+var version5 = "3.9.0";
+
+// node_modules/.pnpm/@tensorflow+tfjs-backend-cpu@3.9.0_@tensorflow+tfjs-core@3.9.0/node_modules/@tensorflow/tfjs-backend-cpu/package.json
+var version6 = "3.9.0";
+
+// node_modules/.pnpm/@tensorflow+tfjs-backend-webgl@3.9.0_@tensorflow+tfjs-core@3.9.0/node_modules/@tensorflow/tfjs-backend-webgl/package.json
+var version7 = "3.9.0";
+
+// node_modules/.pnpm/@tensorflow+tfjs-backend-wasm@3.9.0_@tensorflow+tfjs-core@3.9.0/node_modules/@tensorflow/tfjs-backend-wasm/package.json
+var version8 = "3.9.0";
+
+// tfjs/tf-version.ts
+var version9 = {
+  tfjs: version,
+  "tfjs-core": version2,
+  "tfjs-data": version3,
+  "tfjs-layers": version4,
+  "tfjs-converter": version5,
+  "tfjs-backend-cpu": version6,
+  "tfjs-backend-webgl": version7,
+  "tfjs-backend-wasm": version8
+};
+export {
+  version9 as version
+};
