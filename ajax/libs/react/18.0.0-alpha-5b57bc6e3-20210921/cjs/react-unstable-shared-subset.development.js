441305b6f43bd04ee36fe5ca9aecd956fa26394a
robocdnjs <cdnjs-github@cloudflare.com>
2021-09-21 23:04:32 +0000

    Add react (18.0.0-alpha-5b57bc6e3-20210921)

---
+++ ajax/libs/react/18.0.0-alpha-5b57bc6e3-20210921/cjs/react-unstable-shared-subset.development.js
@@ -0,0 +1,18 @@
+/** @license React vundefined
+ * react-unstable-shared-subset.development.js
+ *
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+'use strict';
+
+if (process.env.NODE_ENV !== "production") {
+  (function() {
+'use strict';
+
+throw new Error('This entry point is not yet supported outside of experimental channels');
+  })();
+}
