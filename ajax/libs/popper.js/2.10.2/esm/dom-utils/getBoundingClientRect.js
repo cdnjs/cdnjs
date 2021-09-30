fede78336b87928d1b1622f9c6c6bc5afa74c871
robocdnjs <cdnjs-github@cloudflare.com>
2021-09-30 13:08:12 +0000

    Add popper.js (2.10.2)

---
+++ ajax/libs/popper.js/2.10.2/esm/dom-utils/getBoundingClientRect.js
@@ -0,0 +1,38 @@
+// import { isHTMLElement } from './instanceOf';
+export default function getBoundingClientRect(element, // eslint-disable-next-line unused-imports/no-unused-vars
+includeScale) {
+  if (includeScale === void 0) {
+    includeScale = false;
+  }
+
+  var rect = element.getBoundingClientRect();
+  var scaleX = 1;
+  var scaleY = 1; // FIXME:
+  // `offsetWidth` returns an integer while `getBoundingClientRect`
+  // returns a float. This results in `scaleX` or `scaleY` being
+  // non-1 when it should be for elements that aren't a full pixel in
+  // width or height.
+  // if (isHTMLElement(element) && includeScale) {
+  //   const offsetHeight = element.offsetHeight;
+  //   const offsetWidth = element.offsetWidth;
+  //   // Do not attempt to divide by 0, otherwise we get `Infinity` as scale
+  //   // Fallback to 1 in case both values are `0`
+  //   if (offsetWidth > 0) {
+  //     scaleX = rect.width / offsetWidth || 1;
+  //   }
+  //   if (offsetHeight > 0) {
+  //     scaleY = rect.height / offsetHeight || 1;
+  //   }
+  // }
+
+  return {
+    width: rect.width / scaleX,
+    height: rect.height / scaleY,
+    top: rect.top / scaleY,
+    right: rect.right / scaleX,
+    bottom: rect.bottom / scaleY,
+    left: rect.left / scaleX,
+    x: rect.left / scaleX,
+    y: rect.top / scaleY
+  };
+}
---
+++ ajax/libs/popper.js/2.10.2/umd/enums.min.js
@@ -0,0 +1,6 @@
+/**
+ * @popperjs/core v2.10.2 - MIT License
+ */
+
+"use strict";!function(e,t){"object"==typeof exports&&"undefined"!=typeof module?t(exports):"function"==typeof define&&define.amd?define(["exports"],t):t((e="undefined"!=typeof globalThis?globalThis:e||self).Popper={})}(this,(function(e){var t=["top","bottom","right","left"],r=t.reduce((function(e,t){return e.concat([t+"-start",t+"-end"])}),[]),a=[].concat(t,["auto"]).reduce((function(e,t){return e.concat([t,t+"-start",t+"-end"])}),[]);e.afterMain="afterMain",e.afterRead="afterRead",e.afterWrite="afterWrite",e.auto="auto",e.basePlacements=t,e.beforeMain="beforeMain",e.beforeRead="beforeRead",e.beforeWrite="beforeWrite",e.bottom="bottom",e.clippingParents="clippingParents",e.end="end",e.left="left",e.main="main",e.modifierPhases="beforeRead read afterRead beforeMain main afterMain beforeWrite write afterWrite".split(" "),e.placements=a,e.popper="popper",e.read="read",e.reference="reference",e.right="right",e.start="start",e.top="top",e.variationPlacements=r,e.viewport="viewport",e.write="write",Object.defineProperty(e,"__esModule",{value:!0})}));
+//# sourceMappingURL=enums.min.js.map
