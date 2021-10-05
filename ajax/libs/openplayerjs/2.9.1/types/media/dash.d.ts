4aab10471bedb1e4cb30147cde45ca71621f80be
robocdnjs <cdnjs-github@cloudflare.com>
2021-10-05 13:08:32 +0000

    Add openplayerjs (2.9.1)

---
+++ ajax/libs/openplayerjs/2.9.1/types/media/dash.d.ts
@@ -0,0 +1,74 @@
+import DashOptions from '../interfaces/dash-options';
+import Level from '../interfaces/level';
+import Source from '../interfaces/source';
+import Native from './native';
+/**
+ * M(PEG)-DASH Media.
+ *
+ * @description Class that handles MPD files using dash.js within the player
+ * @see https://github.com/Dash-Industry-Forum/dash.js/
+ * @see https://github.com/Dash-Industry-Forum/dash.js/wiki/Migration-3.0
+ * @class DashMedia
+ */
+declare class DashMedia extends Native {
+    #private;
+    /**
+     * Creates an instance of DashMedia.
+     *
+     * @param {HTMLMediaElement} element
+     * @param {Source} mediaSource
+     * @memberof DashMedia
+     */
+    constructor(element: HTMLMediaElement, mediaSource: Source, options?: DashOptions);
+    /**
+     *
+     * @inheritDoc
+     * @memberof DashMedia
+     */
+    canPlayType(mimeType: string): boolean;
+    /**
+     *
+     * @inheritDoc
+     * @memberof DashMedia
+     */
+    load(): void;
+    /**
+     *
+     * @inheritDoc
+     * @memberof DashMedia
+     */
+    destroy(): void;
+    /**
+     *
+     * @inheritDoc
+     * @memberof DashMedia
+     */
+    set src(media: Source);
+    get levels(): Level[];
+    set level(level: number);
+    get level(): number;
+    /**
+     * Custom M(PEG)-DASH events
+     *
+     * These events can be attached to the original node using addEventListener and the name of the event,
+     * not using dashjs.MediaPlayer.events object
+     * @see http://cdn.dashjs.org/latest/jsdoc/MediaPlayerEvents.html
+     * @param {dashjs.MediaPlayerEvents.events} event
+     */
+    private _assign;
+    /**
+     * Remove all dash.js events and destroy dashjs player instance.
+     *
+     * @memberof DashMedia
+     */
+    private _revoke;
+    /**
+     * Set player with proper configuration to have better performance.
+     *
+     * Also, considers the addition of DRM settings.
+     *
+     * @memberof DashMedia
+     */
+    private _preparePlayer;
+}
+export default DashMedia;
---
+++ ajax/libs/openplayerjs/2.9.1/types/media/flv.d.ts
@@ -0,0 +1,78 @@
+import Level from '../interfaces/level';
+import Source from '../interfaces/source';
+import Native from './native';
+/**
+ * FLV Media.
+ *
+ * @description Class that handles FLV and RTMP files using flv.js within the player
+ * @see https://github.com/bilibili/flv.js
+ * @class FlvMedia
+ */
+declare class FlvMedia extends Native {
+    #private;
+    /**
+     * Creates an instance of FlvMedia.
+     *
+     * @param {HTMLMediaElement} element
+     * @param {Source} mediaSource
+     * @memberof FlvMedia
+     */
+    constructor(element: HTMLMediaElement, mediaSource: Source, options?: Record<string, unknown>);
+    /**
+     * Provide support via flv.js for modern browsers only
+     *
+     * @inheritDoc
+     * @memberof FlvMedia
+     */
+    canPlayType(mimeType: string): boolean;
+    /**
+     *
+     * @inheritDoc
+     * @memberof FlvMedia
+     */
+    load(): void;
+    /**
+     *
+     * @inheritDoc
+     * @memberof FlvMedia
+     */
+    destroy(): void;
+    /**
+     *
+     * @inheritDoc
+     * @memberof FlvMedia
+     */
+    set src(media: Source);
+    get levels(): Level[];
+    set level(level: number);
+    get level(): number;
+    /**
+     * Setup Flv player with options and config.
+     *
+     * Some of the options/events will be overridden to improve performance and user's experience.
+     *
+     * @private
+     * @memberof FlvMedia
+     */
+    private _create;
+    /**
+     * Custom FLV events
+     *
+     * These events can be attached to the original node using addEventListener and the name of the event,
+     * using or not flvjs.Events object
+     * @see https://github.com/bilibili/flv.js/blob/master/docs/api.md#flvjsevents
+     * @see https://github.com/bilibili/flv.js/blob/master/docs/api.md#flvjserrortypes
+     * @see https://github.com/bilibili/flv.js/blob/master/docs/api.md#flvjserrordetails
+     * @param {string} event The name of the FLV event
+     * @param {any} data The data passed to the event, could be an object or an array
+     * @memberof FlvMedia
+     */
+    private _assign;
+    /**
+     * Destroy flvjs player instance.
+     *
+     * @memberof FlvMedia
+     */
+    private _revoke;
+}
+export default FlvMedia;
