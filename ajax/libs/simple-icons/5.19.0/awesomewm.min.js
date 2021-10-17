c7c5e14da513f3202a8e37dafa46b809806147d6
robocdnjs <cdnjs-github@cloudflare.com>
2021-10-17 10:04:45 +0000

    Add simple-icons (5.19.0)

---
+++ ajax/libs/simple-icons/5.19.0/awesomewm.min.js
@@ -0,0 +1,1 @@
+module.exports={title:"awesomeWM",slug:"awesomewm",svg:'<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>awesomeWM</title><path d="M0 24V8.25h16.5V7.5H0V0h24v24h-7.5v-8.25h-9v.75h8.25V24z"/></svg>',get path(){return this.svg.match(/<path\s+d="([^"]*)/)[1]},source:"https://awesomewm.org/",hex:"535D6C",guidelines:void 0,license:void 0};
