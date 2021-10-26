e9dee91b935b74678bac0cbe9b65d4e4533fd990
robocdnjs <cdnjs-github@cloudflare.com>
2021-10-26 18:10:01 +0000

    Add laravel-echo (1.11.3)

---
+++ ajax/libs/laravel-echo/1.11.3/channel/pusher-presence-channel.d.ts
@@ -0,0 +1,23 @@
+import { PusherChannel } from './pusher-channel';
+import { PresenceChannel } from './presence-channel';
+/**
+ * This class represents a Pusher presence channel.
+ */
+export declare class PusherPresenceChannel extends PusherChannel implements PresenceChannel {
+    /**
+     * Register a callback to be called anytime the member list changes.
+     */
+    here(callback: Function): PusherPresenceChannel;
+    /**
+     * Listen for someone joining the channel.
+     */
+    joining(callback: Function): PusherPresenceChannel;
+    /**
+     * Listen for someone leaving the channel.
+     */
+    leaving(callback: Function): PusherPresenceChannel;
+    /**
+     * Trigger client event on the channel.
+     */
+    whisper(eventName: string, data: any): PusherPresenceChannel;
+}
