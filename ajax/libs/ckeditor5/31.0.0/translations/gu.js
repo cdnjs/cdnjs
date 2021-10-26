6fbdee99d267a471ede6bdd082664b99d17cd32c
robocdnjs <cdnjs-github@cloudflare.com>
2021-10-26 11:04:27 +0000

    Add ckeditor5 (31.0.0)

---
+++ ajax/libs/ckeditor5/31.0.0/translations/gu.js
@@ -0,0 +1,1 @@
+!function(n){const o=n.gu=n.gu||{};o.dictionary=Object.assign(o.dictionary||{},{"Block quote":" વિચાર ટાંકો",Bold:"ઘાટુ - બોલ્ડ્","Cannot upload file:":"ફાઇલ અપલોડ ન થઇ શકી",Italic:"ત્રાંસુ - ઇટલિક્"}),o.getPluralForm=function(n){return 1!=n}}(window.CKEDITOR_TRANSLATIONS||(window.CKEDITOR_TRANSLATIONS={}));
