45a393c4cd64783ccf4fc9a96cb71fa3a61cbd45
robocdnjs <cdnjs-github@cloudflare.com>
2021-11-03 15:05:19 +0000

    Add qoopido.demand (8.0.1)

---
+++ ajax/libs/qoopido.demand/8.0.1/cache/states.js
@@ -0,0 +1,3 @@
+/**! Qoopido.demand 8.0.1 | https://github.com/dlueth/qoopido.demand | (c) 2021 Dirk Lueth */
+!function(e){"use strict";provide(["/demand/function/iterate"],(function(t){var n=new RegExp("^\\[demand\\]\\[(.+?)\\]\\[state\\]$"),a=/^(.+?),(\d+),(\d*),(.+?),(\d+)$/;function r(t){var r,i=t.match(n);i&&(r=function(t){var n,r=e.getItem(t);if(r&&(n=r.match(a)))return Array.prototype.slice.call(n,1)}("[demand]["+i[1]+"][state]"),this.push({id:i[1],version:r[0],size:parseInt(r[1],10),expires:r[2]?new Date(parseInt(r[2],10)):null,demand:r[3],accessed:new Date(parseInt(r[4],10))}))}return function(){return t(e,r,[])}}))}(localStorage);
+//# sourceMappingURL=states.js.map
---
+++ ajax/libs/qoopido.demand/8.0.1/handler/json.js
@@ -0,0 +1,3 @@
+/**! Qoopido.demand 8.0.1 | https://github.com/dlueth/qoopido.demand | (c) 2021 Dirk Lueth */
+!function(){"use strict";provide(["path","/demand/abstract/handler","/demand/task","/demand/pledge","/demand/failure","/demand/validator/isObject","/demand/function/merge"],(function(n,e,t,a,o,d,i){var r=/^application\/json/,u={suffix:".json"},c=new t((function(n,e,t){try{n(JSON.parse(t))}catch(n){e(n)}}));function s(){}return demand.on("postConfigure:"+n,(function(n){d(n)&&i(u,n)})),s.prototype={validate:function(n){return r.test(n)},onPreRequest:function(n,e){var t;(e="undefined"!=typeof e?e:u.suffix)&&(t=n.url.pathname,n.url.pathname=t.slice(-e.length)!==e?t+e:t)},process:function(n){provide(c(n.source))}},new(s.extends(e))}))}();
+//# sourceMappingURL=json.js.map
