faca825616e78522a32487dea4365caec0dd6882
robocdnjs <cdnjs-github@cloudflare.com>
2021-11-05 20:04:33 +0000

    Add boardgame-io (0.49.5)

---
+++ ajax/libs/boardgame-io/0.49.5/cjs/debug.js
@@ -0,0 +1,17 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+var Debug = require('./Debug-057dd95c.js');
+require('./turn-order-9099d084.js');
+require('immer');
+require('lodash.isplainobject');
+require('./reducer-4d6573e0.js');
+require('rfc6902');
+require('flatted');
+require('setimmediate');
+require('./ai-3736f26d.js');
+
+
+
+exports.Debug = Debug.Debug;
