f5408c2a121673d3273818bc6e728f2de563f814
robocdnjs <cdnjs-github@cloudflare.com>
2021-11-06 19:03:36 +0000

    Add tom-select (2.0.0-rc.5)

---
+++ ajax/libs/tom-select/2.0.0-rc.5/types/types/core.d.ts
@@ -0,0 +1,39 @@
+import TomSelect from '../tom-select';
+import { escape_html } from '../utils';
+export interface TomInput extends HTMLInputElement {
+    tomselect?: TomSelect;
+}
+export declare type TomArgObject = {
+    silent?: boolean;
+};
+export declare type TomOption = {
+    [key: string]: any;
+};
+export declare type TomOptions = {
+    [key: string]: TomOption;
+};
+export declare type TomCreateFilter = (input: string) => boolean;
+export declare type TomCreateCallback = (data?: TomOption) => void;
+export declare type TomCreate = (input: string, create: TomCreateCallback) => boolean;
+export interface TomItem extends HTMLElement {
+    dataset: {
+        value: string;
+    };
+}
+export declare type TomLoadCallback = TomSelect['loadCallback'];
+export declare type TomTemplate = (data: TomOption, escape: typeof escape_html) => string | HTMLElement;
+export declare type TomTemplateNull = (data: TomOption, escape: typeof escape_html) => null | string | HTMLElement;
+export declare type TomTemplates = {
+    'dropdown': TomTemplate;
+    'optgroup': TomTemplate;
+    'optgroup_header': TomTemplate;
+    'option': TomTemplate;
+    'item': TomTemplate;
+    'option_create': TomTemplate;
+    'no_results': TomTemplate;
+    'loading': TomTemplate;
+    'not_loading': TomTemplateNull;
+    'loading_more': TomTemplateNull;
+    'no_more_results': TomTemplateNull;
+};
+export declare type TomTemplateNames = keyof TomTemplates;
