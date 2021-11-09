3b6112ddbd4d88e6e4eca4ccd25a8cbca74ce3ce
robocdnjs <cdnjs-github@cloudflare.com>
2021-11-09 22:04:11 +0000

    Add jose (4.2.1)

---
+++ ajax/libs/jose/4.2.1/jws/general/sign.js
@@ -0,0 +1,61 @@
+import { FlattenedSign } from '../flattened/sign.js';
+import { JWSInvalid } from '../../util/errors.js';
+const signatureRef = new WeakMap();
+class IndividualSignature {
+    setProtectedHeader(protectedHeader) {
+        const ref = signatureRef.get(this);
+        if (ref.protectedHeader) {
+            throw new TypeError('setProtectedHeader can only be called once');
+        }
+        ref.protectedHeader = protectedHeader;
+        return this;
+    }
+    setUnprotectedHeader(unprotectedHeader) {
+        const ref = signatureRef.get(this);
+        if (ref.unprotectedHeader) {
+            throw new TypeError('setUnprotectedHeader can only be called once');
+        }
+        ref.unprotectedHeader = unprotectedHeader;
+        return this;
+    }
+}
+export class GeneralSign {
+    constructor(payload) {
+        this._signatures = [];
+        this._payload = payload;
+    }
+    addSignature(key, options) {
+        const signature = new IndividualSignature();
+        signatureRef.set(signature, { key, options });
+        this._signatures.push(signature);
+        return signature;
+    }
+    async sign() {
+        if (!this._signatures.length) {
+            throw new JWSInvalid('at least one signature must be added');
+        }
+        const jws = {
+            signatures: [],
+            payload: '',
+        };
+        let payloads = new Set();
+        await Promise.all(this._signatures.map(async (sig) => {
+            const { protectedHeader, unprotectedHeader, options, key } = signatureRef.get(sig);
+            const flattened = new FlattenedSign(this._payload);
+            if (protectedHeader) {
+                flattened.setProtectedHeader(protectedHeader);
+            }
+            if (unprotectedHeader) {
+                flattened.setUnprotectedHeader(unprotectedHeader);
+            }
+            const { payload, ...rest } = await flattened.sign(key, options);
+            payloads.add(payload);
+            jws.payload = payload;
+            jws.signatures.push(rest);
+        }));
+        if (payloads.size !== 1) {
+            throw new JWSInvalid('inconsistent use of JWS Unencoded Payload Option (RFC7797)');
+        }
+        return jws;
+    }
+}
