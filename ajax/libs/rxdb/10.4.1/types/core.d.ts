e13fa93a39d43456e2509b2d370242808079deca
robocdnjs <cdnjs-github@cloudflare.com>
2021-11-13 05:04:11 +0000

    Add rxdb (10.4.1)

---
+++ ajax/libs/rxdb/10.4.1/types/core.d.ts
@@ -0,0 +1,23 @@
+/**
+ * this is the main entry-point for custom builds
+ * it can be used as standalone but is also used in the batteries-included main-export
+ */
+import './types/modules/graphql-client.d';
+import './types/modules/mocha.parallel.d';
+import './types/modules/modifiyjs.d';
+export { addRxPlugin } from './plugin';
+export { createRxDatabase, removeRxDatabase, isRxDatabase, dbCount, _collectionNamePrimary } from './rx-database';
+export { overwritable } from './overwritable';
+export { isRxCollection, RxCollectionBase, createRxCollection } from './rx-collection';
+export { _handleFromStorageInstance, _handleToStorageInstance, fillObjectDataBeforeInsert } from './rx-collection-helper';
+export { isRxDocument } from './rx-document';
+export { getDocumentOrmPrototype, getDocumentPrototype } from './rx-document-prototype-merge';
+export { isInstanceOf as isRxQuery } from './rx-query';
+export { isInstanceOf as isRxSchema, createRxSchema, RxSchema, getIndexes, normalize, getFinalFields, getPreviousVersions } from './rx-schema';
+export { getPseudoSchemaForVersion, getSchemaByObjectPath } from './rx-schema-helper';
+export { findLocalDocument, getSingleDocument, getAllDocuments, writeSingleLocal, writeSingle, countAllUndeleted, getBatch } from './rx-storage-helper';
+export { _clearHook } from './hooks';
+export { createCrypter } from './crypter';
+export * from './query-cache';
+export * from './util';
+export type { JsonSchemaTypes, GraphQLSyncPullOptions, GraphQLSyncPushOptions, AtomicUpdateFunction, CollectionsOfDatabase, MangoQuery, MangoQueryNoLimit, JsonSchema, KeyFunctionMap, MangoQuerySelector, MangoQuerySortDirection, MangoQuerySortPart, MigrationState, NumberFunctionMap, DeepReadonlyObject, PouchDBInstance, PouchReplicationOptions, PouchSettings, PouchSyncHandler, PouchSyncHandlerEvents, PouchdbQuery, RxAttachment, RxAttachmentCreator, RxCollection, RxCacheReplacementPolicy, RxChangeEvent, RxCollectionCreator, RxCollectionGenerated, RxCollectionHookCallback, RxCollectionHookCallbackNonAsync, RxCollectionHookNoInstance, RxCollectionHookNoInstanceCallback, RxDatabase, RxDatabaseCreator, RxDocument, RxDumpCollection, RxDumpCollectionAny, RxDumpCollectionAsAny, RxDumpDatabase, Buffer, Debug, ExtractDTcol, RxDatabaseGenerated, RxDocumentBase, RxDumpCollectionBase, RxDumpDatabaseAny, RxDumpDatabaseBase, RxError, RxErrorItem, RxErrorParameters, RxGraphQLReplicationQueryBuilder, RxJsonSchema, RxLocalDocument, RxPlugin, RxQuery, RxQueryOP, RxQueryObject, RxQueryOptions, RxCouchDBReplicationState, RxTypeError, ServerOptions, SyncOptions, SyncOptionsGraphQL, MigrationStrategy, MigrationStrategies, RxStorage, OldRxCollection, WithAttachmentsData } from './types';
