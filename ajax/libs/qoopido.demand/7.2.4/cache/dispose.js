6705ce61d76e59b063035f1c0dbe7c7140b932e2
robocdnjs <cdnjs-github@cloudflare.com>
2021-11-19 16:04:39 +0000

    Add qoopido.demand (7.2.4)

---
+++ ajax/libs/qoopido.demand/7.2.4/cache/dispose.js
@@ -0,0 +1,3 @@
+/**! Qoopido.demand 7.2.4 | https://github.com/dlueth/qoopido.demand | (c) 2021 Dirk Lueth */
+!function(){"use strict";provide(["/demand/function/iterate","./states"],(function(e,c){function t(e,c){return e.accessed<c.accessed?-1:e.accessed>c.accessed?1:0}return function(e){var n,s=c();for(s.sort(t);e>0&&s.length;)e-=(n=s.shift()).size,demand.cache.clear(n.id)}}))}();
+//# sourceMappingURL=dispose.js.map
