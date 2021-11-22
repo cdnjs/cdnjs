55992a6dfb5c4e24640dfecae72a6d09de4413f9
robocdnjs <cdnjs-github@cloudflare.com>
2021-11-22 06:04:41 +0000

    Add chartjs-adapter-luxon (0.2.0)

---
+++ ajax/libs/chartjs-adapter-luxon/0.2.0/chartjs-adapter-luxon.js
@@ -0,0 +1,94 @@
+/*!
+ * chartjs-adapter-luxon v0.2.0
+ * https://www.chartjs.org
+ * (c) 2019 Chart.js Contributors
+ * Released under the MIT license
+ */
+(function (global, factory) {
+typeof exports === 'object' && typeof module !== 'undefined' ? factory(require('chart.js'), require('luxon')) :
+typeof define === 'function' && define.amd ? define(['chart.js', 'luxon'], factory) :
+(global = global || self, factory(global.Chart, global.luxon));
+}(this, function (chart_js, luxon) { 'use strict';
+
+var FORMATS = {
+	datetime: luxon.DateTime.DATETIME_MED_WITH_SECONDS,
+	millisecond: 'h:mm:ss.SSS a',
+	second: luxon.DateTime.TIME_WITH_SECONDS,
+	minute: luxon.DateTime.TIME_SIMPLE,
+	hour: { hour: 'numeric' },
+	day: { day: 'numeric', month: 'short' },
+	week: 'DD',
+	month: { month: 'short', year: 'numeric' },
+	quarter: "'Q'q - yyyy",
+	year: { year: 'numeric' }
+};
+
+chart_js._adapters._date.override({
+	_id: 'luxon', // DEBUG
+
+	/**
+	 * @private
+	 */
+	_create: function(time) {
+		return luxon.DateTime.fromMillis(time, this.options);
+	},
+
+	formats: function() {
+		return FORMATS;
+	},
+
+	parse: function(value, format) {
+		var options = this.options;
+
+		if (chart_js.helpers.isNullOrUndef(value)) {
+			return null;
+		}
+
+		var type = typeof value;
+		if (type === 'number') {
+			value = this._create(value);
+		} else if (type === 'string') {
+			if (typeof format === 'string') {
+				value = luxon.DateTime.fromFormat(value, format, options);
+			} else {
+				value = luxon.DateTime.fromISO(value, options);
+			}
+		} else if (type === 'object') {
+			value = luxon.DateTime.fromObject(value);
+		} else if (value instanceof Date) {
+			value = luxon.DateTime.fromJSDate(value, options);
+		}
+
+		return value.isValid ? value.valueOf() : null;
+	},
+
+	format: function(time, format) {
+		var datetime = this._create(time);
+		return typeof format === 'string'
+			? datetime.toFormat(format, this.options)
+			: datetime.toLocaleString(format);
+	},
+
+	add: function(time, amount, unit) {
+		var args = {};
+		args[unit] = amount;
+		return this._create(time).plus(args).valueOf();
+	},
+
+	diff: function(max, min, unit) {
+		return this._create(max).diff(this._create(min)).as(unit).valueOf();
+	},
+
+	startOf: function(time, unit, weekday) {
+		if (unit === 'isoWeek') {
+			return this._create(time).isoWeekday(weekday).valueOf();
+		}
+		return unit ? this._create(time).startOf(unit).valueOf() : time;
+	},
+
+	endOf: function(time, unit) {
+		return this._create(time).endOf(unit).valueOf();
+	}
+});
+
+}));
