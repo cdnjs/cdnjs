1878b14bb8fc05306a653c10c4a534eeb43826c8
robocdnjs <cdnjs-github@cloudflare.com>
2022-01-16 23:20:49 +0000

    Add tom-select (2.0.0)

---
+++ ajax/libs/tom-select/2.0.0/types/getSettings.d.ts
@@ -0,0 +1,3 @@
+import { TomSettings } from './types/index';
+import { TomInput } from './types/index';
+export default function getSettings(input: TomInput, settings_user: Partial<TomSettings>): TomSettings;
