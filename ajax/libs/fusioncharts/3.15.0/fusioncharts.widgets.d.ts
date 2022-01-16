6395e25ae803dcfd1731dbdef0e88bd74ffc72c2
robocdnjs <cdnjs-github@cloudflare.com>
2022-01-16 23:17:40 +0000

    Add fusioncharts (3.15.0)

---
+++ ajax/libs/fusioncharts/3.15.0/fusioncharts.widgets.d.ts
@@ -0,0 +1,8 @@
+
+import { FusionChartStatic } from 'fusioncharts';
+
+declare namespace Widgets {}
+declare var Widgets: (H: FusionChartStatic) => FusionChartStatic;
+export = Widgets;
+export as namespace Widgets;
+
