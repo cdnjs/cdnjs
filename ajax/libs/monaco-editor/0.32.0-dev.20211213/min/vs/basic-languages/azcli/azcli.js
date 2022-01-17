8b4146f5cc99d3278a288c9986d2299da16efcfc
robocdnjs <cdnjs-github@cloudflare.com>
2022-01-17 16:04:49 +0000

    Add monaco-editor (0.32.0-dev.20211213)

---
+++ ajax/libs/monaco-editor/0.32.0-dev.20211213/min/vs/basic-languages/azcli/azcli.js
@@ -0,0 +1,10 @@
+/*!-----------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Version: 0.32.0-dev.20211213(3e433551a9be5f9f8ac073422756677b8f5b4b6c)
+ * Released under the MIT license
+ * https://github.com/microsoft/monaco-editor/blob/main/LICENSE.txt
+ *-----------------------------------------------------------------------------*/
+define("vs/basic-languages/azcli/azcli",[],()=>{
+var moduleExports=(()=>{var o=Object.defineProperty;var s=e=>o(e,"__esModule",{value:!0});var a=(e,t)=>{s(e);for(var n in t)o(e,n,{get:t[n],enumerable:!0})};var l={};a(l,{conf:()=>i,language:()=>r});var i={comments:{lineComment:"#"}},r={defaultToken:"keyword",ignoreCase:!0,tokenPostfix:".azcli",str:/[^#\s]/,tokenizer:{root:[{include:"@comment"},[/\s-+@str*\s*/,{cases:{"@eos":{token:"key.identifier",next:"@popall"},"@default":{token:"key.identifier",next:"@type"}}}],[/^-+@str*\s*/,{cases:{"@eos":{token:"key.identifier",next:"@popall"},"@default":{token:"key.identifier",next:"@type"}}}]],type:[{include:"@comment"},[/-+@str*\s*/,{cases:{"@eos":{token:"key.identifier",next:"@popall"},"@default":"key.identifier"}}],[/@str+\s*/,{cases:{"@eos":{token:"string",next:"@popall"},"@default":"string"}}]],comment:[[/#.*$/,{cases:{"@eos":{token:"comment",next:"@popall"}}}]]}};return l;})();
+return moduleExports;
+});
