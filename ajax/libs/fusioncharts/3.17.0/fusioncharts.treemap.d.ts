898e3c698cc820aa50ad939862732b29d76faf14
robocdnjs <cdnjs-github@cloudflare.com>
2022-01-17 02:33:58 +0000

    Add fusioncharts (3.17.0)

---
+++ ajax/libs/fusioncharts/3.17.0/fusioncharts.treemap.d.ts
@@ -0,0 +1,8 @@
+
+import { FusionChartStatic } from 'fusioncharts';
+
+declare namespace Treemap {}
+declare var Treemap: (H: FusionChartStatic) => FusionChartStatic;
+export = Treemap;
+export as namespace Treemap;
+
