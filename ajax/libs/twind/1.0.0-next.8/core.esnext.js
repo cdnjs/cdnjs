4ccdbea48518c5e5020324b3e515e60dbf5a6951
robocdnjs <cdnjs-github@cloudflare.com>
2022-01-18 07:20:46 +0000

    Add twind (1.0.0-next.8)

---
+++ ajax/libs/twind/1.0.0-next.8/core.esnext.js
@@ -0,0 +1,15 @@
+// src/core.ts
+import { init, autoInit } from "@twind/runtime";
+export * from "@twind/core";
+import { tw, theme } from "@twind/runtime";
+var cancelAutoInit = autoInit(setup);
+function setup(config = {}, sheet, target) {
+  cancelAutoInit();
+  return init(config, sheet, target);
+}
+export {
+  setup,
+  theme,
+  tw
+};
+//# sourceMappingURL=core.esnext.js.map
---
+++ ajax/libs/twind/1.0.0-next.8/mini.esnext.js
@@ -0,0 +1,23 @@
+// src/mini.ts
+import {
+  defineConfig
+} from "@twind/core";
+import { init, autoInit } from "@twind/runtime";
+import autoprefix from "@twind/preset-autoprefix";
+import mini from "@twind/preset-mini";
+export * from "@twind/core";
+import { tw, theme } from "@twind/runtime";
+var cancelAutoInit = autoInit(setup);
+function setup(config = {}, sheet, target) {
+  cancelAutoInit();
+  return init(defineConfig({
+    ...config,
+    presets: [autoprefix(), ...config.presets || [], mini()]
+  }), sheet, target);
+}
+export {
+  setup,
+  theme,
+  tw
+};
+//# sourceMappingURL=mini.esnext.js.map
