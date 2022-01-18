18e1d9b5fd04e966267a0773cf955bd02587dae3
robocdnjs <cdnjs-github@cloudflare.com>
2022-01-18 08:26:03 +0000

    Add feathers (5.0.0-pre.16)

---
+++ ajax/libs/feathers/5.0.0-pre.16/events.js
@@ -0,0 +1,29 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.eventMixin = exports.eventHook = void 0;
+const dependencies_1 = require("./dependencies");
+const service_1 = require("./service");
+function eventHook(context, next) {
+    const { events } = (0, service_1.getServiceOptions)(context.self);
+    const defaultEvent = service_1.defaultEventMap[context.method] || null;
+    context.event = defaultEvent;
+    return next().then(() => {
+        // Send the event only if the service does not do so already (indicated in the `events` option)
+        // This is used for custom events and for client services receiving event from the server
+        if (typeof context.event === 'string' && !events.includes(context.event)) {
+            const results = Array.isArray(context.result) ? context.result : [context.result];
+            results.forEach(element => context.self.emit(context.event, element, context));
+        }
+    });
+}
+exports.eventHook = eventHook;
+function eventMixin(service) {
+    const isEmitter = typeof service.on === 'function' &&
+        typeof service.emit === 'function';
+    if (!isEmitter) {
+        Object.assign(service, dependencies_1.EventEmitter.prototype);
+    }
+    return service;
+}
+exports.eventMixin = eventMixin;
+//# sourceMappingURL=events.js.map
