13e06a1f30e185e716d8f7a89bc48e75fd935b37
robocdnjs <cdnjs-github@cloudflare.com>
2022-01-18 06:16:34 +0000

    Add twind (1.0.0-next.1)

---
+++ ajax/libs/twind/1.0.0-next.1/twind.esnext.js
@@ -0,0 +1,15 @@
+// src/index.ts
+import { defineConfig, twind, cssom, observe } from "@twind/core";
+import autoprefix from "@twind/preset-autoprefix";
+import tailwind from "@twind/preset-tailwind";
+var config = defineConfig({
+  presets: [
+    autoprefix(),
+    tailwind()
+  ]
+});
+var tw = observe(twind(config, cssom()));
+export {
+  tw
+};
+//# sourceMappingURL=twind.esnext.js.map
