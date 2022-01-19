9f58ceea243c704866f391988282a82296ceaf77
robocdnjs <cdnjs-github@cloudflare.com>
2022-01-19 17:07:39 +0000

    Add react-chartjs-2 (4.0.1)

---
+++ ajax/libs/react-chartjs-2/4.0.1/chart.d.ts
@@ -0,0 +1,5 @@
+/// <reference types="react" />
+import type { ForwardedRef, ChartProps } from './types';
+export declare const Chart: <TType extends keyof import("chart.js").ChartTypeRegistry = keyof import("chart.js").ChartTypeRegistry, TData = import("chart.js/types/utils").DistributiveArray<import("chart.js").ChartTypeRegistry[TType]["defaultDataPoint"]>, TLabel = unknown>(props: ChartProps<TType, TData, TLabel> & {
+    ref?: ForwardedRef<import("./types").ChartJSOrUndefined<TType, TData, TLabel>> | undefined;
+}) => JSX.Element;
