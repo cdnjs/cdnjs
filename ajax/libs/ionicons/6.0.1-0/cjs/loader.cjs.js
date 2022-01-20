fdd7fa40f9e29c0c4cfb197f6abe1ba6d456ee29
robocdnjs <cdnjs-github@cloudflare.com>
2022-01-20 00:08:50 +0000

    Add ionicons (6.0.1-0)

---
+++ ajax/libs/ionicons/6.0.1-0/cjs/loader.cjs.js
@@ -0,0 +1,21 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+const index = require('./index-2cbd4517.js');
+
+/*
+ Stencil Client Patch Esm v2.12.1 | MIT Licensed | https://stenciljs.com
+ */
+const patchEsm = () => {
+    return index.promiseResolve();
+};
+
+const defineCustomElements = (win, options) => {
+  if (typeof window === 'undefined') return Promise.resolve();
+  return patchEsm().then(() => {
+  return index.bootstrapLazy([["ion-icon.cjs",[[1,"ion-icon",{"mode":[1025],"color":[1],"ios":[1],"md":[1],"flipRtl":[4,"flip-rtl"],"name":[513],"src":[1],"icon":[8],"size":[1],"lazy":[4],"sanitize":[4],"svgContent":[32],"isVisible":[32],"ariaLabel":[32]}]]]], options);
+  });
+};
+
+exports.defineCustomElements = defineCustomElements;
---
+++ ajax/libs/ionicons/6.0.1-0/esm-es5/index.min.js
@@ -0,0 +1,1 @@
+export{a as addIcons}from"./utils-d6d6c83d.js";import"./index-cb2fa58f.js";
