ff2d919cecc76698fa5f06b5463253619f1268be
robocdnjs <cdnjs-github@cloudflare.com>
2022-01-27 04:04:29 +0000

    Add otpauth (7.0.10)

---
+++ ajax/libs/otpauth/7.0.10/index.d.ts
@@ -0,0 +1,6 @@
+import { HOTP } from "./hotp.js";
+import { TOTP } from "./totp.js";
+import { URI } from "./uri.js";
+import { Secret } from "./secret.js";
+import { version } from "./version.js";
+export { HOTP, TOTP, URI, Secret, version };
