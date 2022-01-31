21f515f1d66fb96d135901528bc6065fe2ba22be
robocdnjs <cdnjs-github@cloudflare.com>
2022-01-31 22:03:48 +0000

    Add mmenu-js (9.0.4)

---
+++ ajax/libs/mmenu-js/9.0.4/addons/searchfield/translations/ru.js
@@ -0,0 +1,7 @@
+export default {
+    'cancel': 'отменить',
+    'Cancel searching': 'Отменить поиск',
+    'Clear searchfield': 'Очистить поле поиска',
+    'No results found.': 'Ничего не найдено.',
+    'Search': 'Найти',
+};
