c2180d6442d811b6c09a89a8f7d2d9e1b7bd7bdc
robocdnjs <cdnjs-github@cloudflare.com>
2022-02-07 08:04:18 +0000

    Add simple-icons (6.9.0)

---
+++ ajax/libs/simple-icons/6.9.0/checkio.d.ts
@@ -0,0 +1,1 @@
+/**@deprecated use "const { siCheckio } = require('simple-icons/icons');" instead*/declare const i:import("../alias").I;export default i;
---
+++ ajax/libs/simple-icons/6.9.0/spreadshirt.d.ts
@@ -0,0 +1,1 @@
+/**@deprecated use "const { siSpreadshirt } = require('simple-icons/icons');" instead*/declare const i:import("../alias").I;export default i;
