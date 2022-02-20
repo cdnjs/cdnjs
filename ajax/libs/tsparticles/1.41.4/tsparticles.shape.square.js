a394687e79bd70625135a407a2aea73db41acd96
robocdnjs <cdnjs-github@cloudflare.com>
2022-02-20 23:04:26 +0000

    Add tsparticles (1.41.4)

---
+++ ajax/libs/tsparticles/1.41.4/tsparticles.shape.square.js
@@ -0,0 +1,62 @@
+(function webpackUniversalModuleDefinition(root, factory) {
+  if (typeof exports === "object" && typeof module === "object") module.exports = factory(); else if (typeof define === "function" && define.amd) define([], factory); else {
+    var a = factory();
+    for (var i in a) (typeof exports === "object" ? exports : root)[i] = a[i];
+  }
+})(window, (function() {
+  return function() {
+    "use strict";
+    var __webpack_require__ = {};
+    !function() {
+      __webpack_require__.d = function(exports, definition) {
+        for (var key in definition) {
+          if (__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+            Object.defineProperty(exports, key, {
+              enumerable: true,
+              get: definition[key]
+            });
+          }
+        }
+      };
+    }();
+    !function() {
+      __webpack_require__.o = function(obj, prop) {
+        return Object.prototype.hasOwnProperty.call(obj, prop);
+      };
+    }();
+    !function() {
+      __webpack_require__.r = function(exports) {
+        if (typeof Symbol !== "undefined" && Symbol.toStringTag) {
+          Object.defineProperty(exports, Symbol.toStringTag, {
+            value: "Module"
+          });
+        }
+        Object.defineProperty(exports, "__esModule", {
+          value: true
+        });
+      };
+    }();
+    var __webpack_exports__ = {};
+    __webpack_require__.r(__webpack_exports__);
+    __webpack_require__.d(__webpack_exports__, {
+      loadSquareShape: function() {
+        return loadSquareShape;
+      }
+    });
+    const fixFactor = Math.sqrt(2);
+    class SquareDrawer {
+      getSidesCount() {
+        return 4;
+      }
+      draw(context, particle, radius) {
+        context.rect(-radius / fixFactor, -radius / fixFactor, radius * 2 / fixFactor, radius * 2 / fixFactor);
+      }
+    }
+    async function loadSquareShape(engine) {
+      const drawer = new SquareDrawer;
+      await engine.addShape("edge", drawer);
+      await engine.addShape("square", drawer);
+    }
+    return __webpack_exports__;
+  }();
+}));
