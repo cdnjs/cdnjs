3b0d4ac41c1ade3c84b3bc87c03499d703688b2a
robocdnjs <cdnjs-github@cloudflare.com>
2022-02-25 06:04:15 +0000

    Add caf (15.0.0-preB)

---
+++ ajax/libs/caf/15.0.0-preB/esm/index.mjs
@@ -0,0 +1,5 @@
+/*! CAF: index.mjs
+	v15.0.0-preB (c) 2022 Kyle Simpson
+	MIT License: http://getify.mit-license.org
+*/
+export{default as CAF}from"./caf.mjs";export{default as CAG}from"./cag.mjs";export{default as CAFShared}from"./shared.mjs";
