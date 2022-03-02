cc6712bbf0ac9d2709b331bc316f57979f567b2c
robocdnjs <cdnjs-github@cloudflare.com>
2022-03-02 01:32:55 +0000

    Add rxdb (12.0.0-beta.3)

---
+++ ajax/libs/rxdb/12.0.0-beta.3/types/index.d.ts
@@ -0,0 +1,24 @@
+/**
+ * this is the main entry-point for custom builds
+ * it can be used as standalone but is also used in the batteries-included main-export
+ */
+import './types/modules/graphql-client.d';
+import './types/modules/mocha.parallel.d';
+import './types/modules/modifiyjs.d';
+export { addRxPlugin } from './plugin';
+export { createRxDatabase, removeRxDatabase, isRxDatabase, dbCount, _collectionNamePrimary } from './rx-database';
+export { INTERNAL_CONTEXT_COLLECTION, INTERNAL_CONTEXT_ENCRYPTION, INTERNAL_CONTEXT_REPLICATION_PRIMITIVES, getPrimaryKeyOfInternalDocument, STORAGE_TOKEN_DOCUMENT_KEY } from './rx-database-internal-store';
+export { overwritable } from './overwritable';
+export { isRxCollection, RxCollectionBase, createRxCollection } from './rx-collection';
+export { fillObjectDataBeforeInsert } from './rx-collection-helper';
+export { isRxDocument } from './rx-document';
+export { flattenEvents } from './rx-change-event';
+export { getDocumentOrmPrototype, getDocumentPrototype } from './rx-document-prototype-merge';
+export { isInstanceOf as isRxQuery, normalizeMangoQuery } from './rx-query';
+export { isInstanceOf as isRxSchema, createRxSchema, RxSchema, getIndexes, normalizeRxJsonSchema, getFinalFields, getPreviousVersions, toTypedRxJsonSchema } from './rx-schema';
+export { getPseudoSchemaForVersion, getSchemaByObjectPath } from './rx-schema-helper';
+export { getSingleDocument, getAllDocuments, writeSingle } from './rx-storage-helper';
+export { _clearHook } from './hooks';
+export * from './query-cache';
+export * from './util';
+export type { JsonSchemaTypes, GraphQLSyncPullOptions, GraphQLSyncPushOptions, AtomicUpdateFunction, CollectionsOfDatabase, MangoQuery, MangoQueryNoLimit, JsonSchema, ExtractDocumentTypeFromTypedRxJsonSchema, KeyFunctionMap, MangoQuerySelector, MangoQuerySortDirection, MangoQuerySortPart, MigrationState, NumberFunctionMap, DeepReadonlyObject, RxAttachment, RxAttachmentCreator, RxCollection, RxCacheReplacementPolicy, RxChangeEvent, RxChangeEventBulk, RxCollectionCreator, RxCollectionGenerated, RxCollectionHookCallback, RxCollectionHookCallbackNonAsync, RxCollectionHookNoInstance, RxCollectionHookNoInstanceCallback, RxDatabase, RxDatabaseCreator, RxDocument, RxDumpCollection, RxDumpCollectionAny, RxDumpCollectionAsAny, RxDumpDatabase, Buffer, Debug, ExtractDTcol, RxDatabaseGenerated, RxDocumentBase, RxDumpCollectionBase, RxDumpDatabaseAny, RxDumpDatabaseBase, RxError, RxErrorItem, RxErrorParameters, RxGraphQLReplicationPushQueryBuilder, RxGraphQLReplicationPullQueryBuilder, RxJsonSchema, RxLocalDocument, RxPlugin, RxQuery, RxQueryOP, RxQueryObject, RxQueryOptions, RxCouchDBReplicationState, RxTypeError, ServerOptions, SyncOptions, SyncOptionsGraphQL, MigrationStrategy, MigrationStrategies, RxStorage, RxStorageStatics, FilledMangoQuery, OldRxCollection, WithAttachmentsData } from './types';
