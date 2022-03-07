2f504a7a0bf262d4c354e481988b4b05451a51eb
robocdnjs <cdnjs-github@cloudflare.com>
2022-03-07 10:05:26 +0000

    Add react-inputs-validation (4.9.1)

---
+++ ajax/libs/react-inputs-validation/4.9.1/index.d.ts
@@ -0,0 +1,6 @@
+import Textbox from './Textbox';
+import Textarea from './Textarea';
+import Select from './Select';
+import Checkbox from './Checkbox';
+import Radiobox from './Radiobox';
+export { Textbox, Textarea, Select, Checkbox, Radiobox };
