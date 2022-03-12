a5b2050e4417415cec3fbe0e41c883e4be1a2592
robocdnjs <cdnjs-github@cloudflare.com>
2022-03-12 02:16:48 +0000

    Add highlight.js (11.5.0)

---
+++ ajax/libs/highlight.js/11.5.0/languages/clojure-repl.min.js
@@ -0,0 +1,4 @@
+/*! `clojure-repl` grammar compiled for Highlight.js 11.5.0 */
+(()=>{var e=(()=>{"use strict";return e=>({name:"Clojure REPL",contains:[{
+className:"meta.prompt",begin:/^([\w.-]+|\s*#_)?=>/,starts:{end:/$/,
+subLanguage:"clojure"}}]})})();hljs.registerLanguage("clojure-repl",e)})();
---
+++ ajax/libs/highlight.js/11.5.0/languages/node-repl.min.js
@@ -0,0 +1,5 @@
+/*! `node-repl` grammar compiled for Highlight.js 11.5.0 */
+(()=>{var a=(()=>{"use strict";return a=>({name:"Node REPL",contains:[{
+className:"meta.prompt",starts:{end:/ |$/,starts:{end:"$",
+subLanguage:"javascript"}},variants:[{begin:/^>(?=[ ]|$)/},{
+begin:/^\.\.\.(?=[ ]|$)/}]}]})})();hljs.registerLanguage("node-repl",a)})();
