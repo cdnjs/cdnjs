efcdf6794f25c6152e2383e617eb2da2f7bc82a9
robocdnjs <cdnjs-github@cloudflare.com>
2022-04-26 14:04:18 +0000

    Add openlayers (6.0.0-dev.25)

---
+++ ajax/libs/openlayers/6.0.0-dev.25/render/canvas/Instruction.js
@@ -0,0 +1,39 @@
+/**
+ * @module ol/render/canvas/Instruction
+ */
+/**
+ * @enum {number}
+ */
+var Instruction = {
+    BEGIN_GEOMETRY: 0,
+    BEGIN_PATH: 1,
+    CIRCLE: 2,
+    CLOSE_PATH: 3,
+    CUSTOM: 4,
+    DRAW_CHARS: 5,
+    DRAW_IMAGE: 6,
+    END_GEOMETRY: 7,
+    FILL: 8,
+    MOVE_TO_LINE_TO: 9,
+    SET_FILL_STYLE: 10,
+    SET_STROKE_STYLE: 11,
+    STROKE: 12
+};
+/**
+ * @type {Array<Instruction>}
+ */
+export var fillInstruction = [Instruction.FILL];
+/**
+ * @type {Array<Instruction>}
+ */
+export var strokeInstruction = [Instruction.STROKE];
+/**
+ * @type {Array<Instruction>}
+ */
+export var beginPathInstruction = [Instruction.BEGIN_PATH];
+/**
+ * @type {Array<Instruction>}
+ */
+export var closePathInstruction = [Instruction.CLOSE_PATH];
+export default Instruction;
+//# sourceMappingURL=Instruction.js.map
