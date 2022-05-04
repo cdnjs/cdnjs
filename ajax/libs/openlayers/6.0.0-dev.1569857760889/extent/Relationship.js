ae9b0e203437d34bcb155dc79c75cad242a3b706
robocdnjs <cdnjs-github@cloudflare.com>
2022-05-04 19:07:28 +0000

    Add openlayers (6.0.0-dev.1569857760889)

---
+++ ajax/libs/openlayers/6.0.0-dev.1569857760889/extent/Relationship.js
@@ -0,0 +1,16 @@
+/**
+ * @module ol/extent/Relationship
+ */
+/**
+ * Relationship to an extent.
+ * @enum {number}
+ */
+export default {
+    UNKNOWN: 0,
+    INTERSECTING: 1,
+    ABOVE: 2,
+    RIGHT: 4,
+    BELOW: 8,
+    LEFT: 16
+};
+//# sourceMappingURL=Relationship.js.map
