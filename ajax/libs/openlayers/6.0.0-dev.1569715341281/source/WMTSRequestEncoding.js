580ebca4106aec65a56142d7d22fe93f2c03bc5a
robocdnjs <cdnjs-github@cloudflare.com>
2022-05-04 08:07:04 +0000

    Add openlayers (6.0.0-dev.1569715341281)

---
+++ ajax/libs/openlayers/6.0.0-dev.1569715341281/source/WMTSRequestEncoding.js
@@ -0,0 +1,12 @@
+/**
+ * @module ol/source/WMTSRequestEncoding
+ */
+/**
+ * Request encoding. One of 'KVP', 'REST'.
+ * @enum {string}
+ */
+export default {
+    KVP: 'KVP',
+    REST: 'REST' // see spec §10
+};
+//# sourceMappingURL=WMTSRequestEncoding.js.map
