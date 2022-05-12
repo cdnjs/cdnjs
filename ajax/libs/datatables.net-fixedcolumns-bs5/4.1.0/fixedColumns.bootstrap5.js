851514cf1ef88a1e0a02d81780b01c229f7bcdfc
robocdnjs <cdnjs-github@cloudflare.com>
2022-05-12 12:11:09 +0000

    Add datatables.net-fixedcolumns-bs5 (4.1.0)

---
+++ ajax/libs/datatables.net-fixedcolumns-bs5/4.1.0/fixedColumns.bootstrap5.js
@@ -0,0 +1,36 @@
+/*! Bootstrap 5 integration for DataTables' FixedColumns
+ * ©2016 SpryMedia Ltd - datatables.net/license
+ */
+(function (factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD
+        define(['jquery', 'datatables.net-bs5', 'datatables.net-fixedcolumns'], function ($) {
+            return factory($);
+        });
+    }
+    else if (typeof exports === 'object') {
+        // CommonJS
+        module.exports = function (root, $) {
+            if (!root) {
+                root = window;
+            }
+            if (!$ || !$.fn.dataTable) {
+                // eslint-disable-next-line @typescript-eslint/no-var-requires
+                $ = require('datatables.net-bs5')(root, $).$;
+            }
+            if (!$.fn.dataTable.SearchPanes) {
+                // eslint-disable-next-line @typescript-eslint/no-var-requires
+                require('datatables.net-fixedcolumns')(root, $);
+            }
+            return factory($);
+        };
+    }
+    else {
+        // Browser
+        factory(jQuery);
+    }
+}(function ($) {
+    'use strict';
+    var dataTable = $.fn.dataTable;
+    return dataTable.fixedColumns;
+}));
