c04cc5d6d5cd35df3fec07356073ba6e50cae61a
robocdnjs <cdnjs-github@cloudflare.com>
2022-05-12 13:24:33 +0000

    Add datatables.net-select-bs (1.4.0)

---
+++ ajax/libs/datatables.net-select-bs/1.4.0/select.bootstrap.min.js
@@ -0,0 +1,5 @@
+/*!
+ Bootstrap 3 styling wrapper for Select
+ ©2018 SpryMedia Ltd - datatables.net/license
+*/
+(function(c){"function"===typeof define&&define.amd?define(["jquery","datatables.net-bs","datatables.net-select"],function(a){return c(a,window,document)}):"object"===typeof exports?module.exports=function(a,b){a||(a=window);b&&b.fn.dataTable||(b=require("datatables.net-bs")(a,b).$);b.fn.dataTable.select||require("datatables.net-select")(a,b);return c(b,a,a.document)}:c(jQuery,window,document)})(function(c,a,b,d){return c.fn.dataTable});
