420f436e6c12bbd36f2368117b6258f4bb7db2aa
robocdnjs <cdnjs-github@cloudflare.com>
2022-05-13 13:20:31 +0000

    Add openlayers (6.0.1-dev.1572276228556)

---
+++ ajax/libs/openlayers/6.0.1-dev.1572276228556/src/style/IconAnchorUnits.js
@@ -0,0 +1,12 @@
+/**
+ * @module ol/style/IconAnchorUnits
+ */
+
+/**
+ * Icon anchor units. One of 'fraction', 'pixels'.
+ * @enum {string}
+ */
+export default {
+  FRACTION: 'fraction',
+  PIXELS: 'pixels'
+};
