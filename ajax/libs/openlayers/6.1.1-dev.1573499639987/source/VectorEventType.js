30a95a847e10bf5eda7287e57a88797e5692599e
robocdnjs <cdnjs-github@cloudflare.com>
2022-05-17 21:04:42 +0000

    Add openlayers (6.1.1-dev.1573499639987)

---
+++ ajax/libs/openlayers/6.1.1-dev.1573499639987/source/VectorEventType.js
@@ -0,0 +1,34 @@
+/**
+ * @module ol/source/VectorEventType
+ */
+/**
+ * @enum {string}
+ */
+export default {
+    /**
+     * Triggered when a feature is added to the source.
+     * @event module:ol/source/Vector.VectorSourceEvent#addfeature
+     * @api
+     */
+    ADDFEATURE: 'addfeature',
+    /**
+     * Triggered when a feature is updated.
+     * @event module:ol/source/Vector.VectorSourceEvent#changefeature
+     * @api
+     */
+    CHANGEFEATURE: 'changefeature',
+    /**
+     * Triggered when the clear method is called on the source.
+     * @event module:ol/source/Vector.VectorSourceEvent#clear
+     * @api
+     */
+    CLEAR: 'clear',
+    /**
+     * Triggered when a feature is removed from the source.
+     * See {@link module:ol/source/Vector#clear source.clear()} for exceptions.
+     * @event module:ol/source/Vector.VectorSourceEvent#removefeature
+     * @api
+     */
+    REMOVEFEATURE: 'removefeature'
+};
+//# sourceMappingURL=VectorEventType.js.map
