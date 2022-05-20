328b22ee1e30c36c2789971892c1805796d972da
robocdnjs <cdnjs-github@cloudflare.com>
2022-05-20 13:18:00 +0000

    Add openlayers (6.1.1-dev.1575017106417)

---
+++ ajax/libs/openlayers/6.1.1-dev.1575017106417/src/format/filter/GreaterThan.js
@@ -0,0 +1,23 @@
+/**
+ * @module ol/format/filter/GreaterThan
+ */
+import ComparisonBinary from './ComparisonBinary.js';
+
+/**
+ * @classdesc
+ * Represents a `<PropertyIsGreaterThan>` comparison operator.
+ * @api
+ */
+class GreaterThan extends ComparisonBinary {
+
+  /**
+   * @param {!string} propertyName Name of the context property to compare.
+   * @param {!number} expression The value to compare.
+   */
+  constructor(propertyName, expression) {
+    super('PropertyIsGreaterThan', propertyName, expression);
+  }
+
+}
+
+export default GreaterThan;
