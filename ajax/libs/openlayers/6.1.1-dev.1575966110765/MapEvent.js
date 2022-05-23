4dfb57f75091d384148ec3433316211c10ad927c
robocdnjs <cdnjs-github@cloudflare.com>
2022-05-23 12:12:32 +0000

    Add openlayers (6.1.1-dev.1575966110765)

---
+++ ajax/libs/openlayers/6.1.1-dev.1575966110765/MapEvent.js
@@ -0,0 +1,49 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+/**
+ * @module ol/MapEvent
+ */
+import Event from './events/Event.js';
+/**
+ * @classdesc
+ * Events emitted as map events are instances of this type.
+ * See {@link module:ol/PluggableMap~PluggableMap} for which events trigger a map event.
+ */
+var MapEvent = /** @class */ (function (_super) {
+    __extends(MapEvent, _super);
+    /**
+     * @param {string} type Event type.
+     * @param {import("./PluggableMap.js").default} map Map.
+     * @param {?import("./PluggableMap.js").FrameState=} opt_frameState Frame state.
+     */
+    function MapEvent(type, map, opt_frameState) {
+        var _this = _super.call(this, type) || this;
+        /**
+         * The map where the event occurred.
+         * @type {import("./PluggableMap.js").default}
+         * @api
+         */
+        _this.map = map;
+        /**
+         * The frame state at the time of the event.
+         * @type {?import("./PluggableMap.js").FrameState}
+         * @api
+         */
+        _this.frameState = opt_frameState !== undefined ? opt_frameState : null;
+        return _this;
+    }
+    return MapEvent;
+}(Event));
+export default MapEvent;
+//# sourceMappingURL=MapEvent.js.map
