e5e0b8dca91b6628e0b614209f43a47196796bce
robocdnjs <cdnjs-github@cloudflare.com>
2022-05-24 02:45:24 +0000

    Add react-minimal-datetime-range (2.0.7)

---
+++ ajax/libs/react-minimal-datetime-range/2.0.7/RangeDate.d.ts
@@ -0,0 +1,24 @@
+import React from 'react';
+interface IObjectKeysAny {
+    [key: string]: any;
+}
+interface IndexProps {
+    selected: boolean;
+    setSelected: (res: boolean) => void;
+    locale?: string;
+    defaultDateStart?: string;
+    defaultDateEnd?: string;
+    rangeDirection?: string;
+    startDatePickedArray?: Array<string>;
+    endDatePickedArray?: Array<string>;
+    markedDates?: Array<string>;
+    supportDateRange?: Array<string>;
+    handleChooseStartDate?: (res: object) => void;
+    handleChooseEndDate?: (res: object) => void;
+    currentDateObjStart?: IObjectKeysAny;
+    setCurrentDateObjStart?: (res: object) => void;
+    currentDateObjEnd?: IObjectKeysAny;
+    setCurrentDateObjEnd?: (res: object) => void;
+}
+declare const Index: React.FC<IndexProps>;
+export default Index;
