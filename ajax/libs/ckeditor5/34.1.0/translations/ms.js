f0c37de258aa6c212436f7ad0b7d2e3e52f7d0ac
robocdnjs <cdnjs-github@cloudflare.com>
2022-05-25 17:09:41 +0000

    Add ckeditor5 (34.1.0)

---
+++ ajax/libs/ckeditor5/34.1.0/translations/ms.js
@@ -0,0 +1,1 @@
+!function(n){const a=n.ms=n.ms||{};a.dictionary=Object.assign(a.dictionary||{},{"%0 of %1":"",Cancel:"","Cannot upload file:":"Gagal memuat naik fail",Save:"","Show more items":""}),a.getPluralForm=function(n){return 0}}(window.CKEDITOR_TRANSLATIONS||(window.CKEDITOR_TRANSLATIONS={}));
