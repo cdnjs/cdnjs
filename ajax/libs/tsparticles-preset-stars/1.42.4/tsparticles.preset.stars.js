39260bfdd8bc415cf4eecb2b8be615b9e0abb869
robocdnjs <cdnjs-github@cloudflare.com>
2022-05-30 10:09:23 +0000

    Add tsparticles-preset-stars (1.42.4)

---
+++ ajax/libs/tsparticles-preset-stars/1.42.4/tsparticles.preset.stars.js
@@ -0,0 +1,111 @@
+/*!
+ * Author : Matteo Bruni
+ * MIT license: https://opensource.org/licenses/MIT
+ * Demo / Generator : https://particles.js.org/
+ * GitHub : https://www.github.com/matteobruni/tsparticles
+ * How to use? : Check the GitHub README
+ * v1.42.4
+ */
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define([], factory);
+	else {
+		var a = factory();
+		for(var i in a) (typeof exports === 'object' ? exports : root)[i] = a[i];
+	}
+})(this, function() {
+return /******/ (function() { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	// The require scope
+/******/ 	var __webpack_require__ = {};
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	!function() {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = function(exports, definition) {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	!function() {
+/******/ 		__webpack_require__.o = function(obj, prop) { return Object.prototype.hasOwnProperty.call(obj, prop); }
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	!function() {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = function(exports) {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	}();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "loadStarsPreset": function() { return /* binding */ loadStarsPreset; }
+});
+
+;// CONCATENATED MODULE: ./dist/options.js
+const options = {
+  background: {
+    color: "#000"
+  },
+  particles: {
+    color: {
+      value: "#ffffff"
+    },
+    lineLinked: {
+      enable: false
+    },
+    move: {
+      bounce: false,
+      direction: "none",
+      enable: true,
+      outMode: "out",
+      random: true,
+      speed: 0.1,
+      straight: false
+    },
+    opacity: {
+      animation: {
+        enable: true,
+        minimumValue: 0,
+        speed: 1,
+        sync: false
+      },
+      random: true,
+      value: 1
+    },
+    shape: {
+      type: "circle"
+    },
+    size: {
+      random: true,
+      value: 3
+    }
+  }
+};
+;// CONCATENATED MODULE: ./dist/index.js
+
+function loadStarsPreset(engine) {
+  engine.addPreset("stars", options);
+}
+/******/ 	return __webpack_exports__;
+/******/ })()
+;
+});
