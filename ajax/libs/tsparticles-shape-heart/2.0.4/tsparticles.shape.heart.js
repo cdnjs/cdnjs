d35437623468655228af7183f9a615298e55d99b
robocdnjs <cdnjs-github@cloudflare.com>
2022-05-30 17:08:40 +0000

    Add tsparticles-shape-heart (2.0.4)

---
+++ ajax/libs/tsparticles-shape-heart/2.0.4/tsparticles.shape.heart.js
@@ -0,0 +1,144 @@
+/*!
+ * Author : Matteo Bruni
+ * MIT license: https://opensource.org/licenses/MIT
+ * Demo / Generator : https://particles.js.org/
+ * GitHub : https://www.github.com/matteobruni/tsparticles
+ * How to use? : Check the GitHub README
+ * v2.0.4
+ */
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define([], factory);
+	else {
+		var a = factory();
+		for(var i in a) (typeof exports === 'object' ? exports : root)[i] = a[i];
+	}
+})(this, function() {
+return /******/ (function() { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ 444:
+/***/ (function(__unused_webpack_module, exports) {
+
+
+
+Object.defineProperty(exports, "__esModule", ({
+  value: true
+}));
+exports.HeartDrawer = void 0;
+
+class HeartDrawer {
+  draw(context, particle, radius) {
+    const x = -radius;
+    const y = -radius;
+    context.moveTo(x, y + radius / 2);
+    context.quadraticCurveTo(x, y, x + radius / 2, y);
+    context.quadraticCurveTo(x + radius, y, x + radius, y + radius / 2);
+    context.quadraticCurveTo(x + radius, y, x + radius * 3 / 2, y);
+    context.quadraticCurveTo(x + radius * 2, y, x + radius * 2, y + radius / 2);
+    context.quadraticCurveTo(x + radius * 2, y + radius, x + radius * 3 / 2, y + radius * 3 / 2);
+    context.lineTo(x + radius, y + radius * 2);
+    context.lineTo(x + radius / 2, y + radius * 3 / 2);
+    context.quadraticCurveTo(x, y + radius, x, y + radius / 2);
+  }
+
+}
+
+exports.HeartDrawer = HeartDrawer;
+
+/***/ }),
+
+/***/ 153:
+/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
+
+
+
+var __awaiter = this && this.__awaiter || function (thisArg, _arguments, P, generator) {
+  function adopt(value) {
+    return value instanceof P ? value : new P(function (resolve) {
+      resolve(value);
+    });
+  }
+
+  return new (P || (P = Promise))(function (resolve, reject) {
+    function fulfilled(value) {
+      try {
+        step(generator.next(value));
+      } catch (e) {
+        reject(e);
+      }
+    }
+
+    function rejected(value) {
+      try {
+        step(generator["throw"](value));
+      } catch (e) {
+        reject(e);
+      }
+    }
+
+    function step(result) {
+      result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected);
+    }
+
+    step((generator = generator.apply(thisArg, _arguments || [])).next());
+  });
+};
+
+Object.defineProperty(exports, "__esModule", ({
+  value: true
+}));
+exports.loadHeartShape = void 0;
+
+const HeartDrawer_1 = __webpack_require__(444);
+
+function loadHeartShape(engine) {
+  return __awaiter(this, void 0, void 0, function* () {
+    yield engine.addShape("heart", new HeartDrawer_1.HeartDrawer());
+  });
+}
+
+exports.loadHeartShape = loadHeartShape;
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module is referenced by other modules so it can't be inlined
+/******/ 	var __webpack_exports__ = __webpack_require__(153);
+/******/ 	
+/******/ 	return __webpack_exports__;
+/******/ })()
+;
+});
