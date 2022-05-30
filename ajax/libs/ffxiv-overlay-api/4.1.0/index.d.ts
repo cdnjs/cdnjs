83bd992225112654883da490be0f8ca516b3a73d
robocdnjs <cdnjs-github@cloudflare.com>
2022-05-30 14:18:35 +0000

    Add ffxiv-overlay-api (4.1.0)

---
+++ ajax/libs/ffxiv-overlay-api/4.1.0/index.d.ts
@@ -0,0 +1,5 @@
+/*! ffxiv-overlay-plugin | DSRKafuU (https://dsrkafuu.net) | Copyright (c) MIT License */
+import OverlayAPI from './overlay';
+export { OverlayAPI };
+export default OverlayAPI;
+export type { JobType, EncounterData, LimitBreakData, CombatantData, ExtendData, EventType, EventData, EventCallback, } from './types';
