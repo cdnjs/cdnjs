9c4e8d53d9f017283ac9c5a6d13be3cc6b2d1954
robocdnjs <cdnjs-github@cloudflare.com>
2022-05-30 13:18:04 +0000

    Add tsparticles-shape-heart (1.43.1)

---
+++ ajax/libs/tsparticles-shape-heart/1.43.1/tsparticles.shape.heart.js
@@ -0,0 +1,107 @@
+/*!
+ * Author : Matteo Bruni
+ * MIT license: https://opensource.org/licenses/MIT
+ * Demo / Generator : https://particles.js.org/
+ * GitHub : https://www.github.com/matteobruni/tsparticles
+ * How to use? : Check the GitHub README
+ * v1.43.1
+ */
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory(require("tsparticles"));
+	else if(typeof define === 'function' && define.amd)
+		define(["tsparticles"], factory);
+	else {
+		var a = typeof exports === 'object' ? factory(require("tsparticles")) : factory(root["window"]);
+		for(var i in a) (typeof exports === 'object' ? exports : root)[i] = a[i];
+	}
+})(this, function(__WEBPACK_EXTERNAL_MODULE__764__) {
+return /******/ (function() { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ 764:
+/***/ (function(module) {
+
+module.exports = __WEBPACK_EXTERNAL_MODULE__764__;
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	!function() {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = function(exports) {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	}();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+!function() {
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXTERNAL MODULE: external {"commonjs":"tsparticles","commonjs2":"tsparticles","amd":"tsparticles","root":"window"}
+var external_commonjs_tsparticles_commonjs2_tsparticles_amd_tsparticles_root_window_ = __webpack_require__(764);
+;// CONCATENATED MODULE: ./dist/browser/HeartDrawer.js
+class HeartDrawer {
+  draw(context, particle, radius) {
+    const x = -radius;
+    const y = -radius;
+    context.moveTo(x, y + radius / 2);
+    context.quadraticCurveTo(x, y, x + radius / 2, y);
+    context.quadraticCurveTo(x + radius, y, x + radius, y + radius / 2);
+    context.quadraticCurveTo(x + radius, y, x + radius * 3 / 2, y);
+    context.quadraticCurveTo(x + radius * 2, y, x + radius * 2, y + radius / 2);
+    context.quadraticCurveTo(x + radius * 2, y + radius, x + radius * 3 / 2, y + radius * 3 / 2);
+    context.lineTo(x + radius, y + radius * 2);
+    context.lineTo(x + radius / 2, y + radius * 3 / 2);
+    context.quadraticCurveTo(x, y + radius, x, y + radius / 2);
+  }
+
+}
+;// CONCATENATED MODULE: ./dist/browser/shape.js
+
+function loadHeartShape(engine) {
+  engine.addShape("heart", new HeartDrawer());
+}
+;// CONCATENATED MODULE: ./dist/browser/index.js
+
+
+loadHeartShape(external_commonjs_tsparticles_commonjs2_tsparticles_amd_tsparticles_root_window_.tsParticles);
+}();
+/******/ 	return __webpack_exports__;
+/******/ })()
+;
+});
