92e1a8934e8c73f3fd58b5ba491ca8d1c5c7c989
robocdnjs <cdnjs-github@cloudflare.com>
2022-05-30 07:12:18 +0000

    Add tsparticles-preset-bubbles (1.42.4)

---
+++ ajax/libs/tsparticles-preset-bubbles/1.42.4/tsparticles.preset.bubbles.js
@@ -0,0 +1,125 @@
+/*!
+ * Author : Matteo Bruni
+ * MIT license: https://opensource.org/licenses/MIT
+ * Demo / Generator : https://particles.js.org/
+ * GitHub : https://www.github.com/matteobruni/tsparticles
+ * How to use? : Check the GitHub README
+ * v1.42.4
+ */
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define([], factory);
+	else {
+		var a = factory();
+		for(var i in a) (typeof exports === 'object' ? exports : root)[i] = a[i];
+	}
+})(this, function() {
+return /******/ (function() { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	// The require scope
+/******/ 	var __webpack_require__ = {};
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	!function() {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = function(exports, definition) {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	!function() {
+/******/ 		__webpack_require__.o = function(obj, prop) { return Object.prototype.hasOwnProperty.call(obj, prop); }
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	!function() {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = function(exports) {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	}();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "loadBubblesPreset": function() { return /* binding */ loadBubblesPreset; }
+});
+
+;// CONCATENATED MODULE: ./dist/options.js
+const options = {
+  fpsLimit: 120,
+  particles: {
+    number: {
+      value: 0
+    },
+    color: {
+      value: "random"
+    },
+    shape: {
+      type: "circle"
+    },
+    opacity: {
+      value: 0.3
+    },
+    size: {
+      value: 10,
+      random: {
+        enable: true,
+        minimumValue: 5
+      }
+    },
+    move: {
+      angle: {
+        offset: 0,
+        value: 30
+      },
+      enable: true,
+      speed: 15,
+      direction: "top",
+      random: false,
+      straight: false,
+      outModes: {
+        default: "destroy"
+      }
+    }
+  },
+  detectRetina: true,
+  background: {
+    color: "#fff"
+  },
+  emitters: [{
+    direction: "top",
+    position: {
+      y: 100
+    },
+    life: {
+      duration: 3,
+      delay: 5,
+      count: 0
+    }
+  }]
+};
+;// CONCATENATED MODULE: ./dist/index.js
+
+function loadBubblesPreset(engine) {
+  engine.addPreset("bubbles", options);
+}
+/******/ 	return __webpack_exports__;
+/******/ })()
+;
+});
