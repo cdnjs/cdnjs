d9a5992795ddd619d5a1646536b2d356f28395c7
robocdnjs <cdnjs-github@cloudflare.com>
2022-05-30 01:37:07 +0000

    Add tsparticles-preset-fireworks (1.42.2)

---
+++ ajax/libs/tsparticles-preset-fireworks/1.42.2/tsparticles.preset.fireworks.js
@@ -0,0 +1,222 @@
+/*!
+ * Author : Matteo Bruni
+ * MIT license: https://opensource.org/licenses/MIT
+ * Demo / Generator : https://particles.js.org/
+ * GitHub : https://www.github.com/matteobruni/tsparticles
+ * How to use? : Check the GitHub README
+ * v1.42.2
+ */
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define([], factory);
+	else {
+		var a = factory();
+		for(var i in a) (typeof exports === 'object' ? exports : root)[i] = a[i];
+	}
+})(this, function() {
+return /******/ (function() { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	// The require scope
+/******/ 	var __webpack_require__ = {};
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	!function() {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = function(exports, definition) {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	!function() {
+/******/ 		__webpack_require__.o = function(obj, prop) { return Object.prototype.hasOwnProperty.call(obj, prop); }
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	!function() {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = function(exports) {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	}();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "loadFireworksPreset": function() { return /* binding */ loadFireworksPreset; }
+});
+
+;// CONCATENATED MODULE: ./dist/options.js
+const options = {
+  detectRetina: true,
+  background: {
+    color: "#000"
+  },
+  fpsLimit: 120,
+  emitters: {
+    direction: "top",
+    life: {
+      count: 0,
+      duration: 0.1,
+      delay: 0.1
+    },
+    rate: {
+      delay: 0.5,
+      quantity: 1
+    },
+    size: {
+      width: 100,
+      height: 0
+    },
+    position: {
+      y: 100,
+      x: 50
+    }
+  },
+  particles: {
+    number: {
+      value: 0
+    },
+    destroy: {
+      mode: "split",
+      split: {
+        count: 1,
+        factor: {
+          value: 0.333333
+        },
+        rate: {
+          value: 100
+        },
+        particles: {
+          stroke: {
+            width: 0
+          },
+          color: {
+            value: ["#ff595e", "#ffca3a", "#8ac926", "#1982c4", "#6a4c93"]
+          },
+          number: {
+            value: 0
+          },
+          collisions: {
+            enable: false
+          },
+          opacity: {
+            value: {
+              min: 0.1,
+              max: 1
+            },
+            animation: {
+              enable: true,
+              speed: 0.7,
+              sync: false,
+              startValue: "max",
+              destroy: "min"
+            }
+          },
+          shape: {
+            type: "circle"
+          },
+          size: {
+            value: 2,
+            animation: {
+              enable: false
+            }
+          },
+          life: {
+            count: 1,
+            duration: {
+              value: {
+                min: 1,
+                max: 2
+              }
+            }
+          },
+          move: {
+            enable: true,
+            gravity: {
+              enable: false
+            },
+            speed: 2,
+            direction: "none",
+            random: true,
+            straight: false,
+            outMode: "destroy"
+          }
+        }
+      }
+    },
+    life: {
+      count: 1
+    },
+    shape: {
+      type: "line"
+    },
+    size: {
+      value: {
+        min: 0.1,
+        max: 50
+      },
+      animation: {
+        enable: true,
+        sync: true,
+        speed: 90,
+        startValue: "max",
+        destroy: "min"
+      }
+    },
+    stroke: {
+      color: {
+        value: "#ffffff"
+      },
+      width: 1
+    },
+    rotate: {
+      path: true
+    },
+    move: {
+      enable: true,
+      gravity: {
+        acceleration: 15,
+        enable: true,
+        inverse: true,
+        maxSpeed: 100
+      },
+      speed: {
+        min: 10,
+        max: 20
+      },
+      outModes: {
+        default: "destroy",
+        top: "none"
+      },
+      trail: {
+        fillColor: "#000",
+        enable: true,
+        length: 10
+      }
+    }
+  }
+};
+;// CONCATENATED MODULE: ./dist/index.js
+
+function loadFireworksPreset(engine) {
+  engine.addPreset("fireworks", options);
+}
+/******/ 	return __webpack_exports__;
+/******/ })()
+;
+});
