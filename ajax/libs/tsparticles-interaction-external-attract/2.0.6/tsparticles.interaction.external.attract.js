e5c921bc1df42e18e07224938f06d494b20ad9e2
robocdnjs <cdnjs-github@cloudflare.com>
2022-05-31 23:07:21 +0000

    Add tsparticles-interaction-external-attract (2.0.6)

---
+++ ajax/libs/tsparticles-interaction-external-attract/2.0.6/tsparticles.interaction.external.attract.js
@@ -0,0 +1,275 @@
+/*!
+ * Author : Matteo Bruni
+ * MIT license: https://opensource.org/licenses/MIT
+ * Demo / Generator : https://particles.js.org/
+ * GitHub : https://www.github.com/matteobruni/tsparticles
+ * How to use? : Check the GitHub README
+ * v2.0.6
+ */
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory(require("tsparticles-engine"));
+	else if(typeof define === 'function' && define.amd)
+		define(["tsparticles-engine"], factory);
+	else {
+		var a = typeof exports === 'object' ? factory(require("tsparticles-engine")) : factory(root["window"]);
+		for(var i in a) (typeof exports === 'object' ? exports : root)[i] = a[i];
+	}
+})(this, (__WEBPACK_EXTERNAL_MODULE__818__) => {
+return /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ 818:
+/***/ ((module) => {
+
+module.exports = __WEBPACK_EXTERNAL_MODULE__818__;
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "loadExternalAttractInteraction": () => (/* binding */ loadExternalAttractInteraction)
+});
+
+// EXTERNAL MODULE: external {"commonjs":"tsparticles-engine","commonjs2":"tsparticles-engine","amd":"tsparticles-engine","root":"window"}
+var external_commonjs_tsparticles_engine_commonjs2_tsparticles_engine_amd_tsparticles_engine_root_window_ = __webpack_require__(818);
+;// CONCATENATED MODULE: ./dist/browser/Attractor.js
+
+/**
+ * Particle external attract manager
+ * @category Interactions
+ */
+
+class Attractor extends external_commonjs_tsparticles_engine_commonjs2_tsparticles_engine_amd_tsparticles_engine_root_window_.ExternalInteractorBase {
+  constructor(container) {
+    super(container);
+
+    if (!container.attract) {
+      container.attract = {
+        particles: []
+      };
+    }
+
+    this.handleClickMode = mode => {
+      const options = this.container.actualOptions;
+
+      if (mode !== "attract"
+      /* attract */
+      ) {
+        return;
+      }
+
+      if (!container.attract) {
+        container.attract = {
+          particles: []
+        };
+      }
+
+      container.attract.clicking = true;
+      container.attract.count = 0;
+
+      for (const particle of container.attract.particles) {
+        particle.velocity.setTo(particle.initialVelocity);
+      }
+
+      container.attract.particles = [];
+      container.attract.finish = false;
+      setTimeout(() => {
+        if (!container.destroyed) {
+          if (!container.attract) {
+            container.attract = {
+              particles: []
+            };
+          }
+
+          container.attract.clicking = false;
+        }
+      }, options.interactivity.modes.attract.duration * 1000);
+    };
+  }
+
+  isEnabled() {
+    const container = this.container,
+          options = container.actualOptions,
+          mouse = container.interactivity.mouse,
+          events = options.interactivity.events;
+
+    if ((!mouse.position || !events.onHover.enable) && (!mouse.clickPosition || !events.onClick.enable)) {
+      return false;
+    }
+
+    const hoverMode = events.onHover.mode,
+          clickMode = events.onClick.mode;
+    return (0,external_commonjs_tsparticles_engine_commonjs2_tsparticles_engine_amd_tsparticles_engine_root_window_.isInArray)("attract"
+    /* attract */
+    , hoverMode) || (0,external_commonjs_tsparticles_engine_commonjs2_tsparticles_engine_amd_tsparticles_engine_root_window_.isInArray)("attract"
+    /* attract */
+    , clickMode);
+  }
+
+  reset() {// do nothing
+  }
+
+  async interact() {
+    const container = this.container,
+          options = container.actualOptions,
+          mouseMoveStatus = container.interactivity.status === external_commonjs_tsparticles_engine_commonjs2_tsparticles_engine_amd_tsparticles_engine_root_window_.mouseMoveEvent,
+          events = options.interactivity.events,
+          hoverEnabled = events.onHover.enable,
+          hoverMode = events.onHover.mode,
+          clickEnabled = events.onClick.enable,
+          clickMode = events.onClick.mode;
+
+    if (mouseMoveStatus && hoverEnabled && (0,external_commonjs_tsparticles_engine_commonjs2_tsparticles_engine_amd_tsparticles_engine_root_window_.isInArray)("attract"
+    /* attract */
+    , hoverMode)) {
+      this.hoverAttract();
+    } else if (clickEnabled && (0,external_commonjs_tsparticles_engine_commonjs2_tsparticles_engine_amd_tsparticles_engine_root_window_.isInArray)("attract"
+    /* attract */
+    , clickMode)) {
+      this.clickAttract();
+    }
+  }
+
+  hoverAttract() {
+    const container = this.container;
+    const mousePos = container.interactivity.mouse.position;
+
+    if (!mousePos) {
+      return;
+    }
+
+    const attractRadius = container.retina.attractModeDistance;
+    this.processAttract(mousePos, attractRadius, new external_commonjs_tsparticles_engine_commonjs2_tsparticles_engine_amd_tsparticles_engine_root_window_.Circle(mousePos.x, mousePos.y, attractRadius));
+  }
+
+  processAttract(position, attractRadius, area) {
+    const container = this.container;
+    const attractOptions = container.actualOptions.interactivity.modes.attract;
+    const query = container.particles.quadTree.query(area);
+
+    for (const particle of query) {
+      const {
+        dx,
+        dy,
+        distance
+      } = (0,external_commonjs_tsparticles_engine_commonjs2_tsparticles_engine_amd_tsparticles_engine_root_window_.getDistances)(particle.position, position);
+      const velocity = attractOptions.speed * attractOptions.factor;
+      const attractFactor = (0,external_commonjs_tsparticles_engine_commonjs2_tsparticles_engine_amd_tsparticles_engine_root_window_.clamp)((0,external_commonjs_tsparticles_engine_commonjs2_tsparticles_engine_amd_tsparticles_engine_root_window_.calcEasing)(1 - distance / attractRadius, attractOptions.easing) * velocity, 0, attractOptions.maxSpeed);
+      const normVec = external_commonjs_tsparticles_engine_commonjs2_tsparticles_engine_amd_tsparticles_engine_root_window_.Vector.create(distance === 0 ? velocity : dx / distance * attractFactor, distance === 0 ? velocity : dy / distance * attractFactor);
+      particle.position.subFrom(normVec);
+    }
+  }
+
+  clickAttract() {
+    const container = this.container;
+
+    if (!container.attract) {
+      container.attract = {
+        particles: []
+      };
+    }
+
+    if (!container.attract.finish) {
+      if (!container.attract.count) {
+        container.attract.count = 0;
+      }
+
+      container.attract.count++;
+
+      if (container.attract.count === container.particles.count) {
+        container.attract.finish = true;
+      }
+    }
+
+    if (container.attract.clicking) {
+      const mousePos = container.interactivity.mouse.clickPosition;
+
+      if (!mousePos) {
+        return;
+      }
+
+      const attractRadius = container.retina.attractModeDistance;
+      this.processAttract(mousePos, attractRadius, new external_commonjs_tsparticles_engine_commonjs2_tsparticles_engine_amd_tsparticles_engine_root_window_.Circle(mousePos.x, mousePos.y, attractRadius));
+    } else if (container.attract.clicking === false) {
+      container.attract.particles = [];
+    }
+
+    return;
+  }
+
+}
+;// CONCATENATED MODULE: ./dist/browser/index.js
+
+async function loadExternalAttractInteraction(engine) {
+  await engine.addInteractor("externalAttract", container => new Attractor(container));
+}
+})();
+
+/******/ 	return __webpack_exports__;
+/******/ })()
+;
+});
