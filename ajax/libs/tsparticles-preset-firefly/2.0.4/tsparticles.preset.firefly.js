fcc3107ba214e7b47d5049316b7000cf3d6b68d4
robocdnjs <cdnjs-github@cloudflare.com>
2022-05-31 04:16:51 +0000

    Add tsparticles-preset-firefly (2.0.4)

---
+++ ajax/libs/tsparticles-preset-firefly/2.0.4/tsparticles.preset.firefly.js
@@ -0,0 +1,258 @@
+/*!
+ * Author : Matteo Bruni
+ * MIT license: https://opensource.org/licenses/MIT
+ * Demo / Generator : https://particles.js.org/
+ * GitHub : https://www.github.com/matteobruni/tsparticles
+ * How to use? : Check the GitHub README
+ * v2.0.4
+ */
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory(require("tsparticles-interaction-external-trail"), require("tsparticles-move-base"), require("tsparticles-shape-circle"), require("tsparticles-updater-color"), require("tsparticles-updater-life"), require("tsparticles-updater-opacity"), require("tsparticles-updater-size"));
+	else if(typeof define === 'function' && define.amd)
+		define(["tsparticles-interaction-external-trail", "tsparticles-move-base", "tsparticles-shape-circle", "tsparticles-updater-color", "tsparticles-updater-life", "tsparticles-updater-opacity", "tsparticles-updater-size"], factory);
+	else {
+		var a = typeof exports === 'object' ? factory(require("tsparticles-interaction-external-trail"), require("tsparticles-move-base"), require("tsparticles-shape-circle"), require("tsparticles-updater-color"), require("tsparticles-updater-life"), require("tsparticles-updater-opacity"), require("tsparticles-updater-size")) : factory(root["window"], root["window"], root["window"], root["window"], root["window"], root["window"], root["window"]);
+		for(var i in a) (typeof exports === 'object' ? exports : root)[i] = a[i];
+	}
+})(this, function(__WEBPACK_EXTERNAL_MODULE__165__, __WEBPACK_EXTERNAL_MODULE__919__, __WEBPACK_EXTERNAL_MODULE__941__, __WEBPACK_EXTERNAL_MODULE__841__, __WEBPACK_EXTERNAL_MODULE__489__, __WEBPACK_EXTERNAL_MODULE__838__, __WEBPACK_EXTERNAL_MODULE__328__) {
+return /******/ (function() { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ 153:
+/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
+
+
+
+var __awaiter = this && this.__awaiter || function (thisArg, _arguments, P, generator) {
+  function adopt(value) {
+    return value instanceof P ? value : new P(function (resolve) {
+      resolve(value);
+    });
+  }
+
+  return new (P || (P = Promise))(function (resolve, reject) {
+    function fulfilled(value) {
+      try {
+        step(generator.next(value));
+      } catch (e) {
+        reject(e);
+      }
+    }
+
+    function rejected(value) {
+      try {
+        step(generator["throw"](value));
+      } catch (e) {
+        reject(e);
+      }
+    }
+
+    function step(result) {
+      result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected);
+    }
+
+    step((generator = generator.apply(thisArg, _arguments || [])).next());
+  });
+};
+
+Object.defineProperty(exports, "__esModule", ({
+  value: true
+}));
+exports.loadFireflyPreset = void 0;
+
+const tsparticles_move_base_1 = __webpack_require__(919);
+
+const tsparticles_shape_circle_1 = __webpack_require__(941);
+
+const tsparticles_updater_color_1 = __webpack_require__(841);
+
+const tsparticles_interaction_external_trail_1 = __webpack_require__(165);
+
+const tsparticles_updater_life_1 = __webpack_require__(489);
+
+const tsparticles_updater_opacity_1 = __webpack_require__(838);
+
+const tsparticles_updater_size_1 = __webpack_require__(328);
+
+const options_1 = __webpack_require__(689);
+
+function loadFireflyPreset(engine) {
+  return __awaiter(this, void 0, void 0, function* () {
+    yield (0, tsparticles_move_base_1.loadBaseMover)(engine);
+    yield (0, tsparticles_interaction_external_trail_1.loadExternalTrailInteraction)(engine);
+    yield (0, tsparticles_shape_circle_1.loadCircleShape)(engine);
+    yield (0, tsparticles_updater_color_1.loadColorUpdater)(engine);
+    yield (0, tsparticles_updater_life_1.loadLifeUpdater)(engine);
+    yield (0, tsparticles_updater_opacity_1.loadOpacityUpdater)(engine);
+    yield (0, tsparticles_updater_size_1.loadSizeUpdater)(engine);
+    yield engine.addPreset("firefly", options_1.options);
+  });
+}
+
+exports.loadFireflyPreset = loadFireflyPreset;
+
+/***/ }),
+
+/***/ 689:
+/***/ (function(__unused_webpack_module, exports) {
+
+
+
+Object.defineProperty(exports, "__esModule", ({
+  value: true
+}));
+exports.options = void 0;
+exports.options = {
+  fullScreen: {
+    enable: true,
+    zIndex: -1
+  },
+  fpsLimit: 120,
+  particles: {
+    number: {
+      value: 0
+    },
+    color: {
+      value: "#fff"
+    },
+    life: {
+      duration: {
+        value: 5,
+        sync: false
+      },
+      count: 1
+    },
+    opacity: {
+      value: {
+        min: 0.1,
+        max: 1
+      },
+      animation: {
+        enable: true,
+        speed: 3
+      }
+    },
+    size: {
+      value: {
+        min: 3,
+        max: 6
+      }
+    },
+    move: {
+      enable: true,
+      speed: 3,
+      random: false,
+      size: true
+    }
+  },
+  interactivity: {
+    events: {
+      onHover: {
+        enable: true,
+        mode: "trail"
+      },
+      resize: true
+    },
+    modes: {
+      trail: {
+        delay: 0.5,
+        pauseOnStop: true,
+        quantity: 4
+      }
+    }
+  },
+  background: {
+    color: "#000"
+  }
+};
+
+/***/ }),
+
+/***/ 165:
+/***/ (function(module) {
+
+module.exports = __WEBPACK_EXTERNAL_MODULE__165__;
+
+/***/ }),
+
+/***/ 919:
+/***/ (function(module) {
+
+module.exports = __WEBPACK_EXTERNAL_MODULE__919__;
+
+/***/ }),
+
+/***/ 941:
+/***/ (function(module) {
+
+module.exports = __WEBPACK_EXTERNAL_MODULE__941__;
+
+/***/ }),
+
+/***/ 841:
+/***/ (function(module) {
+
+module.exports = __WEBPACK_EXTERNAL_MODULE__841__;
+
+/***/ }),
+
+/***/ 489:
+/***/ (function(module) {
+
+module.exports = __WEBPACK_EXTERNAL_MODULE__489__;
+
+/***/ }),
+
+/***/ 838:
+/***/ (function(module) {
+
+module.exports = __WEBPACK_EXTERNAL_MODULE__838__;
+
+/***/ }),
+
+/***/ 328:
+/***/ (function(module) {
+
+module.exports = __WEBPACK_EXTERNAL_MODULE__328__;
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module is referenced by other modules so it can't be inlined
+/******/ 	var __webpack_exports__ = __webpack_require__(153);
+/******/ 	
+/******/ 	return __webpack_exports__;
+/******/ })()
+;
+});
