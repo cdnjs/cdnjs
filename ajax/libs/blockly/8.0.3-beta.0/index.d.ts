3f6880698520202a1a14969bec4c34d25f603202
robocdnjs <cdnjs-github@cloudflare.com>
2022-06-02 09:04:32 +0000

    Add blockly (8.0.3-beta.0)

---
+++ ajax/libs/blockly/8.0.3-beta.0/index.d.ts
@@ -0,0 +1,21 @@
+/**
+ * @license
+ * Copyright 2020 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @fileoverview Type definitions for Blockly.
+ * @author samelh@google.com (Sam El-Husseini)
+ */
+
+/// <reference path="core.d.ts" />
+/// <reference path="blocks.d.ts" />
+/// <reference path="javascript.d.ts" />
+/// <reference path="msg/msg.d.ts" />
+
+import { Generator } from 'core/blockly';
+export * from './core';
+export * as libraryBlocks from './blocks';
+export const JavaScript: Generator;
+import './msg/msg';
