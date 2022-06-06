db05823aaaf1e60a8481895a3857d7c6ea3dae2f
robocdnjs <cdnjs-github@cloudflare.com>
2022-06-06 17:07:11 +0000

    Add vkui (4.32.0)

---
+++ ajax/libs/vkui/4.32.0/cjs/tokenized/index.js
@@ -0,0 +1,334 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "Badge", {
+  enumerable: true,
+  get: function get() {
+    return _Badge.Badge;
+  }
+});
+Object.defineProperty(exports, "Button", {
+  enumerable: true,
+  get: function get() {
+    return _Button.Button;
+  }
+});
+Object.defineProperty(exports, "ButtonGroup", {
+  enumerable: true,
+  get: function get() {
+    return _ButtonGroup.ButtonGroup;
+  }
+});
+Object.defineProperty(exports, "Calendar", {
+  enumerable: true,
+  get: function get() {
+    return _Calendar.Calendar;
+  }
+});
+Object.defineProperty(exports, "CalendarRange", {
+  enumerable: true,
+  get: function get() {
+    return _CalendarRange.CalendarRange;
+  }
+});
+Object.defineProperty(exports, "Caption", {
+  enumerable: true,
+  get: function get() {
+    return _Caption.Caption;
+  }
+});
+Object.defineProperty(exports, "Card", {
+  enumerable: true,
+  get: function get() {
+    return _Card.Card;
+  }
+});
+Object.defineProperty(exports, "CardScroll", {
+  enumerable: true,
+  get: function get() {
+    return _CardScroll.CardScroll;
+  }
+});
+Object.defineProperty(exports, "Chip", {
+  enumerable: true,
+  get: function get() {
+    return _Chip.Chip;
+  }
+});
+Object.defineProperty(exports, "ChipsInput", {
+  enumerable: true,
+  get: function get() {
+    return _ChipsInput.ChipsInput;
+  }
+});
+Object.defineProperty(exports, "ChipsSelect", {
+  enumerable: true,
+  get: function get() {
+    return _ChipsSelect.ChipsSelect;
+  }
+});
+Object.defineProperty(exports, "Counter", {
+  enumerable: true,
+  get: function get() {
+    return _Counter.Counter;
+  }
+});
+Object.defineProperty(exports, "CustomSelect", {
+  enumerable: true,
+  get: function get() {
+    return _CustomSelect.CustomSelect;
+  }
+});
+Object.defineProperty(exports, "CustomSelectDropdown", {
+  enumerable: true,
+  get: function get() {
+    return _CustomSelectDropdown.CustomSelectDropdown;
+  }
+});
+Object.defineProperty(exports, "CustomSelectOption", {
+  enumerable: true,
+  get: function get() {
+    return _CustomSelectOption.CustomSelectOption;
+  }
+});
+Object.defineProperty(exports, "DateInput", {
+  enumerable: true,
+  get: function get() {
+    return _DateInput.DateInput;
+  }
+});
+Object.defineProperty(exports, "DateRangeInput", {
+  enumerable: true,
+  get: function get() {
+    return _DateRangeInput.DateRangeInput;
+  }
+});
+Object.defineProperty(exports, "Div", {
+  enumerable: true,
+  get: function get() {
+    return _Div.Div;
+  }
+});
+Object.defineProperty(exports, "File", {
+  enumerable: true,
+  get: function get() {
+    return _File.File;
+  }
+});
+Object.defineProperty(exports, "Footer", {
+  enumerable: true,
+  get: function get() {
+    return _Footer.Footer;
+  }
+});
+Object.defineProperty(exports, "Footnote", {
+  enumerable: true,
+  get: function get() {
+    return _Footnote.Footnote;
+  }
+});
+Object.defineProperty(exports, "FormField", {
+  enumerable: true,
+  get: function get() {
+    return _FormField.FormField;
+  }
+});
+Object.defineProperty(exports, "Headline", {
+  enumerable: true,
+  get: function get() {
+    return _Headline.Headline;
+  }
+});
+Object.defineProperty(exports, "HorizontalScroll", {
+  enumerable: true,
+  get: function get() {
+    return _HorizontalScroll.HorizontalScroll;
+  }
+});
+Object.defineProperty(exports, "InfoRow", {
+  enumerable: true,
+  get: function get() {
+    return _InfoRow.InfoRow;
+  }
+});
+Object.defineProperty(exports, "Input", {
+  enumerable: true,
+  get: function get() {
+    return _Input.Input;
+  }
+});
+Object.defineProperty(exports, "Pagination", {
+  enumerable: true,
+  get: function get() {
+    return _Pagination.Pagination;
+  }
+});
+Object.defineProperty(exports, "PanelSpinner", {
+  enumerable: true,
+  get: function get() {
+    return _PanelSpinner.PanelSpinner;
+  }
+});
+Object.defineProperty(exports, "Paragraph", {
+  enumerable: true,
+  get: function get() {
+    return _Paragraph.Paragraph;
+  }
+});
+Object.defineProperty(exports, "Progress", {
+  enumerable: true,
+  get: function get() {
+    return _Progress.Progress;
+  }
+});
+Object.defineProperty(exports, "PullToRefresh", {
+  enumerable: true,
+  get: function get() {
+    return _PullToRefresh.PullToRefresh;
+  }
+});
+Object.defineProperty(exports, "Radio", {
+  enumerable: true,
+  get: function get() {
+    return _Radio.Radio;
+  }
+});
+Object.defineProperty(exports, "RadioGroup", {
+  enumerable: true,
+  get: function get() {
+    return _RadioGroup.RadioGroup;
+  }
+});
+Object.defineProperty(exports, "SegmentedControl", {
+  enumerable: true,
+  get: function get() {
+    return _SegmentedControl.SegmentedControl;
+  }
+});
+Object.defineProperty(exports, "Select", {
+  enumerable: true,
+  get: function get() {
+    return _Select.Select;
+  }
+});
+Object.defineProperty(exports, "Spinner", {
+  enumerable: true,
+  get: function get() {
+    return _Spinner.Spinner;
+  }
+});
+Object.defineProperty(exports, "SplitCol", {
+  enumerable: true,
+  get: function get() {
+    return _SplitCol.SplitCol;
+  }
+});
+Object.defineProperty(exports, "Subhead", {
+  enumerable: true,
+  get: function get() {
+    return _Subhead.Subhead;
+  }
+});
+Object.defineProperty(exports, "Switch", {
+  enumerable: true,
+  get: function get() {
+    return _Switch.Switch;
+  }
+});
+Object.defineProperty(exports, "Text", {
+  enumerable: true,
+  get: function get() {
+    return _Text.Text;
+  }
+});
+Object.defineProperty(exports, "Title", {
+  enumerable: true,
+  get: function get() {
+    return _Title.Title;
+  }
+});
+
+var _Button = require("../components/Button/Button");
+
+var _Card = require("../components/Card/Card");
+
+var _CardScroll = require("../components/CardScroll/CardScroll");
+
+var _HorizontalScroll = require("../components/HorizontalScroll/HorizontalScroll");
+
+var _InfoRow = require("../components/InfoRow/InfoRow");
+
+var _ButtonGroup = require("../components/ButtonGroup/ButtonGroup");
+
+var _Switch = require("../components/Switch/Switch");
+
+var _Footnote = require("../components/Typography/Footnote/Footnote");
+
+var _Radio = require("../components/Radio/Radio");
+
+var _RadioGroup = require("../components/RadioGroup/RadioGroup");
+
+var _SegmentedControl = require("../components/SegmentedControl/SegmentedControl");
+
+var _Title = require("../components/Typography/Title/Title");
+
+var _Subhead = require("../components/Typography/Subhead/Subhead");
+
+var _Pagination = require("../components/Pagination/Pagination");
+
+var _Calendar = require("../components/Calendar/Calendar");
+
+var _CalendarRange = require("../components/CalendarRange/CalendarRange");
+
+var _Caption = require("../components/Typography/Caption/Caption");
+
+var _PanelSpinner = require("../components/PanelSpinner/PanelSpinner");
+
+var _Paragraph = require("../components/Typography/Paragraph/Paragraph");
+
+var _Text = require("../components/Typography/Text/Text");
+
+var _PullToRefresh = require("../components/PullToRefresh/PullToRefresh");
+
+var _Input = require("../components/Input/Input");
+
+var _File = require("../components/File/File");
+
+var _FormField = require("../components/FormField/FormField");
+
+var _Select = require("../components/Select/Select");
+
+var _Spinner = require("../components/Spinner/Spinner");
+
+var _CustomSelect = require("../components/CustomSelect/CustomSelect");
+
+var _CustomSelectDropdown = require("../components/CustomSelectDropdown/CustomSelectDropdown");
+
+var _CustomSelectOption = require("../components/CustomSelectOption/CustomSelectOption");
+
+var _Chip = require("../components/Chip/Chip");
+
+var _ChipsInput = require("../components/ChipsInput/ChipsInput");
+
+var _ChipsSelect = require("../components/ChipsSelect/ChipsSelect");
+
+var _SplitCol = require("../components/SplitCol/SplitCol");
+
+var _Headline = require("../components/Typography/Headline/Headline");
+
+var _Div = require("../components/Div/Div");
+
+var _Counter = require("../components/Counter/Counter");
+
+var _Progress = require("../components/Progress/Progress");
+
+var _DateInput = require("../components/DateInput/DateInput");
+
+var _Badge = require("../components/Badge/Badge");
+
+var _DateRangeInput = require("../components/DateRangeInput/DateRangeInput");
+
+var _Footer = require("../components/Footer/Footer");
+//# sourceMappingURL=index.js.map
