c378d62082d2ed84af642d01f390064bac29f9ab
robocdnjs <cdnjs-github@cloudflare.com>
2022-06-07 17:08:16 +0000

    Add openlayers (6.2.2-dev.1581667585039)

---
+++ ajax/libs/openlayers/6.2.2-dev.1581667585039/src/style/IconOrigin.js
@@ -0,0 +1,14 @@
+/**
+ * @module ol/style/IconOrigin
+ */
+
+/**
+ * Icon origin. One of 'bottom-left', 'bottom-right', 'top-left', 'top-right'.
+ * @enum {string}
+ */
+export default {
+  BOTTOM_LEFT: 'bottom-left',
+  BOTTOM_RIGHT: 'bottom-right',
+  TOP_LEFT: 'top-left',
+  TOP_RIGHT: 'top-right'
+};
