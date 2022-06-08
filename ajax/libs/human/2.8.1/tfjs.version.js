52dca52a50bfccbb5529f05dc885b05c4a580ad2
robocdnjs <cdnjs-github@cloudflare.com>
2022-06-08 19:04:46 +0000

    Add human (2.8.1)

---
+++ ajax/libs/human/2.8.1/tfjs.version.js
@@ -0,0 +1,7 @@
+/*
+  Human
+  homepage: <https://github.com/vladmandic/human>
+  author: <https://github.com/vladmandic>'
+*/
+
+var e="3.18.0";var s="3.18.0";var t="3.18.0";var r="3.18.0";var l="3.18.0";var i="3.18.0";var a="3.18.0";var V={tfjs:e,"tfjs-core":s,"tfjs-data":t,"tfjs-layers":r,"tfjs-converter":l,"tfjs-backend-webgl":i,"tfjs-backend-wasm":a};export{V as version};
