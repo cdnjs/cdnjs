c72288cbabb459cd168670fac92ca3ee23ff273f
robocdnjs <cdnjs-github@cloudflare.com>
2022-06-08 03:30:37 +0000

    Add typeit (8.5.5)

---
+++ ajax/libs/typeit/8.5.5/helpers/duplicate.d.ts
@@ -0,0 +1,2 @@
+declare const _default: <T>(value: T, times: number) => T[];
+export default _default;
---
+++ ajax/libs/typeit/8.5.5/helpers/expandTextNodes.d.ts
@@ -0,0 +1,3 @@
+import { Element } from "../types";
+declare let expandTextNodes: (element: Element) => Element;
+export default expandTextNodes;
