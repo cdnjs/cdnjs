e727590a0fe4a825f46197e56c9ad0d2534f0a67
robocdnjs <cdnjs-github@cloudflare.com>
2022-06-08 23:04:53 +0000

    Add blockly (8.0.3)

---
+++ ajax/libs/blockly/8.0.3/blocks.d.ts
@@ -0,0 +1,24 @@
+/**
+ * @license
+ * Copyright 2020 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @fileoverview Type definitions for Blockly Blocks.
+ * @author samelh@google.com (Sam El-Husseini)
+ */
+
+/// <reference path="core.d.ts" />
+
+export const colour: any;
+export const lists: any;
+export const logic: any;
+export const loops: any;
+export const math: any;
+export const procedures: any;
+export const texts: any;
+export const variables: any;
+export const variablesDynamic: any;
+
+export const blocks: any;
---
+++ ajax/libs/blockly/8.0.3/index.d.ts
@@ -0,0 +1,20 @@
+/**
+ * @license
+ * Copyright 2020 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @fileoverview Type definitions for Blockly.
+ * @author samelh@google.com (Sam El-Husseini)
+ */
+
+/// <reference path="core.d.ts" />
+/// <reference path="blocks.d.ts" />
+/// <reference path="javascript.d.ts" />
+/// <reference path="msg/msg.d.ts" />
+
+export * from './core';
+export * as libraryBlocks from './blocks';
+export const JavaScript: any;
+import './msg/msg';
