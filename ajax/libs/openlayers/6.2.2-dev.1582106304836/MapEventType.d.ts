f52b4708fa77ac68e91ca23d58adaadc6d75d56f
robocdnjs <cdnjs-github@cloudflare.com>
2022-06-09 14:04:58 +0000

    Add openlayers (6.2.2-dev.1582106304836)

---
+++ ajax/libs/openlayers/6.2.2-dev.1582106304836/MapEventType.d.ts
@@ -0,0 +1,7 @@
+declare namespace _default {
+    export const POSTRENDER: string;
+    export const MOVESTART: string;
+    export const MOVEEND: string;
+}
+export default _default;
+//# sourceMappingURL=MapEventType.d.ts.map
---
+++ ajax/libs/openlayers/6.2.2-dev.1582106304836/reproj/common.d.ts
@@ -0,0 +1,16 @@
+/**
+ * @module ol/reproj/common
+ */
+/**
+ * Default maximum allowed threshold  (in pixels) for reprojection
+ * triangulation.
+ * @type {number}
+ */
+export const ERROR_THRESHOLD: number;
+/**
+ * Enable automatic reprojection of raster sources. Default is `true`.
+ * TODO: decide if we want to expose this as a build flag or remove it
+ * @type {boolean}
+ */
+export const ENABLE_RASTER_REPROJECTION: boolean;
+//# sourceMappingURL=common.d.ts.map
