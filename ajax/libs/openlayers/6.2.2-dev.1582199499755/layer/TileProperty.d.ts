7af2b72f9e3ce5d67f6e4d49fb0f031ea9aecab7
robocdnjs <cdnjs-github@cloudflare.com>
2022-06-10 17:09:52 +0000

    Add openlayers (6.2.2-dev.1582199499755)

---
+++ ajax/libs/openlayers/6.2.2-dev.1582199499755/layer/TileProperty.d.ts
@@ -0,0 +1,6 @@
+declare namespace _default {
+    export const PRELOAD: string;
+    export const USE_INTERIM_TILES_ON_ERROR: string;
+}
+export default _default;
+//# sourceMappingURL=TileProperty.d.ts.map
