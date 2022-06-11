e0cc678208834520ae518ac5eccdbfc075605dd3
robocdnjs <cdnjs-github@cloudflare.com>
2022-06-11 11:04:27 +0000

    Add openlayers (6.2.2-dev.1582782354426)

---
+++ ajax/libs/openlayers/6.2.2-dev.1582782354426/format/OWS.d.ts
@@ -0,0 +1,13 @@
+export default OWS;
+declare class OWS extends XML {
+    /**
+     * @inheritDoc
+     */
+    readFromDocument(doc: any): {};
+    /**
+     * @inheritDoc
+     */
+    readFromNode(node: any): {};
+}
+import XML from "./XML.js";
+//# sourceMappingURL=OWS.d.ts.map
