8c174858b59d496674d76a4b7125fdbc9e1e7a0e
robocdnjs <cdnjs-github@cloudflare.com>
2022-06-13 09:04:48 +0000

    Add typeit (8.6.0)

---
+++ ajax/libs/typeit/8.6.0/helpers/destroyTimeouts.d.ts
@@ -0,0 +1,2 @@
+declare const _default: (timeouts: number[]) => [];
+export default _default;
---
+++ ajax/libs/typeit/8.6.0/helpers/getParsedBody.d.ts
@@ -0,0 +1,7 @@
+import { Element } from "../types";
+/**
+ * Parse a string as HTML and return the body
+ * of the parsed document, with all text nodes expanded.
+ */
+declare const _default: (content: any) => Element;
+export default _default;
