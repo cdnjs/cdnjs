fc1053b8a8264a438111bed0dc015c18315995a4
robocdnjs <cdnjs-github@cloudflare.com>
2022-06-15 01:34:37 +0000

    Add openlayers (6.2.2-dev.1583864298940)

---
+++ ajax/libs/openlayers/6.2.2-dev.1583864298940/loadingstrategy.d.ts
@@ -0,0 +1,28 @@
+/**
+ * @module ol/loadingstrategy
+ */
+/**
+ * Strategy function for loading all features with a single request.
+ * @param {import("./extent.js").Extent} extent Extent.
+ * @param {number} resolution Resolution.
+ * @return {Array<import("./extent.js").Extent>} Extents.
+ * @api
+ */
+export function all(extent: number[], resolution: number): number[][];
+/**
+ * Strategy function for loading features based on the view's extent and
+ * resolution.
+ * @param {import("./extent.js").Extent} extent Extent.
+ * @param {number} resolution Resolution.
+ * @return {Array<import("./extent.js").Extent>} Extents.
+ * @api
+ */
+export function bbox(extent: number[], resolution: number): number[][];
+/**
+ * Creates a strategy function for loading features based on a tile grid.
+ * @param {import("./tilegrid/TileGrid.js").default} tileGrid Tile grid.
+ * @return {function(import("./extent.js").Extent, number): Array<import("./extent.js").Extent>} Loading strategy.
+ * @api
+ */
+export function tile(tileGrid: import("./tilegrid/TileGrid.js").default): (arg0: number[], arg1: number) => number[][];
+//# sourceMappingURL=loadingstrategy.d.ts.map
