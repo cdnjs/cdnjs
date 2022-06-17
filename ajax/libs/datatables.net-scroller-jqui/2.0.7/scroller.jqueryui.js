5fbc5ffbb6f51eb96ff8985363a104726a6c805b
robocdnjs <cdnjs-github@cloudflare.com>
2022-06-17 12:07:20 +0000

    Add datatables.net-scroller-jqui (2.0.7)

---
+++ ajax/libs/datatables.net-scroller-jqui/2.0.7/scroller.jqueryui.js
@@ -0,0 +1,38 @@
+/*! jQuery UI styling wrapper for Scroller
+ * ©2018 SpryMedia Ltd - datatables.net/license
+ */
+
+(function( factory ){
+	if ( typeof define === 'function' && define.amd ) {
+		// AMD
+		define( ['jquery', 'datatables.net-jqui', 'datatables.net-scroller'], function ( $ ) {
+			return factory( $, window, document );
+		} );
+	}
+	else if ( typeof exports === 'object' ) {
+		// CommonJS
+		module.exports = function (root, $) {
+			if ( ! root ) {
+				root = window;
+			}
+
+			if ( ! $ || ! $.fn.dataTable ) {
+				$ = require('datatables.net-jqui')(root, $).$;
+			}
+
+			if ( ! $.fn.dataTable.Scroller ) {
+				require('datatables.net-scroller')(root, $);
+			}
+
+			return factory( $, root, root.document );
+		};
+	}
+	else {
+		// Browser
+		factory( jQuery, window, document );
+	}
+}(function( $, window, document, undefined ) {
+
+return $.fn.dataTable;
+
+}));
