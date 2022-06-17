32ec29f5f56c27c16bce0bfc9da27f095e7cbb7d
robocdnjs <cdnjs-github@cloudflare.com>
2022-06-17 23:05:32 +0000

    Add blockly (8.0.4-beta.1)

---
+++ ajax/libs/blockly/8.0.4-beta.1/core.d.ts
@@ -0,0 +1,14 @@
+/**
+ * @license
+ * Copyright 2020 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @fileoverview Type definitions for Blockly core.
+ * @author samelh@google.com (Sam El-Husseini)
+ */
+
+/// <reference path="blockly.d.ts" />
+
+export * from 'core/blockly';
