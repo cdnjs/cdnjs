c6bff5adb298d1ecf0295ae6a9c26e31d108cfec
robocdnjs <cdnjs-github@cloudflare.com>
2022-06-17 22:04:24 +0000

    Add openlayers (6.2.2-dev.1585307287194)

---
+++ ajax/libs/openlayers/6.2.2-dev.1585307287194/format/FormatType.d.ts
@@ -0,0 +1,8 @@
+declare namespace _default {
+    export const ARRAY_BUFFER: string;
+    export const JSON: string;
+    export const TEXT: string;
+    export const XML: string;
+}
+export default _default;
+//# sourceMappingURL=FormatType.d.ts.map
