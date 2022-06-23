68e1d480a58b194aa3c5f0d232c40eef7921ee84
robocdnjs <cdnjs-github@cloudflare.com>
2022-06-23 19:05:13 +0000

    Add monaco-editor (0.34.0-dev.20220623)

---
+++ ajax/libs/monaco-editor/0.34.0-dev.20220623/min/vs/base/common/worker/simpleWorker.nls.zh-tw.js
@@ -0,0 +1,8 @@
+/*!-----------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Version: 0.34.0-dev.20220623(4b48304259402b5dd5761389b6f17600f51f2d18)
+ * Released under the MIT license
+ * https://github.com/microsoft/vscode/blob/main/LICENSE.txt
+ *-----------------------------------------------------------*/define("vs/base/common/worker/simpleWorker.nls.zh-tw",{"vs/base/common/platform":["this ensures the nls loader plugin is loaded before resolving the true locale"]});
+
+//# sourceMappingURL=../../../../../min-maps/vs/base/common/worker/simpleWorker.nls.zh-tw.js.map
