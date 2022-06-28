087c8c3d3cc8f03c70c45c6c7da15b912b0a6bed
robocdnjs <cdnjs-github@cloudflare.com>
2022-06-28 19:06:33 +0000

    Add ace (1.7.0)

---
+++ ajax/libs/ace/1.7.0/snippets/fsl.min.js
@@ -0,0 +1,1 @@
+define("ace/snippets/fsl",["require","exports","module"],function(e,o,t){"use strict";o.snippetText=void 0,o.scope=""}),window.require(["ace/snippets/fsl"],function(e){"object"==typeof module&&"object"==typeof exports&&module&&(module.exports=e)});
