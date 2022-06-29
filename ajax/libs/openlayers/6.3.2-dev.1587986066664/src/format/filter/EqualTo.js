facb4e40867857fa70ae20e4837504fb1318f38b
robocdnjs <cdnjs-github@cloudflare.com>
2022-06-29 15:23:25 +0000

    Add openlayers (6.3.2-dev.1587986066664)

---
+++ ajax/libs/openlayers/6.3.2-dev.1587986066664/src/format/filter/EqualTo.js
@@ -0,0 +1,22 @@
+/**
+ * @module ol/format/filter/EqualTo
+ */
+import ComparisonBinary from './ComparisonBinary.js';
+
+/**
+ * @classdesc
+ * Represents a `<PropertyIsEqualTo>` comparison operator.
+ * @api
+ */
+class EqualTo extends ComparisonBinary {
+  /**
+   * @param {!string} propertyName Name of the context property to compare.
+   * @param {!(string|number)} expression The value to compare.
+   * @param {boolean=} opt_matchCase Case-sensitive?
+   */
+  constructor(propertyName, expression, opt_matchCase) {
+    super('PropertyIsEqualTo', propertyName, expression, opt_matchCase);
+  }
+}
+
+export default EqualTo;
