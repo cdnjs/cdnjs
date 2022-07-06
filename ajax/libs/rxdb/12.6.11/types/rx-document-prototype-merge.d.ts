3bfb0475e1066164342ba4a12b0e34623a66812a
robocdnjs <cdnjs-github@cloudflare.com>
2022-07-06 00:13:19 +0000

    Add rxdb (12.6.11)

---
+++ ajax/libs/rxdb/12.6.11/types/rx-document-prototype-merge.d.ts
@@ -0,0 +1,28 @@
+/**
+ * For the ORM capabilities,
+ * we have to merge the document prototype
+ * with the ORM functions and the data
+ * We do this itterating over the properties and
+ * adding them to a new object.
+ * In the future we should do this by chaining the __proto__ objects
+ */
+import type { RxCollection, RxDocument, RxDocumentData } from './types';
+export declare function getDocumentPrototype(rxCollection: RxCollection): any;
+export declare function getRxDocumentConstructor(rxCollection: RxCollection): any;
+/**
+ * Create a RxDocument-instance from the jsonData
+ * and the prototype merge.
+ * If the document already exists in the _docCache,
+ * return that instead to ensure we have no duplicates.
+ */
+export declare function createRxDocument<RxDocType, ORM>(rxCollection: RxCollection<RxDocType, ORM>, docData: RxDocumentData<RxDocType>): RxDocument<RxDocType, ORM>;
+/**
+ * create RxDocument from the docs-array
+ */
+export declare function createRxDocuments<DT, OM>(rxCollection: RxCollection, docsJSON: any[]): RxDocument<DT, OM>[];
+/**
+ * returns the prototype-object
+ * that contains the orm-methods,
+ * used in the proto-merge
+ */
+export declare function getDocumentOrmPrototype(rxCollection: RxCollection): any;
