de0fde3d31dc73977e59cba24dcbbd8d9d672474
robocdnjs <cdnjs-github@cloudflare.com>
2022-07-07 07:05:44 +0000

    Add rxdb (12.6.13)

---
+++ ajax/libs/rxdb/12.6.13/types/rx-change-event.d.ts
@@ -0,0 +1,13 @@
+/**
+ * RxChangeEvents a emitted when something in the database changes
+ * they can be grabbed by the observables of database, collection and document
+ */
+import { ChangeEvent as EventReduceChangeEvent } from 'event-reduce-js';
+import type { EventBulk, RxChangeEvent, RxDocumentData } from './types';
+export declare function getDocumentDataOfRxChangeEvent<T>(rxChangeEvent: RxChangeEvent<T>): RxDocumentData<T>;
+export declare function rxChangeEventToEventReduceChangeEvent<DocType>(rxChangeEvent: RxChangeEvent<DocType>): EventReduceChangeEvent<DocType>;
+/**
+ * Flattens the given events into a single array of events.
+ * Used mostly in tests.
+ */
+export declare function flattenEvents<EventType>(input: EventBulk<EventType> | EventBulk<EventType>[] | EventType | EventType[]): EventType[];
