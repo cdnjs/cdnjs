17133e4de6390b874376695d6fbb8c58107562a8
robocdnjs <cdnjs-github@cloudflare.com>
2022-07-08 14:07:49 +0000

    Add openlayers (6.3.2-dev.1589843798021)

---
+++ ajax/libs/openlayers/6.3.2-dev.1589843798021/src/render/EventType.js
@@ -0,0 +1,47 @@
+/**
+ * @module ol/render/EventType
+ */
+
+/**
+ * @enum {string}
+ */
+export default {
+  /**
+   * Triggered before a layer is rendered.
+   * @event module:ol/render/Event~RenderEvent#prerender
+   * @api
+   */
+  PRERENDER: 'prerender',
+
+  /**
+   * Triggered after a layer is rendered.
+   * @event module:ol/render/Event~RenderEvent#postrender
+   * @api
+   */
+  POSTRENDER: 'postrender',
+
+  /**
+   * Triggered before layers are rendered.
+   * The event object will not have a `context` set.
+   * @event module:ol/render/Event~RenderEvent#precompose
+   * @api
+   */
+  PRECOMPOSE: 'precompose',
+
+  /**
+   * Triggered after all layers are rendered.
+   * The event object will not have a `context` set.
+   * @event module:ol/render/Event~RenderEvent#postcompose
+   * @api
+   */
+  POSTCOMPOSE: 'postcompose',
+
+  /**
+   * Triggered when rendering is complete, i.e. all sources and tiles have
+   * finished loading for the current viewport, and all tiles are faded in.
+   * The event object will not have a `context` set.
+   * @event module:ol/render/Event~RenderEvent#rendercomplete
+   * @api
+   */
+  RENDERCOMPLETE: 'rendercomplete',
+};
