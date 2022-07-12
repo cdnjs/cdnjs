7c8254c893c273a75fecbe2c50940941e696d31d
robocdnjs <cdnjs-github@cloudflare.com>
2022-07-12 06:04:32 +0000

    Add typeit (8.6.4)

---
+++ ajax/libs/typeit/8.6.4/helpers/getComputedStyle.d.ts
@@ -0,0 +1,2 @@
+declare const _default: (el: any) => CSSStyleDeclaration;
+export default _default;
---
+++ ajax/libs/typeit/8.6.4/helpers/setCursorStyles.d.ts
@@ -0,0 +1,11 @@
+import { El } from "../types";
+export declare let cursorFontStyles: {
+    readonly "font-family": "";
+    readonly "font-weight": "";
+    readonly "font-size": "";
+    readonly "font-style": "";
+    readonly "line-height": "";
+    readonly color: "";
+    readonly transform: "translateX(-.125em)";
+};
+export declare let setCursorStyles: (id: string, element: El) => void;
