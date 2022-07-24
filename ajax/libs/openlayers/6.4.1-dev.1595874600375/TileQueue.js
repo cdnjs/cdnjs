535321467c121dcc096d916afa3736d38093a665
robocdnjs <cdnjs-github@cloudflare.com>
2022-07-24 17:07:16 +0000

    Add openlayers (6.4.1-dev.1595874600375)

---
+++ ajax/libs/openlayers/6.4.1-dev.1595874600375/TileQueue.js
@@ -0,0 +1,154 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+/**
+ * @module ol/TileQueue
+ */
+import EventType from './events/EventType.js';
+import PriorityQueue, { DROP } from './structs/PriorityQueue.js';
+import TileState from './TileState.js';
+/**
+ * @typedef {function(import("./Tile.js").default, string, import("./coordinate.js").Coordinate, number): number} PriorityFunction
+ */
+var TileQueue = /** @class */ (function (_super) {
+    __extends(TileQueue, _super);
+    /**
+     * @param {PriorityFunction} tilePriorityFunction Tile priority function.
+     * @param {function(): ?} tileChangeCallback Function called on each tile change event.
+     */
+    function TileQueue(tilePriorityFunction, tileChangeCallback) {
+        var _this = _super.call(this, 
+        /**
+         * @param {Array} element Element.
+         * @return {number} Priority.
+         */
+        function (element) {
+            return tilePriorityFunction.apply(null, element);
+        }, 
+        /**
+         * @param {Array} element Element.
+         * @return {string} Key.
+         */
+        function (element) {
+            return /** @type {import("./Tile.js").default} */ (element[0]).getKey();
+        }) || this;
+        /** @private */
+        _this.boundHandleTileChange_ = _this.handleTileChange.bind(_this);
+        /**
+         * @private
+         * @type {function(): ?}
+         */
+        _this.tileChangeCallback_ = tileChangeCallback;
+        /**
+         * @private
+         * @type {number}
+         */
+        _this.tilesLoading_ = 0;
+        /**
+         * @private
+         * @type {!Object<string,boolean>}
+         */
+        _this.tilesLoadingKeys_ = {};
+        return _this;
+    }
+    /**
+     * @param {Array} element Element.
+     * @return {boolean} The element was added to the queue.
+     */
+    TileQueue.prototype.enqueue = function (element) {
+        var added = _super.prototype.enqueue.call(this, element);
+        if (added) {
+            var tile = element[0];
+            tile.addEventListener(EventType.CHANGE, this.boundHandleTileChange_);
+        }
+        return added;
+    };
+    /**
+     * @return {number} Number of tiles loading.
+     */
+    TileQueue.prototype.getTilesLoading = function () {
+        return this.tilesLoading_;
+    };
+    /**
+     * @param {import("./events/Event.js").default} event Event.
+     * @protected
+     */
+    TileQueue.prototype.handleTileChange = function (event) {
+        var tile = /** @type {import("./Tile.js").default} */ (event.target);
+        var state = tile.getState();
+        if ((tile.hifi && state === TileState.LOADED) ||
+            state === TileState.ERROR ||
+            state === TileState.EMPTY) {
+            tile.removeEventListener(EventType.CHANGE, this.boundHandleTileChange_);
+            var tileKey = tile.getKey();
+            if (tileKey in this.tilesLoadingKeys_) {
+                delete this.tilesLoadingKeys_[tileKey];
+                --this.tilesLoading_;
+            }
+            this.tileChangeCallback_();
+        }
+    };
+    /**
+     * @param {number} maxTotalLoading Maximum number tiles to load simultaneously.
+     * @param {number} maxNewLoads Maximum number of new tiles to load.
+     */
+    TileQueue.prototype.loadMoreTiles = function (maxTotalLoading, maxNewLoads) {
+        var newLoads = 0;
+        var state, tile, tileKey;
+        while (this.tilesLoading_ < maxTotalLoading &&
+            newLoads < maxNewLoads &&
+            this.getCount() > 0) {
+            tile = /** @type {import("./Tile.js").default} */ (this.dequeue()[0]);
+            tileKey = tile.getKey();
+            state = tile.getState();
+            if (state === TileState.IDLE && !(tileKey in this.tilesLoadingKeys_)) {
+                this.tilesLoadingKeys_[tileKey] = true;
+                ++this.tilesLoading_;
+                ++newLoads;
+                tile.load();
+            }
+        }
+    };
+    return TileQueue;
+}(PriorityQueue));
+export default TileQueue;
+/**
+ * @param {import('./PluggableMap.js').FrameState} frameState Frame state.
+ * @param {import("./Tile.js").default} tile Tile.
+ * @param {string} tileSourceKey Tile source key.
+ * @param {import("./coordinate.js").Coordinate} tileCenter Tile center.
+ * @param {number} tileResolution Tile resolution.
+ * @return {number} Tile priority.
+ */
+export function getTilePriority(frameState, tile, tileSourceKey, tileCenter, tileResolution) {
+    // Filter out tiles at higher zoom levels than the current zoom level, or that
+    // are outside the visible extent.
+    if (!frameState || !(tileSourceKey in frameState.wantedTiles)) {
+        return DROP;
+    }
+    if (!frameState.wantedTiles[tileSourceKey][tile.getKey()]) {
+        return DROP;
+    }
+    // Prioritize the highest zoom level tiles closest to the focus.
+    // Tiles at higher zoom levels are prioritized using Math.log(tileResolution).
+    // Within a zoom level, tiles are prioritized by the distance in pixels between
+    // the center of the tile and the center of the viewport.  The factor of 65536
+    // means that the prioritization should behave as desired for tiles up to
+    // 65536 * Math.log(2) = 45426 pixels from the focus.
+    var center = frameState.viewState.center;
+    var deltaX = tileCenter[0] - center[0];
+    var deltaY = tileCenter[1] - center[1];
+    return (65536 * Math.log(tileResolution) +
+        Math.sqrt(deltaX * deltaX + deltaY * deltaY) / tileResolution);
+}
+//# sourceMappingURL=TileQueue.js.map
---
+++ ajax/libs/openlayers/6.4.1-dev.1595874600375/control/ZoomSlider.js
@@ -0,0 +1,334 @@
+/**
+ * @module ol/control/ZoomSlider
+ */
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+import Control from './Control.js';
+import EventType from '../events/EventType.js';
+import PointerEventType from '../pointer/EventType.js';
+import { CLASS_CONTROL, CLASS_UNSELECTABLE } from '../css.js';
+import { clamp } from '../math.js';
+import { easeOut } from '../easing.js';
+import { listen, unlistenByKey } from '../events.js';
+import { stopPropagation } from '../events/Event.js';
+/**
+ * The enum for available directions.
+ *
+ * @enum {number}
+ */
+var Direction = {
+    VERTICAL: 0,
+    HORIZONTAL: 1,
+};
+/**
+ * @typedef {Object} Options
+ * @property {string} [className='ol-zoomslider'] CSS class name.
+ * @property {number} [duration=200] Animation duration in milliseconds.
+ * @property {function(import("../MapEvent.js").default):void} [render] Function called when the control
+ * should be re-rendered. This is called in a `requestAnimationFrame` callback.
+ */
+/**
+ * @classdesc
+ * A slider type of control for zooming.
+ *
+ * Example:
+ *
+ *     map.addControl(new ZoomSlider());
+ *
+ * @api
+ */
+var ZoomSlider = /** @class */ (function (_super) {
+    __extends(ZoomSlider, _super);
+    /**
+     * @param {Options=} opt_options Zoom slider options.
+     */
+    function ZoomSlider(opt_options) {
+        var _this = this;
+        var options = opt_options ? opt_options : {};
+        _this = _super.call(this, {
+            element: document.createElement('div'),
+            render: options.render,
+        }) || this;
+        /**
+         * @type {!Array.<import("../events.js").EventsKey>}
+         * @private
+         */
+        _this.dragListenerKeys_ = [];
+        /**
+         * Will hold the current resolution of the view.
+         *
+         * @type {number|undefined}
+         * @private
+         */
+        _this.currentResolution_ = undefined;
+        /**
+         * The direction of the slider. Will be determined from actual display of the
+         * container and defaults to Direction.VERTICAL.
+         *
+         * @type {Direction}
+         * @private
+         */
+        _this.direction_ = Direction.VERTICAL;
+        /**
+         * @type {boolean}
+         * @private
+         */
+        _this.dragging_;
+        /**
+         * @type {number}
+         * @private
+         */
+        _this.heightLimit_ = 0;
+        /**
+         * @type {number}
+         * @private
+         */
+        _this.widthLimit_ = 0;
+        /**
+         * @type {number|undefined}
+         * @private
+         */
+        _this.startX_;
+        /**
+         * @type {number|undefined}
+         * @private
+         */
+        _this.startY_;
+        /**
+         * The calculated thumb size (border box plus margins).  Set when initSlider_
+         * is called.
+         * @type {import("../size.js").Size}
+         * @private
+         */
+        _this.thumbSize_ = null;
+        /**
+         * Whether the slider is initialized.
+         * @type {boolean}
+         * @private
+         */
+        _this.sliderInitialized_ = false;
+        /**
+         * @type {number}
+         * @private
+         */
+        _this.duration_ = options.duration !== undefined ? options.duration : 200;
+        var className = options.className !== undefined ? options.className : 'ol-zoomslider';
+        var thumbElement = document.createElement('button');
+        thumbElement.setAttribute('type', 'button');
+        thumbElement.className = className + '-thumb ' + CLASS_UNSELECTABLE;
+        var containerElement = _this.element;
+        containerElement.className =
+            className + ' ' + CLASS_UNSELECTABLE + ' ' + CLASS_CONTROL;
+        containerElement.appendChild(thumbElement);
+        containerElement.addEventListener(PointerEventType.POINTERDOWN, _this.handleDraggerStart_.bind(_this), false);
+        containerElement.addEventListener(PointerEventType.POINTERMOVE, _this.handleDraggerDrag_.bind(_this), false);
+        containerElement.addEventListener(PointerEventType.POINTERUP, _this.handleDraggerEnd_.bind(_this), false);
+        containerElement.addEventListener(EventType.CLICK, _this.handleContainerClick_.bind(_this), false);
+        thumbElement.addEventListener(EventType.CLICK, stopPropagation, false);
+        return _this;
+    }
+    /**
+     * Remove the control from its current map and attach it to the new map.
+     * Subclasses may set up event handlers to get notified about changes to
+     * the map here.
+     * @param {import("../PluggableMap.js").default} map Map.
+     * @api
+     */
+    ZoomSlider.prototype.setMap = function (map) {
+        _super.prototype.setMap.call(this, map);
+        if (map) {
+            map.render();
+        }
+    };
+    /**
+     * Initializes the slider element. This will determine and set this controls
+     * direction_ and also constrain the dragging of the thumb to always be within
+     * the bounds of the container.
+     *
+     * @return {boolean} Initialization successful
+     * @private
+     */
+    ZoomSlider.prototype.initSlider_ = function () {
+        var container = this.element;
+        var containerWidth = container.offsetWidth;
+        var containerHeight = container.offsetHeight;
+        if (containerWidth === 0 && containerHeight === 0) {
+            return (this.sliderInitialized_ = false);
+        }
+        var thumb = /** @type {HTMLElement} */ (container.firstElementChild);
+        var computedStyle = getComputedStyle(thumb);
+        var thumbWidth = thumb.offsetWidth +
+            parseFloat(computedStyle['marginRight']) +
+            parseFloat(computedStyle['marginLeft']);
+        var thumbHeight = thumb.offsetHeight +
+            parseFloat(computedStyle['marginTop']) +
+            parseFloat(computedStyle['marginBottom']);
+        this.thumbSize_ = [thumbWidth, thumbHeight];
+        if (containerWidth > containerHeight) {
+            this.direction_ = Direction.HORIZONTAL;
+            this.widthLimit_ = containerWidth - thumbWidth;
+        }
+        else {
+            this.direction_ = Direction.VERTICAL;
+            this.heightLimit_ = containerHeight - thumbHeight;
+        }
+        return (this.sliderInitialized_ = true);
+    };
+    /**
+     * @param {PointerEvent} event The browser event to handle.
+     * @private
+     */
+    ZoomSlider.prototype.handleContainerClick_ = function (event) {
+        var view = this.getMap().getView();
+        var relativePosition = this.getRelativePosition_(event.offsetX - this.thumbSize_[0] / 2, event.offsetY - this.thumbSize_[1] / 2);
+        var resolution = this.getResolutionForPosition_(relativePosition);
+        var zoom = view.getConstrainedZoom(view.getZoomForResolution(resolution));
+        view.animateInternal({
+            zoom: zoom,
+            duration: this.duration_,
+            easing: easeOut,
+        });
+    };
+    /**
+     * Handle dragger start events.
+     * @param {PointerEvent} event The drag event.
+     * @private
+     */
+    ZoomSlider.prototype.handleDraggerStart_ = function (event) {
+        if (!this.dragging_ && event.target === this.element.firstElementChild) {
+            var element = /** @type {HTMLElement} */ (this.element
+                .firstElementChild);
+            this.getMap().getView().beginInteraction();
+            this.startX_ = event.clientX - parseFloat(element.style.left);
+            this.startY_ = event.clientY - parseFloat(element.style.top);
+            this.dragging_ = true;
+            if (this.dragListenerKeys_.length === 0) {
+                var drag = this.handleDraggerDrag_;
+                var end = this.handleDraggerEnd_;
+                this.dragListenerKeys_.push(listen(document, PointerEventType.POINTERMOVE, drag, this), listen(document, PointerEventType.POINTERUP, end, this));
+            }
+        }
+    };
+    /**
+     * Handle dragger drag events.
+     *
+     * @param {PointerEvent} event The drag event.
+     * @private
+     */
+    ZoomSlider.prototype.handleDraggerDrag_ = function (event) {
+        if (this.dragging_) {
+            var deltaX = event.clientX - this.startX_;
+            var deltaY = event.clientY - this.startY_;
+            var relativePosition = this.getRelativePosition_(deltaX, deltaY);
+            this.currentResolution_ = this.getResolutionForPosition_(relativePosition);
+            this.getMap().getView().setResolution(this.currentResolution_);
+        }
+    };
+    /**
+     * Handle dragger end events.
+     * @param {PointerEvent} event The drag event.
+     * @private
+     */
+    ZoomSlider.prototype.handleDraggerEnd_ = function (event) {
+        if (this.dragging_) {
+            var view = this.getMap().getView();
+            view.endInteraction();
+            this.dragging_ = false;
+            this.startX_ = undefined;
+            this.startY_ = undefined;
+            this.dragListenerKeys_.forEach(unlistenByKey);
+            this.dragListenerKeys_.length = 0;
+        }
+    };
+    /**
+     * Positions the thumb inside its container according to the given resolution.
+     *
+     * @param {number} res The res.
+     * @private
+     */
+    ZoomSlider.prototype.setThumbPosition_ = function (res) {
+        var position = this.getPositionForResolution_(res);
+        var thumb = /** @type {HTMLElement} */ (this.element.firstElementChild);
+        if (this.direction_ == Direction.HORIZONTAL) {
+            thumb.style.left = this.widthLimit_ * position + 'px';
+        }
+        else {
+            thumb.style.top = this.heightLimit_ * position + 'px';
+        }
+    };
+    /**
+     * Calculates the relative position of the thumb given x and y offsets.  The
+     * relative position scales from 0 to 1.  The x and y offsets are assumed to be
+     * in pixel units within the dragger limits.
+     *
+     * @param {number} x Pixel position relative to the left of the slider.
+     * @param {number} y Pixel position relative to the top of the slider.
+     * @return {number} The relative position of the thumb.
+     * @private
+     */
+    ZoomSlider.prototype.getRelativePosition_ = function (x, y) {
+        var amount;
+        if (this.direction_ === Direction.HORIZONTAL) {
+            amount = x / this.widthLimit_;
+        }
+        else {
+            amount = y / this.heightLimit_;
+        }
+        return clamp(amount, 0, 1);
+    };
+    /**
+     * Calculates the corresponding resolution of the thumb given its relative
+     * position (where 0 is the minimum and 1 is the maximum).
+     *
+     * @param {number} position The relative position of the thumb.
+     * @return {number} The corresponding resolution.
+     * @private
+     */
+    ZoomSlider.prototype.getResolutionForPosition_ = function (position) {
+        var fn = this.getMap().getView().getResolutionForValueFunction();
+        return fn(1 - position);
+    };
+    /**
+     * Determines the relative position of the slider for the given resolution.  A
+     * relative position of 0 corresponds to the minimum view resolution.  A
+     * relative position of 1 corresponds to the maximum view resolution.
+     *
+     * @param {number} res The resolution.
+     * @return {number} The relative position value (between 0 and 1).
+     * @private
+     */
+    ZoomSlider.prototype.getPositionForResolution_ = function (res) {
+        var fn = this.getMap().getView().getValueForResolutionFunction();
+        return clamp(1 - fn(res), 0, 1);
+    };
+    /**
+     * Update the zoomslider element.
+     * @param {import("../MapEvent.js").default} mapEvent Map event.
+     * @override
+     */
+    ZoomSlider.prototype.render = function (mapEvent) {
+        if (!mapEvent.frameState) {
+            return;
+        }
+        if (!this.sliderInitialized_ && !this.initSlider_()) {
+            return;
+        }
+        var res = mapEvent.frameState.viewState.resolution;
+        this.currentResolution_ = res;
+        this.setThumbPosition_(res);
+    };
+    return ZoomSlider;
+}(Control));
+export default ZoomSlider;
+//# sourceMappingURL=ZoomSlider.js.map
