2fb5587ddb66b3be9fd7d5313f5a0d52caef5d84
robocdnjs <cdnjs-github@cloudflare.com>
2022-07-25 20:09:43 +0000

    Add openlayers (6.4.1-dev.1596029716612)

---
+++ ajax/libs/openlayers/6.4.1-dev.1596029716612/src/format/filter/Filter.js
@@ -0,0 +1,33 @@
+/**
+ * @module ol/format/filter/Filter
+ */
+
+/**
+ * @classdesc
+ * Abstract class; normally only used for creating subclasses and not instantiated in apps.
+ * Base class for WFS GetFeature filters.
+ *
+ * @abstract
+ */
+class Filter {
+  /**
+   * @param {!string} tagName The XML tag name for this filter.
+   */
+  constructor(tagName) {
+    /**
+     * @private
+     * @type {!string}
+     */
+    this.tagName_ = tagName;
+  }
+
+  /**
+   * The XML tag name for a filter.
+   * @returns {!string} Name.
+   */
+  getTagName() {
+    return this.tagName_;
+  }
+}
+
+export default Filter;
