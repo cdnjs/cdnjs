cfcea91f127e83e5dd271d222dae24848436142a
robocdnjs <cdnjs-github@cloudflare.com>
2022-08-01 09:07:13 +0000

    Add hexo-theme-next (8.12.3)

---
+++ ajax/libs/hexo-theme-next/8.12.3/third-party/comments/gitalk.js
@@ -0,0 +1,24 @@
+/* global NexT, CONFIG, Gitalk */
+
+document.addEventListener('page:loaded', () => {
+  if (!CONFIG.page.comments) return;
+
+  NexT.utils.loadComments('.gitalk-container')
+    .then(() => NexT.utils.getScript(CONFIG.gitalk.js, {
+      condition: window.Gitalk
+    }))
+    .then(() => {
+      const gitalk = new Gitalk({
+        clientID           : CONFIG.gitalk.client_id,
+        clientSecret       : CONFIG.gitalk.client_secret,
+        repo               : CONFIG.gitalk.repo,
+        owner              : CONFIG.gitalk.github_id,
+        admin              : [CONFIG.gitalk.admin_user],
+        id                 : CONFIG.gitalk.path_md5,
+        proxy              : CONFIG.gitalk.proxy,
+        language           : CONFIG.gitalk.language || window.navigator.language,
+        distractionFreeMode: CONFIG.gitalk.distraction_free_mode
+      });
+      gitalk.render(document.querySelector('.gitalk-container'));
+    });
+});
