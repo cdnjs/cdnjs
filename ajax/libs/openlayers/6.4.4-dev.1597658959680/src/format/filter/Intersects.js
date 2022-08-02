dd973e533e0072c421989d25310cc9fe9f4bac57
robocdnjs <cdnjs-github@cloudflare.com>
2022-08-02 02:59:11 +0000

    Add openlayers (6.4.4-dev.1597658959680)

---
+++ ajax/libs/openlayers/6.4.4-dev.1597658959680/src/format/filter/Intersects.js
@@ -0,0 +1,24 @@
+/**
+ * @module ol/format/filter/Intersects
+ */
+import Spatial from './Spatial.js';
+
+/**
+ * @classdesc
+ * Represents a `<Intersects>` operator to test whether a geometry-valued property
+ * intersects a given geometry.
+ * @api
+ */
+class Intersects extends Spatial {
+  /**
+   * @param {!string} geometryName Geometry name to use.
+   * @param {!import("../../geom/Geometry.js").default} geometry Geometry.
+   * @param {string=} opt_srsName SRS name. No srsName attribute will be
+   *    set on geometries when this is not provided.
+   */
+  constructor(geometryName, geometry, opt_srsName) {
+    super('Intersects', geometryName, geometry, opt_srsName);
+  }
+}
+
+export default Intersects;
