9432d93f924cfc9e8d242f8b810c18a58241c3b6
robocdnjs <cdnjs-github@cloudflare.com>
2022-08-09 04:19:28 +0000

    Add ace (1.9.2)

---
+++ ajax/libs/ace/1.9.2/snippets/fortran.js
@@ -0,0 +1,8 @@
+;                (function() {
+                    window.require(["ace/snippets/fortran"], function(m) {
+                        if (typeof module == "object" && typeof exports == "object" && module) {
+                            module.exports = m;
+                        }
+                    });
+                })();
+            
