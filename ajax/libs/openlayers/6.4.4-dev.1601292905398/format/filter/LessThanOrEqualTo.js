725c2769851ff7bd3a3951fce521f2b62e8aa728
robocdnjs <cdnjs-github@cloudflare.com>
2022-08-12 11:05:07 +0000

    Add openlayers (6.4.4-dev.1601292905398)

---
+++ ajax/libs/openlayers/6.4.4-dev.1601292905398/format/filter/LessThanOrEqualTo.js
@@ -0,0 +1,35 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+/**
+ * @module ol/format/filter/LessThanOrEqualTo
+ */
+import ComparisonBinary from './ComparisonBinary.js';
+/**
+ * @classdesc
+ * Represents a `<PropertyIsLessThanOrEqualTo>` comparison operator.
+ * @api
+ */
+var LessThanOrEqualTo = /** @class */ (function (_super) {
+    __extends(LessThanOrEqualTo, _super);
+    /**
+     * @param {!string} propertyName Name of the context property to compare.
+     * @param {!number} expression The value to compare.
+     */
+    function LessThanOrEqualTo(propertyName, expression) {
+        return _super.call(this, 'PropertyIsLessThanOrEqualTo', propertyName, expression) || this;
+    }
+    return LessThanOrEqualTo;
+}(ComparisonBinary));
+export default LessThanOrEqualTo;
+//# sourceMappingURL=LessThanOrEqualTo.js.map
