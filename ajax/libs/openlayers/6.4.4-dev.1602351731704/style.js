840e1cfcad28e62477ff454bd57cae4398a25790
robocdnjs <cdnjs-github@cloudflare.com>
2022-08-14 00:12:47 +0000

    Add openlayers (6.4.4-dev.1602351731704)

---
+++ ajax/libs/openlayers/6.4.4-dev.1602351731704/style.js
@@ -0,0 +1,13 @@
+/**
+ * @module ol/style
+ */
+export { default as Circle } from './style/Circle.js';
+export { default as Fill } from './style/Fill.js';
+export { default as Icon } from './style/Icon.js';
+export { default as IconImage } from './style/IconImage.js';
+export { default as Image } from './style/Image.js';
+export { default as RegularShape } from './style/RegularShape.js';
+export { default as Stroke } from './style/Stroke.js';
+export { default as Style } from './style/Style.js';
+export { default as Text } from './style/Text.js';
+//# sourceMappingURL=style.js.map
