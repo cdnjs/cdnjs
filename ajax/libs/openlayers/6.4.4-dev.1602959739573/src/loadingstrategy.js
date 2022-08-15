9c4ef5b25fb48086d9d6093b275c72f373e814e7
robocdnjs <cdnjs-github@cloudflare.com>
2022-08-15 05:20:25 +0000

    Add openlayers (6.4.4-dev.1602959739573)

---
+++ ajax/libs/openlayers/6.4.4-dev.1602959739573/src/loadingstrategy.js
@@ -0,0 +1,64 @@
+/**
+ * @module ol/loadingstrategy
+ */
+
+/**
+ * Strategy function for loading all features with a single request.
+ * @param {import("./extent.js").Extent} extent Extent.
+ * @param {number} resolution Resolution.
+ * @return {Array<import("./extent.js").Extent>} Extents.
+ * @api
+ */
+export function all(extent, resolution) {
+  return [[-Infinity, -Infinity, Infinity, Infinity]];
+}
+
+/**
+ * Strategy function for loading features based on the view's extent and
+ * resolution.
+ * @param {import("./extent.js").Extent} extent Extent.
+ * @param {number} resolution Resolution.
+ * @return {Array<import("./extent.js").Extent>} Extents.
+ * @api
+ */
+export function bbox(extent, resolution) {
+  return [extent];
+}
+
+/**
+ * Creates a strategy function for loading features based on a tile grid.
+ * @param {import("./tilegrid/TileGrid.js").default} tileGrid Tile grid.
+ * @return {function(import("./extent.js").Extent, number): Array<import("./extent.js").Extent>} Loading strategy.
+ * @api
+ */
+export function tile(tileGrid) {
+  return (
+    /**
+     * @param {import("./extent.js").Extent} extent Extent.
+     * @param {number} resolution Resolution.
+     * @return {Array<import("./extent.js").Extent>} Extents.
+     */
+    function (extent, resolution) {
+      const z = tileGrid.getZForResolution(resolution);
+      const tileRange = tileGrid.getTileRangeForExtentAndZ(extent, z);
+      /** @type {Array<import("./extent.js").Extent>} */
+      const extents = [];
+      /** @type {import("./tilecoord.js").TileCoord} */
+      const tileCoord = [z, 0, 0];
+      for (
+        tileCoord[1] = tileRange.minX;
+        tileCoord[1] <= tileRange.maxX;
+        ++tileCoord[1]
+      ) {
+        for (
+          tileCoord[2] = tileRange.minY;
+          tileCoord[2] <= tileRange.maxY;
+          ++tileCoord[2]
+        ) {
+          extents.push(tileGrid.getTileCoordExtent(tileCoord));
+        }
+      }
+      return extents;
+    }
+  );
+}
