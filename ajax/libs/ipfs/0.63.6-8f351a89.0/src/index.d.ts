c90cadd8a068aa40aa99129b0b39556e1684a90f
robocdnjs <cdnjs-github@cloudflare.com>
2022-08-18 02:56:39 +0000

    Add ipfs (0.63.6-8f351a89.0)

---
+++ ajax/libs/ipfs/0.63.6-8f351a89.0/src/index.d.ts
@@ -0,0 +1,10 @@
+/**
+ * @typedef {import('ipfs-core-types').IPFS} IPFS
+ */
+export const create: typeof import("ipfs-core/dist/src/components").create;
+export const globSource: typeof import("ipfs-utils/src/files/glob-source");
+export const urlSource: typeof import("ipfs-utils/src/files/url-source");
+export const path: typeof pathImport;
+export type IPFS = import('ipfs-core-types').IPFS;
+import { path as pathImport } from "./path.js";
+//# sourceMappingURL=index.d.ts.map
