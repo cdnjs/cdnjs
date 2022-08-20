9eee5b3cf4be04b0a0c3a7173cb913d9f4b089ee
robocdnjs <cdnjs-github@cloudflare.com>
2022-08-20 12:06:06 +0000

    Add openlayers (6.4.4-dev.1604907342858)

---
+++ ajax/libs/openlayers/6.4.4-dev.1604907342858/extent/Corner.js
@@ -0,0 +1,14 @@
+/**
+ * @module ol/extent/Corner
+ */
+/**
+ * Extent corner.
+ * @enum {string}
+ */
+export default {
+    BOTTOM_LEFT: 'bottom-left',
+    BOTTOM_RIGHT: 'bottom-right',
+    TOP_LEFT: 'top-left',
+    TOP_RIGHT: 'top-right',
+};
+//# sourceMappingURL=Corner.js.map
