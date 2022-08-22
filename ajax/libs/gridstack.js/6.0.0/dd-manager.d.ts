8b507f4ee012aad3ce539168e6f52978d5a57adc
robocdnjs <cdnjs-github@cloudflare.com>
2022-08-22 03:46:38 +0000

    Add gridstack.js (6.0.0)

---
+++ ajax/libs/gridstack.js/6.0.0/dd-manager.d.ts
@@ -0,0 +1,17 @@
+/**
+ * dd-manager.ts 6.0.0
+ * Copyright (c) 2021 Alain Dumesny - see GridStack root license
+ */
+import { DDDraggable } from './dd-draggable';
+import { DDDroppable } from './dd-droppable';
+/**
+ * globals that are shared across Drag & Drop instances
+ */
+export declare class DDManager {
+    /** true if a mouse down event was handled */
+    static mouseHandled: boolean;
+    /** item being dragged */
+    static dragElement: DDDraggable;
+    /** item we are currently over as drop target */
+    static dropElement: DDDroppable;
+}
