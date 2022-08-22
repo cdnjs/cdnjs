739a3b8464e0ea533c7166f990b73724e6c178d9
robocdnjs <cdnjs-github@cloudflare.com>
2022-08-22 23:04:20 +0000

    Add vscode-webview-ui-toolkit (1.0.1)

---
+++ ajax/libs/vscode-webview-ui-toolkit/1.0.1/index-rollup.d.ts
@@ -0,0 +1,8 @@
+export * from './index';
+/**
+ * The global VSCode Design System.
+ * @remarks
+ * Only available if the components are added through a script tag
+ * rather than a module/build system.
+ */
+export declare const VSCodeDesignSystem: import("@microsoft/fast-foundation").DesignSystem;
