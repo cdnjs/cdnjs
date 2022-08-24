12aef92898e1f9fcec88e093126c26f3cbd683fe
robocdnjs <cdnjs-github@cloudflare.com>
2022-08-24 23:04:30 +0000

    Add web3 (4.0.0-alpha.0)

---
+++ ajax/libs/web3/4.0.0-alpha.0/version.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Web3PkgInfo = void 0;
+exports.Web3PkgInfo = { version: "4.0.0-alpha.0" };
+//# sourceMappingURL=version.js.map
