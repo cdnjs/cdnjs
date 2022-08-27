d7beb1cb68485a297ca4c65c867af0ef97097774
robocdnjs <cdnjs-github@cloudflare.com>
2022-08-27 12:06:25 +0000

    Add openlayers (6.4.4-dev.1607501771906)

---
+++ ajax/libs/openlayers/6.4.4-dev.1607501771906/proj/projections.js
@@ -0,0 +1,32 @@
+/**
+ * @module ol/proj/projections
+ */
+/**
+ * @type {Object<string, import("./Projection.js").default>}
+ */
+var cache = {};
+/**
+ * Clear the projections cache.
+ */
+export function clear() {
+    cache = {};
+}
+/**
+ * Get a cached projection by code.
+ * @param {string} code The code for the projection.
+ * @return {import("./Projection.js").default} The projection (if cached).
+ */
+export function get(code) {
+    return (cache[code] ||
+        cache[code.replace(/urn:(x-)?ogc:def:crs:EPSG:(.*:)?(\w+)$/, 'EPSG:$3')] ||
+        null);
+}
+/**
+ * Add a projection to the cache.
+ * @param {string} code The projection code.
+ * @param {import("./Projection.js").default} projection The projection to cache.
+ */
+export function add(code, projection) {
+    cache[code] = projection;
+}
+//# sourceMappingURL=projections.js.map
