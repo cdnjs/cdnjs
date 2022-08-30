f0f8489b4b8f9dac5eea1b6da9e2ca274bb5c5ab
robocdnjs <cdnjs-github@cloudflare.com>
2022-08-30 12:06:05 +0000

    Add sakana-widget (2.2.1)

---
+++ ajax/libs/sakana-widget/2.2.1/icons/index.d.ts
@@ -0,0 +1,4 @@
+export { default as svgClose } from './close.svg';
+export { default as svgGitHub } from './github.svg';
+export { default as svgPerson } from './person.svg';
+export { default as svgSync } from './sync.svg';
