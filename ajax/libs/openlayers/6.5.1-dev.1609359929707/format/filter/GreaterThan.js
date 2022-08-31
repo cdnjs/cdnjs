696dabea7501af03b7eecaa85e01e3a716f9bc02
robocdnjs <cdnjs-github@cloudflare.com>
2022-08-31 13:18:54 +0000

    Add openlayers (6.5.1-dev.1609359929707)

---
+++ ajax/libs/openlayers/6.5.1-dev.1609359929707/format/filter/GreaterThan.js
@@ -0,0 +1,35 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+/**
+ * @module ol/format/filter/GreaterThan
+ */
+import ComparisonBinary from './ComparisonBinary.js';
+/**
+ * @classdesc
+ * Represents a `<PropertyIsGreaterThan>` comparison operator.
+ * @api
+ */
+var GreaterThan = /** @class */ (function (_super) {
+    __extends(GreaterThan, _super);
+    /**
+     * @param {!string} propertyName Name of the context property to compare.
+     * @param {!number} expression The value to compare.
+     */
+    function GreaterThan(propertyName, expression) {
+        return _super.call(this, 'PropertyIsGreaterThan', propertyName, expression) || this;
+    }
+    return GreaterThan;
+}(ComparisonBinary));
+export default GreaterThan;
+//# sourceMappingURL=GreaterThan.js.map
