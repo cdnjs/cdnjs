9e75038775671b017a321f52acb86bda3422c4ed
robocdnjs <cdnjs-github@cloudflare.com>
2022-09-06 16:05:37 +0000

    Add ace (1.10.1)

---
+++ ajax/libs/ace/1.10.1/snippets/batchfile.js
@@ -0,0 +1,8 @@
+;                (function() {
+                    window.require(["ace/snippets/batchfile"], function(m) {
+                        if (typeof module == "object" && typeof exports == "object" && module) {
+                            module.exports = m;
+                        }
+                    });
+                })();
+            
