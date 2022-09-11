3ba042b60d3737137d1f5baef17f0198e9a2bd03
robocdnjs <cdnjs-github@cloudflare.com>
2022-09-11 09:04:21 +0000

    Add openlayers (6.5.1-dev.1613055147359)

---
+++ ajax/libs/openlayers/6.5.1-dev.1613055147359/asserts.js
@@ -0,0 +1,14 @@
+/**
+ * @module ol/asserts
+ */
+import AssertionError from './AssertionError.js';
+/**
+ * @param {*} assertion Assertion we expected to be truthy.
+ * @param {number} errorCode Error code.
+ */
+export function assert(assertion, errorCode) {
+    if (!assertion) {
+        throw new AssertionError(errorCode);
+    }
+}
+//# sourceMappingURL=asserts.js.map
