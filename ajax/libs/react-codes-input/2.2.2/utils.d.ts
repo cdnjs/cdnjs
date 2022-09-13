e7c9525c04acf038b4c8cd0072f212f39afe323c
robocdnjs <cdnjs-github@cloudflare.com>
2022-09-13 05:32:58 +0000

    Add react-codes-input (2.2.2)

---
+++ ajax/libs/react-codes-input/2.2.2/utils.d.ts
@@ -0,0 +1,15 @@
+export declare enum CASE_TYPES {
+    UPPERCASE = "upper",
+    LOWERCASE = "lower",
+    AUTO = "auto"
+}
+export declare const cx: (...params: Array<any>) => string;
+export declare const getRandomId: () => string;
+export declare const getAlphanumeric: (v: string) => string;
+export declare const getAlpha: (v: string) => string;
+export declare const getNumeric: (v: string) => string;
+export declare const getCased: (v: string, type: string) => string;
+export declare const getClassName: (className: string) => string;
+export declare const isAndroid: () => boolean;
+export declare const isChrome: () => boolean;
+export declare const isMobile: () => boolean;
