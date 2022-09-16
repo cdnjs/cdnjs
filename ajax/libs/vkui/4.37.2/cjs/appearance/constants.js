ca4bc54a1c3126f824a3b6ec18367a7ef4cfd24f
robocdnjs <cdnjs-github@cloudflare.com>
2022-09-16 20:07:04 +0000

    Add vkui (4.37.2)

---
+++ ajax/libs/vkui/4.37.2/cjs/appearance/constants.js
@@ -0,0 +1,9 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.tabbarHeight = void 0;
+var tabbarHeight = 48;
+exports.tabbarHeight = tabbarHeight;
+//# sourceMappingURL=constants.js.map
