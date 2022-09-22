7c21b3cf2b7648ca751c3502a70fc31795053858
robocdnjs <cdnjs-github@cloudflare.com>
2022-09-22 03:54:06 +0000

    Add tsparticles-updater-color (2.3.2)

---
+++ ajax/libs/tsparticles-updater-color/2.3.2/tsparticles.updater.color.js
@@ -0,0 +1,200 @@
+/*!
+ * Author : Matteo Bruni
+ * MIT license: https://opensource.org/licenses/MIT
+ * Demo / Generator : https://particles.js.org/
+ * GitHub : https://www.github.com/matteobruni/tsparticles
+ * How to use? : Check the GitHub README
+ * v2.3.2
+ */
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory(require("tsparticles-engine"));
+	else if(typeof define === 'function' && define.amd)
+		define(["tsparticles-engine"], factory);
+	else {
+		var a = typeof exports === 'object' ? factory(require("tsparticles-engine")) : factory(root["window"]);
+		for(var i in a) (typeof exports === 'object' ? exports : root)[i] = a[i];
+	}
+})(this, (__WEBPACK_EXTERNAL_MODULE__961__) => {
+return /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ 961:
+/***/ ((module) => {
+
+module.exports = __WEBPACK_EXTERNAL_MODULE__961__;
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "loadColorUpdater": () => (/* binding */ loadColorUpdater)
+});
+
+// EXTERNAL MODULE: external {"commonjs":"tsparticles-engine","commonjs2":"tsparticles-engine","amd":"tsparticles-engine","root":"window"}
+var external_commonjs_tsparticles_engine_commonjs2_tsparticles_engine_amd_tsparticles_engine_root_window_ = __webpack_require__(961);
+;// CONCATENATED MODULE: ./dist/browser/ColorUpdater.js
+
+
+function updateColorValue(delta, value, valueAnimation, max, decrease) {
+  var _a, _b;
+
+  const colorValue = value;
+
+  if (!colorValue || !valueAnimation.enable) {
+    return;
+  }
+
+  const offset = (0,external_commonjs_tsparticles_engine_commonjs2_tsparticles_engine_amd_tsparticles_engine_root_window_.randomInRange)(valueAnimation.offset),
+        velocity = ((_a = value.velocity) !== null && _a !== void 0 ? _a : 0) * delta.factor + offset * 3.6,
+        decay = (_b = value.decay) !== null && _b !== void 0 ? _b : 1;
+
+  if (!decrease || colorValue.status === 0
+  /* AnimationStatus.increasing */
+  ) {
+    colorValue.value += velocity;
+
+    if (decrease && colorValue.value > max) {
+      colorValue.status = 1
+      /* AnimationStatus.decreasing */
+      ;
+      colorValue.value -= colorValue.value % max;
+    }
+  } else {
+    colorValue.value -= velocity;
+
+    if (colorValue.value < 0) {
+      colorValue.status = 0
+      /* AnimationStatus.increasing */
+      ;
+      colorValue.value += colorValue.value;
+    }
+  }
+
+  if (colorValue.velocity && decay !== 1) {
+    colorValue.velocity *= decay;
+  }
+
+  if (colorValue.value > max) {
+    colorValue.value %= max;
+  }
+}
+
+function updateColor(particle, delta) {
+  var _a, _b, _c;
+
+  const animationOptions = particle.options.color.animation;
+
+  if (((_a = particle.color) === null || _a === void 0 ? void 0 : _a.h) !== undefined) {
+    updateColorValue(delta, particle.color.h, animationOptions.h, 360, false);
+  }
+
+  if (((_b = particle.color) === null || _b === void 0 ? void 0 : _b.s) !== undefined) {
+    updateColorValue(delta, particle.color.s, animationOptions.s, 100, true);
+  }
+
+  if (((_c = particle.color) === null || _c === void 0 ? void 0 : _c.l) !== undefined) {
+    updateColorValue(delta, particle.color.l, animationOptions.l, 100, true);
+  }
+}
+
+class ColorUpdater {
+  constructor(container) {
+    this.container = container;
+  }
+
+  init(particle) {
+    /* color */
+    const hslColor = (0,external_commonjs_tsparticles_engine_commonjs2_tsparticles_engine_amd_tsparticles_engine_root_window_.rangeColorToHsl)(particle.options.color, particle.id, particle.options.reduceDuplicates);
+
+    if (hslColor) {
+      particle.color = (0,external_commonjs_tsparticles_engine_commonjs2_tsparticles_engine_amd_tsparticles_engine_root_window_.getHslAnimationFromHsl)(hslColor, particle.options.color.animation, this.container.retina.reduceFactor);
+    }
+  }
+
+  isEnabled(particle) {
+    var _a, _b, _c;
+
+    const animationOptions = particle.options.color.animation;
+    return !particle.destroyed && !particle.spawning && (((_a = particle.color) === null || _a === void 0 ? void 0 : _a.h.value) !== undefined && animationOptions.h.enable || ((_b = particle.color) === null || _b === void 0 ? void 0 : _b.s.value) !== undefined && animationOptions.s.enable || ((_c = particle.color) === null || _c === void 0 ? void 0 : _c.l.value) !== undefined && animationOptions.l.enable);
+  }
+
+  update(particle, delta) {
+    updateColor(particle, delta);
+  }
+
+}
+;// CONCATENATED MODULE: ./dist/browser/index.js
+
+async function loadColorUpdater(engine) {
+  await engine.addParticleUpdater("color", container => new ColorUpdater(container));
+}
+})();
+
+/******/ 	return __webpack_exports__;
+/******/ })()
+;
+});
