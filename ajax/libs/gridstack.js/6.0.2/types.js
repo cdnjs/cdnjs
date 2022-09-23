d717fd7949ebd3848aeed83b475632f3e5a55f5d
robocdnjs <cdnjs-github@cloudflare.com>
2022-09-23 16:08:51 +0000

    Add gridstack.js (6.0.2)

---
+++ ajax/libs/gridstack.js/6.0.2/types.js
@@ -0,0 +1,31 @@
+"use strict";
+/**
+ * types.ts 6.0.2
+ * Copyright (c) 2021 Alain Dumesny - see GridStack root license
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.GridDefaults = void 0;
+// default values for grid options - used during init and when saving out
+exports.GridDefaults = {
+    alwaysShowResizeHandle: 'mobile',
+    animate: true,
+    auto: true,
+    cellHeight: 'auto',
+    cellHeightThrottle: 100,
+    cellHeightUnit: 'px',
+    column: 12,
+    draggable: { handle: '.grid-stack-item-content', appendTo: 'body' },
+    handle: '.grid-stack-item-content',
+    itemClass: 'grid-stack-item',
+    margin: 10,
+    marginUnit: 'px',
+    maxRow: 0,
+    minRow: 0,
+    oneColumnSize: 768,
+    placeholderClass: 'grid-stack-placeholder',
+    placeholderText: '',
+    removableOptions: { accept: '.grid-stack-item' },
+    resizable: { handles: 'se' },
+    rtl: 'auto',
+};
+//# sourceMappingURL=types.js.map
