47d3bc3ef499ef1bc48a106edaf0acfcd3afc361
robocdnjs <cdnjs-github@cloudflare.com>
2022-10-01 05:30:52 +0000

    Add openlayers (6.5.1-dev.1620570785148)

---
+++ ajax/libs/openlayers/6.5.1-dev.1620570785148/format/filter/IsNull.js
@@ -0,0 +1,36 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+/**
+ * @module ol/format/filter/IsNull
+ */
+import Comparison from './Comparison.js';
+/**
+ * @classdesc
+ * Represents a `<PropertyIsNull>` comparison operator.
+ * @api
+ */
+var IsNull = /** @class */ (function (_super) {
+    __extends(IsNull, _super);
+    /**
+     * @param {!string} propertyName Name of the context property to compare.
+     */
+    function IsNull(propertyName) {
+        return _super.call(this, 'PropertyIsNull', propertyName) || this;
+    }
+    return IsNull;
+}(Comparison));
+export default IsNull;
+//# sourceMappingURL=IsNull.js.map
