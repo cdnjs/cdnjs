0ad05eb1c94251a44ee91741a01ec47eda9900fa
robocdnjs <cdnjs-github@cloudflare.com>
2022-10-03 00:19:46 +0000

    Add hanabi (0.3.1)

---
+++ ajax/libs/hanabi/0.3.1/hanabi.js
@@ -0,0 +1,32 @@
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+  typeof define === 'function' && define.amd ? define(factory) :
+  (global.hanabi = factory());
+}(this, (function () { 'use strict';
+
+var defaultColors = ['23AC69', '91C132', 'F19726', 'E8552D', '1AAB8E', 'E1147F', '2980C1', '1BA1E6', '9FA0A0', 'F19726', 'E30B20', 'E30B20', 'A3338B'];
+
+var index = function (input, ref) {
+  if ( ref === void 0 ) ref = {};
+  var colors = ref.colors; if ( colors === void 0 ) colors = defaultColors;
+
+  var index = 0;
+  var cache = {};
+  return input
+  .replace(/[a-zA-Z0-9_\u0392-\u03c9\u0400-\u04FF]+|[\u4E00-\u9FFF\u3400-\u4dbf\uf900-\ufaff\u3040-\u309f\uac00-\ud7af\u0400-\u04FF]+|\w+|</g, function (m) {
+    var color;
+    if (cache[m]) {
+      color = cache[m];
+    } else {
+      color = colors[index];
+      cache[m] = color;
+    }
+    var out = "<span style=\"color: #" + color + "\">" + (m === '<' ? '&lt;' : m) + "</span>";
+    index = ++index % colors.length;
+    return out
+  })
+};
+
+return index;
+
+})));
