0088032affeabb45f3112abe2d7618de32c9c91f
robocdnjs <cdnjs-github@cloudflare.com>
2022-10-08 09:07:51 +0000

    Add openlayers (6.5.1-dev.1622491509207)

---
+++ ajax/libs/openlayers/6.5.1-dev.1622491509207/src/source/State.js
@@ -0,0 +1,14 @@
+/**
+ * @module ol/source/State
+ */
+
+/**
+ * @enum {string}
+ * State of the source, one of 'undefined', 'loading', 'ready' or 'error'.
+ */
+export default {
+  UNDEFINED: 'undefined',
+  LOADING: 'loading',
+  READY: 'ready',
+  ERROR: 'error',
+};
