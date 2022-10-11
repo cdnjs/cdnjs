c4b7160023c8fff3702345676a3780fdb58d170a
robocdnjs <cdnjs-github@cloudflare.com>
2022-10-11 13:23:46 +0000

    Add vkui (4.38.0)

---
+++ ajax/libs/vkui/4.38.0/cjs/components/LocaleProviderContext/LocaleProviderContext.js
@@ -0,0 +1,14 @@
+"use strict";
+
+var _interopRequireWildcard = require("@babel/runtime/helpers/interopRequireWildcard").default;
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.LocaleProviderContext = void 0;
+
+var React = _interopRequireWildcard(require("react"));
+
+var LocaleProviderContext = /*#__PURE__*/React.createContext(undefined);
+exports.LocaleProviderContext = LocaleProviderContext;
+//# sourceMappingURL=LocaleProviderContext.js.map
