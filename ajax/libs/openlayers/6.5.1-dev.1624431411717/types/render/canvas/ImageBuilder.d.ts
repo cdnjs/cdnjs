bf30ed03398c25b47772f3cf25d6dc1dee6b2fca
robocdnjs <cdnjs-github@cloudflare.com>
2022-10-15 16:05:27 +0000

    Add openlayers (6.5.1-dev.1624431411717)

---
+++ ajax/libs/openlayers/6.5.1-dev.1624431411717/types/render/canvas/ImageBuilder.d.ts
@@ -0,0 +1,76 @@
+export default CanvasImageBuilder;
+declare class CanvasImageBuilder extends CanvasBuilder {
+    /**
+     * @private
+     * @type {HTMLCanvasElement|HTMLVideoElement|HTMLImageElement}
+     */
+    private hitDetectionImage_;
+    /**
+     * @private
+     * @type {HTMLCanvasElement|HTMLVideoElement|HTMLImageElement}
+     */
+    private image_;
+    /**
+     * @private
+     * @type {number|undefined}
+     */
+    private imagePixelRatio_;
+    /**
+     * @private
+     * @type {number|undefined}
+     */
+    private anchorX_;
+    /**
+     * @private
+     * @type {number|undefined}
+     */
+    private anchorY_;
+    /**
+     * @private
+     * @type {number|undefined}
+     */
+    private height_;
+    /**
+     * @private
+     * @type {number|undefined}
+     */
+    private opacity_;
+    /**
+     * @private
+     * @type {number|undefined}
+     */
+    private originX_;
+    /**
+     * @private
+     * @type {number|undefined}
+     */
+    private originY_;
+    /**
+     * @private
+     * @type {boolean|undefined}
+     */
+    private rotateWithView_;
+    /**
+     * @private
+     * @type {number|undefined}
+     */
+    private rotation_;
+    /**
+     * @private
+     * @type {import("../../size.js").Size|undefined}
+     */
+    private scale_;
+    /**
+     * @private
+     * @type {number|undefined}
+     */
+    private width_;
+    /**
+     * Data shared with a text builder for combined decluttering.
+     * @private
+     * @type {import("../canvas.js").DeclutterImageWithText}
+     */
+    private declutterImageWithText_;
+}
+import CanvasBuilder from "./Builder.js";
+//# sourceMappingURL=ImageBuilder.d.ts.map
