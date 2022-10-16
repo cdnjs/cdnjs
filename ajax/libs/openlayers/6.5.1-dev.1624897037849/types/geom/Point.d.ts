6c16486b35b1994e1b2b5b42f986b68f6d040f0b
robocdnjs <cdnjs-github@cloudflare.com>
2022-10-16 05:23:41 +0000

    Add openlayers (6.5.1-dev.1624897037849)

---
+++ ajax/libs/openlayers/6.5.1-dev.1624897037849/types/geom/Point.d.ts
@@ -0,0 +1,16 @@
+export default Point;
+/**
+ * @classdesc
+ * Point geometry.
+ *
+ * @api
+ */
+declare class Point extends SimpleGeometry {
+    /**
+     * @param {import("../coordinate.js").Coordinate} coordinates Coordinates.
+     * @param {import("./GeometryLayout.js").default} [opt_layout] Layout.
+     */
+    constructor(coordinates: import("../coordinate.js").Coordinate, opt_layout?: any);
+}
+import SimpleGeometry from "./SimpleGeometry.js";
+//# sourceMappingURL=Point.d.ts.map
