bff1c3d5a1dc464a24b01144f8638192757a9713
robocdnjs <cdnjs-github@cloudflare.com>
2022-10-17 07:19:34 +0000

    Add openlayers (6.5.1-dev.1625546784386)

---
+++ ajax/libs/openlayers/6.5.1-dev.1625546784386/src/render/canvas/BuilderGroup.js
@@ -0,0 +1,105 @@
+/**
+ * @module ol/render/canvas/BuilderGroup
+ */
+
+import Builder from './Builder.js';
+import ImageBuilder from './ImageBuilder.js';
+import LineStringBuilder from './LineStringBuilder.js';
+import PolygonBuilder from './PolygonBuilder.js';
+import TextBuilder from './TextBuilder.js';
+
+/**
+ * @type {Object<import("./BuilderType").default, typeof Builder>}
+ */
+const BATCH_CONSTRUCTORS = {
+  'Circle': PolygonBuilder,
+  'Default': Builder,
+  'Image': ImageBuilder,
+  'LineString': LineStringBuilder,
+  'Polygon': PolygonBuilder,
+  'Text': TextBuilder,
+};
+
+class BuilderGroup {
+  /**
+   * @param {number} tolerance Tolerance.
+   * @param {import("../../extent.js").Extent} maxExtent Max extent.
+   * @param {number} resolution Resolution.
+   * @param {number} pixelRatio Pixel ratio.
+   */
+  constructor(tolerance, maxExtent, resolution, pixelRatio) {
+    /**
+     * @private
+     * @type {number}
+     */
+    this.tolerance_ = tolerance;
+
+    /**
+     * @private
+     * @type {import("../../extent.js").Extent}
+     */
+    this.maxExtent_ = maxExtent;
+
+    /**
+     * @private
+     * @type {number}
+     */
+    this.pixelRatio_ = pixelRatio;
+
+    /**
+     * @private
+     * @type {number}
+     */
+    this.resolution_ = resolution;
+
+    /**
+     * @private
+     * @type {!Object<string, !Object<import("./BuilderType").default, Builder>>}
+     */
+    this.buildersByZIndex_ = {};
+  }
+
+  /**
+   * @return {!Object<string, !Object<import("./BuilderType").default, import("./Builder.js").SerializableInstructions>>} The serializable instructions
+   */
+  finish() {
+    const builderInstructions = {};
+    for (const zKey in this.buildersByZIndex_) {
+      builderInstructions[zKey] = builderInstructions[zKey] || {};
+      const builders = this.buildersByZIndex_[zKey];
+      for (const builderKey in builders) {
+        const builderInstruction = builders[builderKey].finish();
+        builderInstructions[zKey][builderKey] = builderInstruction;
+      }
+    }
+    return builderInstructions;
+  }
+
+  /**
+   * @param {number|undefined} zIndex Z index.
+   * @param {import("./BuilderType.js").default} builderType Replay type.
+   * @return {import("../VectorContext.js").default} Replay.
+   */
+  getBuilder(zIndex, builderType) {
+    const zIndexKey = zIndex !== undefined ? zIndex.toString() : '0';
+    let replays = this.buildersByZIndex_[zIndexKey];
+    if (replays === undefined) {
+      replays = {};
+      this.buildersByZIndex_[zIndexKey] = replays;
+    }
+    let replay = replays[builderType];
+    if (replay === undefined) {
+      const Constructor = BATCH_CONSTRUCTORS[builderType];
+      replay = new Constructor(
+        this.tolerance_,
+        this.maxExtent_,
+        this.resolution_,
+        this.pixelRatio_
+      );
+      replays[builderType] = replay;
+    }
+    return replay;
+  }
+}
+
+export default BuilderGroup;
