24c8eb8150f3589f5956b20f8fc826ef823eec31
robocdnjs <cdnjs-github@cloudflare.com>
2022-10-17 23:05:13 +0000

    Add openlayers (6.5.1-dev.1625556664468)

---
+++ ajax/libs/openlayers/6.5.1-dev.1625556664468/types/format/IGC.d.ts
@@ -0,0 +1,45 @@
+export default IGC;
+export type Options = {
+    /**
+     * Altitude mode. Possible
+     * values are `'barometric'`, `'gps'`, and `'none'`.
+     */
+    altitudeMode?: IGCZ | string;
+};
+/**
+ * @typedef {Object} Options
+ * @property {IGCZ|string} [altitudeMode='none'] Altitude mode. Possible
+ * values are `'barometric'`, `'gps'`, and `'none'`.
+ */
+/**
+ * @classdesc
+ * Feature format for `*.igc` flight recording files.
+ *
+ * As IGC sources contain a single feature,
+ * {@link module:ol/format/IGC~IGC#readFeatures} will return the feature in an
+ * array
+ *
+ * @api
+ */
+declare class IGC extends TextFeature {
+    /**
+     * @param {Options} [opt_options] Options.
+     */
+    constructor(opt_options?: Options);
+    /**
+     * @private
+     * @type {IGCZ}
+     */
+    private altitudeMode_;
+}
+/**
+ * IGC altitude/z. One of 'barometric', 'gps', 'none'.
+ */
+type IGCZ = string;
+declare namespace IGCZ {
+    const BAROMETRIC: string;
+    const GPS: string;
+    const NONE: string;
+}
+import TextFeature from "./TextFeature.js";
+//# sourceMappingURL=IGC.d.ts.map
---
+++ ajax/libs/openlayers/6.5.1-dev.1625556664468/types/render/EventType.d.ts
@@ -0,0 +1,9 @@
+declare namespace _default {
+    const PRERENDER: string;
+    const POSTRENDER: string;
+    const PRECOMPOSE: string;
+    const POSTCOMPOSE: string;
+    const RENDERCOMPLETE: string;
+}
+export default _default;
+//# sourceMappingURL=EventType.d.ts.map
