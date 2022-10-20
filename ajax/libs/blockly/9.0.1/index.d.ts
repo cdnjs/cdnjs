991436aefcc84ef2d7bd816998c57713efc1fe37
robocdnjs <cdnjs-github@cloudflare.com>
2022-10-20 13:22:10 +0000

    Add blockly (9.0.1)

---
+++ ajax/libs/blockly/9.0.1/index.d.ts
@@ -0,0 +1,10 @@
+/**
+ * @license
+ * Copyright 2020 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import * as libraryBlocks from './blocks';  // Handcrafted file.
+export * from './core/blockly';
+export {libraryBlocks};
+export const JavaScript: any;
