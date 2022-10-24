b51e9e6136c1916b3e844a4b0a3136ecca7d34a3
robocdnjs <cdnjs-github@cloudflare.com>
2022-10-24 09:11:55 +0000

    Add openlayers (6.6.2-dev.1627294389471)

---
+++ ajax/libs/openlayers/6.6.2-dev.1627294389471/source/common.js
@@ -0,0 +1,17 @@
+/**
+ * @module ol/source/common
+ */
+/**
+ * Default WMS version.
+ * @type {string}
+ */
+export var DEFAULT_WMS_VERSION = '1.3.0';
+/**
+ * Context options to disable image smoothing.
+ * @type {Object}
+ */
+export var IMAGE_SMOOTHING_DISABLED = {
+    imageSmoothingEnabled: false,
+    msImageSmoothingEnabled: false,
+};
+//# sourceMappingURL=common.js.map
