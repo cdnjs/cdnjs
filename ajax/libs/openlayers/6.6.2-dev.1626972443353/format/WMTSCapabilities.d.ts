57f5309f344313a7defd967632941eea0b28867c
robocdnjs <cdnjs-github@cloudflare.com>
2022-10-24 01:34:47 +0000

    Add openlayers (6.6.2-dev.1626972443353)

---
+++ ajax/libs/openlayers/6.6.2-dev.1626972443353/format/WMTSCapabilities.d.ts
@@ -0,0 +1,16 @@
+export default WMTSCapabilities;
+/**
+ * @classdesc
+ * Format for reading WMTS capabilities data.
+ *
+ * @api
+ */
+declare class WMTSCapabilities extends XML {
+    /**
+     * @type {OWS}
+     * @private
+     */
+    private owsParser_;
+}
+import XML from "./XML.js";
+//# sourceMappingURL=WMTSCapabilities.d.ts.map
