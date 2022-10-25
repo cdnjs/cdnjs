e7bb16533421b401e700be5a00fec0711cd8eaf1
robocdnjs <cdnjs-github@cloudflare.com>
2022-10-25 11:07:28 +0000

    Add styled-components (6.0.0-beta.4)

---
+++ ajax/libs/styled-components/6.0.0-beta.4/index.d.ts
@@ -0,0 +1,4 @@
+import styled from './constructors/styled';
+export * from './base';
+export { CSSProp, IStyledComponent, IStyledComponentFactory, IStyledStatics, StyledOptions, WebTarget, } from './types';
+export { styled as default };
