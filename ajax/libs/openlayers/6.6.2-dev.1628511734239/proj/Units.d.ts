0a11da94fedd6766792fc46a164dcb08a977562e
robocdnjs <cdnjs-github@cloudflare.com>
2022-10-28 05:14:48 +0000

    Add openlayers (6.6.2-dev.1628511734239)

---
+++ ajax/libs/openlayers/6.6.2-dev.1628511734239/proj/Units.d.ts
@@ -0,0 +1,22 @@
+/**
+ * Meters per unit lookup table.
+ * @const
+ * @type {Object<Units, number>}
+ * @api
+ */
+export const METERS_PER_UNIT: Object<Units, number>;
+export default Units;
+/**
+ * Projection units: `'degrees'`, `'ft'`, `'m'`, `'pixels'`, `'tile-pixels'` or
+ * `'us-ft'`.
+ */
+export type Units = string;
+declare namespace Units {
+    export const DEGREES: string;
+    export const FEET: string;
+    export const METERS: string;
+    export const PIXELS: string;
+    export const TILE_PIXELS: string;
+    export const USFEET: string;
+}
+//# sourceMappingURL=Units.d.ts.map
