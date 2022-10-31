5bac60fed04866dde9c1730f27a6eb66d0e61a7e
robocdnjs <cdnjs-github@cloudflare.com>
2022-10-31 23:06:17 +0000

    Add openlayers (6.6.2-dev.1630173313164)

---
+++ ajax/libs/openlayers/6.6.2-dev.1630173313164/layer/Tile.js
@@ -0,0 +1,52 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+/**
+ * @module ol/layer/Tile
+ */
+import BaseTileLayer from './BaseTile.js';
+import CanvasTileLayerRenderer from '../renderer/canvas/TileLayer.js';
+/**
+ * @classdesc
+ * For layer sources that provide pre-rendered, tiled images in grids that are
+ * organized by zoom levels for specific resolutions.
+ * Note that any property set in the options is set as a {@link module:ol/Object~BaseObject}
+ * property on the layer object; for example, setting `title: 'My Title'` in the
+ * options means that `title` is observable, and has get/set accessors.
+ *
+ * @template {import("../source/Tile.js").default} TileSourceType
+ * @extends {BaseTileLayer<TileSourceType>}
+ * @api
+ */
+var TileLayer = /** @class */ (function (_super) {
+    __extends(TileLayer, _super);
+    /**
+     * @param {import("./BaseTile.js").Options<TileSourceType>} [opt_options] Tile layer options.
+     */
+    function TileLayer(opt_options) {
+        return _super.call(this, opt_options) || this;
+    }
+    /**
+     * Create a renderer for this layer.
+     * @return {import("../renderer/Layer.js").default} A layer renderer.
+     * @protected
+     */
+    TileLayer.prototype.createRenderer = function () {
+        return new CanvasTileLayerRenderer(this);
+    };
+    return TileLayer;
+}(BaseTileLayer));
+export default TileLayer;
+//# sourceMappingURL=Tile.js.map
---
+++ ajax/libs/openlayers/6.6.2-dev.1630173313164/src/proj/Units.js
@@ -0,0 +1,56 @@
+/**
+ * @module ol/proj/Units
+ */
+
+/**
+ * Projection units: `'degrees'`, `'ft'`, `'m'`, `'pixels'`, `'tile-pixels'` or
+ * `'us-ft'`.
+ * @enum {string}
+ */
+const Units = {
+  /**
+   * Degrees
+   * @api
+   */
+  DEGREES: 'degrees',
+  /**
+   * Feet
+   * @api
+   */
+  FEET: 'ft',
+  /**
+   * Meters
+   * @api
+   */
+  METERS: 'm',
+  /**
+   * Pixels
+   * @api
+   */
+  PIXELS: 'pixels',
+  /**
+   * Tile Pixels
+   * @api
+   */
+  TILE_PIXELS: 'tile-pixels',
+  /**
+   * US Feet
+   * @api
+   */
+  USFEET: 'us-ft',
+};
+
+/**
+ * Meters per unit lookup table.
+ * @const
+ * @type {Object<Units, number>}
+ * @api
+ */
+export const METERS_PER_UNIT = {};
+// use the radius of the Normal sphere
+METERS_PER_UNIT[Units.DEGREES] = (2 * Math.PI * 6370997) / 360;
+METERS_PER_UNIT[Units.FEET] = 0.3048;
+METERS_PER_UNIT[Units.METERS] = 1;
+METERS_PER_UNIT[Units.USFEET] = 1200 / 3937;
+
+export default Units;
