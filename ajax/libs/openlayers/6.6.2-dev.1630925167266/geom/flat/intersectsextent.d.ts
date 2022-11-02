f7c2c1013a43e3d86f1a7a65ffa46d920ed47bc8
robocdnjs <cdnjs-github@cloudflare.com>
2022-11-02 14:12:52 +0000

    Add openlayers (6.6.2-dev.1630925167266)

---
+++ ajax/libs/openlayers/6.6.2-dev.1630925167266/geom/flat/intersectsextent.d.ts
@@ -0,0 +1,46 @@
+/**
+ * @param {Array<number>} flatCoordinates Flat coordinates.
+ * @param {number} offset Offset.
+ * @param {number} end End.
+ * @param {number} stride Stride.
+ * @param {import("../../extent.js").Extent} extent Extent.
+ * @return {boolean} True if the geometry and the extent intersect.
+ */
+export function intersectsLineString(flatCoordinates: number[], offset: number, end: number, stride: number, extent: number[]): boolean;
+/**
+ * @param {Array<number>} flatCoordinates Flat coordinates.
+ * @param {number} offset Offset.
+ * @param {Array<number>} ends Ends.
+ * @param {number} stride Stride.
+ * @param {import("../../extent.js").Extent} extent Extent.
+ * @return {boolean} True if the geometry and the extent intersect.
+ */
+export function intersectsLineStringArray(flatCoordinates: number[], offset: number, ends: number[], stride: number, extent: number[]): boolean;
+/**
+ * @param {Array<number>} flatCoordinates Flat coordinates.
+ * @param {number} offset Offset.
+ * @param {number} end End.
+ * @param {number} stride Stride.
+ * @param {import("../../extent.js").Extent} extent Extent.
+ * @return {boolean} True if the geometry and the extent intersect.
+ */
+export function intersectsLinearRing(flatCoordinates: number[], offset: number, end: number, stride: number, extent: number[]): boolean;
+/**
+ * @param {Array<number>} flatCoordinates Flat coordinates.
+ * @param {number} offset Offset.
+ * @param {Array<number>} ends Ends.
+ * @param {number} stride Stride.
+ * @param {import("../../extent.js").Extent} extent Extent.
+ * @return {boolean} True if the geometry and the extent intersect.
+ */
+export function intersectsLinearRingArray(flatCoordinates: number[], offset: number, ends: number[], stride: number, extent: number[]): boolean;
+/**
+ * @param {Array<number>} flatCoordinates Flat coordinates.
+ * @param {number} offset Offset.
+ * @param {Array<Array<number>>} endss Endss.
+ * @param {number} stride Stride.
+ * @param {import("../../extent.js").Extent} extent Extent.
+ * @return {boolean} True if the geometry and the extent intersect.
+ */
+export function intersectsLinearRingMultiArray(flatCoordinates: number[], offset: number, endss: number[][], stride: number, extent: number[]): boolean;
+//# sourceMappingURL=intersectsextent.d.ts.map
