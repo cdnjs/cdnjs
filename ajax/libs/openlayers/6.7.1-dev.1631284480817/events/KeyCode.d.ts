aeb90b46c18473b0e53e40897f2ee96dced4da28
robocdnjs <cdnjs-github@cloudflare.com>
2022-11-05 16:05:34 +0000

    Add openlayers (6.7.1-dev.1631284480817)

---
+++ ajax/libs/openlayers/6.7.1-dev.1631284480817/events/KeyCode.d.ts
@@ -0,0 +1,8 @@
+declare namespace _default {
+    export const LEFT: number;
+    export const UP: number;
+    export const RIGHT: number;
+    export const DOWN: number;
+}
+export default _default;
+//# sourceMappingURL=KeyCode.d.ts.map
