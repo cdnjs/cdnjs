ccbbbfa6c99421877c0a353437accd31fa7e7efa
robocdnjs <cdnjs-github@cloudflare.com>
2022-11-07 19:08:40 +0000

    Add tsparticles-shape-multiline-text (2.5.3)

---
+++ ajax/libs/tsparticles-shape-multiline-text/2.5.3/tsparticles.shape.multiline-text.js
@@ -0,0 +1,165 @@
+/*!
+ * Author : Matteo Bruni
+ * MIT license: https://opensource.org/licenses/MIT
+ * Demo / Generator : https://particles.js.org/
+ * GitHub : https://www.github.com/matteobruni/tsparticles
+ * How to use? : Check the GitHub README
+ * v2.5.3
+ */
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory(require("tsparticles-engine"));
+	else if(typeof define === 'function' && define.amd)
+		define(["tsparticles-engine"], factory);
+	else {
+		var a = typeof exports === 'object' ? factory(require("tsparticles-engine")) : factory(root["window"]);
+		for(var i in a) (typeof exports === 'object' ? exports : root)[i] = a[i];
+	}
+})(this, (__WEBPACK_EXTERNAL_MODULE__961__) => {
+return /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ 961:
+/***/ ((module) => {
+
+module.exports = __WEBPACK_EXTERNAL_MODULE__961__;
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "loadMultilineTextShape": () => (/* binding */ loadMultilineTextShape)
+});
+
+// EXTERNAL MODULE: external {"commonjs":"tsparticles-engine","commonjs2":"tsparticles-engine","amd":"tsparticles-engine","root":"window"}
+var external_commonjs_tsparticles_engine_commonjs2_tsparticles_engine_amd_tsparticles_engine_root_window_ = __webpack_require__(961);
+;// CONCATENATED MODULE: ./dist/browser/MultilineTextDrawer.js
+
+class MultilineTextDrawer {
+  draw(context, particle, radius) {
+    const character = particle.shapeData;
+    if (character === undefined) {
+      return;
+    }
+    const textData = character.value;
+    if (textData === undefined) {
+      return;
+    }
+    const textParticle = particle;
+    if (textParticle.text === undefined) {
+      textParticle.text = (0,external_commonjs_tsparticles_engine_commonjs2_tsparticles_engine_amd_tsparticles_engine_root_window_.itemFromSingleOrMultiple)(textData, particle.randomIndexData);
+    }
+    const text = textParticle.text;
+    const style = character.style;
+    const weight = character.weight;
+    const size = Math.round(radius) * 2;
+    const font = character.font;
+    const fill = particle.fill;
+    context.font = `${style} ${weight} ${size}px "${font}"`;
+    if (fill) {
+      text === null || text === void 0 ? void 0 : text.split("\n").forEach((line, index) => {
+        const offsetX = line.length * radius / 2;
+        const pos = {
+          x: -offsetX,
+          y: radius / 2
+        };
+        context.fillText(line, pos.x, pos.y + radius * 2 * index);
+      });
+    } else {
+      text === null || text === void 0 ? void 0 : text.split("\n").forEach((line, index) => {
+        const offsetX = line.length * radius / 2;
+        const pos = {
+          x: -offsetX,
+          y: radius / 2
+        };
+        context.strokeText(line, pos.x, pos.y + radius * 2 * index);
+      });
+    }
+  }
+  async init(container) {
+    const options = container.options,
+      shapeType = "multiline-text";
+    if ((0,external_commonjs_tsparticles_engine_commonjs2_tsparticles_engine_amd_tsparticles_engine_root_window_.isInArray)(shapeType, options.particles.shape.type)) {
+      const shapeOptions = options.particles.shape.options[shapeType],
+        promises = [];
+      (0,external_commonjs_tsparticles_engine_commonjs2_tsparticles_engine_amd_tsparticles_engine_root_window_.executeOnSingleOrMultiple)(shapeOptions, shape => {
+        promises.push((0,external_commonjs_tsparticles_engine_commonjs2_tsparticles_engine_amd_tsparticles_engine_root_window_.loadFont)(shape.font, shape.weight));
+      });
+      await Promise.all(promises);
+    }
+  }
+}
+;// CONCATENATED MODULE: ./dist/browser/index.js
+
+function loadMultilineTextShape(engine) {
+  engine.addShape("multiline-text", new MultilineTextDrawer());
+}
+})();
+
+/******/ 	return __webpack_exports__;
+/******/ })()
+;
+});
