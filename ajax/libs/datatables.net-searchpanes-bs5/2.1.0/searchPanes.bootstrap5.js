7c1619a8625422de145e162dd5d6d2b8474eb80c
robocdnjs <cdnjs-github@cloudflare.com>
2022-11-08 15:09:17 +0000

    Add datatables.net-searchpanes-bs5 (2.1.0)

---
+++ ajax/libs/datatables.net-searchpanes-bs5/2.1.0/searchPanes.bootstrap5.js
@@ -0,0 +1,74 @@
+/*! Bootstrap 5 integration for DataTables' SearchPanes
+ * © SpryMedia Ltd - datatables.net/license
+ */
+
+(function( factory ){
+	if ( typeof define === 'function' && define.amd ) {
+		// AMD
+		define( ['jquery', 'datatables.net-bs5', 'datatables.net-searchpanes'], function ( $ ) {
+			return factory( $, window, document );
+		} );
+	}
+	else if ( typeof exports === 'object' ) {
+		// CommonJS
+		module.exports = function (root, $) {
+			if ( ! root ) {
+				// CommonJS environments without a window global must pass a
+				// root. This will give an error otherwise
+				root = window;
+			}
+
+			if ( ! $ ) {
+				$ = typeof window !== 'undefined' ? // jQuery's factory checks for a global window
+					require('jquery') :
+					require('jquery')( root );
+			}
+
+			if ( ! $.fn.dataTable ) {
+				require('datatables.net-bs5')(root, $);
+			}
+
+			if ( ! $.fn.dataTable ) {
+				require('datatables.net-searchpanes')(root, $);
+			}
+
+
+			return factory( $, root, root.document );
+		};
+	}
+	else {
+		// Browser
+		factory( jQuery, window, document );
+	}
+}(function( $, window, document, undefined ) {
+'use strict';
+var DataTable = $.fn.dataTable;
+
+
+$.extend(true, DataTable.SearchPane.classes, {
+    buttonGroup: 'btn-group',
+    disabledButton: 'disabled',
+    narrow: 'col',
+    pane: {
+        container: 'table'
+    },
+    paneButton: 'btn btn-light',
+    pill: 'badge rounded-pill bg-secondary',
+    search: 'form-control search',
+    table: 'table table-sm table-borderless',
+    topRow: 'dtsp-topRow'
+});
+$.extend(true, DataTable.SearchPanes.classes, {
+    clearAll: 'dtsp-clearAll btn btn-light',
+    collapseAll: 'dtsp-collapseAll btn btn-light',
+    container: 'dtsp-searchPanes',
+    disabledButton: 'disabled',
+    panes: 'dtsp-panes dtsp-panesContainer',
+    showAll: 'dtsp-showAll btn btn-light',
+    title: 'dtsp-title',
+    titleRow: 'dtsp-titleRow'
+});
+
+
+return DataTable;
+}));
