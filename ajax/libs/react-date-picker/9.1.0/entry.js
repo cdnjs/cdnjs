18442d69a4a0b02b124baf218286cad7ddac2a38
robocdnjs <cdnjs-github@cloudflare.com>
2022-11-08 14:08:18 +0000

    Add react-date-picker (9.1.0)

---
+++ ajax/libs/react-date-picker/9.1.0/entry.js
@@ -0,0 +1,14 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+require("react-calendar/dist/Calendar.css");
+var _DatePicker = _interopRequireDefault(require("./DatePicker"));
+require("./DatePicker.css");
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+// File is created during build phase and placed in dist directory
+// eslint-disable-next-line import/no-unresolved
+var _default = _DatePicker["default"];
+exports["default"] = _default;
---
+++ ajax/libs/react-date-picker/9.1.0/entry.nostyle.js
@@ -0,0 +1,10 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+var _DatePicker = _interopRequireDefault(require("./DatePicker"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+var _default = _DatePicker["default"];
+exports["default"] = _default;
