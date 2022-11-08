7b3a359e379fbbbdd5a971ce23c88b66a439b79d
robocdnjs <cdnjs-github@cloudflare.com>
2022-11-08 02:43:23 +0000

    Add openlayers (6.7.1-dev.1631807870157)

---
+++ ajax/libs/openlayers/6.7.1-dev.1631807870157/format/filter/EqualTo.js
@@ -0,0 +1,38 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+/**
+ * @module ol/format/filter/EqualTo
+ */
+import ComparisonBinary from './ComparisonBinary.js';
+/**
+ * @classdesc
+ * Represents a `<PropertyIsEqualTo>` comparison operator.
+ * @api
+ */
+var EqualTo = /** @class */ (function (_super) {
+    __extends(EqualTo, _super);
+    /**
+     * @param {!string} propertyName Name of the context property to compare.
+     * @param {!(string|number)} expression The value to compare.
+     * @param {boolean} [opt_matchCase] Case-sensitive?
+     */
+    function EqualTo(propertyName, expression, opt_matchCase) {
+        return _super.call(this, 'PropertyIsEqualTo', propertyName, expression, opt_matchCase) || this;
+    }
+    return EqualTo;
+}(ComparisonBinary));
+export default EqualTo;
+//# sourceMappingURL=EqualTo.js.map
