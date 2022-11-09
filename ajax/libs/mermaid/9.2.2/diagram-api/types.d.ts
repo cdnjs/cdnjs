9573076f6b1e6ee4bf3bd343399203a298e10797
robocdnjs <cdnjs-github@cloudflare.com>
2022-11-09 17:12:28 +0000

    Add mermaid (9.2.2)

---
+++ ajax/libs/mermaid/9.2.2/diagram-api/types.d.ts
@@ -0,0 +1,30 @@
+import { MermaidConfig } from '../config.type';
+export interface InjectUtils {
+    _log: any;
+    _setLogLevel: any;
+    _getConfig: any;
+    _sanitizeText: any;
+    _setupGraphViewbox: any;
+}
+export interface DiagramDefinition {
+    db: any;
+    renderer: any;
+    parser: any;
+    styles: any;
+    init?: (config: MermaidConfig) => void;
+    injectUtils?: (_log: InjectUtils['_log'], _setLogLevel: InjectUtils['_setLogLevel'], _getConfig: InjectUtils['_getConfig'], _sanitizeText: InjectUtils['_sanitizeText'], _setupGraphViewbox: InjectUtils['_setupGraphViewbox']) => void;
+}
+export interface DetectorRecord {
+    detector: DiagramDetector;
+    loader?: DiagramLoader;
+}
+export interface ExternalDiagramDefinition {
+    id: string;
+    detector: DiagramDetector;
+    loader: DiagramLoader;
+}
+export declare type DiagramDetector = (text: string, config?: MermaidConfig) => boolean;
+export declare type DiagramLoader = () => Promise<{
+    id: string;
+    diagram: DiagramDefinition;
+}>;
