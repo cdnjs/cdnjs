42360b3bc1de696fbdf20aaa8ab172c4863b6e1c
robocdnjs <cdnjs-github@cloudflare.com>
2022-11-09 01:34:22 +0000

    Add openlayers (6.7.1-dev.1631950173563)

---
+++ ajax/libs/openlayers/6.7.1-dev.1631950173563/layer/Image.js
@@ -0,0 +1,51 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+/**
+ * @module ol/layer/Image
+ */
+import BaseImageLayer from './BaseImage.js';
+import CanvasImageLayerRenderer from '../renderer/canvas/ImageLayer.js';
+/**
+ * @classdesc
+ * Server-rendered images that are available for arbitrary extents and
+ * resolutions.
+ * Note that any property set in the options is set as a {@link module:ol/Object~BaseObject}
+ * property on the layer object; for example, setting `title: 'My Title'` in the
+ * options means that `title` is observable, and has get/set accessors.
+ *
+ * @template {import("../source/Image.js").default} ImageSourceType
+ * @extends {BaseImageLayer<ImageSourceType>}
+ * @api
+ */
+var ImageLayer = /** @class */ (function (_super) {
+    __extends(ImageLayer, _super);
+    /**
+     * @param {import("./BaseImage.js").Options<ImageSourceType>} [opt_options] Layer options.
+     */
+    function ImageLayer(opt_options) {
+        return _super.call(this, opt_options) || this;
+    }
+    /**
+     * Create a renderer for this layer.
+     * @return {import("../renderer/Layer.js").default} A layer renderer.
+     */
+    ImageLayer.prototype.createRenderer = function () {
+        return new CanvasImageLayerRenderer(this);
+    };
+    return ImageLayer;
+}(BaseImageLayer));
+export default ImageLayer;
+//# sourceMappingURL=Image.js.map
