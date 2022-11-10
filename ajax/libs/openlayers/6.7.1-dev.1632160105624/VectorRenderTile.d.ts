caedbd63ce51218df4547453372014654e7d685d
robocdnjs <cdnjs-github@cloudflare.com>
2022-11-10 00:18:11 +0000

    Add openlayers (6.7.1-dev.1632160105624)

---
+++ ajax/libs/openlayers/6.7.1-dev.1632160105624/VectorRenderTile.d.ts
@@ -0,0 +1,121 @@
+export default VectorRenderTile;
+export type ReplayState = {
+    /**
+     * Dirty.
+     */
+    dirty: boolean;
+    /**
+     * RenderedRenderOrder.
+     */
+    renderedRenderOrder: ((arg0: import("./render/Feature.js").default | import("./Feature.js").default<import("./geom/Geometry.js").default>, arg1: import("./render/Feature.js").default | import("./Feature.js").default<import("./geom/Geometry.js").default>) => number) | null;
+    /**
+     * RenderedTileRevision.
+     */
+    renderedTileRevision: number;
+    /**
+     * RenderedResolution.
+     */
+    renderedResolution: number;
+    /**
+     * RenderedRevision.
+     */
+    renderedRevision: number;
+    /**
+     * RenderedTileResolution.
+     */
+    renderedTileResolution: number;
+    /**
+     * RenderedTileZ.
+     */
+    renderedTileZ: number;
+};
+declare class VectorRenderTile extends Tile {
+    /**
+     * @param {import("./tilecoord.js").TileCoord} tileCoord Tile coordinate.
+     * @param {import("./TileState.js").default} state State.
+     * @param {import("./tilecoord.js").TileCoord} urlTileCoord Wrapped tile coordinate for source urls.
+     * @param {function(VectorRenderTile):Array<import("./VectorTile").default>} getSourceTiles Function
+     * to get source tiles for this tile.
+     */
+    constructor(tileCoord: number[], state: any, urlTileCoord: number[], getSourceTiles: (arg0: VectorRenderTile) => import("./VectorTile.js").default[]);
+    /**
+     * @private
+     * @type {!Object<string, CanvasRenderingContext2D>}
+     */
+    private context_;
+    /**
+     * Executor groups by layer uid. Entries are read/written by the renderer.
+     * @type {Object<string, Array<import("./render/canvas/ExecutorGroup.js").default>>}
+     */
+    executorGroups: {
+        [x: string]: Array<import("./render/canvas/ExecutorGroup.js").default>;
+    };
+    /**
+     * Executor groups for decluttering, by layer uid. Entries are read/written by the renderer.
+     * @type {Object<string, Array<import("./render/canvas/ExecutorGroup.js").default>>}
+     */
+    declutterExecutorGroups: {
+        [x: string]: Array<import("./render/canvas/ExecutorGroup.js").default>;
+    };
+    /**
+     * Number of loading source tiles. Read/written by the source.
+     * @type {number}
+     */
+    loadingSourceTiles: number;
+    /**
+     * @type {Object<number, ImageData>}
+     */
+    hitDetectionImageData: {
+        [x: number]: ImageData;
+    };
+    /**
+     * @private
+     * @type {!Object<string, ReplayState>}
+     */
+    private replayState_;
+    /**
+     * @type {Array<import("./VectorTile.js").default>}
+     */
+    sourceTiles: Array<import("./VectorTile.js").default>;
+    /**
+     * @type {Object<string, boolean>}
+     */
+    errorTileKeys: {
+        [x: string]: boolean;
+    };
+    /**
+     * @type {number}
+     */
+    wantedResolution: number;
+    /**
+     * @type {!function():Array<import("./VectorTile.js").default>}
+     */
+    getSourceTiles: () => Array<import("./VectorTile.js").default>;
+    /**
+     * @type {import("./tilecoord.js").TileCoord}
+     */
+    wrappedTileCoord: import("./tilecoord.js").TileCoord;
+    /**
+     * @param {import("./layer/Layer.js").default} layer Layer.
+     * @return {CanvasRenderingContext2D} The rendering context.
+     */
+    getContext(layer: import("./layer/Layer.js").default<any>): CanvasRenderingContext2D;
+    /**
+     * @param {import("./layer/Layer.js").default} layer Layer.
+     * @return {boolean} Tile has a rendering context for the given layer.
+     */
+    hasContext(layer: import("./layer/Layer.js").default<any>): boolean;
+    /**
+     * Get the Canvas for this tile.
+     * @param {import("./layer/Layer.js").default} layer Layer.
+     * @return {HTMLCanvasElement} Canvas.
+     */
+    getImage(layer: import("./layer/Layer.js").default<any>): HTMLCanvasElement;
+    /**
+     * @param {import("./layer/Layer.js").default} layer Layer.
+     * @return {ReplayState} The replay state.
+     */
+    getReplayState(layer: import("./layer/Layer.js").default<any>): ReplayState;
+}
+import Tile from "./Tile.js";
+//# sourceMappingURL=VectorRenderTile.d.ts.map
