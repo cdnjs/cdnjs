91d1a22bf8c31a1db56d0c42a8bb7201eda37146
robocdnjs <cdnjs-github@cloudflare.com>
2022-11-13 07:06:42 +0000

    Add openlayers (6.8.1-dev.1632514137467)

---
+++ ajax/libs/openlayers/6.8.1-dev.1632514137467/functions.js
@@ -0,0 +1,52 @@
+/**
+ * @module ol/functions
+ */
+import { equals as arrayEquals } from './array.js';
+/**
+ * Always returns true.
+ * @return {boolean} true.
+ */
+export function TRUE() {
+    return true;
+}
+/**
+ * Always returns false.
+ * @return {boolean} false.
+ */
+export function FALSE() {
+    return false;
+}
+/**
+ * A reusable function, used e.g. as a default for callbacks.
+ *
+ * @return {void} Nothing.
+ */
+export function VOID() { }
+/**
+ * Wrap a function in another function that remembers the last return.  If the
+ * returned function is called twice in a row with the same arguments and the same
+ * this object, it will return the value from the first call in the second call.
+ *
+ * @param {function(...any): ReturnType} fn The function to memoize.
+ * @return {function(...any): ReturnType} The memoized function.
+ * @template ReturnType
+ */
+export function memoizeOne(fn) {
+    var called = false;
+    /** @type {ReturnType} */
+    var lastResult;
+    /** @type {Array<any>} */
+    var lastArgs;
+    var lastThis;
+    return function () {
+        var nextArgs = Array.prototype.slice.call(arguments);
+        if (!called || this !== lastThis || !arrayEquals(nextArgs, lastArgs)) {
+            called = true;
+            lastThis = this;
+            lastArgs = nextArgs;
+            lastResult = fn.apply(this, arguments);
+        }
+        return lastResult;
+    };
+}
+//# sourceMappingURL=functions.js.map
