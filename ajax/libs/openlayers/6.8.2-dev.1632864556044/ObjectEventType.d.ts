08ff89085258efac8561dfdb81c8eeb5f1727e12
robocdnjs <cdnjs-github@cloudflare.com>
2022-11-16 00:14:54 +0000

    Add openlayers (6.8.2-dev.1632864556044)

---
+++ ajax/libs/openlayers/6.8.2-dev.1632864556044/ObjectEventType.d.ts
@@ -0,0 +1,6 @@
+declare namespace _default {
+    export const PROPERTYCHANGE: string;
+}
+export default _default;
+export type Types = "propertychange";
+//# sourceMappingURL=ObjectEventType.d.ts.map
