3326961c055f08f1e0a088328521697553a54945
robocdnjs <cdnjs-github@cloudflare.com>
2022-11-17 16:04:52 +0000

    Add react-chartjs-2 (5.0.0)

---
+++ ajax/libs/react-chartjs-2/5.0.0/index.d.ts
@@ -0,0 +1,5 @@
+export type { ChartProps } from './types.js';
+export * from './chart.js';
+export * from './typedCharts.js';
+export { getDatasetAtEvent, getElementAtEvent, getElementsAtEvent, } from './utils.js';
+//# sourceMappingURL=index.d.ts.map
