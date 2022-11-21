e1de3145b3b98ed800081977d26eb6c5cf6246f1
robocdnjs <cdnjs-github@cloudflare.com>
2022-11-21 19:09:49 +0000

    Add blockly (9.1.1)

---
+++ ajax/libs/blockly/9.1.1/core-browser.js
@@ -0,0 +1,27 @@
+/* eslint-disable */
+;(function(root, factory) {
+  if (typeof define === 'function' && define.amd) { // AMD
+    define(['./blockly'], factory);
+  } else if (typeof exports === 'object') { // Node.js
+    module.exports = factory(require('./blockly'));
+  } else { // Browser
+    root.Blockly = factory(root.Blockly);
+  }
+}(this, function(Blockly) {
+/**
+ * @license
+ * Copyright 2019 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @fileoverview Blockly core module for the browser. It includes blockly.js
+ *               and adds a helper method for setting the locale.
+ */
+
+/* eslint-disable */
+'use strict';
+
+
+return Blockly;
+}));
---
+++ ajax/libs/blockly/9.1.1/core.js
@@ -0,0 +1,30 @@
+/* eslint-disable */
+(function (Blockly){
+/**
+ * @license
+ * Copyright 2019 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @fileoverview Blockly core module for Node. It includes blockly-node.js
+ *               and adds a helper method for setting the locale.
+ */
+
+/* eslint-disable */
+'use strict';
+
+
+// Override textToDomDocument and provide Node.js alternatives to DOMParser and
+// XMLSerializer.
+if (typeof globalThis.document !== 'object') {
+  const jsdom = require('jsdom/lib/jsdom/living');
+  globalThis.DOMParser = jsdom.DOMParser;
+  globalThis.XMLSerializer = jsdom.XMLSerializer;
+  const xmlDocument = Blockly.utils.xml.textToDomDocument(
+      `<xml xmlns="${Blockly.utils.xml.NAME_SPACE}"></xml>`);
+  Blockly.utils.xml.setDocument(xmlDocument);
+}
+
+module.exports = Blockly;
+})(require('./blockly'));
