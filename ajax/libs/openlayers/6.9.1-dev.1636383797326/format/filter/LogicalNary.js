a18da48cb4792932918b80be885d1c1baeef955e
robocdnjs <cdnjs-github@cloudflare.com>
2022-11-28 20:06:02 +0000

    Add openlayers (6.9.1-dev.1636383797326)

---
+++ ajax/libs/openlayers/6.9.1-dev.1636383797326/format/filter/LogicalNary.js
@@ -0,0 +1,46 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+/**
+ * @module ol/format/filter/LogicalNary
+ */
+import Filter from './Filter.js';
+import { assert } from '../../asserts.js';
+/**
+ * @classdesc
+ * Abstract class; normally only used for creating subclasses and not instantiated in apps.
+ * Base class for WFS GetFeature n-ary logical filters.
+ *
+ * @abstract
+ */
+var LogicalNary = /** @class */ (function (_super) {
+    __extends(LogicalNary, _super);
+    /**
+     * @param {!string} tagName The XML tag name for this filter.
+     * @param {Array<import("./Filter.js").default>} conditions Conditions.
+     */
+    function LogicalNary(tagName, conditions) {
+        var _this = _super.call(this, tagName) || this;
+        /**
+         * @type {Array<import("./Filter.js").default>}
+         */
+        _this.conditions = conditions;
+        assert(_this.conditions.length >= 2, 57); // At least 2 conditions are required.
+        return _this;
+    }
+    return LogicalNary;
+}(Filter));
+export default LogicalNary;
+//# sourceMappingURL=LogicalNary.js.map
---
+++ ajax/libs/openlayers/6.9.1-dev.1636383797326/format/filter/Within.js
@@ -0,0 +1,40 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+/**
+ * @module ol/format/filter/Within
+ */
+import Spatial from './Spatial.js';
+/**
+ * @classdesc
+ * Represents a `<Within>` operator to test whether a geometry-valued property
+ * is within a given geometry.
+ * @api
+ */
+var Within = /** @class */ (function (_super) {
+    __extends(Within, _super);
+    /**
+     * @param {!string} geometryName Geometry name to use.
+     * @param {!import("../../geom/Geometry.js").default} geometry Geometry.
+     * @param {string} [opt_srsName] SRS name. No srsName attribute will be
+     *    set on geometries when this is not provided.
+     */
+    function Within(geometryName, geometry, opt_srsName) {
+        return _super.call(this, 'Within', geometryName, geometry, opt_srsName) || this;
+    }
+    return Within;
+}(Spatial));
+export default Within;
+//# sourceMappingURL=Within.js.map
