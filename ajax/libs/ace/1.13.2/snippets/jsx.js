e37af6b2077c777d4ff97696ab3275e7fdbd05cf
robocdnjs <cdnjs-github@cloudflare.com>
2022-12-07 23:06:23 +0000

    Add ace (1.13.2)

---
+++ ajax/libs/ace/1.13.2/snippets/jsx.js
@@ -0,0 +1,8 @@
+;                (function() {
+                    window.require(["ace/snippets/jsx"], function(m) {
+                        if (typeof module == "object" && typeof exports == "object" && module) {
+                            module.exports = m;
+                        }
+                    });
+                })();
+            
