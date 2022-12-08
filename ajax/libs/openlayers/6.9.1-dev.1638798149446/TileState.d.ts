e235dc71b767d442d597883ce30b640d54d2d250
robocdnjs <cdnjs-github@cloudflare.com>
2022-12-08 18:06:37 +0000

    Add openlayers (6.9.1-dev.1638798149446)

---
+++ ajax/libs/openlayers/6.9.1-dev.1638798149446/TileState.d.ts
@@ -0,0 +1,9 @@
+declare namespace _default {
+    export const IDLE: number;
+    export const LOADING: number;
+    export const LOADED: number;
+    export const ERROR: number;
+    export const EMPTY: number;
+}
+export default _default;
+//# sourceMappingURL=TileState.d.ts.map
