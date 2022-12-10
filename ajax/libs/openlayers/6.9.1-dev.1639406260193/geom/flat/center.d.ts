556bd6037fc815cebf6f4e5abce7032813603033
robocdnjs <cdnjs-github@cloudflare.com>
2022-12-10 14:06:12 +0000

    Add openlayers (6.9.1-dev.1639406260193)

---
+++ ajax/libs/openlayers/6.9.1-dev.1639406260193/geom/flat/center.d.ts
@@ -0,0 +1,9 @@
+/**
+ * @param {Array<number>} flatCoordinates Flat coordinates.
+ * @param {number} offset Offset.
+ * @param {Array<Array<number>>} endss Endss.
+ * @param {number} stride Stride.
+ * @return {Array<number>} Flat centers.
+ */
+export function linearRingss(flatCoordinates: number[], offset: number, endss: number[][], stride: number): number[];
+//# sourceMappingURL=center.d.ts.map
