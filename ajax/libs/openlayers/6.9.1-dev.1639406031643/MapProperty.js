83e725d7b36f410e616290e157a88b14e2406a0d
robocdnjs <cdnjs-github@cloudflare.com>
2022-12-10 13:08:02 +0000

    Add openlayers (6.9.1-dev.1639406031643)

---
+++ ajax/libs/openlayers/6.9.1-dev.1639406031643/MapProperty.js
@@ -0,0 +1,13 @@
+/**
+ * @module ol/MapProperty
+ */
+/**
+ * @enum {string}
+ */
+export default {
+    LAYERGROUP: 'layergroup',
+    SIZE: 'size',
+    TARGET: 'target',
+    VIEW: 'view',
+};
+//# sourceMappingURL=MapProperty.js.map
