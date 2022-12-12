9d25779784b9cae0267d22cac3cdc459cdea9dfd
robocdnjs <cdnjs-github@cloudflare.com>
2022-12-12 14:04:36 +0000

    Add openlayers (6.9.1-dev.1639995158001)

---
+++ ajax/libs/openlayers/6.9.1-dev.1639995158001/OverlayPositioning.d.ts
@@ -0,0 +1,13 @@
+declare namespace _default {
+    export const BOTTOM_LEFT: string;
+    export const BOTTOM_CENTER: string;
+    export const BOTTOM_RIGHT: string;
+    export const CENTER_LEFT: string;
+    export const CENTER_CENTER: string;
+    export const CENTER_RIGHT: string;
+    export const TOP_LEFT: string;
+    export const TOP_CENTER: string;
+    export const TOP_RIGHT: string;
+}
+export default _default;
+//# sourceMappingURL=OverlayPositioning.d.ts.map
---
+++ ajax/libs/openlayers/6.9.1-dev.1639995158001/functions.js
@@ -0,0 +1,73 @@
+/**
+ * @module ol/functions
+ */
+import { equals as arrayEquals } from './array.js';
+/**
+ * Always returns true.
+ * @return {boolean} true.
+ */
+export function TRUE() {
+    return true;
+}
+/**
+ * Always returns false.
+ * @return {boolean} false.
+ */
+export function FALSE() {
+    return false;
+}
+/**
+ * A reusable function, used e.g. as a default for callbacks.
+ *
+ * @return {void} Nothing.
+ */
+export function VOID() { }
+/**
+ * Wrap a function in another function that remembers the last return.  If the
+ * returned function is called twice in a row with the same arguments and the same
+ * this object, it will return the value from the first call in the second call.
+ *
+ * @param {function(...any): ReturnType} fn The function to memoize.
+ * @return {function(...any): ReturnType} The memoized function.
+ * @template ReturnType
+ */
+export function memoizeOne(fn) {
+    var called = false;
+    /** @type {ReturnType} */
+    var lastResult;
+    /** @type {Array<any>} */
+    var lastArgs;
+    var lastThis;
+    return function () {
+        var nextArgs = Array.prototype.slice.call(arguments);
+        if (!called || this !== lastThis || !arrayEquals(nextArgs, lastArgs)) {
+            called = true;
+            lastThis = this;
+            lastArgs = nextArgs;
+            lastResult = fn.apply(this, arguments);
+        }
+        return lastResult;
+    };
+}
+/**
+ * @template T
+ * @param {function(): (T | Promise<T>)} getter A function that returns a value or a promise for a value.
+ * @return {Promise<T>} A promise for the value.
+ */
+export function toPromise(getter) {
+    function promiseGetter() {
+        var value;
+        try {
+            value = getter();
+        }
+        catch (err) {
+            return Promise.reject(err);
+        }
+        if (value instanceof Promise) {
+            return value;
+        }
+        return Promise.resolve(value);
+    }
+    return promiseGetter();
+}
+//# sourceMappingURL=functions.js.map
