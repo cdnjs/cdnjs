da827c6499a72b7fd9b517b50dc7abd907927249
robocdnjs <cdnjs-github@cloudflare.com>
2022-12-12 06:06:11 +0000

    Add openlayers (6.9.1-dev.1639927068114)

---
+++ ajax/libs/openlayers/6.9.1-dev.1639927068114/layer/WebGLPoints.d.ts
@@ -0,0 +1,151 @@
+export default WebGLPointsLayer;
+export type Options<VectorSourceType extends import("../source/Vector.js").default<any>> = {
+    /**
+     * Literal style to apply to the layer features.
+     */
+    style: import("../style/literal.js").LiteralStyle;
+    /**
+     * A CSS class name to set to the layer element.
+     */
+    className?: string;
+    /**
+     * Opacity (0, 1).
+     */
+    opacity?: number;
+    /**
+     * Visibility.
+     */
+    visible?: boolean;
+    /**
+     * The bounding extent for layer rendering.  The layer will not be
+     * rendered outside of this extent.
+     */
+    extent?: number[];
+    /**
+     * The z-index for layer rendering.  At rendering time, the layers
+     * will be ordered, first by Z-index and then by position. When `undefined`, a `zIndex` of 0 is assumed
+     * for layers that are added to the map's `layers` collection, or `Infinity` when the layer's `setMap()`
+     * method was used.
+     */
+    zIndex?: number;
+    /**
+     * The minimum resolution (inclusive) at which this layer will be
+     * visible.
+     */
+    minResolution?: number;
+    /**
+     * The maximum resolution (exclusive) below which this layer will
+     * be visible.
+     */
+    maxResolution?: number;
+    /**
+     * The minimum view zoom level (exclusive) above which this layer will be
+     * visible.
+     */
+    minZoom?: number;
+    /**
+     * The maximum view zoom level (inclusive) at which this layer will
+     * be visible.
+     */
+    maxZoom?: number;
+    /**
+     * Source.
+     */
+    source?: VectorSourceType;
+    /**
+     * Setting this to true will provide a slight performance boost, but will
+     * prevent all hit detection on the layer.
+     */
+    disableHitDetection?: boolean;
+    /**
+     * Arbitrary observable properties. Can be accessed with `#get()` and `#set()`.
+     */
+    properties?: {
+        [x: string]: any;
+    };
+};
+/**
+ * @template {import("../source/Vector.js").default} VectorSourceType
+ * @typedef {Object} Options
+ * @property {import('../style/literal.js').LiteralStyle} style Literal style to apply to the layer features.
+ * @property {string} [className='ol-layer'] A CSS class name to set to the layer element.
+ * @property {number} [opacity=1] Opacity (0, 1).
+ * @property {boolean} [visible=true] Visibility.
+ * @property {import("../extent.js").Extent} [extent] The bounding extent for layer rendering.  The layer will not be
+ * rendered outside of this extent.
+ * @property {number} [zIndex] The z-index for layer rendering.  At rendering time, the layers
+ * will be ordered, first by Z-index and then by position. When `undefined`, a `zIndex` of 0 is assumed
+ * for layers that are added to the map's `layers` collection, or `Infinity` when the layer's `setMap()`
+ * method was used.
+ * @property {number} [minResolution] The minimum resolution (inclusive) at which this layer will be
+ * visible.
+ * @property {number} [maxResolution] The maximum resolution (exclusive) below which this layer will
+ * be visible.
+ * @property {number} [minZoom] The minimum view zoom level (exclusive) above which this layer will be
+ * visible.
+ * @property {number} [maxZoom] The maximum view zoom level (inclusive) at which this layer will
+ * be visible.
+ * @property {VectorSourceType} [source] Source.
+ * @property {boolean} [disableHitDetection=false] Setting this to true will provide a slight performance boost, but will
+ * prevent all hit detection on the layer.
+ * @property {Object<string, *>} [properties] Arbitrary observable properties. Can be accessed with `#get()` and `#set()`.
+ */
+/**
+ * @classdesc
+ * Layer optimized for rendering large point datasets. Takes a `style` property which
+ * is a serializable JSON object describing how the layer should be rendered.
+ *
+ * Here are a few samples of literal style objects:
+ * ```js
+ * const style = {
+ *   symbol: {
+ *     symbolType: 'circle',
+ *     size: 8,
+ *     color: '#33AAFF',
+ *     opacity: 0.9
+ *   }
+ * }
+ * ```
+ *
+ * ```js
+ * const style = {
+ *   symbol: {
+ *     symbolType: 'image',
+ *     offset: [0, 12],
+ *     size: [4, 8],
+ *     src: '../static/exclamation-mark.png'
+ *   }
+ * }
+ * ```
+ *
+ * **Important: a `WebGLPoints` layer must be manually disposed when removed, otherwise the underlying WebGL context
+ * will not be garbage collected.**
+ *
+ * Note that any property set in the options is set as a {@link module:ol/Object~BaseObject}
+ * property on the layer object; for example, setting `title: 'My Title'` in the
+ * options means that `title` is observable, and has get/set accessors.
+ *
+ * @template {import("../source/Vector.js").default} VectorSourceType
+ * @extends {Layer<VectorSourceType, WebGLPointsLayerRenderer>}
+ * @fires import("../render/Event.js").RenderEvent
+ */
+declare class WebGLPointsLayer<VectorSourceType extends import("../source/Vector.js").default<any>> extends Layer<VectorSourceType, WebGLPointsLayerRenderer> {
+    /**
+     * @param {Options<VectorSourceType>} options Options.
+     */
+    constructor(options: Options<VectorSourceType>);
+    /**
+     * @private
+     * @type {import('../webgl/ShaderBuilder.js').StyleParseResult}
+     */
+    private parseResult_;
+    /**
+     * @private
+     * @type {boolean}
+     */
+    private hitDetectionDisabled_;
+    createRenderer(): any;
+}
+import WebGLPointsLayerRenderer from "../renderer/webgl/PointsLayer.js";
+import Layer from "./Layer.js";
+//# sourceMappingURL=WebGLPoints.d.ts.map
