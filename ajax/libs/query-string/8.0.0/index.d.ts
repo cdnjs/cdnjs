332486aa9ff71d127cae7da5fe23605995d2508d
robocdnjs <cdnjs-github@cloudflare.com>
2022-12-12 19:05:10 +0000

    Add query-string (8.0.0)

---
+++ ajax/libs/query-string/8.0.0/index.d.ts
@@ -0,0 +1,11 @@
+export * as default from './base.js';
+
+export {
+	type ParseOptions,
+	type ParsedQuery,
+	type ParsedUrl,
+	type StringifyOptions,
+	type Stringifiable,
+	type StringifiableRecord,
+	type UrlObject,
+} from './base.js';
