f4c34d352b4d71d1be8b0ec655a034b7bcf5cea0
robocdnjs <cdnjs-github@cloudflare.com>
2022-12-14 09:05:37 +0000

    Add openlayers (6.9.1-dev.1640172533924)

---
+++ ajax/libs/openlayers/6.9.1-dev.1640172533924/worker/version.js
@@ -0,0 +1,7 @@
+export function create() {
+    var source = "var e=self;e.onmessage=function(s){console.log(\"version worker received message:\",s.data),e.postMessage(\"version: \".concat(\"latest\"))};";
+    var blob = new Blob([source], { type: 'application/javascript' });
+    var url = URL.createObjectURL(blob);
+    return new Worker(url);
+}
+//# sourceMappingURL=version.js.map
