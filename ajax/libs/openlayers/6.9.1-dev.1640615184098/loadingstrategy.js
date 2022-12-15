cdff2c560c8a1a355ef7dbd472574fd787d49d3e
robocdnjs <cdnjs-github@cloudflare.com>
2022-12-15 05:06:35 +0000

    Add openlayers (6.9.1-dev.1640615184098)

---
+++ ajax/libs/openlayers/6.9.1-dev.1640615184098/loadingstrategy.js
@@ -0,0 +1,55 @@
+/**
+ * @module ol/loadingstrategy
+ */
+import { fromUserExtent, fromUserResolution, toUserExtent } from './proj.js';
+/**
+ * Strategy function for loading all features with a single request.
+ * @param {import("./extent.js").Extent} extent Extent.
+ * @param {number} resolution Resolution.
+ * @return {Array<import("./extent.js").Extent>} Extents.
+ * @api
+ */
+export function all(extent, resolution) {
+    return [[-Infinity, -Infinity, Infinity, Infinity]];
+}
+/**
+ * Strategy function for loading features based on the view's extent and
+ * resolution.
+ * @param {import("./extent.js").Extent} extent Extent.
+ * @param {number} resolution Resolution.
+ * @return {Array<import("./extent.js").Extent>} Extents.
+ * @api
+ */
+export function bbox(extent, resolution) {
+    return [extent];
+}
+/**
+ * Creates a strategy function for loading features based on a tile grid.
+ * @param {import("./tilegrid/TileGrid.js").default} tileGrid Tile grid.
+ * @return {function(import("./extent.js").Extent, number, import("./proj.js").Projection): Array<import("./extent.js").Extent>} Loading strategy.
+ * @api
+ */
+export function tile(tileGrid) {
+    return (
+    /**
+     * @param {import("./extent.js").Extent} extent Extent.
+     * @param {number} resolution Resolution.
+     * @param {import("./proj.js").Projection} projection Projection.
+     * @return {Array<import("./extent.js").Extent>} Extents.
+     */
+    function (extent, resolution, projection) {
+        var z = tileGrid.getZForResolution(fromUserResolution(resolution, projection));
+        var tileRange = tileGrid.getTileRangeForExtentAndZ(fromUserExtent(extent, projection), z);
+        /** @type {Array<import("./extent.js").Extent>} */
+        var extents = [];
+        /** @type {import("./tilecoord.js").TileCoord} */
+        var tileCoord = [z, 0, 0];
+        for (tileCoord[1] = tileRange.minX; tileCoord[1] <= tileRange.maxX; ++tileCoord[1]) {
+            for (tileCoord[2] = tileRange.minY; tileCoord[2] <= tileRange.maxY; ++tileCoord[2]) {
+                extents.push(toUserExtent(tileGrid.getTileCoordExtent(tileCoord), projection));
+            }
+        }
+        return extents;
+    });
+}
+//# sourceMappingURL=loadingstrategy.js.map
