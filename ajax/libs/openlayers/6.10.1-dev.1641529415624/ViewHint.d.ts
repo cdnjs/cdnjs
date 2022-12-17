637d82379ebfdf0748556277ff83fdf11e0bc0a7
robocdnjs <cdnjs-github@cloudflare.com>
2022-12-17 14:04:25 +0000

    Add openlayers (6.10.1-dev.1641529415624)

---
+++ ajax/libs/openlayers/6.10.1-dev.1641529415624/ViewHint.d.ts
@@ -0,0 +1,6 @@
+declare namespace _default {
+    export const ANIMATING: number;
+    export const INTERACTING: number;
+}
+export default _default;
+//# sourceMappingURL=ViewHint.d.ts.map
---
+++ ajax/libs/openlayers/6.10.1-dev.1641529415624/renderer/canvas/common.js
@@ -0,0 +1,12 @@
+/**
+ * @module ol/renderer/canvas/common
+ */
+/**
+ * Context options to disable image smoothing.
+ * @type {Object}
+ */
+export var IMAGE_SMOOTHING_DISABLED = {
+    imageSmoothingEnabled: false,
+    msImageSmoothingEnabled: false,
+};
+//# sourceMappingURL=common.js.map
