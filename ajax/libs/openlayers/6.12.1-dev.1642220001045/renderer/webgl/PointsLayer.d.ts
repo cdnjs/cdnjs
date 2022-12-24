961b6a0309a483f4fd9e66d2feac425f4de902c3
robocdnjs <cdnjs-github@cloudflare.com>
2022-12-24 06:04:59 +0000

    Add openlayers (6.12.1-dev.1642220001045)

---
+++ ajax/libs/openlayers/6.12.1-dev.1642220001045/renderer/webgl/PointsLayer.d.ts
@@ -0,0 +1,315 @@
+export default WebGLPointsLayerRenderer;
+/**
+ * A description of a custom attribute to be passed on to the GPU, with a value different
+ * for each feature.
+ */
+export type CustomAttribute = {
+    /**
+     * Attribute name.
+     */
+    name: string;
+    /**
+     * This callback computes the numerical value of the
+     * attribute for a given feature (properties are available as 2nd arg for quicker access).
+     */
+    callback: (arg0: import("../../Feature.js").default<any>, arg1: {
+        [x: string]: any;
+    }) => number;
+};
+/**
+ * Object that holds a reference to a feature, its geometry and properties. Used to optimize
+ * rebuildBuffers by accessing these objects quicker.
+ */
+export type FeatureCacheItem = {
+    /**
+     * Feature
+     */
+    feature: import("../../Feature.js").default<any>;
+    /**
+     * Feature properties
+     */
+    properties: {
+        [x: string]: any;
+    };
+    /**
+     * Feature geometry
+     */
+    geometry: import("../../geom/Geometry.js").default;
+};
+export type Options = {
+    /**
+     * A CSS class name to set to the canvas element.
+     */
+    className?: string;
+    /**
+     * These attributes will be read from the features in the source and then
+     * passed to the GPU. The `name` property of each attribute will serve as its identifier:
+     * * In the vertex shader as an `attribute` by prefixing it with `a_`
+     * * In the fragment shader as a `varying` by prefixing it with `v_`
+     * Please note that these can only be numerical values.
+     */
+    attributes?: CustomAttribute[];
+    /**
+     * Vertex shader source, mandatory.
+     */
+    vertexShader: string;
+    /**
+     * Fragment shader source, mandatory.
+     */
+    fragmentShader: string;
+    /**
+     * Vertex shader source for hit detection rendering.
+     */
+    hitVertexShader?: string;
+    /**
+     * Fragment shader source for hit detection rendering.
+     */
+    hitFragmentShader?: string;
+    /**
+     * Uniform definitions for the post process steps
+     * Please note that `u_texture` is reserved for the main texture slot and `u_opacity` is reserved for the layer opacity.
+     */
+    uniforms?: {
+        [x: string]: number | number[] | HTMLCanvasElement | HTMLImageElement | ImageData | ((arg0: import("../../PluggableMap.js").FrameState) => number | number[] | HTMLCanvasElement | HTMLImageElement | ImageData);
+    };
+    /**
+     * Post-processes definitions
+     */
+    postProcesses?: import("./Layer.js").PostProcessesOptions[];
+};
+/**
+ * @typedef {Object} CustomAttribute A description of a custom attribute to be passed on to the GPU, with a value different
+ * for each feature.
+ * @property {string} name Attribute name.
+ * @property {function(import("../../Feature").default, Object<string, *>):number} callback This callback computes the numerical value of the
+ * attribute for a given feature (properties are available as 2nd arg for quicker access).
+ */
+/**
+ * @typedef {Object} FeatureCacheItem Object that holds a reference to a feature, its geometry and properties. Used to optimize
+ * rebuildBuffers by accessing these objects quicker.
+ * @property {import("../../Feature").default} feature Feature
+ * @property {Object<string, *>} properties Feature properties
+ * @property {import("../../geom").Geometry} geometry Feature geometry
+ */
+/**
+ * @typedef {Object} Options
+ * @property {string} [className='ol-layer'] A CSS class name to set to the canvas element.
+ * @property {Array<CustomAttribute>} [attributes] These attributes will be read from the features in the source and then
+ * passed to the GPU. The `name` property of each attribute will serve as its identifier:
+ *  * In the vertex shader as an `attribute` by prefixing it with `a_`
+ *  * In the fragment shader as a `varying` by prefixing it with `v_`
+ * Please note that these can only be numerical values.
+ * @property {string} vertexShader Vertex shader source, mandatory.
+ * @property {string} fragmentShader Fragment shader source, mandatory.
+ * @property {string} [hitVertexShader] Vertex shader source for hit detection rendering.
+ * @property {string} [hitFragmentShader] Fragment shader source for hit detection rendering.
+ * @property {Object<string,import("../../webgl/Helper").UniformValue>} [uniforms] Uniform definitions for the post process steps
+ * Please note that `u_texture` is reserved for the main texture slot and `u_opacity` is reserved for the layer opacity.
+ * @property {Array<import("./Layer").PostProcessesOptions>} [postProcesses] Post-processes definitions
+ */
+/**
+ * @classdesc
+ * WebGL vector renderer optimized for points.
+ * All features will be rendered as quads (two triangles forming a square). New data will be flushed to the GPU
+ * every time the vector source changes.
+ *
+ * You need to provide vertex and fragment shaders for rendering. This can be done using
+ * {@link module:ol/webgl/ShaderBuilder} utilities. These shaders shall expect a `a_position` attribute
+ * containing the screen-space projected center of the quad, as well as a `a_index` attribute
+ * whose value (0, 1, 2 or 3) indicates which quad vertex is currently getting processed (see structure below).
+ *
+ * To include variable attributes in the shaders, you need to declare them using the `attributes` property of
+ * the options object like so:
+ * ```js
+ * new WebGLPointsLayerRenderer(layer, {
+ *   attributes: [
+ *     {
+ *       name: 'size',
+ *       callback: function(feature) {
+ *         // compute something with the feature
+ *       }
+ *     },
+ *     {
+ *       name: 'weight',
+ *       callback: function(feature) {
+ *         // compute something with the feature
+ *       }
+ *     },
+ *   ],
+ *   vertexShader:
+ *     // shader using attribute a_weight and a_size
+ *   fragmentShader:
+ *     // shader using varying v_weight and v_size
+ * ```
+ *
+ * To enable hit detection, you must as well provide dedicated shaders using the `hitVertexShader`
+ * and `hitFragmentShader` properties. These shall expect the `a_hitColor` attribute to contain
+ * the final color that will have to be output for hit detection to work.
+ *
+ * The following uniform is used for the main texture: `u_texture`.
+ * The following uniform is used for the layer opacity: `u_opacity`.
+ *
+ * Please note that the main shader output should have premultiplied alpha, otherwise visual anomalies may occur.
+ *
+ * Points are rendered as quads with the following structure:
+ *
+ * ```
+ *   (u0, v1)      (u1, v1)
+ *  [3]----------[2]
+ *   |`           |
+ *   |  `         |
+ *   |    `       |
+ *   |      `     |
+ *   |        `   |
+ *   |          ` |
+ *  [0]----------[1]
+ *   (u0, v0)      (u1, v0)
+ *  ```
+ *
+ * This uses {@link module:ol/webgl/Helper~WebGLHelper} internally.
+ *
+ * @api
+ */
+declare class WebGLPointsLayerRenderer extends WebGLLayerRenderer<any> {
+    /**
+     * @param {import("../../layer/Layer.js").default} layer Layer.
+     * @param {Options} options Options.
+     */
+    constructor(layer: import("../../layer/Layer.js").default<any, any>, options: Options);
+    sourceRevision_: number;
+    verticesBuffer_: WebGLArrayBuffer;
+    hitVerticesBuffer_: WebGLArrayBuffer;
+    indicesBuffer_: WebGLArrayBuffer;
+    /**
+     * @private
+     */
+    private vertexShader_;
+    /**
+     * @private
+     */
+    private fragmentShader_;
+    /**
+     * @type {WebGLProgram}
+     * @private
+     */
+    private program_;
+    /**
+     * @type {boolean}
+     * @private
+     */
+    private hitDetectionEnabled_;
+    /**
+     * @private
+     */
+    private hitVertexShader_;
+    /**
+     * @private
+     */
+    private hitFragmentShader_;
+    /**
+     * @type {WebGLProgram}
+     * @private
+     */
+    private hitProgram_;
+    /**
+     * A list of attributes used by the renderer. By default only the position and
+     * index of the vertex (0 to 3) are required.
+     * @type {Array<import('../../webgl/Helper.js').AttributeDescription>}
+     */
+    attributes: Array<import('../../webgl/Helper.js').AttributeDescription>;
+    /**
+     * A list of attributes used for hit detection.
+     * @type {Array<import('../../webgl/Helper.js').AttributeDescription>}
+     */
+    hitDetectionAttributes: Array<import('../../webgl/Helper.js').AttributeDescription>;
+    customAttributes: CustomAttribute[];
+    previousExtent_: number[];
+    /**
+     * This transform is updated on every frame and is the composition of:
+     * - invert of the world->screen transform that was used when rebuilding buffers (see `this.renderTransform_`)
+     * - current world->screen transform
+     * @type {import("../../transform.js").Transform}
+     * @private
+     */
+    private currentTransform_;
+    /**
+     * This transform is updated when buffers are rebuilt and converts world space coordinates to screen space
+     * @type {import("../../transform.js").Transform}
+     * @private
+     */
+    private renderTransform_;
+    /**
+     * @type {import("../../transform.js").Transform}
+     * @private
+     */
+    private invertRenderTransform_;
+    /**
+     * @type {Float32Array}
+     * @private
+     */
+    private renderInstructions_;
+    /**
+     * These instructions are used for hit detection
+     * @type {Float32Array}
+     * @private
+     */
+    private hitRenderInstructions_;
+    /**
+     * @type {WebGLRenderTarget}
+     * @private
+     */
+    private hitRenderTarget_;
+    worker_: Worker;
+    /**
+     * This object will be updated when the source changes. Key is uid.
+     * @type {Object<string, FeatureCacheItem>}
+     * @private
+     */
+    private featureCache_;
+    /**
+     * Amount of features in the cache.
+     * @type {number}
+     * @private
+     */
+    private featureCount_;
+    sourceListenKeys_: import("../../events.js").EventsKey[];
+    /**
+     * @param {import("../../source/Vector.js").VectorSourceEvent} event Event.
+     * @private
+     */
+    private handleSourceFeatureAdded_;
+    /**
+     * @param {import("../../source/Vector.js").VectorSourceEvent} event Event.
+     * @private
+     */
+    private handleSourceFeatureChanged_;
+    /**
+     * @param {import("../../source/Vector.js").VectorSourceEvent} event Event.
+     * @private
+     */
+    private handleSourceFeatureDelete_;
+    /**
+     * @private
+     */
+    private handleSourceFeatureClear_;
+    /**
+     * Render the layer.
+     * @param {import("../../PluggableMap.js").FrameState} frameState Frame state.
+     * @return {HTMLElement} The rendered element.
+     */
+    renderFrame(frameState: import("../../PluggableMap.js").FrameState): HTMLElement;
+    /**
+     * Rebuild internal webgl buffers based on current view extent; costly, should not be called too much
+     * @param {import("../../PluggableMap").FrameState} frameState Frame state.
+     * @private
+     */
+    private rebuildBuffers_;
+    /**
+     * Render the hit detection data to the corresponding render target
+     * @param {import("../../PluggableMap.js").FrameState} frameState current frame state
+     */
+    renderHitDetection(frameState: import("../../PluggableMap.js").FrameState): void;
+}
+import WebGLLayerRenderer from "./Layer.js";
+import WebGLArrayBuffer from "../../webgl/Buffer.js";
+//# sourceMappingURL=PointsLayer.d.ts.map
---
+++ ajax/libs/openlayers/6.12.1-dev.1642220001045/source/GeoTIFF.d.ts
@@ -0,0 +1,316 @@
+export default GeoTIFFSource;
+export type SourceInfo = {
+    /**
+     * URL for the source GeoTIFF.
+     */
+    url: string;
+    /**
+     * List of any overview URLs.
+     */
+    overviews?: string[];
+    /**
+     * The minimum source data value.  Rendered values are scaled from 0 to 1 based on
+     * the configured min and max.  If not provided and raster statistics are available, those will be used instead.
+     * If neither are available, the minimum for the data type will be used.  To disable this behavior, set
+     * the `normalize` option to `false` in the constructor.
+     */
+    min?: number;
+    /**
+     * The maximum source data value.  Rendered values are scaled from 0 to 1 based on
+     * the configured min and max.  If not provided and raster statistics are available, those will be used instead.
+     * If neither are available, the maximum for the data type will be used.  To disable this behavior, set
+     * the `normalize` option to `false` in the constructor.
+     */
+    max?: number;
+    /**
+     * Values to discard (overriding any nodata values in the metadata).
+     * When provided, an additional alpha band will be added to the data.  Often the GeoTIFF metadata
+     * will include information about nodata values, so you should only need to set this property if
+     * you find that it is not already extracted from the metadata.
+     */
+    nodata?: number;
+    /**
+     * Band numbers to be read from (where the first band is `1`). If not provided, all bands will
+     * be read. For example, if a GeoTIFF has blue (1), green (2), red (3), and near-infrared (4) bands, and you only need the
+     * near-infrared band, configure `bands: [4]`.
+     */
+    bands?: number[];
+};
+export type GeoKeys = {
+    /**
+     * Model type.
+     */
+    GTModelTypeGeoKey: number;
+    /**
+     * Raster type.
+     */
+    GTRasterTypeGeoKey: number;
+    /**
+     * Angular units.
+     */
+    GeogAngularUnitsGeoKey: number;
+    /**
+     * Inverse flattening.
+     */
+    GeogInvFlatteningGeoKey: number;
+    /**
+     * Semi-major axis.
+     */
+    GeogSemiMajorAxisGeoKey: number;
+    /**
+     * Geographic coordinate system code.
+     */
+    GeographicTypeGeoKey: number;
+    /**
+     * Projected linear unit code.
+     */
+    ProjLinearUnitsGeoKey: number;
+    /**
+     * Projected coordinate system code.
+     */
+    ProjectedCSTypeGeoKey: number;
+};
+export type GeoTIFF = {
+    /**
+     * Get the number of internal subfile images.
+     */
+    getImageCount: () => Promise<number>;
+    /**
+     * Get the image at the specified index.
+     */
+    getImage: (arg0: number) => Promise<GeoTIFFImage>;
+};
+export type MultiGeoTIFF = {
+    /**
+     * Get the number of internal subfile images.
+     */
+    getImageCount: () => Promise<number>;
+    /**
+     * Get the image at the specified index.
+     */
+    getImage: (arg0: number) => Promise<GeoTIFFImage>;
+};
+export type GDALMetadata = {
+    /**
+     * The minimum value (as a string).
+     */
+    STATISTICS_MINIMUM: string;
+    /**
+     * The maximum value (as a string).
+     */
+    STATISTICS_MAXIMUM: string;
+};
+export type GeoTIFFImage = {
+    /**
+     * The file directory.
+     */
+    fileDirectory: any;
+    /**
+     * The parsed geo-keys.
+     */
+    geoKeys: GeoKeys;
+    /**
+     * Uses little endian byte order.
+     */
+    littleEndian: boolean;
+    /**
+     * The tile cache.
+     */
+    tiles: any;
+    /**
+     * The image is tiled.
+     */
+    isTiled: boolean;
+    /**
+     * Get the image bounding box.
+     */
+    getBoundingBox: () => number[];
+    /**
+     * Get the image origin.
+     */
+    getOrigin: () => number[];
+    /**
+     * Get the image resolution.
+     */
+    getResolution: (arg0: GeoTIFFImage) => number[];
+    /**
+     * Get the pixel width of the image.
+     */
+    getWidth: () => number;
+    /**
+     * Get the pixel height of the image.
+     */
+    getHeight: () => number;
+    /**
+     * Get the pixel width of image tiles.
+     */
+    getTileWidth: () => number;
+    /**
+     * Get the pixel height of image tiles.
+     */
+    getTileHeight: () => number;
+    /**
+     * Get the nodata value (or null if none).
+     */
+    getGDALNoData: () => number | null;
+    /**
+     * Get the raster stats (or null if none).
+     */
+    getGDALMetadata: () => GDALMetadata | null;
+    /**
+     * Get the number of samples per pixel.
+     */
+    getSamplesPerPixel: () => number;
+};
+export type Options = {
+    /**
+     * List of information about GeoTIFF sources.
+     * Multiple sources can be combined when their resolution sets are equal after applying a scale.
+     * The list of sources defines a mapping between input bands as they are read from each GeoTIFF and
+     * the output bands that are provided by data tiles. To control which bands to read from each GeoTIFF,
+     * use the {@link import("./GeoTIFF.js").SourceInfo bands} property. If, for example, you specify two
+     * sources, one with 3 bands and {@link import("./GeoTIFF.js").SourceInfo nodata} configured, and
+     * another with 1 band, the resulting data tiles will have 5 bands: 3 from the first source, 1 alpha
+     * band from the first source, and 1 band from the second source.
+     */
+    sources: SourceInfo[];
+    /**
+     * By default, bands from the sources are read as-is. When
+     * reading GeoTIFFs with the purpose of displaying them as RGB images, setting this to `true` will
+     * convert other color spaces (YCbCr, CMYK) to RGB.
+     */
+    convertToRGB?: boolean;
+    /**
+     * By default, the source data is normalized to values between
+     * 0 and 1 with scaling factors based on the raster statistics or `min` and `max` properties of each source.
+     * If instead you want to work with the raw values in a style expression, set this to `false`.  Setting this option
+     * to `false` will make it so any `min` and `max` properties on sources are ignored.
+     */
+    normalize?: boolean;
+    /**
+     * Whether the layer is opaque.
+     */
+    opaque?: boolean;
+    /**
+     * Duration of the opacity transition for rendering.
+     * To disable the opacity transition, pass `transition: 0`.
+     */
+    transition?: number;
+    /**
+     * Render tiles beyond the tile grid extent.
+     */
+    wrapX?: boolean;
+    /**
+     * Use interpolated values when resampling.  By default,
+     * the linear interpolation is used to resample the data.  If false, nearest neighbor is used.
+     */
+    interpolate?: boolean;
+};
+/**
+ * @typedef {Object} Options
+ * @property {Array<SourceInfo>} sources List of information about GeoTIFF sources.
+ * Multiple sources can be combined when their resolution sets are equal after applying a scale.
+ * The list of sources defines a mapping between input bands as they are read from each GeoTIFF and
+ * the output bands that are provided by data tiles. To control which bands to read from each GeoTIFF,
+ * use the {@link import("./GeoTIFF.js").SourceInfo bands} property. If, for example, you specify two
+ * sources, one with 3 bands and {@link import("./GeoTIFF.js").SourceInfo nodata} configured, and
+ * another with 1 band, the resulting data tiles will have 5 bands: 3 from the first source, 1 alpha
+ * band from the first source, and 1 band from the second source.
+ * @property {boolean} [convertToRGB = false] By default, bands from the sources are read as-is. When
+ * reading GeoTIFFs with the purpose of displaying them as RGB images, setting this to `true` will
+ * convert other color spaces (YCbCr, CMYK) to RGB.
+ * @property {boolean} [normalize=true] By default, the source data is normalized to values between
+ * 0 and 1 with scaling factors based on the raster statistics or `min` and `max` properties of each source.
+ * If instead you want to work with the raw values in a style expression, set this to `false`.  Setting this option
+ * to `false` will make it so any `min` and `max` properties on sources are ignored.
+ * @property {boolean} [opaque=false] Whether the layer is opaque.
+ * @property {number} [transition=250] Duration of the opacity transition for rendering.
+ * To disable the opacity transition, pass `transition: 0`.
+ * @property {boolean} [wrapX=false] Render tiles beyond the tile grid extent.
+ * @property {boolean} [interpolate=true] Use interpolated values when resampling.  By default,
+ * the linear interpolation is used to resample the data.  If false, nearest neighbor is used.
+ */
+/**
+ * @classdesc
+ * A source for working with GeoTIFF data.
+ * @api
+ */
+declare class GeoTIFFSource extends DataTile {
+    /**
+     * @param {Options} options Data tile options.
+     */
+    constructor(options: Options);
+    /**
+     * @type {Array<SourceInfo>}
+     * @private
+     */
+    private sourceInfo_;
+    /**
+     * @type {Array<Array<GeoTIFFImage>>}
+     * @private
+     */
+    private sourceImagery_;
+    /**
+     * @type {Array<number>}
+     * @private
+     */
+    private resolutionFactors_;
+    /**
+     * @type {Array<number>}
+     * @private
+     */
+    private samplesPerPixel_;
+    /**
+     * @type {Array<Array<number>>}
+     * @private
+     */
+    private nodataValues_;
+    /**
+     * @type {Array<Array<GDALMetadata>>}
+     * @private
+     */
+    private metadata_;
+    /**
+     * @type {boolean}
+     * @private
+     */
+    private normalize_;
+    /**
+     * @type {boolean}
+     * @private
+     */
+    private addAlpha_;
+    /**
+     * @type {Error}
+     * @private
+     */
+    private error_;
+    /**
+     * @type {'readRasters' | 'readRGB'}
+     */
+    readMethod_: 'readRasters' | 'readRGB';
+    /**
+     * @return {Error} A source loading error. When the source state is `error`, use this function
+     * to get more information about the error. To debug a faulty configuration, you may want to use
+     * a listener like
+     * ```js
+     * geotiffSource.on('change', () => {
+     *   if (geotiffSource.getState() === 'error') {
+     *     console.error(geotiffSource.getError());
+     *   }
+     * });
+     * ```
+     */
+    getError(): Error;
+    /**
+     * Configure the tile grid based on images within the source GeoTIFFs.  Each GeoTIFF
+     * must have the same internal tiled structure.
+     * @param {Array<Array<GeoTIFFImage>>} sources Each source is a list of images
+     * from a single GeoTIFF.
+     * @private
+     */
+    private configure_;
+    loadTile_(z: any, x: any, y: any): Promise<Uint8Array | Float32Array>;
+}
+import DataTile from "./DataTile.js";
+//# sourceMappingURL=GeoTIFF.d.ts.map
