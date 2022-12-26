18406772836fbd2542ae79bb61f6a086c2d9a1e9
robocdnjs <cdnjs-github@cloudflare.com>
2022-12-26 00:13:18 +0000

    Add tsparticles-particles.js (2.7.1)

---
+++ ajax/libs/tsparticles-particles.js/2.7.1/tsparticles.pjs.js
@@ -0,0 +1,85 @@
+/*!
+ * Author : Matteo Bruni
+ * MIT license: https://opensource.org/licenses/MIT
+ * Demo / Generator : https://particles.js.org/
+ * GitHub : https://www.github.com/matteobruni/tsparticles
+ * How to use? : Check the GitHub README
+ * v2.7.1
+ */
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define([], factory);
+	else {
+		var a = factory();
+		for(var i in a) (typeof exports === 'object' ? exports : root)[i] = a[i];
+	}
+})(this, () => {
+return /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	// The require scope
+/******/ 	var __webpack_require__ = {};
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "initPjs": () => (/* binding */ initPjs)
+/* harmony export */ });
+const initPjs = engine => {
+  const particlesJS = (tagId, options) => {
+    return engine.load(tagId, options);
+  };
+  particlesJS.load = (tagId, pathConfigJson, callback) => {
+    engine.loadJSON(tagId, pathConfigJson).then(container => {
+      if (container) {
+        callback(container);
+      }
+    }).catch(() => {
+      callback(undefined);
+    });
+  };
+  particlesJS.setOnClickHandler = callback => {
+    engine.setOnClickHandler(callback);
+  };
+  const pJSDom = engine.dom();
+  return {
+    particlesJS,
+    pJSDom
+  };
+};
+
+/******/ 	return __webpack_exports__;
+/******/ })()
+;
+});
