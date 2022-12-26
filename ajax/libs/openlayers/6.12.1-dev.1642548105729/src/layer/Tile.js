83884ef8057cc0dc180eacdd1356a4ee1f6d9b8e
robocdnjs <cdnjs-github@cloudflare.com>
2022-12-26 06:07:07 +0000

    Add openlayers (6.12.1-dev.1642548105729)

---
+++ ajax/libs/openlayers/6.12.1-dev.1642548105729/src/layer/Tile.js
@@ -0,0 +1,32 @@
+/**
+ * @module ol/layer/Tile
+ */
+import BaseTileLayer from './BaseTile.js';
+import CanvasTileLayerRenderer from '../renderer/canvas/TileLayer.js';
+
+/**
+ * @classdesc
+ * For layer sources that provide pre-rendered, tiled images in grids that are
+ * organized by zoom levels for specific resolutions.
+ * Note that any property set in the options is set as a {@link module:ol/Object~BaseObject}
+ * property on the layer object; for example, setting `title: 'My Title'` in the
+ * options means that `title` is observable, and has get/set accessors.
+ *
+ * @template {import("../source/Tile.js").default} TileSourceType
+ * @extends BaseTileLayer<TileSourceType, CanvasTileLayerRenderer>
+ * @api
+ */
+class TileLayer extends BaseTileLayer {
+  /**
+   * @param {import("./BaseTile.js").Options<TileSourceType>} [opt_options] Tile layer options.
+   */
+  constructor(opt_options) {
+    super(opt_options);
+  }
+
+  createRenderer() {
+    return new CanvasTileLayerRenderer(this);
+  }
+}
+
+export default TileLayer;
