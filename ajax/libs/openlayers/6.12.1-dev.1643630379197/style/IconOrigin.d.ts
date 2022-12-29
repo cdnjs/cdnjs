bec6da94b6b4632b44ca7a372d1f18ea26bb128c
robocdnjs <cdnjs-github@cloudflare.com>
2022-12-29 08:05:04 +0000

    Add openlayers (6.12.1-dev.1643630379197)

---
+++ ajax/libs/openlayers/6.12.1-dev.1643630379197/style/IconOrigin.d.ts
@@ -0,0 +1,8 @@
+declare namespace _default {
+    const BOTTOM_LEFT: string;
+    const BOTTOM_RIGHT: string;
+    const TOP_LEFT: string;
+    const TOP_RIGHT: string;
+}
+export default _default;
+//# sourceMappingURL=IconOrigin.d.ts.map
