7c9be5cbed27c520e04463f4b6ea337d6457a873
robocdnjs <cdnjs-github@cloudflare.com>
2022-12-30 01:32:40 +0000

    Add openlayers (6.12.1-dev.1643631201176)

---
+++ ajax/libs/openlayers/6.12.1-dev.1643631201176/proj/Projection.d.ts
@@ -0,0 +1,245 @@
+export default Projection;
+export type Options = {
+    /**
+     * The SRS identifier code, e.g. `EPSG:4326`.
+     */
+    code: string;
+    /**
+     * Units. Required unless a
+     * proj4 projection is defined for `code`.
+     */
+    units?: string | undefined;
+    /**
+     * The validity extent for the SRS.
+     */
+    extent?: import("../extent.js").Extent | undefined;
+    /**
+     * The axis orientation as specified in Proj4.
+     */
+    axisOrientation?: string | undefined;
+    /**
+     * Whether the projection is valid for the whole globe.
+     */
+    global?: boolean | undefined;
+    /**
+     * The meters per unit for the SRS.
+     * If not provided, the `units` are used to get the meters per unit from the {@link module :ol/proj/Units~METERS_PER_UNIT}lookup table.
+     */
+    metersPerUnit?: number | undefined;
+    /**
+     * The world extent for the SRS.
+     */
+    worldExtent?: import("../extent.js").Extent | undefined;
+    /**
+     * Function to determine resolution at a point. The function is called with a
+     * `number` view resolution and a {@link module :ol/coordinate~Coordinate Coordinate} as arguments, and returns
+     * the `number` resolution in projection units at the passed coordinate. If this is `undefined`,
+     * the default {@link module :ol/proj.getPointResolution getPointResolution()} function will be used.
+     */
+    getPointResolution?: ((arg0: number, arg1: import("../coordinate.js").Coordinate) => number) | undefined;
+};
+/**
+ * @typedef {Object} Options
+ * @property {string} code The SRS identifier code, e.g. `EPSG:4326`.
+ * @property {import("./Units.js").default|string} [units] Units. Required unless a
+ * proj4 projection is defined for `code`.
+ * @property {import("../extent.js").Extent} [extent] The validity extent for the SRS.
+ * @property {string} [axisOrientation='enu'] The axis orientation as specified in Proj4.
+ * @property {boolean} [global=false] Whether the projection is valid for the whole globe.
+ * @property {number} [metersPerUnit] The meters per unit for the SRS.
+ * If not provided, the `units` are used to get the meters per unit from the {@link module:ol/proj/Units~METERS_PER_UNIT}
+ * lookup table.
+ * @property {import("../extent.js").Extent} [worldExtent] The world extent for the SRS.
+ * @property {function(number, import("../coordinate.js").Coordinate):number} [getPointResolution]
+ * Function to determine resolution at a point. The function is called with a
+ * `number` view resolution and a {@link module:ol/coordinate~Coordinate Coordinate} as arguments, and returns
+ * the `number` resolution in projection units at the passed coordinate. If this is `undefined`,
+ * the default {@link module:ol/proj.getPointResolution getPointResolution()} function will be used.
+ */
+/**
+ * @classdesc
+ * Projection definition class. One of these is created for each projection
+ * supported in the application and stored in the {@link module:ol/proj} namespace.
+ * You can use these in applications, but this is not required, as API params
+ * and options use {@link module:ol/proj~ProjectionLike} which means the simple string
+ * code will suffice.
+ *
+ * You can use {@link module:ol/proj.get} to retrieve the object for a particular
+ * projection.
+ *
+ * The library includes definitions for `EPSG:4326` and `EPSG:3857`, together
+ * with the following aliases:
+ * * `EPSG:4326`: CRS:84, urn:ogc:def:crs:EPSG:6.6:4326,
+ *     urn:ogc:def:crs:OGC:1.3:CRS84, urn:ogc:def:crs:OGC:2:84,
+ *     http://www.opengis.net/gml/srs/epsg.xml#4326,
+ *     urn:x-ogc:def:crs:EPSG:4326
+ * * `EPSG:3857`: EPSG:102100, EPSG:102113, EPSG:900913,
+ *     urn:ogc:def:crs:EPSG:6.18:3:3857,
+ *     http://www.opengis.net/gml/srs/epsg.xml#3857
+ *
+ * If you use [proj4js](https://github.com/proj4js/proj4js), aliases can
+ * be added using `proj4.defs()`. After all required projection definitions are
+ * added, call the {@link module:ol/proj/proj4.register} function.
+ *
+ * @api
+ */
+declare class Projection {
+    /**
+     * @param {Options} options Projection options.
+     */
+    constructor(options: Options);
+    /**
+     * @private
+     * @type {string}
+     */
+    private code_;
+    /**
+     * Units of projected coordinates. When set to `TILE_PIXELS`, a
+     * `this.extent_` and `this.worldExtent_` must be configured properly for each
+     * tile.
+     * @private
+     * @type {import("./Units.js").default}
+     */
+    private units_;
+    /**
+     * Validity extent of the projection in projected coordinates. For projections
+     * with `TILE_PIXELS` units, this is the extent of the tile in
+     * tile pixel space.
+     * @private
+     * @type {import("../extent.js").Extent}
+     */
+    private extent_;
+    /**
+     * Extent of the world in EPSG:4326. For projections with
+     * `TILE_PIXELS` units, this is the extent of the tile in
+     * projected coordinate space.
+     * @private
+     * @type {import("../extent.js").Extent}
+     */
+    private worldExtent_;
+    /**
+     * @private
+     * @type {string}
+     */
+    private axisOrientation_;
+    /**
+     * @private
+     * @type {boolean}
+     */
+    private global_;
+    /**
+     * @private
+     * @type {boolean}
+     */
+    private canWrapX_;
+    /**
+     * @private
+     * @type {function(number, import("../coordinate.js").Coordinate):number|undefined}
+     */
+    private getPointResolutionFunc_;
+    /**
+     * @private
+     * @type {import("../tilegrid/TileGrid.js").default}
+     */
+    private defaultTileGrid_;
+    /**
+     * @private
+     * @type {number|undefined}
+     */
+    private metersPerUnit_;
+    /**
+     * @return {boolean} The projection is suitable for wrapping the x-axis
+     */
+    canWrapX(): boolean;
+    /**
+     * Get the code for this projection, e.g. 'EPSG:4326'.
+     * @return {string} Code.
+     * @api
+     */
+    getCode(): string;
+    /**
+     * Get the validity extent for this projection.
+     * @return {import("../extent.js").Extent} Extent.
+     * @api
+     */
+    getExtent(): import("../extent.js").Extent;
+    /**
+     * Get the units of this projection.
+     * @return {import("./Units.js").default} Units.
+     * @api
+     */
+    getUnits(): import("./Units.js").default;
+    /**
+     * Get the amount of meters per unit of this projection.  If the projection is
+     * not configured with `metersPerUnit` or a units identifier, the return is
+     * `undefined`.
+     * @return {number|undefined} Meters.
+     * @api
+     */
+    getMetersPerUnit(): number | undefined;
+    /**
+     * Get the world extent for this projection.
+     * @return {import("../extent.js").Extent} Extent.
+     * @api
+     */
+    getWorldExtent(): import("../extent.js").Extent;
+    /**
+     * Get the axis orientation of this projection.
+     * Example values are:
+     * enu - the default easting, northing, elevation.
+     * neu - northing, easting, up - useful for "lat/long" geographic coordinates,
+     *     or south orientated transverse mercator.
+     * wnu - westing, northing, up - some planetary coordinate systems have
+     *     "west positive" coordinate systems
+     * @return {string} Axis orientation.
+     * @api
+     */
+    getAxisOrientation(): string;
+    /**
+     * Is this projection a global projection which spans the whole world?
+     * @return {boolean} Whether the projection is global.
+     * @api
+     */
+    isGlobal(): boolean;
+    /**
+     * Set if the projection is a global projection which spans the whole world
+     * @param {boolean} global Whether the projection is global.
+     * @api
+     */
+    setGlobal(global: boolean): void;
+    /**
+     * @return {import("../tilegrid/TileGrid.js").default} The default tile grid.
+     */
+    getDefaultTileGrid(): import("../tilegrid/TileGrid.js").default;
+    /**
+     * @param {import("../tilegrid/TileGrid.js").default} tileGrid The default tile grid.
+     */
+    setDefaultTileGrid(tileGrid: import("../tilegrid/TileGrid.js").default): void;
+    /**
+     * Set the validity extent for this projection.
+     * @param {import("../extent.js").Extent} extent Extent.
+     * @api
+     */
+    setExtent(extent: import("../extent.js").Extent): void;
+    /**
+     * Set the world extent for this projection.
+     * @param {import("../extent.js").Extent} worldExtent World extent
+     *     [minlon, minlat, maxlon, maxlat].
+     * @api
+     */
+    setWorldExtent(worldExtent: import("../extent.js").Extent): void;
+    /**
+     * Set the getPointResolution function (see {@link module:ol/proj.getPointResolution}
+     * for this projection.
+     * @param {function(number, import("../coordinate.js").Coordinate):number} func Function
+     * @api
+     */
+    setGetPointResolution(func: (arg0: number, arg1: import("../coordinate.js").Coordinate) => number): void;
+    /**
+     * Get the custom point resolution function for this projection (if set).
+     * @return {function(number, import("../coordinate.js").Coordinate):number|undefined} The custom point
+     * resolution function (if set).
+     */
+    getPointResolutionFunc(): (arg0: number, arg1: import("../coordinate.js").Coordinate) => number | undefined;
+}
+//# sourceMappingURL=Projection.d.ts.map
---
+++ ajax/libs/openlayers/6.12.1-dev.1643631201176/src/geom/SimpleGeometry.js
@@ -0,0 +1,355 @@
+/**
+ * @module ol/geom/SimpleGeometry
+ */
+import Geometry from './Geometry.js';
+import GeometryLayout from './GeometryLayout.js';
+import {abstract} from '../util.js';
+import {createOrUpdateFromFlatCoordinates, getCenter} from '../extent.js';
+import {rotate, scale, transform2D, translate} from './flat/transform.js';
+
+/**
+ * @classdesc
+ * Abstract base class; only used for creating subclasses; do not instantiate
+ * in apps, as cannot be rendered.
+ *
+ * @abstract
+ * @api
+ */
+class SimpleGeometry extends Geometry {
+  constructor() {
+    super();
+
+    /**
+     * @protected
+     * @type {import("./GeometryLayout.js").default}
+     */
+    this.layout = GeometryLayout.XY;
+
+    /**
+     * @protected
+     * @type {number}
+     */
+    this.stride = 2;
+
+    /**
+     * @protected
+     * @type {Array<number>}
+     */
+    this.flatCoordinates = null;
+  }
+
+  /**
+   * @param {import("../extent.js").Extent} extent Extent.
+   * @protected
+   * @return {import("../extent.js").Extent} extent Extent.
+   */
+  computeExtent(extent) {
+    return createOrUpdateFromFlatCoordinates(
+      this.flatCoordinates,
+      0,
+      this.flatCoordinates.length,
+      this.stride,
+      extent
+    );
+  }
+
+  /**
+   * @abstract
+   * @return {Array<*>} Coordinates.
+   */
+  getCoordinates() {
+    return abstract();
+  }
+
+  /**
+   * Return the first coordinate of the geometry.
+   * @return {import("../coordinate.js").Coordinate} First coordinate.
+   * @api
+   */
+  getFirstCoordinate() {
+    return this.flatCoordinates.slice(0, this.stride);
+  }
+
+  /**
+   * @return {Array<number>} Flat coordinates.
+   */
+  getFlatCoordinates() {
+    return this.flatCoordinates;
+  }
+
+  /**
+   * Return the last coordinate of the geometry.
+   * @return {import("../coordinate.js").Coordinate} Last point.
+   * @api
+   */
+  getLastCoordinate() {
+    return this.flatCoordinates.slice(
+      this.flatCoordinates.length - this.stride
+    );
+  }
+
+  /**
+   * Return the {@link module:ol/geom/GeometryLayout layout} of the geometry.
+   * @return {import("./GeometryLayout.js").default} Layout.
+   * @api
+   */
+  getLayout() {
+    return this.layout;
+  }
+
+  /**
+   * Create a simplified version of this geometry using the Douglas Peucker algorithm.
+   * @param {number} squaredTolerance Squared tolerance.
+   * @return {SimpleGeometry} Simplified geometry.
+   */
+  getSimplifiedGeometry(squaredTolerance) {
+    if (this.simplifiedGeometryRevision !== this.getRevision()) {
+      this.simplifiedGeometryMaxMinSquaredTolerance = 0;
+      this.simplifiedGeometryRevision = this.getRevision();
+    }
+    // If squaredTolerance is negative or if we know that simplification will not
+    // have any effect then just return this.
+    if (
+      squaredTolerance < 0 ||
+      (this.simplifiedGeometryMaxMinSquaredTolerance !== 0 &&
+        squaredTolerance <= this.simplifiedGeometryMaxMinSquaredTolerance)
+    ) {
+      return this;
+    }
+
+    const simplifiedGeometry =
+      this.getSimplifiedGeometryInternal(squaredTolerance);
+    const simplifiedFlatCoordinates = simplifiedGeometry.getFlatCoordinates();
+    if (simplifiedFlatCoordinates.length < this.flatCoordinates.length) {
+      return simplifiedGeometry;
+    } else {
+      // Simplification did not actually remove any coordinates.  We now know
+      // that any calls to getSimplifiedGeometry with a squaredTolerance less
+      // than or equal to the current squaredTolerance will also not have any
+      // effect.  This allows us to short circuit simplification (saving CPU
+      // cycles) and prevents the cache of simplified geometries from filling
+      // up with useless identical copies of this geometry (saving memory).
+      this.simplifiedGeometryMaxMinSquaredTolerance = squaredTolerance;
+      return this;
+    }
+  }
+
+  /**
+   * @param {number} squaredTolerance Squared tolerance.
+   * @return {SimpleGeometry} Simplified geometry.
+   * @protected
+   */
+  getSimplifiedGeometryInternal(squaredTolerance) {
+    return this;
+  }
+
+  /**
+   * @return {number} Stride.
+   */
+  getStride() {
+    return this.stride;
+  }
+
+  /**
+   * @param {import("./GeometryLayout.js").default} layout Layout.
+   * @param {Array<number>} flatCoordinates Flat coordinates.
+   */
+  setFlatCoordinates(layout, flatCoordinates) {
+    this.stride = getStrideForLayout(layout);
+    this.layout = layout;
+    this.flatCoordinates = flatCoordinates;
+  }
+
+  /**
+   * @abstract
+   * @param {!Array<*>} coordinates Coordinates.
+   * @param {import("./GeometryLayout.js").default} [opt_layout] Layout.
+   */
+  setCoordinates(coordinates, opt_layout) {
+    abstract();
+  }
+
+  /**
+   * @param {import("./GeometryLayout.js").default|undefined} layout Layout.
+   * @param {Array<*>} coordinates Coordinates.
+   * @param {number} nesting Nesting.
+   * @protected
+   */
+  setLayout(layout, coordinates, nesting) {
+    /** @type {number} */
+    let stride;
+    if (layout) {
+      stride = getStrideForLayout(layout);
+    } else {
+      for (let i = 0; i < nesting; ++i) {
+        if (coordinates.length === 0) {
+          this.layout = GeometryLayout.XY;
+          this.stride = 2;
+          return;
+        } else {
+          coordinates = /** @type {Array} */ (coordinates[0]);
+        }
+      }
+      stride = coordinates.length;
+      layout = getLayoutForStride(stride);
+    }
+    this.layout = layout;
+    this.stride = stride;
+  }
+
+  /**
+   * Apply a transform function to the coordinates of the geometry.
+   * The geometry is modified in place.
+   * If you do not want the geometry modified in place, first `clone()` it and
+   * then use this function on the clone.
+   * @param {import("../proj.js").TransformFunction} transformFn Transform function.
+   * Called with a flat array of geometry coordinates.
+   * @api
+   */
+  applyTransform(transformFn) {
+    if (this.flatCoordinates) {
+      transformFn(this.flatCoordinates, this.flatCoordinates, this.stride);
+      this.changed();
+    }
+  }
+
+  /**
+   * Rotate the geometry around a given coordinate. This modifies the geometry
+   * coordinates in place.
+   * @param {number} angle Rotation angle in counter-clockwise radians.
+   * @param {import("../coordinate.js").Coordinate} anchor The rotation center.
+   * @api
+   */
+  rotate(angle, anchor) {
+    const flatCoordinates = this.getFlatCoordinates();
+    if (flatCoordinates) {
+      const stride = this.getStride();
+      rotate(
+        flatCoordinates,
+        0,
+        flatCoordinates.length,
+        stride,
+        angle,
+        anchor,
+        flatCoordinates
+      );
+      this.changed();
+    }
+  }
+
+  /**
+   * Scale the geometry (with an optional origin).  This modifies the geometry
+   * coordinates in place.
+   * @param {number} sx The scaling factor in the x-direction.
+   * @param {number} [opt_sy] The scaling factor in the y-direction (defaults to sx).
+   * @param {import("../coordinate.js").Coordinate} [opt_anchor] The scale origin (defaults to the center
+   *     of the geometry extent).
+   * @api
+   */
+  scale(sx, opt_sy, opt_anchor) {
+    let sy = opt_sy;
+    if (sy === undefined) {
+      sy = sx;
+    }
+    let anchor = opt_anchor;
+    if (!anchor) {
+      anchor = getCenter(this.getExtent());
+    }
+    const flatCoordinates = this.getFlatCoordinates();
+    if (flatCoordinates) {
+      const stride = this.getStride();
+      scale(
+        flatCoordinates,
+        0,
+        flatCoordinates.length,
+        stride,
+        sx,
+        sy,
+        anchor,
+        flatCoordinates
+      );
+      this.changed();
+    }
+  }
+
+  /**
+   * Translate the geometry.  This modifies the geometry coordinates in place.  If
+   * instead you want a new geometry, first `clone()` this geometry.
+   * @param {number} deltaX Delta X.
+   * @param {number} deltaY Delta Y.
+   * @api
+   */
+  translate(deltaX, deltaY) {
+    const flatCoordinates = this.getFlatCoordinates();
+    if (flatCoordinates) {
+      const stride = this.getStride();
+      translate(
+        flatCoordinates,
+        0,
+        flatCoordinates.length,
+        stride,
+        deltaX,
+        deltaY,
+        flatCoordinates
+      );
+      this.changed();
+    }
+  }
+}
+
+/**
+ * @param {number} stride Stride.
+ * @return {import("./GeometryLayout.js").default} layout Layout.
+ */
+function getLayoutForStride(stride) {
+  let layout;
+  if (stride == 2) {
+    layout = GeometryLayout.XY;
+  } else if (stride == 3) {
+    layout = GeometryLayout.XYZ;
+  } else if (stride == 4) {
+    layout = GeometryLayout.XYZM;
+  }
+  return /** @type {import("./GeometryLayout.js").default} */ (layout);
+}
+
+/**
+ * @param {import("./GeometryLayout.js").default} layout Layout.
+ * @return {number} Stride.
+ */
+export function getStrideForLayout(layout) {
+  let stride;
+  if (layout == GeometryLayout.XY) {
+    stride = 2;
+  } else if (layout == GeometryLayout.XYZ || layout == GeometryLayout.XYM) {
+    stride = 3;
+  } else if (layout == GeometryLayout.XYZM) {
+    stride = 4;
+  }
+  return /** @type {number} */ (stride);
+}
+
+/**
+ * @param {SimpleGeometry} simpleGeometry Simple geometry.
+ * @param {import("../transform.js").Transform} transform Transform.
+ * @param {Array<number>} [opt_dest] Destination.
+ * @return {Array<number>} Transformed flat coordinates.
+ */
+export function transformGeom2D(simpleGeometry, transform, opt_dest) {
+  const flatCoordinates = simpleGeometry.getFlatCoordinates();
+  if (!flatCoordinates) {
+    return null;
+  } else {
+    const stride = simpleGeometry.getStride();
+    return transform2D(
+      flatCoordinates,
+      0,
+      flatCoordinates.length,
+      stride,
+      transform,
+      opt_dest
+    );
+  }
+}
+
+export default SimpleGeometry;
