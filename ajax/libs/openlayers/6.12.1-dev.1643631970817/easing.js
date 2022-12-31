ac24a4e010aed469070182233b610331992e2c56
robocdnjs <cdnjs-github@cloudflare.com>
2022-12-31 08:04:46 +0000

    Add openlayers (6.12.1-dev.1643631970817)

---
+++ ajax/libs/openlayers/6.12.1-dev.1643631970817/easing.js
@@ -0,0 +1,56 @@
+/**
+ * @module ol/easing
+ */
+/**
+ * Start slow and speed up.
+ * @param {number} t Input between 0 and 1.
+ * @return {number} Output between 0 and 1.
+ * @api
+ */
+export function easeIn(t) {
+    return Math.pow(t, 3);
+}
+/**
+ * Start fast and slow down.
+ * @param {number} t Input between 0 and 1.
+ * @return {number} Output between 0 and 1.
+ * @api
+ */
+export function easeOut(t) {
+    return 1 - easeIn(1 - t);
+}
+/**
+ * Start slow, speed up, and then slow down again.
+ * @param {number} t Input between 0 and 1.
+ * @return {number} Output between 0 and 1.
+ * @api
+ */
+export function inAndOut(t) {
+    return 3 * t * t - 2 * t * t * t;
+}
+/**
+ * Maintain a constant speed over time.
+ * @param {number} t Input between 0 and 1.
+ * @return {number} Output between 0 and 1.
+ * @api
+ */
+export function linear(t) {
+    return t;
+}
+/**
+ * Start slow, speed up, and at the very end slow down again.  This has the
+ * same general behavior as {@link module:ol/easing.inAndOut}, but the final
+ * slowdown is delayed.
+ * @param {number} t Input between 0 and 1.
+ * @return {number} Output between 0 and 1.
+ * @api
+ */
+export function upAndDown(t) {
+    if (t < 0.5) {
+        return inAndOut(2 * t);
+    }
+    else {
+        return 1 - inAndOut(2 * (t - 0.5));
+    }
+}
+//# sourceMappingURL=easing.js.map
---
+++ ajax/libs/openlayers/6.12.1-dev.1643631970817/layer/BaseTile.js
@@ -0,0 +1,143 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+/**
+ * @module ol/layer/BaseTile
+ */
+import Layer from './Layer.js';
+import TileProperty from './TileProperty.js';
+import { assign } from '../obj.js';
+/***
+ * @template Return
+ * @typedef {import("../Observable").OnSignature<import("../Observable").EventTypes, import("../events/Event.js").default, Return> &
+ *   import("../Observable").OnSignature<import("./Base").BaseLayerObjectEventTypes|
+ *     'change:source'|'change:preload'|'change:useInterimTilesOnError', import("../Object").ObjectEvent, Return> &
+ *   import("../Observable").OnSignature<import("../render/EventType").LayerRenderEventTypes, import("../render/Event").default, Return> &
+ *   import("../Observable").CombinedOnSignature<import("../Observable").EventTypes|import("./Base").BaseLayerObjectEventTypes|
+ *   'change:source'|'change:preload'|'change:useInterimTilesOnError'|import("../render/EventType").LayerRenderEventTypes, Return>} BaseTileLayerOnSignature
+ */
+/**
+ * @template {import("../source/Tile.js").default} TileSourceType
+ * @typedef {Object} Options
+ * @property {string} [className='ol-layer'] A CSS class name to set to the layer element.
+ * @property {number} [opacity=1] Opacity (0, 1).
+ * @property {boolean} [visible=true] Visibility.
+ * @property {import("../extent.js").Extent} [extent] The bounding extent for layer rendering.  The layer will not be
+ * rendered outside of this extent.
+ * @property {number} [zIndex] The z-index for layer rendering.  At rendering time, the layers
+ * will be ordered, first by Z-index and then by position. When `undefined`, a `zIndex` of 0 is assumed
+ * for layers that are added to the map's `layers` collection, or `Infinity` when the layer's `setMap()`
+ * method was used.
+ * @property {number} [minResolution] The minimum resolution (inclusive) at which this layer will be
+ * visible.
+ * @property {number} [maxResolution] The maximum resolution (exclusive) below which this layer will
+ * be visible.
+ * @property {number} [minZoom] The minimum view zoom level (exclusive) above which this layer will be
+ * visible.
+ * @property {number} [maxZoom] The maximum view zoom level (inclusive) at which this layer will
+ * be visible.
+ * @property {number} [preload=0] Preload. Load low-resolution tiles up to `preload` levels. `0`
+ * means no preloading.
+ * @property {TileSourceType} [source] Source for this layer.
+ * @property {import("../PluggableMap.js").default} [map] Sets the layer as overlay on a map. The map will not manage
+ * this layer in its layers collection, and the layer will be rendered on top. This is useful for
+ * temporary layers. The standard way to add a layer to a map and have it managed by the map is to
+ * use {@link import("../PluggableMap.js").default#addLayer map.addLayer()}.
+ * @property {boolean} [useInterimTilesOnError=true] Use interim tiles on error.
+ * @property {Object<string, *>} [properties] Arbitrary observable properties. Can be accessed with `#get()` and `#set()`.
+ */
+/**
+ * @classdesc
+ * For layer sources that provide pre-rendered, tiled images in grids that are
+ * organized by zoom levels for specific resolutions.
+ * Note that any property set in the options is set as a {@link module:ol/Object~BaseObject}
+ * property on the layer object; for example, setting `title: 'My Title'` in the
+ * options means that `title` is observable, and has get/set accessors.
+ *
+ * @template {import("../source/Tile.js").default} TileSourceType
+ * @template {import("../renderer/Layer.js").default} RendererType
+ * @extends {Layer<TileSourceType, RendererType>}
+ * @api
+ */
+var BaseTileLayer = /** @class */ (function (_super) {
+    __extends(BaseTileLayer, _super);
+    /**
+     * @param {Options<TileSourceType>} [opt_options] Tile layer options.
+     */
+    function BaseTileLayer(opt_options) {
+        var _this = this;
+        var options = opt_options ? opt_options : {};
+        var baseOptions = assign({}, options);
+        delete baseOptions.preload;
+        delete baseOptions.useInterimTilesOnError;
+        _this = _super.call(this, baseOptions) || this;
+        /***
+         * @type {BaseTileLayerOnSignature<import("../events").EventsKey>}
+         */
+        _this.on;
+        /***
+         * @type {BaseTileLayerOnSignature<import("../events").EventsKey>}
+         */
+        _this.once;
+        /***
+         * @type {BaseTileLayerOnSignature<void>}
+         */
+        _this.un;
+        _this.setPreload(options.preload !== undefined ? options.preload : 0);
+        _this.setUseInterimTilesOnError(options.useInterimTilesOnError !== undefined
+            ? options.useInterimTilesOnError
+            : true);
+        return _this;
+    }
+    /**
+     * Return the level as number to which we will preload tiles up to.
+     * @return {number} The level to preload tiles up to.
+     * @observable
+     * @api
+     */
+    BaseTileLayer.prototype.getPreload = function () {
+        return /** @type {number} */ (this.get(TileProperty.PRELOAD));
+    };
+    /**
+     * Set the level as number to which we will preload tiles up to.
+     * @param {number} preload The level to preload tiles up to.
+     * @observable
+     * @api
+     */
+    BaseTileLayer.prototype.setPreload = function (preload) {
+        this.set(TileProperty.PRELOAD, preload);
+    };
+    /**
+     * Whether we use interim tiles on error.
+     * @return {boolean} Use interim tiles on error.
+     * @observable
+     * @api
+     */
+    BaseTileLayer.prototype.getUseInterimTilesOnError = function () {
+        return /** @type {boolean} */ (this.get(TileProperty.USE_INTERIM_TILES_ON_ERROR));
+    };
+    /**
+     * Set whether we use interim tiles on error.
+     * @param {boolean} useInterimTilesOnError Use interim tiles on error.
+     * @observable
+     * @api
+     */
+    BaseTileLayer.prototype.setUseInterimTilesOnError = function (useInterimTilesOnError) {
+        this.set(TileProperty.USE_INTERIM_TILES_ON_ERROR, useInterimTilesOnError);
+    };
+    return BaseTileLayer;
+}(Layer));
+export default BaseTileLayer;
+//# sourceMappingURL=BaseTile.js.map
