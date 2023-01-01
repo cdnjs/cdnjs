465ea498c08c30a79892cf44874b96204a10b239
robocdnjs <cdnjs-github@cloudflare.com>
2023-01-01 14:06:45 +0000

    Add hexo-theme-next (8.14.1)

---
+++ ajax/libs/hexo-theme-next/8.14.1/third-party/math/mathjax.js
@@ -0,0 +1,36 @@
+/* global NexT, CONFIG, MathJax */
+
+document.addEventListener('page:loaded', () => {
+  if (!CONFIG.enableMath) return;
+
+  if (typeof MathJax === 'undefined') {
+    window.MathJax = {
+      tex: {
+        inlineMath: { '[+]': [['$', '$']] },
+        tags      : CONFIG.mathjax.tags
+      },
+      options: {
+        renderActions: {
+          insertedScript: [200, () => {
+            document.querySelectorAll('mjx-container').forEach(node => {
+              const target = node.parentNode;
+              if (target.nodeName.toLowerCase() === 'li') {
+                target.parentNode.classList.add('has-jax');
+              }
+            });
+          }, '', false]
+        }
+      }
+    };
+    NexT.utils.getScript(CONFIG.mathjax.js, {
+      attributes: {
+        defer: true
+      }
+    });
+  } else {
+    MathJax.startup.document.state(0);
+    MathJax.typesetClear();
+    MathJax.texReset();
+    MathJax.typesetPromise();
+  }
+});
