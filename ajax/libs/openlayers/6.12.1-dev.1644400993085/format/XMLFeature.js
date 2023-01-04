06758da80aff07f3cf00b24cf31a1de58f27f7bf
robocdnjs <cdnjs-github@cloudflare.com>
2023-01-04 02:27:04 +0000

    Add openlayers (6.12.1-dev.1644400993085)

---
+++ ajax/libs/openlayers/6.12.1-dev.1644400993085/format/XMLFeature.js
@@ -0,0 +1,289 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+/**
+ * @module ol/format/XMLFeature
+ */
+import FeatureFormat from '../format/Feature.js';
+import FormatType from '../format/FormatType.js';
+import { abstract } from '../util.js';
+import { extend } from '../array.js';
+import { getXMLSerializer, isDocument, parse } from '../xml.js';
+/**
+ * @classdesc
+ * Abstract base class; normally only used for creating subclasses and not
+ * instantiated in apps.
+ * Base class for XML feature formats.
+ *
+ * @abstract
+ */
+var XMLFeature = /** @class */ (function (_super) {
+    __extends(XMLFeature, _super);
+    function XMLFeature() {
+        var _this = _super.call(this) || this;
+        /**
+         * @type {XMLSerializer}
+         * @private
+         */
+        _this.xmlSerializer_ = getXMLSerializer();
+        return _this;
+    }
+    /**
+     * @return {import("./FormatType.js").default} Format.
+     */
+    XMLFeature.prototype.getType = function () {
+        return FormatType.XML;
+    };
+    /**
+     * Read a single feature.
+     *
+     * @param {Document|Element|Object|string} source Source.
+     * @param {import("./Feature.js").ReadOptions} [opt_options] Read options.
+     * @return {import("../Feature.js").default} Feature.
+     * @api
+     */
+    XMLFeature.prototype.readFeature = function (source, opt_options) {
+        if (!source) {
+            return null;
+        }
+        else if (typeof source === 'string') {
+            var doc = parse(source);
+            return this.readFeatureFromDocument(doc, opt_options);
+        }
+        else if (isDocument(source)) {
+            return this.readFeatureFromDocument(
+            /** @type {Document} */ (source), opt_options);
+        }
+        else {
+            return this.readFeatureFromNode(
+            /** @type {Element} */ (source), opt_options);
+        }
+    };
+    /**
+     * @param {Document} doc Document.
+     * @param {import("./Feature.js").ReadOptions} [opt_options] Options.
+     * @return {import("../Feature.js").default} Feature.
+     */
+    XMLFeature.prototype.readFeatureFromDocument = function (doc, opt_options) {
+        var features = this.readFeaturesFromDocument(doc, opt_options);
+        if (features.length > 0) {
+            return features[0];
+        }
+        else {
+            return null;
+        }
+    };
+    /**
+     * @param {Element} node Node.
+     * @param {import("./Feature.js").ReadOptions} [opt_options] Options.
+     * @return {import("../Feature.js").default} Feature.
+     */
+    XMLFeature.prototype.readFeatureFromNode = function (node, opt_options) {
+        return null; // not implemented
+    };
+    /**
+     * Read all features from a feature collection.
+     *
+     * @param {Document|Element|Object|string} source Source.
+     * @param {import("./Feature.js").ReadOptions} [opt_options] Options.
+     * @return {Array<import("../Feature.js").default>} Features.
+     * @api
+     */
+    XMLFeature.prototype.readFeatures = function (source, opt_options) {
+        if (!source) {
+            return [];
+        }
+        else if (typeof source === 'string') {
+            var doc = parse(source);
+            return this.readFeaturesFromDocument(doc, opt_options);
+        }
+        else if (isDocument(source)) {
+            return this.readFeaturesFromDocument(
+            /** @type {Document} */ (source), opt_options);
+        }
+        else {
+            return this.readFeaturesFromNode(
+            /** @type {Element} */ (source), opt_options);
+        }
+    };
+    /**
+     * @param {Document} doc Document.
+     * @param {import("./Feature.js").ReadOptions} [opt_options] Options.
+     * @protected
+     * @return {Array<import("../Feature.js").default>} Features.
+     */
+    XMLFeature.prototype.readFeaturesFromDocument = function (doc, opt_options) {
+        /** @type {Array<import("../Feature.js").default>} */
+        var features = [];
+        for (var n = doc.firstChild; n; n = n.nextSibling) {
+            if (n.nodeType == Node.ELEMENT_NODE) {
+                extend(features, this.readFeaturesFromNode(/** @type {Element} */ (n), opt_options));
+            }
+        }
+        return features;
+    };
+    /**
+     * @abstract
+     * @param {Element} node Node.
+     * @param {import("./Feature.js").ReadOptions} [opt_options] Options.
+     * @protected
+     * @return {Array<import("../Feature.js").default>} Features.
+     */
+    XMLFeature.prototype.readFeaturesFromNode = function (node, opt_options) {
+        return abstract();
+    };
+    /**
+     * Read a single geometry from a source.
+     *
+     * @param {Document|Element|Object|string} source Source.
+     * @param {import("./Feature.js").ReadOptions} [opt_options] Read options.
+     * @return {import("../geom/Geometry.js").default} Geometry.
+     */
+    XMLFeature.prototype.readGeometry = function (source, opt_options) {
+        if (!source) {
+            return null;
+        }
+        else if (typeof source === 'string') {
+            var doc = parse(source);
+            return this.readGeometryFromDocument(doc, opt_options);
+        }
+        else if (isDocument(source)) {
+            return this.readGeometryFromDocument(
+            /** @type {Document} */ (source), opt_options);
+        }
+        else {
+            return this.readGeometryFromNode(
+            /** @type {Element} */ (source), opt_options);
+        }
+    };
+    /**
+     * @param {Document} doc Document.
+     * @param {import("./Feature.js").ReadOptions} [opt_options] Options.
+     * @protected
+     * @return {import("../geom/Geometry.js").default} Geometry.
+     */
+    XMLFeature.prototype.readGeometryFromDocument = function (doc, opt_options) {
+        return null; // not implemented
+    };
+    /**
+     * @param {Element} node Node.
+     * @param {import("./Feature.js").ReadOptions} [opt_options] Options.
+     * @protected
+     * @return {import("../geom/Geometry.js").default} Geometry.
+     */
+    XMLFeature.prototype.readGeometryFromNode = function (node, opt_options) {
+        return null; // not implemented
+    };
+    /**
+     * Read the projection from the source.
+     *
+     * @param {Document|Element|Object|string} source Source.
+     * @return {import("../proj/Projection.js").default} Projection.
+     * @api
+     */
+    XMLFeature.prototype.readProjection = function (source) {
+        if (!source) {
+            return null;
+        }
+        else if (typeof source === 'string') {
+            var doc = parse(source);
+            return this.readProjectionFromDocument(doc);
+        }
+        else if (isDocument(source)) {
+            return this.readProjectionFromDocument(/** @type {Document} */ (source));
+        }
+        else {
+            return this.readProjectionFromNode(/** @type {Element} */ (source));
+        }
+    };
+    /**
+     * @param {Document} doc Document.
+     * @protected
+     * @return {import("../proj/Projection.js").default} Projection.
+     */
+    XMLFeature.prototype.readProjectionFromDocument = function (doc) {
+        return this.dataProjection;
+    };
+    /**
+     * @param {Element} node Node.
+     * @protected
+     * @return {import("../proj/Projection.js").default} Projection.
+     */
+    XMLFeature.prototype.readProjectionFromNode = function (node) {
+        return this.dataProjection;
+    };
+    /**
+     * Encode a feature as string.
+     *
+     * @param {import("../Feature.js").default} feature Feature.
+     * @param {import("./Feature.js").WriteOptions} [opt_options] Write options.
+     * @return {string} Encoded feature.
+     */
+    XMLFeature.prototype.writeFeature = function (feature, opt_options) {
+        var node = this.writeFeatureNode(feature, opt_options);
+        return this.xmlSerializer_.serializeToString(node);
+    };
+    /**
+     * @param {import("../Feature.js").default} feature Feature.
+     * @param {import("./Feature.js").WriteOptions} [opt_options] Options.
+     * @protected
+     * @return {Node} Node.
+     */
+    XMLFeature.prototype.writeFeatureNode = function (feature, opt_options) {
+        return null; // not implemented
+    };
+    /**
+     * Encode an array of features as string.
+     *
+     * @param {Array<import("../Feature.js").default>} features Features.
+     * @param {import("./Feature.js").WriteOptions} [opt_options] Write options.
+     * @return {string} Result.
+     * @api
+     */
+    XMLFeature.prototype.writeFeatures = function (features, opt_options) {
+        var node = this.writeFeaturesNode(features, opt_options);
+        return this.xmlSerializer_.serializeToString(node);
+    };
+    /**
+     * @param {Array<import("../Feature.js").default>} features Features.
+     * @param {import("./Feature.js").WriteOptions} [opt_options] Options.
+     * @return {Node} Node.
+     */
+    XMLFeature.prototype.writeFeaturesNode = function (features, opt_options) {
+        return null; // not implemented
+    };
+    /**
+     * Encode a geometry as string.
+     *
+     * @param {import("../geom/Geometry.js").default} geometry Geometry.
+     * @param {import("./Feature.js").WriteOptions} [opt_options] Write options.
+     * @return {string} Encoded geometry.
+     */
+    XMLFeature.prototype.writeGeometry = function (geometry, opt_options) {
+        var node = this.writeGeometryNode(geometry, opt_options);
+        return this.xmlSerializer_.serializeToString(node);
+    };
+    /**
+     * @param {import("../geom/Geometry.js").default} geometry Geometry.
+     * @param {import("./Feature.js").WriteOptions} [opt_options] Options.
+     * @return {Node} Node.
+     */
+    XMLFeature.prototype.writeGeometryNode = function (geometry, opt_options) {
+        return null; // not implemented
+    };
+    return XMLFeature;
+}(FeatureFormat));
+export default XMLFeature;
+//# sourceMappingURL=XMLFeature.js.map
---
+++ ajax/libs/openlayers/6.12.1-dev.1644400993085/src/interaction/Translate.js
@@ -0,0 +1,417 @@
+/**
+ * @module ol/interaction/Translate
+ */
+import Collection from '../Collection.js';
+import Event from '../events/Event.js';
+import InteractionProperty from './Property.js';
+import PointerInteraction from './Pointer.js';
+import {TRUE} from '../functions.js';
+import {always} from '../events/condition.js';
+import {includes} from '../array.js';
+
+/**
+ * @enum {string}
+ */
+const TranslateEventType = {
+  /**
+   * Triggered upon feature translation start.
+   * @event TranslateEvent#translatestart
+   * @api
+   */
+  TRANSLATESTART: 'translatestart',
+  /**
+   * Triggered upon feature translation.
+   * @event TranslateEvent#translating
+   * @api
+   */
+  TRANSLATING: 'translating',
+  /**
+   * Triggered upon feature translation end.
+   * @event TranslateEvent#translateend
+   * @api
+   */
+  TRANSLATEEND: 'translateend',
+};
+
+/**
+ * A function that takes an {@link module:ol/Feature} or
+ * {@link module:ol/render/Feature} and an
+ * {@link module:ol/layer/Layer} and returns `true` if the feature may be
+ * translated or `false` otherwise.
+ * @typedef {function(import("../Feature.js").FeatureLike, import("../layer/Layer.js").default<import("../source/Source").default>):boolean} FilterFunction
+ */
+
+/**
+ * @typedef {Object} Options
+ * @property {import("../events/condition.js").Condition} [condition] A function that
+ * takes an {@link module:ol/MapBrowserEvent~MapBrowserEvent} and returns a
+ * boolean to indicate whether that event should be handled.
+ * Default is {@link module:ol/events/condition.always}.
+ * @property {Collection<import("../Feature.js").default>} [features] Features contained in this collection will be able to be translated together.
+ * @property {Array<import("../layer/Layer.js").default>|function(import("../layer/Layer.js").default<import("../source/Source").default>): boolean} [layers] A list of layers from which features should be
+ * translated. Alternatively, a filter function can be provided. The
+ * function will be called for each layer in the map and should return
+ * `true` for layers that you want to be translatable. If the option is
+ * absent, all visible layers will be considered translatable.
+ * Not used if `features` is provided.
+ * @property {FilterFunction} [filter] A function
+ * that takes an {@link module:ol/Feature} and an
+ * {@link module:ol/layer/Layer} and returns `true` if the feature may be
+ * translated or `false` otherwise. Not used if `features` is provided.
+ * @property {number} [hitTolerance=0] Hit-detection tolerance. Pixels inside the radius around the given position
+ * will be checked for features.
+ */
+
+/**
+ * @classdesc
+ * Events emitted by {@link module:ol/interaction/Translate~Translate} instances
+ * are instances of this type.
+ */
+export class TranslateEvent extends Event {
+  /**
+   * @param {TranslateEventType} type Type.
+   * @param {Collection<import("../Feature.js").default>} features The features translated.
+   * @param {import("../coordinate.js").Coordinate} coordinate The event coordinate.
+   * @param {import("../coordinate.js").Coordinate} startCoordinate The original coordinates before.translation started
+   * @param {import("../MapBrowserEvent.js").default} mapBrowserEvent Map browser event.
+   */
+  constructor(type, features, coordinate, startCoordinate, mapBrowserEvent) {
+    super(type);
+
+    /**
+     * The features being translated.
+     * @type {Collection<import("../Feature.js").default>}
+     * @api
+     */
+    this.features = features;
+
+    /**
+     * The coordinate of the drag event.
+     * @const
+     * @type {import("../coordinate.js").Coordinate}
+     * @api
+     */
+    this.coordinate = coordinate;
+
+    /**
+     * The coordinate of the start position before translation started.
+     * @const
+     * @type {import("../coordinate.js").Coordinate}
+     * @api
+     */
+    this.startCoordinate = startCoordinate;
+
+    /**
+     * Associated {@link module:ol/MapBrowserEvent}.
+     * @type {import("../MapBrowserEvent.js").default}
+     * @api
+     */
+    this.mapBrowserEvent = mapBrowserEvent;
+  }
+}
+
+/***
+ * @template Return
+ * @typedef {import("../Observable").OnSignature<import("../Observable").EventTypes, import("../events/Event.js").default, Return> &
+ *   import("../Observable").OnSignature<import("../ObjectEventType").Types|
+ *     'change:active', import("../Object").ObjectEvent, Return> &
+ *   import("../Observable").OnSignature<'translateend'|'translatestart'|'translating', TranslateEvent, Return> &
+ *   import("../Observable").CombinedOnSignature<import("../Observable").EventTypes|import("../ObjectEventType").Types|
+ *     'change:active'|'translateend'|'translatestart'|'translating', Return>} TranslateOnSignature
+ */
+
+/**
+ * @classdesc
+ * Interaction for translating (moving) features.
+ * If you want to translate multiple features in a single action (for example,
+ * the collection used by a select interaction), construct the interaction with
+ * the `features` option.
+ *
+ * @fires TranslateEvent
+ * @api
+ */
+class Translate extends PointerInteraction {
+  /**
+   * @param {Options} [opt_options] Options.
+   */
+  constructor(opt_options) {
+    const options = opt_options ? opt_options : {};
+
+    super(/** @type {import("./Pointer.js").Options} */ (options));
+
+    /***
+     * @type {TranslateOnSignature<import("../events").EventsKey>}
+     */
+    this.on;
+
+    /***
+     * @type {TranslateOnSignature<import("../events").EventsKey>}
+     */
+    this.once;
+
+    /***
+     * @type {TranslateOnSignature<void>}
+     */
+    this.un;
+
+    /**
+     * The last position we translated to.
+     * @type {import("../coordinate.js").Coordinate}
+     * @private
+     */
+    this.lastCoordinate_ = null;
+
+    /**
+     * The start position before translation started.
+     * @type {import("../coordinate.js").Coordinate}
+     * @private
+     */
+    this.startCoordinate_ = null;
+
+    /**
+     * @type {Collection<import("../Feature.js").default>}
+     * @private
+     */
+    this.features_ = options.features !== undefined ? options.features : null;
+
+    /** @type {function(import("../layer/Layer.js").default<import("../source/Source").default>): boolean} */
+    let layerFilter;
+    if (options.layers && !this.features_) {
+      if (typeof options.layers === 'function') {
+        layerFilter = options.layers;
+      } else {
+        const layers = options.layers;
+        layerFilter = function (layer) {
+          return includes(layers, layer);
+        };
+      }
+    } else {
+      layerFilter = TRUE;
+    }
+
+    /**
+     * @private
+     * @type {function(import("../layer/Layer.js").default<import("../source/Source").default>): boolean}
+     */
+    this.layerFilter_ = layerFilter;
+
+    /**
+     * @private
+     * @type {FilterFunction}
+     */
+    this.filter_ = options.filter && !this.features_ ? options.filter : TRUE;
+
+    /**
+     * @private
+     * @type {number}
+     */
+    this.hitTolerance_ = options.hitTolerance ? options.hitTolerance : 0;
+
+    /**
+     * @private
+     * @type {import("../events/condition.js").Condition}
+     */
+    this.condition_ = options.condition ? options.condition : always;
+
+    /**
+     * @type {import("../Feature.js").default}
+     * @private
+     */
+    this.lastFeature_ = null;
+
+    this.addChangeListener(
+      InteractionProperty.ACTIVE,
+      this.handleActiveChanged_
+    );
+  }
+
+  /**
+   * Handle pointer down events.
+   * @param {import("../MapBrowserEvent.js").default} event Event.
+   * @return {boolean} If the event was consumed.
+   */
+  handleDownEvent(event) {
+    if (!event.originalEvent || !this.condition_(event)) {
+      return false;
+    }
+    this.lastFeature_ = this.featuresAtPixel_(event.pixel, event.map);
+    if (!this.lastCoordinate_ && this.lastFeature_) {
+      this.startCoordinate_ = event.coordinate;
+      this.lastCoordinate_ = event.coordinate;
+      this.handleMoveEvent(event);
+
+      const features = this.features_ || new Collection([this.lastFeature_]);
+
+      this.dispatchEvent(
+        new TranslateEvent(
+          TranslateEventType.TRANSLATESTART,
+          features,
+          event.coordinate,
+          this.startCoordinate_,
+          event
+        )
+      );
+      return true;
+    }
+    return false;
+  }
+
+  /**
+   * Handle pointer up events.
+   * @param {import("../MapBrowserEvent.js").default} event Event.
+   * @return {boolean} If the event was consumed.
+   */
+  handleUpEvent(event) {
+    if (this.lastCoordinate_) {
+      this.lastCoordinate_ = null;
+      this.handleMoveEvent(event);
+
+      const features = this.features_ || new Collection([this.lastFeature_]);
+
+      this.dispatchEvent(
+        new TranslateEvent(
+          TranslateEventType.TRANSLATEEND,
+          features,
+          event.coordinate,
+          this.startCoordinate_,
+          event
+        )
+      );
+      // cleanup
+      this.startCoordinate_ = null;
+      return true;
+    }
+    return false;
+  }
+
+  /**
+   * Handle pointer drag events.
+   * @param {import("../MapBrowserEvent.js").default} event Event.
+   */
+  handleDragEvent(event) {
+    if (this.lastCoordinate_) {
+      const newCoordinate = event.coordinate;
+      const deltaX = newCoordinate[0] - this.lastCoordinate_[0];
+      const deltaY = newCoordinate[1] - this.lastCoordinate_[1];
+
+      const features = this.features_ || new Collection([this.lastFeature_]);
+
+      features.forEach(function (feature) {
+        const geom = feature.getGeometry();
+        geom.translate(deltaX, deltaY);
+        feature.setGeometry(geom);
+      });
+
+      this.lastCoordinate_ = newCoordinate;
+
+      this.dispatchEvent(
+        new TranslateEvent(
+          TranslateEventType.TRANSLATING,
+          features,
+          newCoordinate,
+          this.startCoordinate_,
+          event
+        )
+      );
+    }
+  }
+
+  /**
+   * Handle pointer move events.
+   * @param {import("../MapBrowserEvent.js").default} event Event.
+   */
+  handleMoveEvent(event) {
+    const elem = event.map.getViewport();
+
+    // Change the cursor to grab/grabbing if hovering any of the features managed
+    // by the interaction
+    if (this.featuresAtPixel_(event.pixel, event.map)) {
+      elem.classList.remove(this.lastCoordinate_ ? 'ol-grab' : 'ol-grabbing');
+      elem.classList.add(this.lastCoordinate_ ? 'ol-grabbing' : 'ol-grab');
+    } else {
+      elem.classList.remove('ol-grab', 'ol-grabbing');
+    }
+  }
+
+  /**
+   * Tests to see if the given coordinates intersects any of our selected
+   * features.
+   * @param {import("../pixel.js").Pixel} pixel Pixel coordinate to test for intersection.
+   * @param {import("../PluggableMap.js").default} map Map to test the intersection on.
+   * @return {import("../Feature.js").default} Returns the feature found at the specified pixel
+   * coordinates.
+   * @private
+   */
+  featuresAtPixel_(pixel, map) {
+    return map.forEachFeatureAtPixel(
+      pixel,
+      function (feature, layer) {
+        if (this.filter_(feature, layer)) {
+          if (!this.features_ || includes(this.features_.getArray(), feature)) {
+            return feature;
+          }
+        }
+      }.bind(this),
+      {
+        layerFilter: this.layerFilter_,
+        hitTolerance: this.hitTolerance_,
+      }
+    );
+  }
+
+  /**
+   * Returns the Hit-detection tolerance.
+   * @return {number} Hit tolerance in pixels.
+   * @api
+   */
+  getHitTolerance() {
+    return this.hitTolerance_;
+  }
+
+  /**
+   * Hit-detection tolerance. Pixels inside the radius around the given position
+   * will be checked for features.
+   * @param {number} hitTolerance Hit tolerance in pixels.
+   * @api
+   */
+  setHitTolerance(hitTolerance) {
+    this.hitTolerance_ = hitTolerance;
+  }
+
+  /**
+   * Remove the interaction from its current map and attach it to the new map.
+   * Subclasses may set up event handlers to get notified about changes to
+   * the map here.
+   * @param {import("../PluggableMap.js").default} map Map.
+   */
+  setMap(map) {
+    const oldMap = this.getMap();
+    super.setMap(map);
+    this.updateState_(oldMap);
+  }
+
+  /**
+   * @private
+   */
+  handleActiveChanged_() {
+    this.updateState_(null);
+  }
+
+  /**
+   * @param {import("../PluggableMap.js").default} oldMap Old map.
+   * @private
+   */
+  updateState_(oldMap) {
+    let map = this.getMap();
+    const active = this.getActive();
+    if (!map || !active) {
+      map = map || oldMap;
+      if (map) {
+        const elem = map.getViewport();
+        elem.classList.remove('ol-grab', 'ol-grabbing');
+      }
+    }
+  }
+}
+
+export default Translate;
