bdd47ddf23ff2004eb3403fa43270bbdc968d7b4
robocdnjs <cdnjs-github@cloudflare.com>
2023-01-04 15:04:45 +0000

    Add openlayers (6.12.1-dev.1644522891363)

---
+++ ajax/libs/openlayers/6.12.1-dev.1644522891363/layer/Layer.d.ts
@@ -0,0 +1,343 @@
+/**
+ * Return `true` if the layer is visible and if the provided view state
+ * has resolution and zoom levels that are in range of the layer's min/max.
+ * @param {State} layerState Layer state.
+ * @param {import("../View.js").State} viewState View state.
+ * @return {boolean} The layer is visible at the given view state.
+ */
+export function inView(layerState: State, viewState: import("../View.js").State): boolean;
+export default Layer;
+export type RenderFunction = (arg0: import("../PluggableMap.js").FrameState) => HTMLElement;
+/**
+ * *
+ */
+export type LayerOnSignature<Return> = import("../Observable").OnSignature<import("../Observable").EventTypes, import("../events/Event.js").default, Return> & import("../Observable").OnSignature<import("./Base").BaseLayerObjectEventTypes | 'change:source', import("../Object").ObjectEvent, Return> & import("../Observable").OnSignature<import("../render/EventType").LayerRenderEventTypes, import("../render/Event").default, Return> & import("../Observable").CombinedOnSignature<import("../Observable").EventTypes | import("./Base").BaseLayerObjectEventTypes | 'change:source' | import("../render/EventType").LayerRenderEventTypes, Return>;
+export type Options<SourceType extends import("../source/Source.js").default = import("../source/Source.js").default> = {
+    /**
+     * A CSS class name to set to the layer element.
+     */
+    className?: string | undefined;
+    /**
+     * Opacity (0, 1).
+     */
+    opacity?: number | undefined;
+    /**
+     * Visibility.
+     */
+    visible?: boolean | undefined;
+    /**
+     * The bounding extent for layer rendering.  The layer will not be
+     * rendered outside of this extent.
+     */
+    extent?: import("../extent.js").Extent | undefined;
+    /**
+     * The z-index for layer rendering.  At rendering time, the layers
+     * will be ordered, first by Z-index and then by position. When `undefined`, a `zIndex` of 0 is assumed
+     * for layers that are added to the map's `layers` collection, or `Infinity` when the layer's `setMap()`
+     * method was used.
+     */
+    zIndex?: number | undefined;
+    /**
+     * The minimum resolution (inclusive) at which this layer will be
+     * visible.
+     */
+    minResolution?: number | undefined;
+    /**
+     * The maximum resolution (exclusive) below which this layer will
+     * be visible.
+     */
+    maxResolution?: number | undefined;
+    /**
+     * The minimum view zoom level (exclusive) above which this layer will be
+     * visible.
+     */
+    minZoom?: number | undefined;
+    /**
+     * The maximum view zoom level (inclusive) at which this layer will
+     * be visible.
+     */
+    maxZoom?: number | undefined;
+    /**
+     * Source for this layer.  If not provided to the constructor,
+     * the source can be set by calling {@link module :ol/layer/Layer~Layer#setSource layer.setSource(source)} after
+     * construction.
+     */
+    source?: SourceType | undefined;
+    /**
+     * Map.
+     */
+    map?: import("../PluggableMap.js").default | null | undefined;
+    /**
+     * Render function. Takes the frame state as input and is expected to return an
+     * HTML element. Will overwrite the default rendering for the layer.
+     */
+    render?: RenderFunction | undefined;
+    /**
+     * Arbitrary observable properties. Can be accessed with `#get()` and `#set()`.
+     */
+    properties?: {
+        [x: string]: any;
+    } | undefined;
+};
+export type State = {
+    /**
+     * Layer.
+     */
+    layer: import("./Layer.js").default;
+    /**
+     * Opacity, the value is rounded to two digits to appear after the decimal point.
+     */
+    opacity: number;
+    /**
+     * Source being rendered (only for multi-source layers).
+     */
+    source: import("../source/Source.js").default | undefined;
+    /**
+     * Visible.
+     */
+    visible: boolean;
+    /**
+     * Managed.
+     */
+    managed: boolean;
+    /**
+     * Extent.
+     */
+    extent?: import("../extent.js").Extent | undefined;
+    /**
+     * ZIndex.
+     */
+    zIndex: number;
+    /**
+     * Maximum resolution.
+     */
+    maxResolution: number;
+    /**
+     * Minimum resolution.
+     */
+    minResolution: number;
+    /**
+     * Minimum zoom.
+     */
+    minZoom: number;
+    /**
+     * Maximum zoom.
+     */
+    maxZoom: number;
+};
+/**
+ * @typedef {function(import("../PluggableMap.js").FrameState):HTMLElement} RenderFunction
+ */
+/***
+ * @template Return
+ * @typedef {import("../Observable").OnSignature<import("../Observable").EventTypes, import("../events/Event.js").default, Return> &
+ *   import("../Observable").OnSignature<import("./Base").BaseLayerObjectEventTypes|
+ *     'change:source', import("../Object").ObjectEvent, Return> &
+ *   import("../Observable").OnSignature<import("../render/EventType").LayerRenderEventTypes, import("../render/Event").default, Return> &
+ *   import("../Observable").CombinedOnSignature<import("../Observable").EventTypes|import("./Base").BaseLayerObjectEventTypes|'change:source'|
+ *     import("../render/EventType").LayerRenderEventTypes, Return>} LayerOnSignature
+ */
+/**
+ * @template {import("../source/Source.js").default} [SourceType=import("../source/Source.js").default]
+ * @typedef {Object} Options
+ * @property {string} [className='ol-layer'] A CSS class name to set to the layer element.
+ * @property {number} [opacity=1] Opacity (0, 1).
+ * @property {boolean} [visible=true] Visibility.
+ * @property {import("../extent.js").Extent} [extent] The bounding extent for layer rendering.  The layer will not be
+ * rendered outside of this extent.
+ * @property {number} [zIndex] The z-index for layer rendering.  At rendering time, the layers
+ * will be ordered, first by Z-index and then by position. When `undefined`, a `zIndex` of 0 is assumed
+ * for layers that are added to the map's `layers` collection, or `Infinity` when the layer's `setMap()`
+ * method was used.
+ * @property {number} [minResolution] The minimum resolution (inclusive) at which this layer will be
+ * visible.
+ * @property {number} [maxResolution] The maximum resolution (exclusive) below which this layer will
+ * be visible.
+ * @property {number} [minZoom] The minimum view zoom level (exclusive) above which this layer will be
+ * visible.
+ * @property {number} [maxZoom] The maximum view zoom level (inclusive) at which this layer will
+ * be visible.
+ * @property {SourceType} [source] Source for this layer.  If not provided to the constructor,
+ * the source can be set by calling {@link module:ol/layer/Layer~Layer#setSource layer.setSource(source)} after
+ * construction.
+ * @property {import("../PluggableMap.js").default|null} [map] Map.
+ * @property {RenderFunction} [render] Render function. Takes the frame state as input and is expected to return an
+ * HTML element. Will overwrite the default rendering for the layer.
+ * @property {Object<string, *>} [properties] Arbitrary observable properties. Can be accessed with `#get()` and `#set()`.
+ */
+/**
+ * @typedef {Object} State
+ * @property {import("./Layer.js").default} layer Layer.
+ * @property {number} opacity Opacity, the value is rounded to two digits to appear after the decimal point.
+ * @property {import("../source/Source.js").default|undefined} source Source being rendered (only for multi-source layers).
+ * @property {boolean} visible Visible.
+ * @property {boolean} managed Managed.
+ * @property {import("../extent.js").Extent} [extent] Extent.
+ * @property {number} zIndex ZIndex.
+ * @property {number} maxResolution Maximum resolution.
+ * @property {number} minResolution Minimum resolution.
+ * @property {number} minZoom Minimum zoom.
+ * @property {number} maxZoom Maximum zoom.
+ */
+/**
+ * @classdesc
+ * Base class from which all layer types are derived. This should only be instantiated
+ * in the case where a custom layer is added to the map with a custom `render` function.
+ * Such a function can be specified in the `options` object, and is expected to return an HTML element.
+ *
+ * A visual representation of raster or vector map data.
+ * Layers group together those properties that pertain to how the data is to be
+ * displayed, irrespective of the source of that data.
+ *
+ * Layers are usually added to a map with {@link import("../PluggableMap.js").default#addLayer map.addLayer()}. Components
+ * like {@link module:ol/interaction/Draw~Draw} use unmanaged layers
+ * internally. These unmanaged layers are associated with the map using
+ * {@link module:ol/layer/Layer~Layer#setMap} instead.
+ *
+ * A generic `change` event is fired when the state of the source changes.
+ *
+ * Please note that for performance reasons several layers might get rendered to
+ * the same HTML element, which will cause {@link import("../PluggableMap.js").default#forEachLayerAtPixel map.forEachLayerAtPixel()} to
+ * give false positives. To avoid this, apply different `className` properties to the
+ * layers at creation time.
+ *
+ * @fires import("../render/Event.js").RenderEvent#prerender
+ * @fires import("../render/Event.js").RenderEvent#postrender
+ *
+ * @template {import("../source/Source.js").default} [SourceType=import("../source/Source.js").default]
+ * @template {import("../renderer/Layer.js").default} [RendererType=import("../renderer/Layer.js").default]
+ * @api
+ */
+declare class Layer<SourceType extends import("../source/Source.js").default = import("../source/Source.js").default, RendererType extends import("../renderer/Layer.js").default<any> = import("../renderer/Layer.js").default<any>> extends BaseLayer {
+    /**
+     * @param {Options<SourceType>} options Layer options.
+     */
+    constructor(options: Options<SourceType>);
+    /***
+     * @type {LayerOnSignature<import("../events").EventsKey>}
+     */
+    on: LayerOnSignature<import("../events").EventsKey>;
+    /***
+     * @type {LayerOnSignature<import("../events").EventsKey>}
+     */
+    once: LayerOnSignature<import("../events").EventsKey>;
+    /***
+     * @type {LayerOnSignature<void>}
+     */
+    un: LayerOnSignature<void>;
+    /**
+     * @private
+     * @type {?import("../events.js").EventsKey}
+     */
+    private mapPrecomposeKey_;
+    /**
+     * @private
+     * @type {?import("../events.js").EventsKey}
+     */
+    private mapRenderKey_;
+    /**
+     * @private
+     * @type {?import("../events.js").EventsKey}
+     */
+    private sourceChangeKey_;
+    /**
+     * @private
+     * @type {RendererType}
+     */
+    private renderer_;
+    /**
+     * @protected
+     * @type {boolean}
+     */
+    protected rendered: boolean;
+    /**
+     * In charge to manage the rendering of the layer. One layer type is
+     * bounded with one layer renderer.
+     * @param {?import("../PluggableMap.js").FrameState} frameState Frame state.
+     * @param {HTMLElement} target Target which the renderer may (but need not) use
+     * for rendering its content.
+     * @return {HTMLElement} The rendered element.
+     */
+    render(frameState: import("../PluggableMap.js").FrameState | null, target: HTMLElement): HTMLElement;
+    /**
+     * Get the layer source.
+     * @return {SourceType|null} The layer source (or `null` if not yet set).
+     * @observable
+     * @api
+     */
+    getSource(): SourceType | null;
+    /**
+     * @return {SourceType|null} The source being rendered.
+     */
+    getRenderSource(): SourceType | null;
+    /**
+     * @private
+     */
+    private handleSourceChange_;
+    /**
+     * @private
+     */
+    private handleSourcePropertyChange_;
+    /**
+     * @param {import("../pixel").Pixel} pixel Pixel.
+     * @return {Promise<Array<import("../Feature").default>>} Promise that resolves with
+     * an array of features.
+     */
+    getFeatures(pixel: import("../pixel").Pixel): Promise<Array<import("../Feature").default>>;
+    /**
+     * @param {import("../pixel").Pixel} pixel Pixel.
+     * @return {Uint8ClampedArray|Uint8Array|Float32Array|DataView|null} Pixel data.
+     */
+    getData(pixel: import("../pixel").Pixel): Uint8ClampedArray | Uint8Array | Float32Array | DataView | null;
+    /**
+     * Called when a layer is not visible during a map render.
+     */
+    unrender(): void;
+    /**
+     * For use inside the library only.
+     * @param {import("../PluggableMap.js").default|null} map Map.
+     */
+    setMapInternal(map: import("../PluggableMap.js").default | null): void;
+    /**
+     * For use inside the library only.
+     * @return {import("../PluggableMap.js").default|null} Map.
+     */
+    getMapInternal(): import("../PluggableMap.js").default | null;
+    /**
+     * Sets the layer to be rendered on top of other layers on a map. The map will
+     * not manage this layer in its layers collection, and the callback in
+     * {@link module:ol/Map~Map#forEachLayerAtPixel} will receive `null` as layer. This
+     * is useful for temporary layers. To remove an unmanaged layer from the map,
+     * use `#setMap(null)`.
+     *
+     * To add the layer to a map and have it managed by the map, use
+     * {@link module:ol/Map~Map#addLayer} instead.
+     * @param {import("../PluggableMap.js").default|null} map Map.
+     * @api
+     */
+    setMap(map: import("../PluggableMap.js").default | null): void;
+    /**
+     * Set the layer source.
+     * @param {SourceType|null} source The layer source.
+     * @observable
+     * @api
+     */
+    setSource(source: SourceType | null): void;
+    /**
+     * Get the renderer for this layer.
+     * @return {RendererType} The layer renderer.
+     */
+    getRenderer(): RendererType;
+    /**
+     * @return {boolean} The layer has a renderer.
+     */
+    hasRenderer(): boolean;
+    /**
+     * Create a renderer for this layer.
+     * @return {RendererType} A layer renderer.
+     * @protected
+     */
+    protected createRenderer(): RendererType;
+}
+import BaseLayer from "./Base.js";
+//# sourceMappingURL=Layer.d.ts.map
---
+++ ajax/libs/openlayers/6.12.1-dev.1644522891363/src/source/ImageCanvas.js
@@ -0,0 +1,146 @@
+/**
+ * @module ol/source/ImageCanvas
+ */
+
+import ImageCanvas from '../ImageCanvas.js';
+import ImageSource from './Image.js';
+import {
+  containsExtent,
+  getHeight,
+  getWidth,
+  scaleFromCenter,
+} from '../extent.js';
+
+/**
+ * A function returning the canvas element (`{HTMLCanvasElement}`)
+ * used by the source as an image. The arguments passed to the function are:
+ * {@link module:ol/extent~Extent} the image extent, `{number}` the image resolution,
+ * `{number}` the pixel ratio of the map, {@link module:ol/size~Size} the image size,
+ * and {@link module:ol/proj/Projection} the image projection. The canvas returned by
+ * this function is cached by the source. The this keyword inside the function
+ * references the {@link module:ol/source/ImageCanvas}.
+ *
+ * @typedef {function(this:import("../ImageCanvas.js").default, import("../extent.js").Extent, number,
+ *     number, import("../size.js").Size, import("../proj/Projection.js").default): HTMLCanvasElement} FunctionType
+ */
+
+/**
+ * @typedef {Object} Options
+ * @property {import("./Source.js").AttributionLike} [attributions] Attributions.
+ * @property {FunctionType} [canvasFunction] Canvas function.
+ * The function returning the canvas element used by the source
+ * as an image. The arguments passed to the function are: `{import("../extent.js").Extent}` the
+ * image extent, `{number}` the image resolution, `{number}` the pixel ratio of the map,
+ * `{import("../size.js").Size}` the image size, and `{import("../proj/Projection.js").Projection}` the image
+ * projection. The canvas returned by this function is cached by the source. If
+ * the value returned by the function is later changed then
+ * `changed` should be called on the source for the source to
+ * invalidate the current cached image. See: {@link module:ol/Observable~Observable#changed}
+ * @property {boolean} [imageSmoothing=true] Deprecated.  Use the `interpolate` option instead.
+ * @property {boolean} [interpolate=true] Use interpolated values when resampling.  By default,
+ * linear interpolation is used when resampling.  Set to false to use the nearest neighbor instead.
+ * @property {import("../proj.js").ProjectionLike} [projection] Projection. Default is the view projection.
+ * @property {number} [ratio=1.5] Ratio. 1 means canvases are the size of the map viewport, 2 means twice the
+ * width and height of the map viewport, and so on. Must be `1` or higher.
+ * @property {Array<number>} [resolutions] Resolutions.
+ * If specified, new canvases will be created for these resolutions
+ * @property {import("./State.js").default} [state] Source state.
+ */
+
+/**
+ * @classdesc
+ * Base class for image sources where a canvas element is the image.
+ * @api
+ */
+class ImageCanvasSource extends ImageSource {
+  /**
+   * @param {Options} [opt_options] ImageCanvas options.
+   */
+  constructor(opt_options) {
+    const options = opt_options ? opt_options : {};
+
+    let interpolate =
+      options.imageSmoothing !== undefined ? options.imageSmoothing : true;
+    if (options.interpolate !== undefined) {
+      interpolate = options.interpolate;
+    }
+
+    super({
+      attributions: options.attributions,
+      interpolate: interpolate,
+      projection: options.projection,
+      resolutions: options.resolutions,
+      state: options.state,
+    });
+
+    /**
+     * @private
+     * @type {FunctionType}
+     */
+    this.canvasFunction_ = options.canvasFunction;
+
+    /**
+     * @private
+     * @type {import("../ImageCanvas.js").default}
+     */
+    this.canvas_ = null;
+
+    /**
+     * @private
+     * @type {number}
+     */
+    this.renderedRevision_ = 0;
+
+    /**
+     * @private
+     * @type {number}
+     */
+    this.ratio_ = options.ratio !== undefined ? options.ratio : 1.5;
+  }
+
+  /**
+   * @param {import("../extent.js").Extent} extent Extent.
+   * @param {number} resolution Resolution.
+   * @param {number} pixelRatio Pixel ratio.
+   * @param {import("../proj/Projection.js").default} projection Projection.
+   * @return {import("../ImageCanvas.js").default} Single image.
+   */
+  getImageInternal(extent, resolution, pixelRatio, projection) {
+    resolution = this.findNearestResolution(resolution);
+
+    let canvas = this.canvas_;
+    if (
+      canvas &&
+      this.renderedRevision_ == this.getRevision() &&
+      canvas.getResolution() == resolution &&
+      canvas.getPixelRatio() == pixelRatio &&
+      containsExtent(canvas.getExtent(), extent)
+    ) {
+      return canvas;
+    }
+
+    extent = extent.slice();
+    scaleFromCenter(extent, this.ratio_);
+    const width = getWidth(extent) / resolution;
+    const height = getHeight(extent) / resolution;
+    const size = [width * pixelRatio, height * pixelRatio];
+
+    const canvasElement = this.canvasFunction_.call(
+      this,
+      extent,
+      resolution,
+      pixelRatio,
+      size,
+      projection
+    );
+    if (canvasElement) {
+      canvas = new ImageCanvas(extent, resolution, pixelRatio, canvasElement);
+    }
+    this.canvas_ = canvas;
+    this.renderedRevision_ = this.getRevision();
+
+    return canvas;
+  }
+}
+
+export default ImageCanvasSource;
