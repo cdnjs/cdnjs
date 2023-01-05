aeda51b085ad3912102a43c59721e23aff9bc16d
robocdnjs <cdnjs-github@cloudflare.com>
2023-01-05 03:04:41 +0000

    Add web3 (4.0.1-alpha.3)

---
+++ ajax/libs/web3/4.0.1-alpha.3/abi.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const web3_eth_abi_1 = require("web3-eth-abi");
+exports.default = {
+    encodeEventSignature: web3_eth_abi_1.encodeEventSignature,
+    encodeFunctionCall: web3_eth_abi_1.encodeFunctionCall,
+    encodeFunctionSignature: web3_eth_abi_1.encodeFunctionSignature,
+    encodeParameter: web3_eth_abi_1.encodeParameter,
+    encodeParameters: web3_eth_abi_1.encodeParameters,
+    decodeParameter: web3_eth_abi_1.decodeParameter,
+    decodeParameters: web3_eth_abi_1.decodeParameters,
+    decodeLog: web3_eth_abi_1.decodeLog,
+};
+//# sourceMappingURL=abi.js.map
---
+++ ajax/libs/web3/4.0.1-alpha.3/index.d.ts
@@ -0,0 +1,3 @@
+import Web3 from './web3';
+export = Web3;
+//# sourceMappingURL=index.d.ts.map
