0a08d19face61d36cd943d6e6be3f54d74090b0c
robocdnjs <cdnjs-github@cloudflare.com>
2023-01-09 08:05:37 +0000

    Add openlayers (6.13.1-dev.1646309804959)

---
+++ ajax/libs/openlayers/6.13.1-dev.1646309804959/src/format/filter/Or.js
@@ -0,0 +1,20 @@
+/**
+ * @module ol/format/filter/Or
+ */
+import LogicalNary from './LogicalNary.js';
+
+/**
+ * @classdesc
+ * Represents a logical `<Or>` operator between two ore more filter conditions.
+ * @api
+ */
+class Or extends LogicalNary {
+  /**
+   * @param {...import("./Filter.js").default} conditions Conditions.
+   */
+  constructor(conditions) {
+    super('Or', Array.prototype.slice.call(arguments));
+  }
+}
+
+export default Or;
