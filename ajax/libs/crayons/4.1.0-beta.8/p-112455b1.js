4efb6a24d4b55b59f11fb6a7f6c513901a804ad7
robocdnjs <cdnjs-github@cloudflare.com>
2023-01-10 16:05:45 +0000

    Add crayons (4.1.0-beta.8)

---
+++ ajax/libs/crayons/4.1.0-beta.8/p-112455b1.js
@@ -0,0 +1,1 @@
+var e="undefined"!=typeof globalThis?globalThis:"undefined"!=typeof window?window:"undefined"!=typeof global?global:"undefined"!=typeof self?self:{};function n(e,n,o){return e(o={path:n,exports:{},require:function(){return function(){throw new Error("Dynamic requires are not currently supported by @rollup/plugin-commonjs")}()}},o.exports),o.exports}export{e as a,n as c}
