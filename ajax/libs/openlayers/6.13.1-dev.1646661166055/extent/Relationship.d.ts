2fa2d2f27b348d681a60bfd680a146a3b157d0fb
robocdnjs <cdnjs-github@cloudflare.com>
2023-01-11 02:27:42 +0000

    Add openlayers (6.13.1-dev.1646661166055)

---
+++ ajax/libs/openlayers/6.13.1-dev.1646661166055/extent/Relationship.d.ts
@@ -0,0 +1,10 @@
+declare namespace _default {
+    const UNKNOWN: number;
+    const INTERSECTING: number;
+    const ABOVE: number;
+    const RIGHT: number;
+    const BELOW: number;
+    const LEFT: number;
+}
+export default _default;
+//# sourceMappingURL=Relationship.d.ts.map
---
+++ ajax/libs/openlayers/6.13.1-dev.1646661166055/source/common.d.ts
@@ -0,0 +1,9 @@
+/**
+ * @module ol/source/common
+ */
+/**
+ * Default WMS version.
+ * @type {string}
+ */
+export const DEFAULT_WMS_VERSION: string;
+//# sourceMappingURL=common.d.ts.map
