632bd03a7cb9130af03ada39e62fe0e1649571f2
robocdnjs <cdnjs-github@cloudflare.com>
2023-01-18 08:06:03 +0000

    Add redux-thunk (3.0.0-alpha.1)

---
+++ ajax/libs/redux-thunk/3.0.0-alpha.1/redux-thunk.js
@@ -0,0 +1,44 @@
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+  typeof define === 'function' && define.amd ? define(['exports'], factory) :
+  (global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global.ReduxThunk = {}));
+})(this, (function (exports) { 'use strict';
+
+  /** A function that accepts a potential "extra argument" value to be injected later,
+   * and returns an instance of the thunk middleware that uses that value
+   */
+  function createThunkMiddleware(extraArgument) {
+    // Standard Redux middleware definition pattern:
+    // See: https://redux.js.org/tutorials/fundamentals/part-4-store#writing-custom-middleware
+    var middleware = function middleware(_ref) {
+      var dispatch = _ref.dispatch,
+          getState = _ref.getState;
+      return function (next) {
+        return function (action) {
+          // The thunk middleware looks for any functions that were passed to `store.dispatch`.
+          // If this "action" is really a function, call it and return the result.
+          if (typeof action === 'function') {
+            // Inject the store's `dispatch` and `getState` methods, as well as any "extra arg"
+            return action(dispatch, getState, extraArgument);
+          } // Otherwise, pass the action down the middleware chain as usual
+
+
+          return next(action);
+        };
+      };
+    };
+
+    return middleware;
+  }
+
+  var thunk = createThunkMiddleware(); // Export the factory function so users can create a customized version
+  // with whatever "extra arg" they want to inject into their thunks
+
+  var withExtraArgument = createThunkMiddleware;
+
+  exports.thunk = thunk;
+  exports.withExtraArgument = withExtraArgument;
+
+  Object.defineProperty(exports, '__esModule', { value: true });
+
+}));
