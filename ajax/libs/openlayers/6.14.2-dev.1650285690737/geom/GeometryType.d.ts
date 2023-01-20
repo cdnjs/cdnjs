b0ca198385c558c4a5a495bd8fec0b16493d82f1
robocdnjs <cdnjs-github@cloudflare.com>
2023-01-20 06:06:39 +0000

    Add openlayers (6.14.2-dev.1650285690737)

---
+++ ajax/libs/openlayers/6.14.2-dev.1650285690737/geom/GeometryType.d.ts
@@ -0,0 +1,13 @@
+declare namespace _default {
+    const POINT: string;
+    const LINE_STRING: string;
+    const LINEAR_RING: string;
+    const POLYGON: string;
+    const MULTI_POINT: string;
+    const MULTI_LINE_STRING: string;
+    const MULTI_POLYGON: string;
+    const GEOMETRY_COLLECTION: string;
+    const CIRCLE: string;
+}
+export default _default;
+//# sourceMappingURL=GeometryType.d.ts.map
