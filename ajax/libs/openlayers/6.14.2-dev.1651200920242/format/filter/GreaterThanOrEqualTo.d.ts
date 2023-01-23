b5df20a8c1d96b52487e5cd2422e4f46a5e8e764
robocdnjs <cdnjs-github@cloudflare.com>
2023-01-23 06:05:05 +0000

    Add openlayers (6.14.2-dev.1651200920242)

---
+++ ajax/libs/openlayers/6.14.2-dev.1651200920242/format/filter/GreaterThanOrEqualTo.d.ts
@@ -0,0 +1,15 @@
+export default GreaterThanOrEqualTo;
+/**
+ * @classdesc
+ * Represents a `<PropertyIsGreaterThanOrEqualTo>` comparison operator.
+ * @api
+ */
+declare class GreaterThanOrEqualTo extends ComparisonBinary {
+    /**
+     * @param {!string} propertyName Name of the context property to compare.
+     * @param {!number} expression The value to compare.
+     */
+    constructor(propertyName: string, expression: number);
+}
+import ComparisonBinary from "./ComparisonBinary.js";
+//# sourceMappingURL=GreaterThanOrEqualTo.d.ts.map
