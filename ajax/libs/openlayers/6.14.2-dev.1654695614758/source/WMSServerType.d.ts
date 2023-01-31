728560032985fb8f77e7a585e4cc2d207a6729e4
robocdnjs <cdnjs-github@cloudflare.com>
2023-01-31 13:12:56 +0000

    Add openlayers (6.14.2-dev.1654695614758)

---
+++ ajax/libs/openlayers/6.14.2-dev.1654695614758/source/WMSServerType.d.ts
@@ -0,0 +1,8 @@
+declare namespace _default {
+    const CARMENTA_SERVER: string;
+    const GEOSERVER: string;
+    const MAPSERVER: string;
+    const QGIS: string;
+}
+export default _default;
+//# sourceMappingURL=WMSServerType.d.ts.map
