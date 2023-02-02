903ef761d0804e06223df7a92df428096965f8bd
robocdnjs <cdnjs-github@cloudflare.com>
2023-02-02 06:05:21 +0000

    Add openlayers (6.14.2-dev.1656055195637)

---
+++ ajax/libs/openlayers/6.14.2-dev.1656055195637/geom/flat/reverse.d.ts
@@ -0,0 +1,11 @@
+/**
+ * @module ol/geom/flat/reverse
+ */
+/**
+ * @param {Array<number>} flatCoordinates Flat coordinates.
+ * @param {number} offset Offset.
+ * @param {number} end End.
+ * @param {number} stride Stride.
+ */
+export function coordinates(flatCoordinates: Array<number>, offset: number, end: number, stride: number): void;
+//# sourceMappingURL=reverse.d.ts.map
