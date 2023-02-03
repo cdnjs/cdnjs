2d66e63801d5e628c3ba74a3cef007f21caf259f
robocdnjs <cdnjs-github@cloudflare.com>
2023-02-03 16:08:44 +0000

    Add datatables.net-select-dt (1.6.0)

---
+++ ajax/libs/datatables.net-select-dt/1.6.0/select.dataTables.min.js
@@ -0,0 +1,4 @@
+/*! DataTables styling wrapper for Select
+ * © SpryMedia Ltd - datatables.net/license
+ */
+!function(n){"function"==typeof define&&define.amd?define(["jquery","datatables.net-dt","datatables.net-select"],function(e){return n(e,window,document)}):"object"==typeof exports?module.exports=function(e,t){return e=e||window,(t=t||("undefined"!=typeof window?require("jquery"):require("jquery")(e))).fn.dataTable||require("datatables.net-dt")(e,t),t.fn.dataTable.select||require("datatables.net-select")(e,t),n(t,0,e.document)}:n(jQuery,window,document)}(function(e,t,n,d){"use strict";return e.fn.dataTable});
