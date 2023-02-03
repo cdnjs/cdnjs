8801a6d92240cc629ec3c1a0f053b4ccb1061fbe
robocdnjs <cdnjs-github@cloudflare.com>
2023-02-03 17:04:49 +0000

    Add datatables.net-dt (1.13.2)

---
+++ ajax/libs/datatables.net-dt/1.13.2/js/dataTables.dataTables.min.js
@@ -0,0 +1,4 @@
+/*! DataTables styling integration
+ * ©2018 SpryMedia Ltd - datatables.net/license
+ */
+!function(t){"function"==typeof define&&define.amd?define(["jquery","datatables.net"],function(e){return t(e,window,document)}):"object"==typeof exports?module.exports=function(e,n){return e=e||window,(n=n||("undefined"!=typeof window?require("jquery"):require("jquery")(e))).fn.dataTable||require("datatables.net")(e,n),t(n,0,e.document)}:t(jQuery,window,document)}(function(e,n,t,u){"use strict";return e.fn.dataTable});
