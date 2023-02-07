fff66fa91ac8436c57a1fd7db4879cae09485db0
robocdnjs <cdnjs-github@cloudflare.com>
2023-02-07 11:05:01 +0000

    Add openlayers (6.15.0)

---
+++ ajax/libs/openlayers/6.15.0/geom/Circle.js
@@ -0,0 +1,270 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+/**
+ * @module ol/geom/Circle
+ */
+import SimpleGeometry from './SimpleGeometry.js';
+import { createOrUpdate, forEachCorner, intersects } from '../extent.js';
+import { deflateCoordinate } from './flat/deflate.js';
+import { rotate, translate } from './flat/transform.js';
+/**
+ * @classdesc
+ * Circle geometry.
+ *
+ * @api
+ */
+var Circle = /** @class */ (function (_super) {
+    __extends(Circle, _super);
+    /**
+     * @param {!import("../coordinate.js").Coordinate} center Center.
+     *     For internal use, flat coordinates in combination with `opt_layout` and no
+     *     `opt_radius` are also accepted.
+     * @param {number} [opt_radius] Radius.
+     * @param {import("./GeometryLayout.js").default} [opt_layout] Layout.
+     */
+    function Circle(center, opt_radius, opt_layout) {
+        var _this = _super.call(this) || this;
+        if (opt_layout !== undefined && opt_radius === undefined) {
+            _this.setFlatCoordinates(opt_layout, center);
+        }
+        else {
+            var radius = opt_radius ? opt_radius : 0;
+            _this.setCenterAndRadius(center, radius, opt_layout);
+        }
+        return _this;
+    }
+    /**
+     * Make a complete copy of the geometry.
+     * @return {!Circle} Clone.
+     * @api
+     */
+    Circle.prototype.clone = function () {
+        var circle = new Circle(this.flatCoordinates.slice(), undefined, this.layout);
+        circle.applyProperties(this);
+        return circle;
+    };
+    /**
+     * @param {number} x X.
+     * @param {number} y Y.
+     * @param {import("../coordinate.js").Coordinate} closestPoint Closest point.
+     * @param {number} minSquaredDistance Minimum squared distance.
+     * @return {number} Minimum squared distance.
+     */
+    Circle.prototype.closestPointXY = function (x, y, closestPoint, minSquaredDistance) {
+        var flatCoordinates = this.flatCoordinates;
+        var dx = x - flatCoordinates[0];
+        var dy = y - flatCoordinates[1];
+        var squaredDistance = dx * dx + dy * dy;
+        if (squaredDistance < minSquaredDistance) {
+            if (squaredDistance === 0) {
+                for (var i = 0; i < this.stride; ++i) {
+                    closestPoint[i] = flatCoordinates[i];
+                }
+            }
+            else {
+                var delta = this.getRadius() / Math.sqrt(squaredDistance);
+                closestPoint[0] = flatCoordinates[0] + delta * dx;
+                closestPoint[1] = flatCoordinates[1] + delta * dy;
+                for (var i = 2; i < this.stride; ++i) {
+                    closestPoint[i] = flatCoordinates[i];
+                }
+            }
+            closestPoint.length = this.stride;
+            return squaredDistance;
+        }
+        else {
+            return minSquaredDistance;
+        }
+    };
+    /**
+     * @param {number} x X.
+     * @param {number} y Y.
+     * @return {boolean} Contains (x, y).
+     */
+    Circle.prototype.containsXY = function (x, y) {
+        var flatCoordinates = this.flatCoordinates;
+        var dx = x - flatCoordinates[0];
+        var dy = y - flatCoordinates[1];
+        return dx * dx + dy * dy <= this.getRadiusSquared_();
+    };
+    /**
+     * Return the center of the circle as {@link module:ol/coordinate~Coordinate coordinate}.
+     * @return {import("../coordinate.js").Coordinate} Center.
+     * @api
+     */
+    Circle.prototype.getCenter = function () {
+        return this.flatCoordinates.slice(0, this.stride);
+    };
+    /**
+     * @param {import("../extent.js").Extent} extent Extent.
+     * @protected
+     * @return {import("../extent.js").Extent} extent Extent.
+     */
+    Circle.prototype.computeExtent = function (extent) {
+        var flatCoordinates = this.flatCoordinates;
+        var radius = flatCoordinates[this.stride] - flatCoordinates[0];
+        return createOrUpdate(flatCoordinates[0] - radius, flatCoordinates[1] - radius, flatCoordinates[0] + radius, flatCoordinates[1] + radius, extent);
+    };
+    /**
+     * Return the radius of the circle.
+     * @return {number} Radius.
+     * @api
+     */
+    Circle.prototype.getRadius = function () {
+        return Math.sqrt(this.getRadiusSquared_());
+    };
+    /**
+     * @private
+     * @return {number} Radius squared.
+     */
+    Circle.prototype.getRadiusSquared_ = function () {
+        var dx = this.flatCoordinates[this.stride] - this.flatCoordinates[0];
+        var dy = this.flatCoordinates[this.stride + 1] - this.flatCoordinates[1];
+        return dx * dx + dy * dy;
+    };
+    /**
+     * Get the type of this geometry.
+     * @return {import("./Geometry.js").Type} Geometry type.
+     * @api
+     */
+    Circle.prototype.getType = function () {
+        return 'Circle';
+    };
+    /**
+     * Test if the geometry and the passed extent intersect.
+     * @param {import("../extent.js").Extent} extent Extent.
+     * @return {boolean} `true` if the geometry and the extent intersect.
+     * @api
+     */
+    Circle.prototype.intersectsExtent = function (extent) {
+        var circleExtent = this.getExtent();
+        if (intersects(extent, circleExtent)) {
+            var center = this.getCenter();
+            if (extent[0] <= center[0] && extent[2] >= center[0]) {
+                return true;
+            }
+            if (extent[1] <= center[1] && extent[3] >= center[1]) {
+                return true;
+            }
+            return forEachCorner(extent, this.intersectsCoordinate.bind(this));
+        }
+        return false;
+    };
+    /**
+     * Set the center of the circle as {@link module:ol/coordinate~Coordinate coordinate}.
+     * @param {import("../coordinate.js").Coordinate} center Center.
+     * @api
+     */
+    Circle.prototype.setCenter = function (center) {
+        var stride = this.stride;
+        var radius = this.flatCoordinates[stride] - this.flatCoordinates[0];
+        var flatCoordinates = center.slice();
+        flatCoordinates[stride] = flatCoordinates[0] + radius;
+        for (var i = 1; i < stride; ++i) {
+            flatCoordinates[stride + i] = center[i];
+        }
+        this.setFlatCoordinates(this.layout, flatCoordinates);
+        this.changed();
+    };
+    /**
+     * Set the center (as {@link module:ol/coordinate~Coordinate coordinate}) and the radius (as
+     * number) of the circle.
+     * @param {!import("../coordinate.js").Coordinate} center Center.
+     * @param {number} radius Radius.
+     * @param {import("./GeometryLayout.js").default} [opt_layout] Layout.
+     * @api
+     */
+    Circle.prototype.setCenterAndRadius = function (center, radius, opt_layout) {
+        this.setLayout(opt_layout, center, 0);
+        if (!this.flatCoordinates) {
+            this.flatCoordinates = [];
+        }
+        /** @type {Array<number>} */
+        var flatCoordinates = this.flatCoordinates;
+        var offset = deflateCoordinate(flatCoordinates, 0, center, this.stride);
+        flatCoordinates[offset++] = flatCoordinates[0] + radius;
+        for (var i = 1, ii = this.stride; i < ii; ++i) {
+            flatCoordinates[offset++] = flatCoordinates[i];
+        }
+        flatCoordinates.length = offset;
+        this.changed();
+    };
+    Circle.prototype.getCoordinates = function () {
+        return null;
+    };
+    Circle.prototype.setCoordinates = function (coordinates, opt_layout) { };
+    /**
+     * Set the radius of the circle. The radius is in the units of the projection.
+     * @param {number} radius Radius.
+     * @api
+     */
+    Circle.prototype.setRadius = function (radius) {
+        this.flatCoordinates[this.stride] = this.flatCoordinates[0] + radius;
+        this.changed();
+    };
+    /**
+     * Rotate the geometry around a given coordinate. This modifies the geometry
+     * coordinates in place.
+     * @param {number} angle Rotation angle in counter-clockwise radians.
+     * @param {import("../coordinate.js").Coordinate} anchor The rotation center.
+     * @api
+     */
+    Circle.prototype.rotate = function (angle, anchor) {
+        var center = this.getCenter();
+        var stride = this.getStride();
+        this.setCenter(rotate(center, 0, center.length, stride, angle, anchor, center));
+        this.changed();
+    };
+    /**
+     * Translate the geometry.  This modifies the geometry coordinates in place.  If
+     * instead you want a new geometry, first `clone()` this geometry.
+     * @param {number} deltaX Delta X.
+     * @param {number} deltaY Delta Y.
+     * @api
+     */
+    Circle.prototype.translate = function (deltaX, deltaY) {
+        var center = this.getCenter();
+        var stride = this.getStride();
+        this.setCenter(translate(center, 0, center.length, stride, deltaX, deltaY, center));
+        this.changed();
+    };
+    return Circle;
+}(SimpleGeometry));
+/**
+ * Transform each coordinate of the circle from one coordinate reference system
+ * to another. The geometry is modified in place.
+ * If you do not want the geometry modified in place, first clone() it and
+ * then use this function on the clone.
+ *
+ * Internally a circle is currently represented by two points: the center of
+ * the circle `[cx, cy]`, and the point to the right of the circle
+ * `[cx + r, cy]`. This `transform` function just transforms these two points.
+ * So the resulting geometry is also a circle, and that circle does not
+ * correspond to the shape that would be obtained by transforming every point
+ * of the original circle.
+ *
+ * @param {import("../proj.js").ProjectionLike} source The current projection.  Can be a
+ *     string identifier or a {@link module:ol/proj/Projection~Projection} object.
+ * @param {import("../proj.js").ProjectionLike} destination The desired projection.  Can be a
+ *     string identifier or a {@link module:ol/proj/Projection~Projection} object.
+ * @return {Circle} This geometry.  Note that original geometry is
+ *     modified in place.
+ * @function
+ * @api
+ */
+Circle.prototype.transform;
+export default Circle;
+//# sourceMappingURL=Circle.js.map
---
+++ ajax/libs/openlayers/6.15.0/source/Cluster.js
@@ -0,0 +1,313 @@
+/**
+ * @module ol/source/Cluster
+ */
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+import EventType from '../events/EventType.js';
+import Feature from '../Feature.js';
+import Point from '../geom/Point.js';
+import VectorSource from './Vector.js';
+import { add as addCoordinate, scale as scaleCoordinate } from '../coordinate.js';
+import { assert } from '../asserts.js';
+import { buffer, createEmpty, createOrUpdateFromCoordinate, getCenter, } from '../extent.js';
+import { getUid } from '../util.js';
+/**
+ * @typedef {Object} Options
+ * @property {import("./Source.js").AttributionLike} [attributions] Attributions.
+ * @property {number} [distance=20] Distance in pixels within which features will
+ * be clustered together.
+ * @property {number} [minDistance=0] Minimum distance in pixels between clusters.
+ * Will be capped at the configured distance.
+ * By default no minimum distance is guaranteed. This config can be used to avoid
+ * overlapping icons. As a tradoff, the cluster feature's position will no longer be
+ * the center of all its features.
+ * @property {function(Feature):Point} [geometryFunction]
+ * Function that takes an {@link module:ol/Feature~Feature} as argument and returns an
+ * {@link module:ol/geom/Point~Point} as cluster calculation point for the feature. When a
+ * feature should not be considered for clustering, the function should return
+ * `null`. The default, which works when the underlying source contains point
+ * features only, is
+ * ```js
+ * function(feature) {
+ *   return feature.getGeometry();
+ * }
+ * ```
+ * See {@link module:ol/geom/Polygon~Polygon#getInteriorPoint} for a way to get a cluster
+ * calculation point for polygons.
+ * @property {function(Point, Array<Feature>):Feature} [createCluster]
+ * Function that takes the cluster's center {@link module:ol/geom/Point~Point} and an array
+ * of {@link module:ol/Feature~Feature} included in this cluster. Must return a
+ * {@link module:ol/Feature~Feature} that will be used to render. Default implementation is:
+ * ```js
+ * function(point, features) {
+ *   return new Feature({
+ *     geometry: point,
+ *     features: features
+ *   });
+ * }
+ * ```
+ * @property {VectorSource} [source=null] Source.
+ * @property {boolean} [wrapX=true] Whether to wrap the world horizontally.
+ */
+/**
+ * @classdesc
+ * Layer source to cluster vector data. Works out of the box with point
+ * geometries. For other geometry types, or if not all geometries should be
+ * considered for clustering, a custom `geometryFunction` can be defined.
+ *
+ * If the instance is disposed without also disposing the underlying
+ * source `setSource(null)` has to be called to remove the listener reference
+ * from the wrapped source.
+ * @api
+ */
+var Cluster = /** @class */ (function (_super) {
+    __extends(Cluster, _super);
+    /**
+     * @param {Options} options Cluster options.
+     */
+    function Cluster(options) {
+        var _this = _super.call(this, {
+            attributions: options.attributions,
+            wrapX: options.wrapX,
+        }) || this;
+        /**
+         * @type {number|undefined}
+         * @protected
+         */
+        _this.resolution = undefined;
+        /**
+         * @type {number}
+         * @protected
+         */
+        _this.distance = options.distance !== undefined ? options.distance : 20;
+        /**
+         * @type {number}
+         * @protected
+         */
+        _this.minDistance = options.minDistance || 0;
+        /**
+         * @type {number}
+         * @protected
+         */
+        _this.interpolationRatio = 0;
+        /**
+         * @type {Array<Feature>}
+         * @protected
+         */
+        _this.features = [];
+        /**
+         * @param {Feature} feature Feature.
+         * @return {Point} Cluster calculation point.
+         * @protected
+         */
+        _this.geometryFunction =
+            options.geometryFunction ||
+                function (feature) {
+                    var geometry = /** @type {Point} */ (feature.getGeometry());
+                    assert(geometry.getType() == 'Point', 10); // The default `geometryFunction` can only handle `Point` geometries
+                    return geometry;
+                };
+        /**
+         * @type {function(Point, Array<Feature>):Feature}
+         * @private
+         */
+        _this.createCustomCluster_ = options.createCluster;
+        /**
+         * @type {VectorSource|null}
+         * @protected
+         */
+        _this.source = null;
+        /**
+         * @private
+         */
+        _this.boundRefresh_ = _this.refresh.bind(_this);
+        _this.updateDistance(_this.distance, _this.minDistance);
+        _this.setSource(options.source || null);
+        return _this;
+    }
+    /**
+     * Remove all features from the source.
+     * @param {boolean} [opt_fast] Skip dispatching of {@link module:ol/source/VectorEventType~VectorEventType#removefeature} events.
+     * @api
+     */
+    Cluster.prototype.clear = function (opt_fast) {
+        this.features.length = 0;
+        _super.prototype.clear.call(this, opt_fast);
+    };
+    /**
+     * Get the distance in pixels between clusters.
+     * @return {number} Distance.
+     * @api
+     */
+    Cluster.prototype.getDistance = function () {
+        return this.distance;
+    };
+    /**
+     * Get a reference to the wrapped source.
+     * @return {VectorSource|null} Source.
+     * @api
+     */
+    Cluster.prototype.getSource = function () {
+        return this.source;
+    };
+    /**
+     * @param {import("../extent.js").Extent} extent Extent.
+     * @param {number} resolution Resolution.
+     * @param {import("../proj/Projection.js").default} projection Projection.
+     */
+    Cluster.prototype.loadFeatures = function (extent, resolution, projection) {
+        this.source.loadFeatures(extent, resolution, projection);
+        if (resolution !== this.resolution) {
+            this.resolution = resolution;
+            this.refresh();
+        }
+    };
+    /**
+     * Set the distance within which features will be clusterd together.
+     * @param {number} distance The distance in pixels.
+     * @api
+     */
+    Cluster.prototype.setDistance = function (distance) {
+        this.updateDistance(distance, this.minDistance);
+    };
+    /**
+     * Set the minimum distance between clusters. Will be capped at the
+     * configured distance.
+     * @param {number} minDistance The minimum distance in pixels.
+     * @api
+     */
+    Cluster.prototype.setMinDistance = function (minDistance) {
+        this.updateDistance(this.distance, minDistance);
+    };
+    /**
+     * The configured minimum distance between clusters.
+     * @return {number} The minimum distance in pixels.
+     * @api
+     */
+    Cluster.prototype.getMinDistance = function () {
+        return this.minDistance;
+    };
+    /**
+     * Replace the wrapped source.
+     * @param {VectorSource|null} source The new source for this instance.
+     * @api
+     */
+    Cluster.prototype.setSource = function (source) {
+        if (this.source) {
+            this.source.removeEventListener(EventType.CHANGE, this.boundRefresh_);
+        }
+        this.source = source;
+        if (source) {
+            source.addEventListener(EventType.CHANGE, this.boundRefresh_);
+        }
+        this.refresh();
+    };
+    /**
+     * Handle the source changing.
+     */
+    Cluster.prototype.refresh = function () {
+        this.clear();
+        this.cluster();
+        this.addFeatures(this.features);
+    };
+    /**
+     * Update the distances and refresh the source if necessary.
+     * @param {number} distance The new distance.
+     * @param {number} minDistance The new minimum distance.
+     */
+    Cluster.prototype.updateDistance = function (distance, minDistance) {
+        var ratio = distance === 0 ? 0 : Math.min(minDistance, distance) / distance;
+        var changed = distance !== this.distance || this.interpolationRatio !== ratio;
+        this.distance = distance;
+        this.minDistance = minDistance;
+        this.interpolationRatio = ratio;
+        if (changed) {
+            this.refresh();
+        }
+    };
+    /**
+     * @protected
+     */
+    Cluster.prototype.cluster = function () {
+        if (this.resolution === undefined || !this.source) {
+            return;
+        }
+        var extent = createEmpty();
+        var mapDistance = this.distance * this.resolution;
+        var features = this.source.getFeatures();
+        /** @type {Object<string, true>} */
+        var clustered = {};
+        for (var i = 0, ii = features.length; i < ii; i++) {
+            var feature = features[i];
+            if (!(getUid(feature) in clustered)) {
+                var geometry = this.geometryFunction(feature);
+                if (geometry) {
+                    var coordinates = geometry.getCoordinates();
+                    createOrUpdateFromCoordinate(coordinates, extent);
+                    buffer(extent, mapDistance, extent);
+                    var neighbors = this.source
+                        .getFeaturesInExtent(extent)
+                        .filter(function (neighbor) {
+                        var uid = getUid(neighbor);
+                        if (uid in clustered) {
+                            return false;
+                        }
+                        clustered[uid] = true;
+                        return true;
+                    });
+                    this.features.push(this.createCluster(neighbors, extent));
+                }
+            }
+        }
+    };
+    /**
+     * @param {Array<Feature>} features Features
+     * @param {import("../extent.js").Extent} extent The searched extent for these features.
+     * @return {Feature} The cluster feature.
+     * @protected
+     */
+    Cluster.prototype.createCluster = function (features, extent) {
+        var centroid = [0, 0];
+        for (var i = features.length - 1; i >= 0; --i) {
+            var geometry_1 = this.geometryFunction(features[i]);
+            if (geometry_1) {
+                addCoordinate(centroid, geometry_1.getCoordinates());
+            }
+            else {
+                features.splice(i, 1);
+            }
+        }
+        scaleCoordinate(centroid, 1 / features.length);
+        var searchCenter = getCenter(extent);
+        var ratio = this.interpolationRatio;
+        var geometry = new Point([
+            centroid[0] * (1 - ratio) + searchCenter[0] * ratio,
+            centroid[1] * (1 - ratio) + searchCenter[1] * ratio,
+        ]);
+        if (this.createCustomCluster_) {
+            return this.createCustomCluster_(geometry, features);
+        }
+        else {
+            return new Feature({
+                geometry: geometry,
+                features: features,
+            });
+        }
+    };
+    return Cluster;
+}(VectorSource));
+export default Cluster;
+//# sourceMappingURL=Cluster.js.map
