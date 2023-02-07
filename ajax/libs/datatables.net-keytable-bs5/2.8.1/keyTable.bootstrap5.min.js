eb232075a7abc0350eb65522766fa1ffdf3cdecc
robocdnjs <cdnjs-github@cloudflare.com>
2023-02-07 13:08:59 +0000

    Add datatables.net-keytable-bs5 (2.8.1)

---
+++ ajax/libs/datatables.net-keytable-bs5/2.8.1/keyTable.bootstrap5.min.js
@@ -0,0 +1,4 @@
+/*! Bootstrap 5 styling wrapper for KeyTable
+ * © SpryMedia Ltd - datatables.net/license
+ */
+!function(n){"function"==typeof define&&define.amd?define(["jquery","datatables.net-bs5","datatables.net-keytable"],function(e){return n(e,window,document)}):"object"==typeof exports?module.exports=function(e,t){return e=e||window,(t=t||("undefined"!=typeof window?require("jquery"):require("jquery")(e))).fn.dataTable||require("datatables.net-bs5")(e,t),t.fn.dataTable.KeyTable||require("datatables.net-keytable")(e,t),n(t,0,e.document)}:n(jQuery,window,document)}(function(e,t,n,a){"use strict";return e.fn.dataTable});
