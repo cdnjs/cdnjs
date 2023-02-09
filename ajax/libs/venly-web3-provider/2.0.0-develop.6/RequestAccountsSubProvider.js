e7c5a7ad09f7244ee0c8ac207dee56b017097051
robocdnjs <cdnjs-github@cloudflare.com>
2023-02-09 16:05:08 +0000

    Add venly-web3-provider (2.0.0-develop.6)

---
+++ ajax/libs/venly-web3-provider/2.0.0-develop.6/RequestAccountsSubProvider.js
@@ -0,0 +1,42 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.RequestAccountsSubProvider = void 0;
+const Subprovider = require('@arkane-network/web3-provider-engine/subproviders/subprovider');
+class RequestAccountsSubProvider extends Subprovider {
+    constructor(venlyWalletSubProvider) {
+        super();
+        this.venlyWalletSubProvider = venlyWalletSubProvider;
+    }
+    /**
+     * This method conforms to the web3-provider-engine interface.
+     * It is called internally by the ProviderEngine when it is this subproviders
+     * turn to handle a JSON RPC request.
+     * @param payload JSON RPC payload
+     * @param next Callback to call if this subprovider decides not to handle the request
+     * @param end Callback to call if subprovider handled the request and wants to pass back the request.
+     */
+    // tslint:disable-next-line:prefer-function-over-method async-suffix
+    handleRequest(payload, next, end) {
+        return __awaiter(this, void 0, void 0, function* () {
+            switch (payload.method) {
+                case 'eth_requestAccounts':
+                    let result = yield this.venlyWalletSubProvider.getAccountsAsync();
+                    end(null, result);
+                    return;
+                default:
+                    next();
+                    return;
+            }
+        });
+    }
+}
+exports.RequestAccountsSubProvider = RequestAccountsSubProvider;
