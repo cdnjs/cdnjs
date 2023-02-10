38596f4e570e383540ad15e64f50289b686db8dc
robocdnjs <cdnjs-github@cloudflare.com>
2023-02-10 01:35:21 +0000

    Add ionicons (6.1.3)

---
+++ ajax/libs/ionicons/6.1.3/ionicons/ionicons.esm.js
@@ -0,0 +1,1 @@
+import{p as i,b as o}from"./p-d15ec307.js";(()=>{const o=import.meta.url,e={};return""!==o&&(e.resourcesUrl=new URL(".",o).href),i(e)})().then((i=>o([["p-6329494e",[[1,"ion-icon",{mode:[1025],color:[1],ios:[1],md:[1],flipRtl:[4,"flip-rtl"],name:[513],src:[1],icon:[8],size:[1],lazy:[4],sanitize:[4],svgContent:[32],isVisible:[32],ariaLabel:[32]}]]]],i)));
