f1c1bb13c564ff2c27ddc93354f9f3225967301b
robocdnjs <cdnjs-github@cloudflare.com>
2023-02-11 01:32:53 +0000

    Add openlayers (7.0.0-dev.1659209967753)

---
+++ ajax/libs/openlayers/7.0.0-dev.1659209967753/string.d.ts
@@ -0,0 +1,18 @@
+/**
+ * @module ol/string
+ */
+/**
+ * @param {number} number Number to be formatted
+ * @param {number} width The desired width
+ * @param {number} [opt_precision] Precision of the output string (i.e. number of decimal places)
+ * @return {string} Formatted string
+ */
+export function padNumber(number: number, width: number, opt_precision?: number | undefined): string;
+/**
+ * Adapted from https://github.com/omichelsen/compare-versions/blob/master/index.js
+ * @param {string|number} v1 First version
+ * @param {string|number} v2 Second version
+ * @return {number} Value
+ */
+export function compareVersions(v1: string | number, v2: string | number): number;
+//# sourceMappingURL=string.d.ts.map
