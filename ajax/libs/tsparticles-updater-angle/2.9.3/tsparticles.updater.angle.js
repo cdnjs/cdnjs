660aeeeef747b63bb515334a66da8956e0834b25
robocdnjs <cdnjs-github@cloudflare.com>
2023-02-12 21:13:27 +0000

    Add tsparticles-updater-angle (2.9.3)

---
+++ ajax/libs/tsparticles-updater-angle/2.9.3/tsparticles.updater.angle.js
@@ -0,0 +1,259 @@
+/*!
+ * Author : Matteo Bruni
+ * MIT license: https://opensource.org/licenses/MIT
+ * Demo / Generator : https://particles.js.org/
+ * GitHub : https://www.github.com/matteobruni/tsparticles
+ * How to use? : Check the GitHub README
+ * v2.9.3
+ */
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory(require("tsparticles-engine"));
+	else if(typeof define === 'function' && define.amd)
+		define(["tsparticles-engine"], factory);
+	else {
+		var a = typeof exports === 'object' ? factory(require("tsparticles-engine")) : factory(root["window"]);
+		for(var i in a) (typeof exports === 'object' ? exports : root)[i] = a[i];
+	}
+})(this, (__WEBPACK_EXTERNAL_MODULE__961__) => {
+return /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ 961:
+/***/ ((module) => {
+
+module.exports = __WEBPACK_EXTERNAL_MODULE__961__;
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "loadAngleUpdater": () => (/* binding */ loadAngleUpdater)
+});
+
+// EXTERNAL MODULE: external {"commonjs":"tsparticles-engine","commonjs2":"tsparticles-engine","amd":"tsparticles-engine","root":"window"}
+var external_commonjs_tsparticles_engine_commonjs2_tsparticles_engine_amd_tsparticles_engine_root_window_ = __webpack_require__(961);
+;// CONCATENATED MODULE: ./dist/browser/Options/Classes/RotateAnimation.js
+
+class RotateAnimation {
+  constructor() {
+    this.enable = false;
+    this.speed = 0;
+    this.decay = 0;
+    this.sync = false;
+  }
+  load(data) {
+    if (!data) {
+      return;
+    }
+    if (data.enable !== undefined) {
+      this.enable = data.enable;
+    }
+    if (data.speed !== undefined) {
+      this.speed = (0,external_commonjs_tsparticles_engine_commonjs2_tsparticles_engine_amd_tsparticles_engine_root_window_.setRangeValue)(data.speed);
+    }
+    if (data.decay !== undefined) {
+      this.decay = (0,external_commonjs_tsparticles_engine_commonjs2_tsparticles_engine_amd_tsparticles_engine_root_window_.setRangeValue)(data.decay);
+    }
+    if (data.sync !== undefined) {
+      this.sync = data.sync;
+    }
+  }
+}
+;// CONCATENATED MODULE: ./dist/browser/Options/Classes/Rotate.js
+
+
+class Rotate extends external_commonjs_tsparticles_engine_commonjs2_tsparticles_engine_amd_tsparticles_engine_root_window_.ValueWithRandom {
+  constructor() {
+    super();
+    this.animation = new RotateAnimation();
+    this.direction = "clockwise";
+    this.path = false;
+    this.value = 0;
+  }
+  load(data) {
+    if (!data) {
+      return;
+    }
+    super.load(data);
+    if (data.direction !== undefined) {
+      this.direction = data.direction;
+    }
+    this.animation.load(data.animation);
+    if (data.path !== undefined) {
+      this.path = data.path;
+    }
+  }
+}
+;// CONCATENATED MODULE: ./dist/browser/RotateUpdater.js
+
+
+function updateAngle(particle, delta) {
+  var _a, _b;
+  const rotate = particle.rotate,
+    rotateOptions = particle.options.rotate;
+  if (!rotate || !rotateOptions) {
+    return;
+  }
+  const rotateAnimation = rotateOptions.animation,
+    speed = ((_a = rotate.velocity) !== null && _a !== void 0 ? _a : 0) * delta.factor,
+    max = 2 * Math.PI,
+    decay = (_b = rotate.decay) !== null && _b !== void 0 ? _b : 1;
+  if (!rotateAnimation.enable) {
+    return;
+  }
+  switch (rotate.status) {
+    case "increasing":
+      rotate.value += speed;
+      if (rotate.value > max) {
+        rotate.value -= max;
+      }
+      break;
+    case "decreasing":
+    default:
+      rotate.value -= speed;
+      if (rotate.value < 0) {
+        rotate.value += max;
+      }
+      break;
+  }
+  if (rotate.velocity && decay !== 1) {
+    rotate.velocity *= decay;
+  }
+}
+class RotateUpdater {
+  constructor(container) {
+    this.container = container;
+  }
+  init(particle) {
+    const rotateOptions = particle.options.rotate;
+    if (!rotateOptions) {
+      return;
+    }
+    particle.rotate = {
+      enable: rotateOptions.animation.enable,
+      value: (0,external_commonjs_tsparticles_engine_commonjs2_tsparticles_engine_amd_tsparticles_engine_root_window_.getRangeValue)(rotateOptions.value) * Math.PI / 180
+    };
+    particle.pathRotation = rotateOptions.path;
+    let rotateDirection = rotateOptions.direction;
+    if (rotateDirection === "random") {
+      const index = Math.floor((0,external_commonjs_tsparticles_engine_commonjs2_tsparticles_engine_amd_tsparticles_engine_root_window_.getRandom)() * 2);
+      rotateDirection = index > 0 ? "counter-clockwise" : "clockwise";
+    }
+    switch (rotateDirection) {
+      case "counter-clockwise":
+      case "counterClockwise":
+        particle.rotate.status = "decreasing";
+        break;
+      case "clockwise":
+        particle.rotate.status = "increasing";
+        break;
+    }
+    const rotateAnimation = rotateOptions.animation;
+    if (rotateAnimation.enable) {
+      particle.rotate.decay = 1 - (0,external_commonjs_tsparticles_engine_commonjs2_tsparticles_engine_amd_tsparticles_engine_root_window_.getRangeValue)(rotateAnimation.decay);
+      particle.rotate.velocity = (0,external_commonjs_tsparticles_engine_commonjs2_tsparticles_engine_amd_tsparticles_engine_root_window_.getRangeValue)(rotateAnimation.speed) / 360 * this.container.retina.reduceFactor;
+      if (!rotateAnimation.sync) {
+        particle.rotate.velocity *= (0,external_commonjs_tsparticles_engine_commonjs2_tsparticles_engine_amd_tsparticles_engine_root_window_.getRandom)();
+      }
+    }
+    particle.rotation = particle.rotate.value;
+  }
+  isEnabled(particle) {
+    const rotate = particle.options.rotate;
+    if (!rotate) {
+      return false;
+    }
+    return !particle.destroyed && !particle.spawning && rotate.animation.enable && !rotate.path;
+  }
+  loadOptions(options, ...sources) {
+    if (!options.rotate) {
+      options.rotate = new Rotate();
+    }
+    for (const source of sources) {
+      options.rotate.load(source === null || source === void 0 ? void 0 : source.rotate);
+    }
+  }
+  update(particle, delta) {
+    var _a, _b;
+    if (!this.isEnabled(particle)) {
+      return;
+    }
+    updateAngle(particle, delta);
+    particle.rotation = (_b = (_a = particle.rotate) === null || _a === void 0 ? void 0 : _a.value) !== null && _b !== void 0 ? _b : 0;
+  }
+}
+;// CONCATENATED MODULE: ./dist/browser/index.js
+
+async function loadAngleUpdater(engine) {
+  await engine.addParticleUpdater("rotate", container => new RotateUpdater(container));
+}
+})();
+
+/******/ 	return __webpack_exports__;
+/******/ })()
+;
+});
