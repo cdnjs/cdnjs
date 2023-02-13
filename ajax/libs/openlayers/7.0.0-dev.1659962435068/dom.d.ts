fae71bc752cc19f440da945a2eee686d9bac5d14
robocdnjs <cdnjs-github@cloudflare.com>
2023-02-13 16:07:21 +0000

    Add openlayers (7.0.0-dev.1659962435068)

---
+++ ajax/libs/openlayers/7.0.0-dev.1659962435068/dom.d.ts
@@ -0,0 +1,58 @@
+/**
+ * @module ol/dom
+ */
+/**
+ * Create an html canvas element and returns its 2d context.
+ * @param {number} [opt_width] Canvas width.
+ * @param {number} [opt_height] Canvas height.
+ * @param {Array<HTMLCanvasElement>} [opt_canvasPool] Canvas pool to take existing canvas from.
+ * @param {CanvasRenderingContext2DSettings} [opt_Context2DSettings] CanvasRenderingContext2DSettings
+ * @return {CanvasRenderingContext2D} The context.
+ */
+export function createCanvasContext2D(opt_width?: number | undefined, opt_height?: number | undefined, opt_canvasPool?: HTMLCanvasElement[] | undefined, opt_Context2DSettings?: CanvasRenderingContext2DSettings | undefined): CanvasRenderingContext2D;
+/**
+ * Releases canvas memory to avoid exceeding memory limits in Safari.
+ * See https://pqina.nl/blog/total-canvas-memory-use-exceeds-the-maximum-limit/
+ * @param {CanvasRenderingContext2D} context Context.
+ */
+export function releaseCanvas(context: CanvasRenderingContext2D): void;
+/**
+ * Get the current computed width for the given element including margin,
+ * padding and border.
+ * Equivalent to jQuery's `$(el).outerWidth(true)`.
+ * @param {!HTMLElement} element Element.
+ * @return {number} The width.
+ */
+export function outerWidth(element: HTMLElement): number;
+/**
+ * Get the current computed height for the given element including margin,
+ * padding and border.
+ * Equivalent to jQuery's `$(el).outerHeight(true)`.
+ * @param {!HTMLElement} element Element.
+ * @return {number} The height.
+ */
+export function outerHeight(element: HTMLElement): number;
+/**
+ * @param {Node} newNode Node to replace old node
+ * @param {Node} oldNode The node to be replaced
+ */
+export function replaceNode(newNode: Node, oldNode: Node): void;
+/**
+ * @param {Node} node The node to remove.
+ * @return {Node|null} The node that was removed or null.
+ */
+export function removeNode(node: Node): Node | null;
+/**
+ * @param {Node} node The node to remove the children from.
+ */
+export function removeChildren(node: Node): void;
+/**
+ * Transform the children of a parent node so they match the
+ * provided list of children.  This function aims to efficiently
+ * remove, add, and reorder child nodes while maintaining a simple
+ * implementation (it is not guaranteed to minimize DOM operations).
+ * @param {Node} node The parent node whose children need reworking.
+ * @param {Array<Node>} children The desired children.
+ */
+export function replaceChildren(node: Node, children: Array<Node>): void;
+//# sourceMappingURL=dom.d.ts.map
---
+++ ajax/libs/openlayers/7.0.0-dev.1659962435068/render/canvas/ImageBuilder.d.ts
@@ -0,0 +1,86 @@
+export default CanvasImageBuilder;
+declare class CanvasImageBuilder extends CanvasBuilder {
+    /**
+     * @private
+     * @type {HTMLCanvasElement|HTMLVideoElement|HTMLImageElement}
+     */
+    private hitDetectionImage_;
+    /**
+     * @private
+     * @type {HTMLCanvasElement|HTMLVideoElement|HTMLImageElement}
+     */
+    private image_;
+    /**
+     * @private
+     * @type {number|undefined}
+     */
+    private imagePixelRatio_;
+    /**
+     * @private
+     * @type {number|undefined}
+     */
+    private anchorX_;
+    /**
+     * @private
+     * @type {number|undefined}
+     */
+    private anchorY_;
+    /**
+     * @private
+     * @type {number|undefined}
+     */
+    private height_;
+    /**
+     * @private
+     * @type {number|undefined}
+     */
+    private opacity_;
+    /**
+     * @private
+     * @type {number|undefined}
+     */
+    private originX_;
+    /**
+     * @private
+     * @type {number|undefined}
+     */
+    private originY_;
+    /**
+     * @private
+     * @type {boolean|undefined}
+     */
+    private rotateWithView_;
+    /**
+     * @private
+     * @type {number|undefined}
+     */
+    private rotation_;
+    /**
+     * @private
+     * @type {import("../../size.js").Size|undefined}
+     */
+    private scale_;
+    /**
+     * @private
+     * @type {number|undefined}
+     */
+    private width_;
+    /**
+     * @private
+     * @type {"declutter"|"obstacle"|"none"|undefined}
+     */
+    private declutterMode_;
+    /**
+     * Data shared with a text builder for combined decluttering.
+     * @private
+     * @type {import("../canvas.js").DeclutterImageWithText}
+     */
+    private declutterImageWithText_;
+    /**
+     * @param {import("../../style/Image.js").default} imageStyle Image style.
+     * @param {Object} [opt_sharedData] Shared data.
+     */
+    setImageStyle(imageStyle: import("../../style/Image.js").default, opt_sharedData?: any): void;
+}
+import CanvasBuilder from "./Builder.js";
+//# sourceMappingURL=ImageBuilder.d.ts.map
