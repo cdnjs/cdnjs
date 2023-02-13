36758945d5026badd4608b27a063fea51cbe20d9
robocdnjs <cdnjs-github@cloudflare.com>
2023-02-13 02:29:44 +0000

    Add tsparticles-shape-text (2.9.3)

---
+++ ajax/libs/tsparticles-shape-text/2.9.3/tsparticles.shape.text.js
@@ -0,0 +1,162 @@
+/*!
+ * Author : Matteo Bruni
+ * MIT license: https://opensource.org/licenses/MIT
+ * Demo / Generator : https://particles.js.org/
+ * GitHub : https://www.github.com/matteobruni/tsparticles
+ * How to use? : Check the GitHub README
+ * v2.9.3
+ */
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory(require("tsparticles-engine"));
+	else if(typeof define === 'function' && define.amd)
+		define(["tsparticles-engine"], factory);
+	else {
+		var a = typeof exports === 'object' ? factory(require("tsparticles-engine")) : factory(root["window"]);
+		for(var i in a) (typeof exports === 'object' ? exports : root)[i] = a[i];
+	}
+})(this, (__WEBPACK_EXTERNAL_MODULE__961__) => {
+return /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ 961:
+/***/ ((module) => {
+
+module.exports = __WEBPACK_EXTERNAL_MODULE__961__;
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "loadTextShape": () => (/* binding */ loadTextShape)
+});
+
+// EXTERNAL MODULE: external {"commonjs":"tsparticles-engine","commonjs2":"tsparticles-engine","amd":"tsparticles-engine","root":"window"}
+var external_commonjs_tsparticles_engine_commonjs2_tsparticles_engine_amd_tsparticles_engine_root_window_ = __webpack_require__(961);
+;// CONCATENATED MODULE: ./dist/browser/TextDrawer.js
+
+const validTypes = ["text", "character", "char"];
+class TextDrawer {
+  draw(context, particle, radius, opacity) {
+    var _a, _b, _c;
+    const character = particle.shapeData;
+    if (character === undefined) {
+      return;
+    }
+    const textData = character.value;
+    if (textData === undefined) {
+      return;
+    }
+    const textParticle = particle;
+    if (textParticle.text === undefined) {
+      textParticle.text = (0,external_commonjs_tsparticles_engine_commonjs2_tsparticles_engine_amd_tsparticles_engine_root_window_.itemFromSingleOrMultiple)(textData, particle.randomIndexData);
+    }
+    const text = textParticle.text,
+      style = (_a = character.style) !== null && _a !== void 0 ? _a : "",
+      weight = (_b = character.weight) !== null && _b !== void 0 ? _b : "400",
+      size = Math.round(radius) * 2,
+      font = (_c = character.font) !== null && _c !== void 0 ? _c : "Verdana",
+      fill = particle.fill,
+      offsetX = text.length * radius / 2;
+    context.font = `${style} ${weight} ${size}px "${font}"`;
+    const pos = {
+      x: -offsetX,
+      y: radius / 2
+    };
+    context.globalAlpha = opacity;
+    if (fill) {
+      context.fillText(text, pos.x, pos.y);
+    } else {
+      context.strokeText(text, pos.x, pos.y);
+    }
+    context.globalAlpha = 1;
+  }
+  getSidesCount() {
+    return 12;
+  }
+  async init(container) {
+    const options = container.actualOptions;
+    if (validTypes.find(t => (0,external_commonjs_tsparticles_engine_commonjs2_tsparticles_engine_amd_tsparticles_engine_root_window_.isInArray)(t, options.particles.shape.type))) {
+      const shapeOptions = validTypes.map(t => options.particles.shape.options[t]).find(t => !!t),
+        promises = [];
+      (0,external_commonjs_tsparticles_engine_commonjs2_tsparticles_engine_amd_tsparticles_engine_root_window_.executeOnSingleOrMultiple)(shapeOptions, shape => {
+        promises.push((0,external_commonjs_tsparticles_engine_commonjs2_tsparticles_engine_amd_tsparticles_engine_root_window_.loadFont)(shape.font, shape.weight));
+      });
+      await Promise.all(promises);
+    }
+  }
+}
+;// CONCATENATED MODULE: ./dist/browser/index.js
+
+async function loadTextShape(engine) {
+  await engine.addShape(validTypes, new TextDrawer());
+}
+})();
+
+/******/ 	return __webpack_exports__;
+/******/ })()
+;
+});
