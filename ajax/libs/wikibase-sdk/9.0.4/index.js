3a881dec36aecf4c2146080effd708d6605f75bd
robocdnjs <cdnjs-github@cloudflare.com>
2023-02-18 16:04:40 +0000

    Add wikibase-sdk (9.0.4)

---
+++ ajax/libs/wikibase-sdk/9.0.4/index.js
@@ -0,0 +1,9 @@
+import { WBK as _WBK } from './wikibase-sdk.js';
+export default _WBK;
+export const WBK = _WBK;
+export * from './helpers/helpers.js';
+export * from './helpers/rank.js';
+export * from './helpers/sitelinks.js';
+export { parse } from './helpers/parse_responses.js';
+export { simplify } from './helpers/simplify.js';
+//# sourceMappingURL=index.js.map
