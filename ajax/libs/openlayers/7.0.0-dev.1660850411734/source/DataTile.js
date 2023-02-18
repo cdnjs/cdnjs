500292fe0db33eb4b63c1cefd077cabd63e866d9
robocdnjs <cdnjs-github@cloudflare.com>
2023-02-18 10:04:47 +0000

    Add openlayers (7.0.0-dev.1660850411734)

---
+++ ajax/libs/openlayers/7.0.0-dev.1660850411734/source/DataTile.js
@@ -0,0 +1,233 @@
+/**
+ * @module ol/source/DataTile
+ */
+import DataTile from '../DataTile.js';
+import EventType from '../events/EventType.js';
+import TileEventType from './TileEventType.js';
+import TileSource, {TileSourceEvent} from './Tile.js';
+import TileState from '../TileState.js';
+import {createXYZ, extentFromProjection} from '../tilegrid.js';
+import {getKeyZXY} from '../tilecoord.js';
+import {getUid} from '../util.js';
+import {toPromise} from '../functions.js';
+import {toSize} from '../size.js';
+
+/**
+ * Data tile loading function.  The function is called with z, x, and y tile coordinates and
+ * returns {@link import("../DataTile.js").Data data} for a tile or a promise for the same.
+ * @typedef {function(number, number, number) : (import("../DataTile.js").Data|Promise<import("../DataTile.js").Data>)} Loader
+ */
+
+/**
+ * @typedef {Object} Options
+ * @property {Loader} [loader] Data loader.  Called with z, x, and y tile coordinates.
+ * Returns {@link import("../DataTile.js").Data data} for a tile or a promise for the same.
+ * @property {import("./Source.js").AttributionLike} [attributions] Attributions.
+ * @property {boolean} [attributionsCollapsible=true] Attributions are collapsible.
+ * @property {number} [maxZoom=42] Optional max zoom level. Not used if `tileGrid` is provided.
+ * @property {number} [minZoom=0] Optional min zoom level. Not used if `tileGrid` is provided.
+ * @property {number|import("../size.js").Size} [tileSize=[256, 256]] The pixel width and height of the source tiles.
+ * This may be different than the rendered pixel size if a `tileGrid` is provided.
+ * @property {number} [gutter=0] The size in pixels of the gutter around data tiles to ignore.
+ * This allows artifacts of rendering at tile edges to be ignored.
+ * Supported data should be wider and taller than the tile size by a value of `2 x gutter`.
+ * @property {number} [maxResolution] Optional tile grid resolution at level zero. Not used if `tileGrid` is provided.
+ * @property {import("../proj.js").ProjectionLike} [projection='EPSG:3857'] Tile projection.
+ * @property {import("../tilegrid/TileGrid.js").default} [tileGrid] Tile grid.
+ * @property {boolean} [opaque=false] Whether the layer is opaque.
+ * @property {import("./Source.js").State} [state] The source state.
+ * @property {boolean} [wrapX=false] Render tiles beyond the antimeridian.
+ * @property {number} [transition] Transition time when fading in new tiles (in miliseconds).
+ * @property {number} [bandCount=4] Number of bands represented in the data.
+ * @property {boolean} [interpolate=false] Use interpolated values when resampling.  By default,
+ * the nearest neighbor is used when resampling.
+ */
+
+/**
+ * @classdesc
+ * A source for typed array data tiles.
+ *
+ * @fires import("./Tile.js").TileSourceEvent
+ * @api
+ */
+class DataTileSource extends TileSource {
+  /**
+   * @param {Options} options Image tile options.
+   */
+  constructor(options) {
+    const projection =
+      options.projection === undefined ? 'EPSG:3857' : options.projection;
+
+    let tileGrid = options.tileGrid;
+    if (tileGrid === undefined && projection) {
+      tileGrid = createXYZ({
+        extent: extentFromProjection(projection),
+        maxResolution: options.maxResolution,
+        maxZoom: options.maxZoom,
+        minZoom: options.minZoom,
+        tileSize: options.tileSize,
+      });
+    }
+
+    super({
+      cacheSize: 0.1, // don't cache on the source
+      attributions: options.attributions,
+      attributionsCollapsible: options.attributionsCollapsible,
+      projection: projection,
+      tileGrid: tileGrid,
+      opaque: options.opaque,
+      state: options.state,
+      wrapX: options.wrapX,
+      transition: options.transition,
+      interpolate: options.interpolate,
+    });
+
+    /**
+     * @private
+     * @type {number}
+     */
+    this.gutter_ = options.gutter !== undefined ? options.gutter : 0;
+
+    /**
+     * @private
+     * @type {import('../size.js').Size|null}
+     */
+    this.tileSize_ = options.tileSize ? toSize(options.tileSize) : null;
+
+    /**
+     * @private
+     * @type {Array<import('../size.js').Size>|null}
+     */
+    this.tileSizes_ = null;
+
+    /**
+     * @private
+     * @type {!Object<string, boolean>}
+     */
+    this.tileLoadingKeys_ = {};
+
+    /**
+     * @private
+     */
+    this.loader_ = options.loader;
+
+    this.handleTileChange_ = this.handleTileChange_.bind(this);
+
+    /**
+     * @type {number}
+     */
+    this.bandCount = options.bandCount === undefined ? 4 : options.bandCount; // assume RGBA if undefined
+  }
+
+  /**
+   * Set the source tile sizes.  The length of the array is expected to match the number of
+   * levels in the tile grid.
+   * @protected
+   * @param {Array<import('../size.js').Size>} tileSizes An array of tile sizes.
+   */
+  setTileSizes(tileSizes) {
+    this.tileSizes_ = tileSizes;
+  }
+
+  /**
+   * Get the source tile size at the given zoom level.  This may be different than the rendered tile
+   * size.
+   * @protected
+   * @param {number} z Tile zoom level.
+   * @return {import('../size.js').Size} The source tile size.
+   */
+  getTileSize(z) {
+    if (this.tileSizes_) {
+      return this.tileSizes_[z];
+    }
+    if (this.tileSize_) {
+      return this.tileSize_;
+    }
+    const tileGrid = this.getTileGrid();
+    return tileGrid ? toSize(tileGrid.getTileSize(z)) : [256, 256];
+  }
+
+  /**
+   * @param {import("../proj/Projection.js").default} projection Projection.
+   * @return {number} Gutter.
+   */
+  getGutterForProjection(projection) {
+    return this.gutter_;
+  }
+
+  /**
+   * @param {Loader} loader The data loader.
+   * @protected
+   */
+  setLoader(loader) {
+    this.loader_ = loader;
+  }
+
+  /**
+   * @param {number} z Tile coordinate z.
+   * @param {number} x Tile coordinate x.
+   * @param {number} y Tile coordinate y.
+   * @param {number} pixelRatio Pixel ratio.
+   * @param {import("../proj/Projection.js").default} projection Projection.
+   * @return {!DataTile} Tile.
+   */
+  getTile(z, x, y, pixelRatio, projection) {
+    const size = this.getTileSize(z);
+    const tileCoordKey = getKeyZXY(z, x, y);
+    if (this.tileCache.containsKey(tileCoordKey)) {
+      return this.tileCache.get(tileCoordKey);
+    }
+
+    const sourceLoader = this.loader_;
+
+    function loader() {
+      return toPromise(function () {
+        return sourceLoader(z, x, y);
+      });
+    }
+
+    const options = Object.assign(
+      {
+        tileCoord: [z, x, y],
+        loader: loader,
+        size: size,
+      },
+      this.tileOptions
+    );
+
+    const tile = new DataTile(options);
+    tile.key = this.getKey();
+    tile.addEventListener(EventType.CHANGE, this.handleTileChange_);
+
+    this.tileCache.set(tileCoordKey, tile);
+    return tile;
+  }
+
+  /**
+   * Handle tile change events.
+   * @param {import("../events/Event.js").default} event Event.
+   */
+  handleTileChange_(event) {
+    const tile = /** @type {import("../Tile.js").default} */ (event.target);
+    const uid = getUid(tile);
+    const tileState = tile.getState();
+    let type;
+    if (tileState == TileState.LOADING) {
+      this.tileLoadingKeys_[uid] = true;
+      type = TileEventType.TILELOADSTART;
+    } else if (uid in this.tileLoadingKeys_) {
+      delete this.tileLoadingKeys_[uid];
+      type =
+        tileState == TileState.ERROR
+          ? TileEventType.TILELOADERROR
+          : tileState == TileState.LOADED
+          ? TileEventType.TILELOADEND
+          : undefined;
+    }
+    if (type) {
+      this.dispatchEvent(new TileSourceEvent(type, tile));
+    }
+  }
+}
+
+export default DataTileSource;
---
+++ ajax/libs/openlayers/7.0.0-dev.1660850411734/source/IIIF.d.ts
@@ -0,0 +1,113 @@
+export default IIIF;
+export type Options = {
+    /**
+     * Attributions.
+     */
+    attributions?: import("./Source.js").AttributionLike | undefined;
+    /**
+     * Attributions are collapsible.
+     */
+    attributionsCollapsible?: boolean | undefined;
+    /**
+     * Size of the cache.
+     */
+    cacheSize?: number | undefined;
+    /**
+     * The value for the crossOrigin option of the request.
+     */
+    crossOrigin?: string | null | undefined;
+    /**
+     * The extent.
+     */
+    extent?: import("../extent.js").Extent | undefined;
+    /**
+     * Requested image format.
+     */
+    format?: string | undefined;
+    /**
+     * Use interpolated values when resampling.  By default,
+     * linear interpolation is used when resampling.  Set to false to use the nearest neighbor instead.
+     */
+    interpolate?: boolean | undefined;
+    /**
+     * Projection.
+     */
+    projection?: import("../proj.js").ProjectionLike;
+    /**
+     * Requested IIIF image quality. Default is 'native'
+     * for version 1, 'default' for versions 2 and 3.
+     */
+    quality?: string | undefined;
+    /**
+     * Maximum allowed reprojection error (in pixels).
+     * Higher values can increase reprojection performance, but decrease precision.
+     */
+    reprojectionErrorThreshold?: number | undefined;
+    /**
+     * Supported resolutions as given in IIIF 'scaleFactors'
+     */
+    resolutions?: number[] | undefined;
+    /**
+     * Size of the image [width, height].
+     */
+    size: import("../size.js").Size;
+    /**
+     * Supported scaled image sizes.
+     * Content of the IIIF info.json 'sizes' property, but as array of Size objects.
+     */
+    sizes?: import("../size.js").Size[] | undefined;
+    /**
+     * Source state.
+     */
+    state?: import("./Source.js").State | undefined;
+    /**
+     * Supported IIIF region and size calculation
+     * features.
+     */
+    supports?: string[] | undefined;
+    /**
+     * Tile pixel ratio.
+     */
+    tilePixelRatio?: number | undefined;
+    /**
+     * Tile size.
+     * Same tile size is used for all zoom levels. If tile size is a number,
+     * a square tile is assumed. If the IIIF image service supports arbitrary
+     * tiling (sizeByH, sizeByW, sizeByWh or sizeByPct as well as regionByPx or regionByPct
+     * are supported), the default tilesize is 256.
+     */
+    tileSize?: number | import("../size.js").Size | undefined;
+    /**
+     * Transition.
+     */
+    transition?: number | undefined;
+    /**
+     * Base URL of the IIIF Image service.
+     * This should be the same as the IIIF Image ID.
+     */
+    url?: string | undefined;
+    /**
+     * Service's IIIF Image API version.
+     */
+    version?: string | undefined;
+    /**
+     * Choose whether to use tiles with a higher or lower zoom level when between integer
+     * zoom levels. See {@link module :ol/tilegrid/TileGrid~TileGrid#getZForResolution}.
+     */
+    zDirection?: number | import("../array.js").NearestDirectionFunction | undefined;
+};
+/**
+ * @classdesc
+ * Layer source for IIIF Image API services.
+ * @api
+ */
+declare class IIIF extends TileImage {
+    /**
+     * @param {Options} [options] Tile source options. Use {@link import("../format/IIIFInfo.js").IIIFInfo}
+     * to parse Image API service information responses into constructor options.
+     * @api
+     */
+    constructor(options?: Options | undefined);
+}
+import TileImage from "./TileImage.js";
+//# sourceMappingURL=IIIF.d.ts.map
