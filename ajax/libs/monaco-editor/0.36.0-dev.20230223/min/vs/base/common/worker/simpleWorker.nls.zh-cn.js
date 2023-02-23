c6aa1e9c0f3efbb1019e81bd4a550edc4995cbf9
robocdnjs <cdnjs-github@cloudflare.com>
2023-02-23 15:06:16 +0000

    Add monaco-editor (0.36.0-dev.20230223)

---
+++ ajax/libs/monaco-editor/0.36.0-dev.20230223/min/vs/base/common/worker/simpleWorker.nls.zh-cn.js
@@ -0,0 +1,8 @@
+/*!-----------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Version: 0.36.0-dev.20230223(fb0d6bada18de72e731ba0e0a4e3717879985f66)
+ * Released under the MIT license
+ * https://github.com/microsoft/vscode/blob/main/LICENSE.txt
+ *-----------------------------------------------------------*/define("vs/base/common/worker/simpleWorker.nls.zh-cn",{"vs/base/common/platform":["_"]});
+
+//# sourceMappingURL=../../../../../min-maps/vs/base/common/worker/simpleWorker.nls.zh-cn.js.map
