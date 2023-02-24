0c6f77c5340c5d02e2eccd913b946f2393bdc6bf
robocdnjs <cdnjs-github@cloudflare.com>
2023-02-24 17:06:24 +0000

    Add openlayers (7.0.1-dev.1661770872456)

---
+++ ajax/libs/openlayers/7.0.1-dev.1661770872456/MapEvent.d.ts
@@ -0,0 +1,28 @@
+export default MapEvent;
+/**
+ * @classdesc
+ * Events emitted as map events are instances of this type.
+ * See {@link module:ol/Map~Map} for which events trigger a map event.
+ */
+declare class MapEvent extends Event {
+    /**
+     * @param {string} type Event type.
+     * @param {import("./Map.js").default} map Map.
+     * @param {?import("./Map.js").FrameState} [frameState] Frame state.
+     */
+    constructor(type: string, map: import("./Map.js").default, frameState?: import("./Map.js").FrameState | null | undefined);
+    /**
+     * The map where the event occurred.
+     * @type {import("./Map.js").default}
+     * @api
+     */
+    map: import("./Map.js").default;
+    /**
+     * The frame state at the time of the event.
+     * @type {?import("./Map.js").FrameState}
+     * @api
+     */
+    frameState: import("./Map.js").FrameState | null;
+}
+import Event from "./events/Event.js";
+//# sourceMappingURL=MapEvent.d.ts.map
