2be6488ddf981721fb2cf118320ef548cf9f656a
robocdnjs <cdnjs-github@cloudflare.com>
2023-02-27 18:52:39 +0000

    Add vue-material (1.0.0-beta-16)

---
+++ ajax/libs/vue-material/1.0.0-beta-16/components/MdLayout/index.js
@@ -0,0 +1,6 @@
+/*!
+ * vue-material v1.0.0-beta-16
+ * Made with <3 by marcosmoura 2023
+ * Released under the MIT License.
+ */
+!(function(e,t){var n,o;if("object"==typeof exports&&"object"==typeof module)module.exports=t();else if("function"==typeof define&&define.amd)define([],t);else{n=t();for(o in n)("object"==typeof exports?exports:e)[o]=n[o]}})("undefined"!=typeof self?self:this,(function(){return (function(e){function t(o){if(n[o])return n[o].exports;var r=n[o]={i:o,l:!1,exports:{}};return e[o].call(r.exports,r,r.exports,t),r.l=!0,r.exports}var n={};return t.m=e,t.c=n,t.d=function(e,n,o){t.o(e,n)||Object.defineProperty(e,n,{configurable:!1,enumerable:!0,get:o})},t.n=function(e){var n=e&&e.__esModule?function(){return e.default}:function(){return e};return t.d(n,"a",n),n},t.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},t.p="",t(t.s=540)})({405:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0}),n(406),t.default=function(e){}},406:function(e,t){},540:function(e,t,n){e.exports=n(405)}})}));
