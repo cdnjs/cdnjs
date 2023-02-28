913df9683bfade5e095a90f9c0452901adb36866
robocdnjs <cdnjs-github@cloudflare.com>
2023-02-28 04:05:31 +0000

    Add openlayers (7.1.1-dev.1662403476559)

---
+++ ajax/libs/openlayers/7.1.1-dev.1662403476559/events/KeyCode.d.ts
@@ -0,0 +1,8 @@
+declare namespace _default {
+    const LEFT: number;
+    const UP: number;
+    const RIGHT: number;
+    const DOWN: number;
+}
+export default _default;
+//# sourceMappingURL=KeyCode.d.ts.map
---
+++ ajax/libs/openlayers/7.1.1-dev.1662403476559/format/filter/Disjoint.d.ts
@@ -0,0 +1,18 @@
+export default Disjoint;
+/**
+ * @classdesc
+ * Represents a `<Disjoint>` operator to test whether a geometry-valued property
+ * is disjoint to a given geometry.
+ * @api
+ */
+declare class Disjoint extends Spatial {
+    /**
+     * @param {!string} geometryName Geometry name to use.
+     * @param {!import("../../geom/Geometry.js").default} geometry Geometry.
+     * @param {string} [srsName] SRS name. No srsName attribute will be
+     *    set on geometries when this is not provided.
+     */
+    constructor(geometryName: string, geometry: import("../../geom/Geometry.js").default, srsName?: string | undefined);
+}
+import Spatial from "./Spatial.js";
+//# sourceMappingURL=Disjoint.d.ts.map
