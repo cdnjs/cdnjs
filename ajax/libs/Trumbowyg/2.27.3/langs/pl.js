a430a3249c809c012415cd747287347c0127fd54
robocdnjs <cdnjs-github@cloudflare.com>
2023-02-28 18:07:07 +0000

    Add Trumbowyg (2.27.3)

---
+++ ajax/libs/Trumbowyg/2.27.3/langs/pl.js
@@ -0,0 +1,55 @@
+/* ===========================================================
+ * pl.js
+ * Polish translation for Trumbowyg
+ * http://alex-d.github.com/Trumbowyg
+ * ===========================================================
+ * Author : Paweł Abramowicz
+ *          Github : https://github.com/pawelabrams
+ */
+
+jQuery.trumbowyg.langs.pl = {
+    viewHTML: 'Pokaż HTML',
+
+    formatting: 'Format',
+    p: 'Akapit',
+    blockquote: 'Cytat',
+    code: 'Kod',
+    header: 'Nagłówek',
+
+    bold: 'Pogrubienie',
+    italic: 'Pochylenie',
+    strikethrough: 'Przekreślenie',
+    underline: 'Podkreślenie',
+
+    strong: 'Wytłuszczenie',
+    em: 'Uwydatnienie',
+    del: 'Usunięte',
+
+    unorderedList: 'Lista nieuporządkowana',
+    orderedList: 'Lista uporządkowana',
+
+    insertImage: 'Wstaw obraz',
+    insertVideo: 'Wstaw film',
+    link: 'Link',
+    createLink: 'Wstaw link',
+    unlink: 'Usuń link',
+
+    justifyLeft: 'Wyrównaj do lewej',
+    justifyCenter: 'Wyśrodkuj',
+    justifyRight: 'Wyrównaj do prawej',
+    justifyFull: 'Wyjustuj',
+
+    horizontalRule: 'Odkreśl linią',
+
+    fullscreen: 'Pełny ekran',
+
+    close: 'Zamknij',
+
+    submit: 'Zastosuj',
+    reset: 'Przywróć',
+
+    required: 'Wymagane',
+    description: 'Opis',
+    title: 'Tytuł',
+    text: 'Tekst'
+};
---
+++ ajax/libs/Trumbowyg/2.27.3/langs/sk.js
@@ -0,0 +1,54 @@
+/* ===========================================================
+ * sk.js
+ * Slovak translation for Trumbowyg
+ * http://alex-d.github.com/Trumbowyg
+ * ===========================================================
+ * Author : VeeeneX (https://github.com/VeeeneX)
+ */
+
+jQuery.trumbowyg.langs.sk = {
+    viewHTML: 'Zobraziť HTML',
+
+    formatting: 'Formátovanie',
+    p: 'Paragraf',
+    blockquote: 'Citácia',
+    code: 'Kód',
+    header: 'Nadpis',
+
+    bold: 'Tučné',
+    italic: 'Kurzíva',
+    strikethrough: 'Preškrtnuté',
+    underline: 'Podčiarknuté',
+
+    strong: 'Tučné',
+    em: 'Zvýrazniť',
+    del: 'Zmazať',
+
+    unorderedList: 'Netriedený zoznam',
+    orderedList: 'Triedený zoznam',
+
+    insertImage: 'Vložiť obrázok',
+    insertVideo: 'Vložiť video',
+    link: 'Odkaz',
+    createLink: 'Vložiť odkaz',
+    unlink: 'Zmazať odkaz',
+
+    justifyLeft: 'Zarovnať doľava',
+    justifyCenter: 'Zarovnať na stred',
+    justifyRight: 'Zarovnať doprava',
+    justifyFull: 'Zarovnať do bloku',
+
+    horizontalRule: 'Vložit vodorovnú čiaru',
+
+    fullscreen: 'Režim celej obrazovky',
+
+    close: 'Zavrieť',
+
+    submit: 'Potvrdiť',
+    reset: 'Zrušiť',
+
+    required: 'Povinné',
+    description: 'Popis',
+    title: 'Nadpis',
+    text: 'Text'
+};
