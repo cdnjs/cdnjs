b721098e91f47a16ad1acd243a2f35e420c40935
robocdnjs <cdnjs-github@cloudflare.com>
2023-03-05 18:05:42 +0000

    Add openlayers (7.1.1-dev.1664818950712)

---
+++ ajax/libs/openlayers/7.1.1-dev.1664818950712/layer/BaseImage.d.ts
@@ -0,0 +1,112 @@
+export default BaseImageLayer;
+export type Options<ImageSourceType extends import("../source/Image.js").default> = {
+    /**
+     * A CSS class name to set to the layer element.
+     */
+    className?: string | undefined;
+    /**
+     * Opacity (0, 1).
+     */
+    opacity?: number | undefined;
+    /**
+     * Visibility.
+     */
+    visible?: boolean | undefined;
+    /**
+     * The bounding extent for layer rendering.  The layer will not be
+     * rendered outside of this extent.
+     */
+    extent?: import("../extent.js").Extent | undefined;
+    /**
+     * The z-index for layer rendering.  At rendering time, the layers
+     * will be ordered, first by Z-index and then by position. When `undefined`, a `zIndex` of 0 is assumed
+     * for layers that are added to the map's `layers` collection, or `Infinity` when the layer's `setMap()`
+     * method was used.
+     */
+    zIndex?: number | undefined;
+    /**
+     * The minimum resolution (inclusive) at which this layer will be
+     * visible.
+     */
+    minResolution?: number | undefined;
+    /**
+     * The maximum resolution (exclusive) below which this layer will
+     * be visible.
+     */
+    maxResolution?: number | undefined;
+    /**
+     * The minimum view zoom level (exclusive) above which this layer will be
+     * visible.
+     */
+    minZoom?: number | undefined;
+    /**
+     * The maximum view zoom level (inclusive) at which this layer will
+     * be visible.
+     */
+    maxZoom?: number | undefined;
+    /**
+     * Sets the layer as overlay on a map. The map will not manage
+     * this layer in its layers collection, and the layer will be rendered on top. This is useful for
+     * temporary layers. The standard way to add a layer to a map and have it managed by the map is to
+     * use {@link import ("../Map.js").default#addLayer map.addLayer()}.
+     */
+    map?: import("../Map.js").default | undefined;
+    /**
+     * Source for this layer.
+     */
+    source?: ImageSourceType | undefined;
+    /**
+     * Arbitrary observable properties. Can be accessed with `#get()` and `#set()`.
+     */
+    properties?: {
+        [x: string]: any;
+    } | undefined;
+};
+/**
+ * @template {import("../source/Image.js").default} ImageSourceType
+ * @typedef {Object} Options
+ * @property {string} [className='ol-layer'] A CSS class name to set to the layer element.
+ * @property {number} [opacity=1] Opacity (0, 1).
+ * @property {boolean} [visible=true] Visibility.
+ * @property {import("../extent.js").Extent} [extent] The bounding extent for layer rendering.  The layer will not be
+ * rendered outside of this extent.
+ * @property {number} [zIndex] The z-index for layer rendering.  At rendering time, the layers
+ * will be ordered, first by Z-index and then by position. When `undefined`, a `zIndex` of 0 is assumed
+ * for layers that are added to the map's `layers` collection, or `Infinity` when the layer's `setMap()`
+ * method was used.
+ * @property {number} [minResolution] The minimum resolution (inclusive) at which this layer will be
+ * visible.
+ * @property {number} [maxResolution] The maximum resolution (exclusive) below which this layer will
+ * be visible.
+ * @property {number} [minZoom] The minimum view zoom level (exclusive) above which this layer will be
+ * visible.
+ * @property {number} [maxZoom] The maximum view zoom level (inclusive) at which this layer will
+ * be visible.
+ * @property {import("../Map.js").default} [map] Sets the layer as overlay on a map. The map will not manage
+ * this layer in its layers collection, and the layer will be rendered on top. This is useful for
+ * temporary layers. The standard way to add a layer to a map and have it managed by the map is to
+ * use {@link import("../Map.js").default#addLayer map.addLayer()}.
+ * @property {ImageSourceType} [source] Source for this layer.
+ * @property {Object<string, *>} [properties] Arbitrary observable properties. Can be accessed with `#get()` and `#set()`.
+ */
+/**
+ * @classdesc
+ * Server-rendered images that are available for arbitrary extents and
+ * resolutions.
+ * Note that any property set in the options is set as a {@link module:ol/Object~BaseObject}
+ * property on the layer object; for example, setting `title: 'My Title'` in the
+ * options means that `title` is observable, and has get/set accessors.
+ *
+ * @template {import("../source/Image.js").default} ImageSourceType
+ * @template {import("../renderer/Layer.js").default} RendererType
+ * @extends {Layer<ImageSourceType, RendererType>}
+ * @api
+ */
+declare class BaseImageLayer<ImageSourceType extends import("../source/Image.js").default, RendererType extends import("../renderer/Layer.js").default<any>> extends Layer<ImageSourceType, RendererType> {
+    /**
+     * @param {Options<ImageSourceType>} [options] Layer options.
+     */
+    constructor(options?: Options<ImageSourceType> | undefined);
+}
+import Layer from "./Layer.js";
+//# sourceMappingURL=BaseImage.d.ts.map
---
+++ ajax/libs/openlayers/7.1.1-dev.1664818950712/structs/LinkedList.d.ts
@@ -0,0 +1,130 @@
+export default LinkedList;
+export type Item = {
+    /**
+     * Previous.
+     */
+    prev?: Item | undefined;
+    /**
+     * Next.
+     */
+    next?: Item | undefined;
+    /**
+     * Data.
+     */
+    data: unknown;
+};
+/**
+ * @module ol/structs/LinkedList
+ */
+/**
+ * @typedef {Object} Item
+ * @property {Item} [prev] Previous.
+ * @property {Item} [next] Next.
+ * @property {?} data Data.
+ */
+/**
+ * @classdesc
+ * Creates an empty linked list structure.
+ */
+declare class LinkedList {
+    /**
+     * @param {boolean} [circular] The last item is connected to the first one,
+     * and the first item to the last one. Default is true.
+     */
+    constructor(circular?: boolean | undefined);
+    /**
+     * @private
+     * @type {Item|undefined}
+     */
+    private first_;
+    /**
+     * @private
+     * @type {Item|undefined}
+     */
+    private last_;
+    /**
+     * @private
+     * @type {Item|undefined}
+     */
+    private head_;
+    /**
+     * @private
+     * @type {boolean}
+     */
+    private circular_;
+    /**
+     * @private
+     * @type {number}
+     */
+    private length_;
+    /**
+     * Inserts an item into the linked list right after the current one.
+     *
+     * @param {?} data Item data.
+     */
+    insertItem(data: unknown): void;
+    /**
+     * Removes the current item from the list. Sets the cursor to the next item,
+     * if possible.
+     */
+    removeItem(): void;
+    /**
+     * Sets the cursor to the first item, and returns the associated data.
+     *
+     * @return {?} Item data.
+     */
+    firstItem(): unknown;
+    /**
+     * Sets the cursor to the last item, and returns the associated data.
+     *
+     * @return {?} Item data.
+     */
+    lastItem(): unknown;
+    /**
+     * Sets the cursor to the next item, and returns the associated data.
+     *
+     * @return {?} Item data.
+     */
+    nextItem(): unknown;
+    /**
+     * Returns the next item's data without moving the cursor.
+     *
+     * @return {?} Item data.
+     */
+    getNextItem(): unknown;
+    /**
+     * Sets the cursor to the previous item, and returns the associated data.
+     *
+     * @return {?} Item data.
+     */
+    prevItem(): unknown;
+    /**
+     * Returns the previous item's data without moving the cursor.
+     *
+     * @return {?} Item data.
+     */
+    getPrevItem(): unknown;
+    /**
+     * Returns the current item's data.
+     *
+     * @return {?} Item data.
+     */
+    getCurrItem(): unknown;
+    /**
+     * Sets the first item of the list. This only works for circular lists, and sets
+     * the last item accordingly.
+     */
+    setFirstItem(): void;
+    /**
+     * Concatenates two lists.
+     * @param {LinkedList} list List to merge into the current list.
+     */
+    concat(list: LinkedList): void;
+    /**
+     * Returns the current length of the list.
+     *
+     * @return {number} Length.
+     */
+    getLength(): number;
+}
+//# sourceMappingURL=LinkedList.d.ts.map
