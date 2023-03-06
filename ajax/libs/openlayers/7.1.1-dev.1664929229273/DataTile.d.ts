a8ed561259b75c3abb24d9eab7ccab1d9d720590
robocdnjs <cdnjs-github@cloudflare.com>
2023-03-06 09:06:38 +0000

    Add openlayers (7.1.1-dev.1664929229273)

---
+++ ajax/libs/openlayers/7.1.1-dev.1664929229273/DataTile.d.ts
@@ -0,0 +1,91 @@
+export default DataTile;
+/**
+ * Data that can be used with a DataTile.  For increased browser compatibility, use
+ * Uint8Array instead of Uint8ClampedArray where possible.
+ */
+export type Data = Uint8Array | Uint8ClampedArray | Float32Array | DataView;
+export type Options = {
+    /**
+     * Tile coordinate.
+     */
+    tileCoord: import("./tilecoord.js").TileCoord;
+    /**
+     * Data loader.
+     */
+    loader: () => Promise<Data>;
+    /**
+     * A duration for tile opacity
+     * transitions in milliseconds. A duration of 0 disables the opacity transition.
+     */
+    transition?: number | undefined;
+    /**
+     * Use interpolated values when resampling.  By default,
+     * the nearest neighbor is used when resampling.
+     */
+    interpolate?: boolean | undefined;
+    /**
+     * Tile size.
+     */
+    size?: import("./size.js").Size | undefined;
+};
+/**
+ * Data that can be used with a DataTile.  For increased browser compatibility, use
+ * Uint8Array instead of Uint8ClampedArray where possible.
+ * @typedef {Uint8Array|Uint8ClampedArray|Float32Array|DataView} Data
+ */
+/**
+ * @typedef {Object} Options
+ * @property {import("./tilecoord.js").TileCoord} tileCoord Tile coordinate.
+ * @property {function(): Promise<Data>} loader Data loader.
+ * @property {number} [transition=250] A duration for tile opacity
+ * transitions in milliseconds. A duration of 0 disables the opacity transition.
+ * @property {boolean} [interpolate=false] Use interpolated values when resampling.  By default,
+ * the nearest neighbor is used when resampling.
+ * @property {import('./size.js').Size} [size=[256, 256]] Tile size.
+ * @api
+ */
+declare class DataTile extends Tile {
+    /**
+     * @param {Options} options Tile options.
+     */
+    constructor(options: Options);
+    /**
+     * @type {function(): Promise<Data>}
+     * @private
+     */
+    private loader_;
+    /**
+     * @type {Data}
+     * @private
+     */
+    private data_;
+    /**
+     * @type {Error}
+     * @private
+     */
+    private error_;
+    /**
+     * @type {import('./size.js').Size}
+     * @private
+     */
+    private size_;
+    /**
+     * Get the tile size.
+     * @return {import('./size.js').Size} Tile size.
+     */
+    getSize(): import('./size.js').Size;
+    /**
+     * Get the data for the tile.
+     * @return {Data} Tile data.
+     * @api
+     */
+    getData(): Data;
+    /**
+     * Get any loading error.
+     * @return {Error} Loading error.
+     * @api
+     */
+    getError(): Error;
+}
+import Tile from "./Tile.js";
+//# sourceMappingURL=DataTile.d.ts.map
