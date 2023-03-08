f4e4dc4f547037d0cc1a5d2d3f7efd4b8f6247cd
robocdnjs <cdnjs-github@cloudflare.com>
2023-03-08 00:14:00 +0000

    Add ionicons (7.0.0)

---
+++ ajax/libs/ionicons/7.0.0/cjs/index.cjs.js
@@ -0,0 +1,10 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+const utils = require('./utils-18c46e61.js');
+require('./index-53856ad1.js');
+
+
+
+exports.addIcons = utils.addIcons;
