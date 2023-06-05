0c5d21da6697658a2af2297e02ee1b5ed06c356a
robocdnjs <cdnjs-github@cloudflare.com>
2023-06-05 14:28:53 +0000

    Add ace (1.16.0)

---
+++ ajax/libs/ace/1.16.0/snippets/fortran.min.js
@@ -0,0 +1,1 @@
+window.require(["ace/snippets/fortran"],function(e){"object"==typeof module&&"object"==typeof exports&&module&&(module.exports=e)});
---
+++ ajax/libs/ace/1.16.0/snippets/html_elixir.min.js
@@ -0,0 +1,1 @@
+window.require(["ace/snippets/html_elixir"],function(e){"object"==typeof module&&"object"==typeof exports&&module&&(module.exports=e)});
