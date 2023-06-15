901095850a6f5873c3ddfcb5655c93f0e015346d
robocdnjs <cdnjs-github@cloudflare.com>
2023-06-15 14:38:55 +0000

    Add openlayers (7.2.3-dev.1673265761721)

---
+++ ajax/libs/openlayers/7.2.3-dev.1673265761721/ObjectEventType.js
@@ -0,0 +1,19 @@
+/**
+ * @module ol/ObjectEventType
+ */
+
+/**
+ * @enum {string}
+ */
+export default {
+  /**
+   * Triggered when a property is changed.
+   * @event module:ol/Object.ObjectEvent#propertychange
+   * @api
+   */
+  PROPERTYCHANGE: 'propertychange',
+};
+
+/**
+ * @typedef {'propertychange'} Types
+ */
