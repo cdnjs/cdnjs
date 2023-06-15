bacede7468264198714d115d9a956f2a5c57bd55
robocdnjs <cdnjs-github@cloudflare.com>
2023-06-15 21:24:00 +0000

    Add wavesurfer.js (7.0.0-alpha.10)

---
+++ ajax/libs/wavesurfer.js/7.0.0-alpha.10/plugins/multitrack.d.ts
@@ -0,0 +1,46 @@
+import { type WaveSurferOptions } from '../index.js';
+type MultitrackTracks = Array<{
+    id: string | number;
+    url?: string;
+    peaks?: WaveSurferOptions['peaks'];
+    draggable?: boolean;
+    startPosition: number;
+    startCue?: number;
+    endCue?: number;
+    markers?: Array<{
+        id: string | number;
+        time: number;
+        label?: string;
+        color?: string;
+    }>;
+}>;
+type MultitrackOptions = {
+    container: HTMLElement;
+    minPxPerSec: number;
+} & WaveSurferOptions;
+declare class MultiTrack {
+    private tracks;
+    private options;
+    private audios;
+    private wavesurfers;
+    private durations;
+    private currentTime;
+    private maxDuration;
+    private rendering;
+    private isDragging;
+    private frameRequest;
+    static create(tracks: MultitrackTracks, options: MultitrackOptions): MultiTrack;
+    constructor(tracks: MultitrackTracks, options: MultitrackOptions);
+    private initAudios;
+    private initWavesurfers;
+    private updatePosition;
+    private onSeek;
+    private onDrag;
+    private findCurrentTracks;
+    private startSync;
+    play(): void;
+    pause(): void;
+    isPlaying(): boolean;
+    destroy(): void;
+}
+export default MultiTrack;
---
+++ ajax/libs/wavesurfer.js/7.0.0-alpha.10/plugins/regions.d.ts
@@ -0,0 +1,53 @@
+import BasePlugin from '../base-plugin.js';
+import { WaveSurferPluginParams } from '../index.js';
+type RegionsPluginOptions = {
+    dragSelection?: boolean;
+    draggable?: boolean;
+    resizable?: boolean;
+};
+type Region = {
+    startTime: number;
+    endTime: number;
+    title: string;
+    start: number;
+    end: number;
+    element: HTMLElement;
+};
+type RegionsPluginEvents = {
+    'region-created': {
+        region: Region;
+    };
+    'region-updated': {
+        region: Region;
+    };
+    'region-clicked': {
+        region: Region;
+    };
+};
+declare class RegionsPlugin extends BasePlugin<RegionsPluginEvents, RegionsPluginOptions> {
+    private dragStart;
+    private wrapper;
+    private regions;
+    private createdRegion;
+    private modifiedRegion;
+    private isResizingLeft;
+    private isMoving;
+    /** Create an instance of RegionsPlugin */
+    constructor(params: WaveSurferPluginParams, options: RegionsPluginOptions);
+    /** Unmount */
+    destroy(): void;
+    private initWrapper;
+    private handleMouseDown;
+    private handleMouseMove;
+    private handleMouseUp;
+    private createRegionElement;
+    private createRegion;
+    private addRegion;
+    private updateRegion;
+    private moveRegion;
+    /** Create a region at a given start and end time, with an optional title */
+    add(startTime: number, endTime: number, title?: string, color?: string): Region;
+    /** Set the background color of a region */
+    setRegionColor(region: Region, color: string): void;
+}
+export default RegionsPlugin;
