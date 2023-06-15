e1ea436b21e2666da12bc4bbf834170ebfc9a708
robocdnjs <cdnjs-github@cloudflare.com>
2023-06-15 23:32:55 +0000

    Add fancyapps-ui (5.0.13)

---
+++ ajax/libs/fancyapps-ui/5.0.13/carousel/l10n/ja.umd.js
@@ -0,0 +1,1 @@
+!function(e,o){"object"==typeof exports&&"undefined"!=typeof module?o(exports):"function"==typeof define&&define.amd?define(["exports"],o):o(((e="undefined"!=typeof globalThis?globalThis:e||self).Carousel=e.Carousel||{},e.Carousel.l10n=e.Carousel.l10n||{}))}(this,(function(e){"use strict";e.ja={NEXT:"次のスライド",PREV:"前のスライド",GOTO:"スライド #%d に移動"}}));
