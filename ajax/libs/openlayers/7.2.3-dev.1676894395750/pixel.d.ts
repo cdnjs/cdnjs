d439a3a9f52628498411ecbdbc768be8dc31a89f
robocdnjs <cdnjs-github@cloudflare.com>
2023-06-15 20:07:46 +0000

    Add openlayers (7.2.3-dev.1676894395750)

---
+++ ajax/libs/openlayers/7.2.3-dev.1676894395750/pixel.d.ts
@@ -0,0 +1,16 @@
+/**
+ * @module ol/pixel
+ */
+/**
+ * An array with two elements, representing a pixel. The first element is the
+ * x-coordinate, the second the y-coordinate of the pixel.
+ * @typedef {Array<number>} Pixel
+ * @api
+ */
+export let nothing: any;
+/**
+ * An array with two elements, representing a pixel. The first element is the
+ * x-coordinate, the second the y-coordinate of the pixel.
+ */
+export type Pixel = Array<number>;
+//# sourceMappingURL=pixel.d.ts.map
