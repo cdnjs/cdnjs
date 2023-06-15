372b60a62f78258fbeb10ab7c24b5aae2fe2c389
robocdnjs <cdnjs-github@cloudflare.com>
2023-06-15 19:46:24 +0000

    Add wavesurfer.js (7.0.0-alpha.6)

---
+++ ajax/libs/wavesurfer.js/7.0.0-alpha.6/base-plugin.d.ts
@@ -0,0 +1,11 @@
+import EventEmitter, { type GeneralEventTypes } from './event-emitter.js';
+import type { WaveSurfer, WaveSurferPluginParams } from './index.js';
+export declare class BasePlugin<EventTypes extends GeneralEventTypes, Options extends unknown> extends EventEmitter<EventTypes> {
+    protected wavesurfer: WaveSurfer;
+    protected container: HTMLElement;
+    protected subscriptions: (() => void)[];
+    protected options: Options;
+    constructor(params: WaveSurferPluginParams, options: Options);
+    destroy(): void;
+}
+export default BasePlugin;
