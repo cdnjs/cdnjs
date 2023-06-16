e89cd78b3822c14dd9bc42e9795f380f3f833d95
robocdnjs <cdnjs-github@cloudflare.com>
2023-06-16 02:20:50 +0000

    Add wavesurfer.js (7.0.0-alpha.15)

---
+++ ajax/libs/wavesurfer.js/7.0.0-alpha.15/index.d.ts
@@ -0,0 +1,117 @@
+import EventEmitter, { type GeneralEventTypes } from './event-emitter.js';
+import BasePlugin from './base-plugin.js';
+export type WaveSurferOptions = {
+    /** HTML element or CSS selector */
+    container: HTMLElement | string | null;
+    /** Height of the waveform in pixels */
+    height?: number;
+    /** The color of the waveform */
+    waveColor?: string;
+    /** The color of the progress mask */
+    progressColor?: string;
+    /** The color of the playpack cursort */
+    cursorColor?: string;
+    /** If set, the waveform will be rendered in bars like so: ▁ ▂ ▇ ▃ ▅ ▂ */
+    barWidth?: number;
+    /** Spacing between bars in pixels */
+    barGap?: number;
+    /** Rounded borders for bars */
+    barRadius?: number;
+    /** Minimum pixels per second of audio (zoom) */
+    minPxPerSec?: number;
+    /** Stretch the waveform to fill the container, true by default */
+    fillParent?: boolean;
+    /** Audio URL */
+    url?: string;
+    /** Pre-computed audio data */
+    peaks?: Float32Array[];
+    /** Pre-computed duration */
+    duration?: number;
+    /** Use an existing media element instead of creating one */
+    media?: HTMLMediaElement;
+    /** Play the audio on load */
+    autoplay?: boolean;
+    /** Is the waveform clickable? */
+    interactive?: boolean;
+};
+export type WaveSurferEvents = {
+    decode: {
+        duration: number;
+    };
+    canplay: {
+        duration: number;
+    };
+    ready: {
+        duration: number;
+    };
+    play: void;
+    pause: void;
+    timeupdate: {
+        currentTime: number;
+    };
+    seeking: {
+        currentTime: number;
+    };
+    seekClick: {
+        currentTime: number;
+    };
+    destroy: void;
+};
+export type WaveSurferPluginParams = {
+    wavesurfer: WaveSurfer;
+    container: HTMLElement;
+};
+export declare class WaveSurfer extends EventEmitter<WaveSurferEvents> {
+    private options;
+    private fetcher;
+    private decoder;
+    private renderer;
+    private player;
+    private timer;
+    private plugins;
+    private subscriptions;
+    private decodedData;
+    /** Create a new WaveSurfer instance */
+    static create(options: WaveSurferOptions): WaveSurfer;
+    /** Create a new WaveSurfer instance */
+    constructor(options: WaveSurferOptions);
+    private initPlayerEvents;
+    private initRendererEvents;
+    private initTimerEvents;
+    private initReadyEvent;
+    /** Load an audio file by URL, with optional pre-decoded audio data */
+    load(url: string, channelData?: Float32Array[], duration?: number): Promise<void>;
+    /** Zoom in or out */
+    zoom(minPxPerSec: number): void;
+    /** Start playing the audio */
+    play(): void;
+    /** Pause the audio */
+    pause(): void;
+    /** Skip to a time position in seconds */
+    seekTo(time: number): void;
+    /** Check if the audio is playing */
+    isPlaying(): boolean;
+    /** Get the duration of the audio in seconds */
+    getDuration(): number;
+    /** Get the current audio position in seconds */
+    getCurrentTime(): number;
+    /** Get the audio volume */
+    getVolume(): number;
+    /** Set the audio volume */
+    setVolume(volume: number): void;
+    /** Get the audio muted state */
+    getMuted(): boolean;
+    /** Mute or unmute the audio */
+    setMuted(muted: boolean): void;
+    /** Get playback rate */
+    getPlaybackRate(): number;
+    /** Set playback rate, with an optional parameter to NOT preserve the pitch if false */
+    setPlaybackRate(rate: number, preservePitch?: boolean): void;
+    /** Register and initialize a plugin */
+    registerPlugin<T extends BasePlugin<GeneralEventTypes, Options>, Options>(CustomPlugin: new (params: WaveSurferPluginParams, options: Options) => T, options: Options): T;
+    /** Get the decoded audio data */
+    getDecodedData(): AudioBuffer | null;
+    /** Unmount wavesurfer */
+    destroy(): void;
+}
+export default WaveSurfer;
---
+++ ajax/libs/wavesurfer.js/7.0.0-alpha.15/plugins/multitrack.js
@@ -0,0 +1,307 @@
+import WaveSurfer from '../index.js';
+import RegionsPlugin from './regions.js';
+import TimelinePlugin from './timeline.js';
+class MultiTrack {
+    static create(tracks, options) {
+        return new MultiTrack(tracks, options);
+    }
+    constructor(tracks, options) {
+        this.audios = [];
+        this.wavesurfers = [];
+        this.durations = [];
+        this.currentTime = 0;
+        this.maxDuration = 0;
+        this.isDragging = false;
+        this.frameRequest = null;
+        this.tracks = tracks.map((track) => (Object.assign(Object.assign({}, track), { startPosition: track.startPosition || 0, peaks: track.peaks || (track.url ? undefined : [new Float32Array()]) })));
+        this.options = options;
+        this.rendering = initRendering(this.tracks, this.options);
+        this.initAudios().then((durations) => {
+            this.durations = durations;
+            const maxDuration = this.tracks.reduce((max, track, index) => {
+                return Math.max(max, track.startPosition + durations[index]);
+            }, 0);
+            this.rendering.setMainWidth(durations, maxDuration);
+            this.rendering.addClickHandler((position) => {
+                this.onSeek(position * maxDuration);
+            });
+            this.maxDuration = maxDuration;
+            this.initWavesurfers();
+            this.initTimeline();
+            this.rendering.containers.forEach((container, index) => {
+                const drag = initDragging(container, (delta) => this.onDrag(index, delta));
+                this.wavesurfers[index].once('destroy', () => {
+                    drag === null || drag === void 0 ? void 0 : drag.destroy();
+                });
+            });
+        });
+    }
+    initAudios() {
+        this.audios = this.tracks.map((track) => new Audio(track.url));
+        return Promise.all(this.audios.map((audio) => {
+            return new Promise((resolve) => {
+                audio.src
+                    ? audio.addEventListener('canplay', () => {
+                        resolve(audio.duration);
+                    }, { once: true })
+                    : resolve(0);
+            });
+        }));
+    }
+    initWavesurfers() {
+        const wavesurfers = this.tracks.map((track, index) => {
+            // Create a wavesurfer instance
+            const ws = WaveSurfer.create(Object.assign(Object.assign({}, this.options), { container: this.rendering.containers[index], minPxPerSec: 0, media: this.audios[index], peaks: track.peaks, cursorColor: 'transparent', interactive: false }));
+            const wsRegions = ws.registerPlugin(RegionsPlugin, {
+                draggable: false,
+                resizable: false,
+                dragSelection: false,
+            });
+            ws.once('decode', () => {
+                // Render start and end cues
+                if (track.startCue) {
+                    wsRegions.add(track.startCue, track.startCue, 'start', this.options.cueColor);
+                }
+                if (track.endCue) {
+                    wsRegions.add(track.endCue, track.endCue, 'end', this.options.cueColor);
+                }
+                // Render markers
+                ;
+                (track.markers || []).forEach((marker) => {
+                    wsRegions.add(marker.time, marker.time, marker.label, marker.color);
+                });
+            });
+            return ws;
+        });
+        this.wavesurfers = wavesurfers;
+    }
+    initTimeline() {
+        this.wavesurfers[0].registerPlugin(TimelinePlugin, {
+            duration: this.maxDuration,
+            container: this.rendering.containers[0].parentElement,
+        });
+    }
+    updatePosition(time) {
+        const precisionSeconds = 0.3;
+        this.currentTime = time;
+        this.rendering.updateCursor(time / this.maxDuration);
+        const isPaused = !this.isPlaying();
+        // Update the current time of each audio
+        this.tracks.forEach((track, index) => {
+            const audio = this.audios[index];
+            const duration = this.durations[index];
+            const newTime = time - track.startPosition;
+            if (Math.abs(audio.currentTime - newTime) > precisionSeconds) {
+                audio.currentTime = newTime;
+            }
+            // If the position is out of the track bounds, pause it
+            if (isPaused || newTime < 0 || newTime > duration) {
+                !audio.paused && audio.pause();
+            }
+            else if (!isPaused) {
+                // If the position is in the track bounds, play it
+                audio.paused && audio.play();
+            }
+            // Unmute if cue is reached
+            const newVolume = newTime >= (track.startCue || 0) && newTime < (track.endCue || Infinity) ? 1 : 0;
+            if (newVolume !== audio.volume)
+                audio.volume = newVolume;
+        });
+    }
+    onSeek(time) {
+        if (this.isDragging)
+            return;
+        const wasPlaying = this.isPlaying();
+        this.updatePosition(time);
+        if (wasPlaying)
+            this.play();
+    }
+    onDrag(index, delta) {
+        const track = this.tracks[index];
+        if (!track.draggable)
+            return;
+        this.isDragging = true;
+        setTimeout(() => (this.isDragging = false), 600);
+        const newTime = track.startPosition + delta * this.maxDuration;
+        const minStart = this.tracks.reduce((min, track, i) => {
+            if (i === index || !track.url)
+                return min;
+            return Math.min(min, track.startPosition);
+        }, Infinity);
+        if (newTime + this.durations[index] >= minStart) {
+            track.startPosition = newTime;
+            this.rendering.setContainerOffsets();
+            this.updatePosition(this.currentTime);
+        }
+    }
+    findCurrentTracks() {
+        // Find the audios at the current time
+        const indexes = [];
+        this.tracks.forEach((track, index) => {
+            if (track.url &&
+                this.currentTime >= track.startPosition &&
+                this.currentTime < track.startPosition + this.durations[index]) {
+                indexes.push(index);
+            }
+        });
+        if (indexes.length === 0) {
+            const minStartTime = Math.min(...this.tracks.filter((t) => t.url).map((track) => track.startPosition));
+            indexes.push(this.tracks.findIndex((track) => track.startPosition === minStartTime));
+        }
+        return indexes;
+    }
+    startSync() {
+        const onFrame = () => {
+            const position = this.audios.reduce((pos, audio, index) => {
+                if (!audio.paused) {
+                    pos = Math.max(pos, audio.currentTime + this.tracks[index].startPosition);
+                }
+                return pos;
+            }, this.currentTime);
+            if (position > this.currentTime) {
+                this.updatePosition(position);
+            }
+            this.frameRequest = requestAnimationFrame(onFrame);
+        };
+        onFrame();
+    }
+    play() {
+        this.startSync();
+        const indexes = this.findCurrentTracks();
+        indexes.forEach((index) => {
+            var _a;
+            (_a = this.audios[index]) === null || _a === void 0 ? void 0 : _a.play();
+        });
+    }
+    pause() {
+        this.audios.forEach((audio) => audio.pause());
+    }
+    isPlaying() {
+        return this.audios.some((audio) => !audio.paused);
+    }
+    destroy() {
+        if (this.frameRequest)
+            cancelAnimationFrame(this.frameRequest);
+        this.rendering.destroy();
+        this.audios.forEach((audio) => {
+            audio.pause();
+            audio.src = '';
+        });
+        this.wavesurfers.forEach((ws) => {
+            ws.destroy();
+        });
+    }
+}
+function initRendering(tracks, options) {
+    let pxPerSec = 0;
+    let durations = [];
+    // Create a common container for all tracks
+    const scroll = document.createElement('div');
+    scroll.setAttribute('style', 'width: 100%; overflow-x: scroll; overflow-y: hidden; user-select: none;');
+    const wrapper = document.createElement('div');
+    wrapper.style.position = 'relative';
+    scroll.appendChild(wrapper);
+    options.container.appendChild(scroll);
+    // Create a common cursor
+    const cursor = document.createElement('div');
+    cursor.setAttribute('style', 'width: 1px; height: 100%; position: absolute; z-index: 10; top: 0; left: 0');
+    cursor.style.backgroundColor = options.cursorColor || '#000';
+    wrapper.appendChild(cursor);
+    // Create containers for each track
+    const containers = tracks.map((_, index) => {
+        const container = document.createElement('div');
+        if (options.trackBorderColor && index > 0) {
+            const borderDiv = document.createElement('div');
+            borderDiv.setAttribute('style', `width: 100%; height: 2px; background-color: ${options.trackBorderColor}`);
+            wrapper.appendChild(borderDiv);
+        }
+        if (options.trackBackground) {
+            container.style.background = options.trackBackground;
+        }
+        wrapper.appendChild(container);
+        return container;
+    });
+    // Set the positions of each container
+    const setContainerOffsets = () => {
+        containers.forEach((container, i) => {
+            const offset = tracks[i].startPosition * pxPerSec;
+            container.style.width = `${durations[i] * pxPerSec}px`;
+            container.style.transform = `translateX(${offset}px)`;
+            if (tracks[i].draggable)
+                container.style.cursor = 'ew-resize';
+        });
+    };
+    return {
+        containers,
+        // Set the start offset
+        setContainerOffsets,
+        // Set the container width
+        setMainWidth: (trackDurations, maxDuration) => {
+            durations = trackDurations;
+            const { clientWidth } = wrapper;
+            pxPerSec = Math.max(options.minPxPerSec || 0, clientWidth / maxDuration);
+            const width = pxPerSec * maxDuration;
+            if (width > clientWidth) {
+                wrapper.style.width = `${width}px`;
+            }
+            setContainerOffsets();
+        },
+        // Update cursor position
+        updateCursor: (position) => {
+            cursor.style.left = `${Math.min(100, position * 100)}%`;
+        },
+        // Click to seek
+        addClickHandler: (onClick) => {
+            wrapper.addEventListener('click', (e) => {
+                const rect = wrapper.getBoundingClientRect();
+                const x = e.clientX - rect.left;
+                const position = x / wrapper.offsetWidth;
+                onClick(position);
+            });
+        },
+        // Destroy the container
+        destroy: () => {
+            scroll.remove();
+        },
+    };
+}
+function initDragging(container, onDrag) {
+    const wrapper = container.parentElement;
+    if (!wrapper)
+        return;
+    // Dragging tracks to set position
+    let dragStart = null;
+    // Drag start
+    container.addEventListener('mousedown', (e) => {
+        const rect = wrapper.getBoundingClientRect();
+        dragStart = e.clientX - rect.left;
+    });
+    // Drag end
+    const onMouseUp = (e) => {
+        if (dragStart != null) {
+            e.stopPropagation();
+            dragStart = null;
+        }
+    };
+    // Drag move
+    const onMouseMove = (e) => {
+        if (dragStart == null)
+            return;
+        const rect = wrapper.getBoundingClientRect();
+        const x = e.clientX - rect.left;
+        const diff = x - dragStart;
+        if (diff > 1 || diff < -1) {
+            dragStart = x;
+            onDrag(diff / wrapper.offsetWidth);
+        }
+    };
+    document.body.addEventListener('mouseup', onMouseUp);
+    document.body.addEventListener('mousemove', onMouseMove);
+    return {
+        destroy: () => {
+            document.body.removeEventListener('mouseup', onMouseUp);
+            document.body.removeEventListener('mousemove', onMouseMove);
+        },
+    };
+}
+export default MultiTrack;
