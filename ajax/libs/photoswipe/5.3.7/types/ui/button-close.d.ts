cf0123bb892d3b7da58e71f1c76e2aa881845fce
robocdnjs <cdnjs-github@cloudflare.com>
2023-06-16 06:10:47 +0000

    Add photoswipe (5.3.7)

---
+++ ajax/libs/photoswipe/5.3.7/types/ui/button-close.d.ts
@@ -0,0 +1,3 @@
+export default closeButton;
+/** @type {import('./ui-element.js').UIElementData} UIElementData */
+declare const closeButton: import('./ui-element.js').UIElementData;
---
+++ ajax/libs/photoswipe/5.3.7/types/ui/counter-indicator.d.ts
@@ -0,0 +1,2 @@
+/** @type {import('./ui-element.js').UIElementData} UIElementData */
+export const counterIndicator: import('./ui-element.js').UIElementData;
