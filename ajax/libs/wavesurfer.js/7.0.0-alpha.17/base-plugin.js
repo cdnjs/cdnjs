0389c1145ae13aab2f5daa966c8f5259e48e0b0f
robocdnjs <cdnjs-github@cloudflare.com>
2023-06-16 03:20:24 +0000

    Add wavesurfer.js (7.0.0-alpha.17)

---
+++ ajax/libs/wavesurfer.js/7.0.0-alpha.17/base-plugin.js
@@ -0,0 +1,15 @@
+import EventEmitter from './event-emitter.js';
+export class BasePlugin extends EventEmitter {
+    constructor(params, options) {
+        super();
+        this.subscriptions = [];
+        this.wavesurfer = params.wavesurfer;
+        this.container = params.container;
+        this.wrapper = params.wrapper;
+        this.options = options;
+    }
+    destroy() {
+        this.subscriptions.forEach((unsubscribe) => unsubscribe());
+    }
+}
+export default BasePlugin;
---
+++ ajax/libs/wavesurfer.js/7.0.0-alpha.17/plugins/multitrack.d.ts
@@ -0,0 +1,53 @@
+import { type WaveSurferOptions } from '../index.js';
+type MultitrackTracks = Array<{
+    id: string | number;
+    url?: string;
+    peaks?: WaveSurferOptions['peaks'];
+    draggable?: boolean;
+    startPosition: number;
+    startCue?: number;
+    endCue?: number;
+    markers?: Array<{
+        id: string | number;
+        time: number;
+        label?: string;
+        color?: string;
+    }>;
+}>;
+type MultitrackOptions = {
+    container: HTMLElement;
+    minPxPerSec?: number;
+    cursorColor?: string;
+    trackBackground?: string;
+    trackBorderColor?: string;
+    cueColor?: string;
+    onTrackPositionUpdate?: (id: string | number, startPosition: number) => void;
+};
+declare class MultiTrack {
+    private tracks;
+    private options;
+    private audios;
+    private wavesurfers;
+    private durations;
+    private currentTime;
+    private maxDuration;
+    private rendering;
+    private isDragging;
+    private frameRequest;
+    static create(tracks: MultitrackTracks, options: MultitrackOptions): MultiTrack;
+    constructor(tracks: MultitrackTracks, options: MultitrackOptions);
+    private initAudios;
+    private initWavesurfers;
+    private initTimeline;
+    private updatePosition;
+    private onDrag;
+    private findCurrentTracks;
+    private startSync;
+    play(): void;
+    pause(): void;
+    isPlaying(): boolean;
+    seekTo(time: number): void;
+    zoom(pxPerSec: number): void;
+    destroy(): void;
+}
+export default MultiTrack;
