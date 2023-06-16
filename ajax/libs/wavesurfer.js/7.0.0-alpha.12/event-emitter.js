fa2dda86b0ceaef4b1473077760dd702cf7fd8bf
robocdnjs <cdnjs-github@cloudflare.com>
2023-06-16 00:20:06 +0000

    Add wavesurfer.js (7.0.0-alpha.12)

---
+++ ajax/libs/wavesurfer.js/7.0.0-alpha.12/event-emitter.js
@@ -0,0 +1,25 @@
+class EventEmitter {
+    constructor() {
+        this.eventTarget = new EventTarget();
+    }
+    emit(eventType, detail) {
+        const e = new CustomEvent(String(eventType), { detail });
+        this.eventTarget.dispatchEvent(e);
+    }
+    /** Subscribe to an event and return a function to unsubscribe */
+    on(eventType, callback, once) {
+        const handler = (e) => {
+            if (e instanceof CustomEvent) {
+                callback(e.detail);
+            }
+        };
+        const eventName = String(eventType);
+        this.eventTarget.addEventListener(eventName, handler, { once });
+        return () => this.eventTarget.removeEventListener(eventName, handler);
+    }
+    /** Subscribe to an event once and return a function to unsubscribe */
+    once(eventType, callback) {
+        return this.on(eventType, callback, true);
+    }
+}
+export default EventEmitter;
