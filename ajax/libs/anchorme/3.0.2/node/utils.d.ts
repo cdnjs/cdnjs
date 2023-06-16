a9643144a88bb4d8aaf6c90656b531ecbfff9fca
robocdnjs <cdnjs-github@cloudflare.com>
2023-06-16 20:49:05 +0000

    Add anchorme (3.0.2)

---
+++ ajax/libs/anchorme/3.0.2/node/utils.d.ts
@@ -0,0 +1,5 @@
+export declare function checkParenthesis(opening: string, closing: string, target: string, nextChar: string): boolean | undefined;
+export declare const maximumAttrLength: number;
+export declare function isInsideAttribute(prevFragment: string): boolean;
+export declare function isInsideAnchorTag(target: string, fullInput: string, targetEnd: number): boolean;
+export declare function isInsideImgSrc(target: string, fullInput: string, targetEnd: number): boolean;
