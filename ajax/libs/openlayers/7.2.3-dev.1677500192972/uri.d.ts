62f0d0e9cbd4503c200afaa99c513de86a2d38b1
robocdnjs <cdnjs-github@cloudflare.com>
2023-06-16 03:16:21 +0000

    Add openlayers (7.2.3-dev.1677500192972)

---
+++ ajax/libs/openlayers/7.2.3-dev.1677500192972/uri.d.ts
@@ -0,0 +1,13 @@
+/**
+ * @module ol/uri
+ */
+/**
+ * Appends query parameters to a URI.
+ *
+ * @param {string} uri The original URI, which may already have query data.
+ * @param {!Object} params An object where keys are URI-encoded parameter keys,
+ *     and the values are arbitrary types or arrays.
+ * @return {string} The new URI.
+ */
+export function appendParams(uri: string, params: any): string;
+//# sourceMappingURL=uri.d.ts.map
