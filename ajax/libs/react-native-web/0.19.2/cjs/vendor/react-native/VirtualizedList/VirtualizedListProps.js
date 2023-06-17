b1a0aed53e665ce296310f482208cdae21b016a2
robocdnjs <cdnjs-github@cloudflare.com>
2023-06-17 09:51:48 +0000

    Add react-native-web (0.19.2)

---
+++ ajax/libs/react-native-web/0.19.2/cjs/vendor/react-native/VirtualizedList/VirtualizedListProps.js
@@ -0,0 +1,6 @@
+"use strict";
+
+var _interopRequireWildcard = require("@babel/runtime/helpers/interopRequireWildcard").default;
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault").default;
+var _ScrollView = _interopRequireDefault(require("../../../exports/ScrollView"));
+var React = _interopRequireWildcard(require("react"));
