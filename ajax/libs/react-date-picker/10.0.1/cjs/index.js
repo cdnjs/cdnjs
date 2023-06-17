3a029af22710bd801c94e7a02a6bddf67d950fc3
robocdnjs <cdnjs-github@cloudflare.com>
2023-06-17 09:03:18 +0000

    Add react-date-picker (10.0.1)

---
+++ ajax/libs/react-date-picker/10.0.1/cjs/index.js
@@ -0,0 +1,9 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.DatePicker = void 0;
+var DatePicker_1 = __importDefault(require("./DatePicker"));
+exports.DatePicker = DatePicker_1.default;
+exports.default = DatePicker_1.default;
