aef8bdd9d5ba34f29c4f6fa8852ddafbaf479c3b
robocdnjs <cdnjs-github@cloudflare.com>
2023-06-28 22:32:36 +0000

    Add wavesurfer.js (7.0.0-alpha.47)

---
+++ ajax/libs/wavesurfer.js/7.0.0-alpha.47/plugins/minimap.d.ts
@@ -0,0 +1,35 @@
+/**
+ * Minimap is a tiny copy of the main waveform serving as a navigation tool.
+ */
+import BasePlugin from '../base-plugin.js';
+import { type WaveSurferOptions } from '../wavesurfer.js';
+export type MinimapPluginOptions = {
+    overlayColor?: string;
+    insertPosition?: InsertPosition;
+} & WaveSurferOptions;
+declare const defaultOptions: {
+    height: number;
+    overlayColor: string;
+    insertPosition: string;
+};
+export type MinimapPluginEvents = {
+    ready: [];
+    interaction: [];
+};
+declare class MinimapPlugin extends BasePlugin<MinimapPluginEvents, MinimapPluginOptions> {
+    protected options: MinimapPluginOptions & typeof defaultOptions;
+    private minimapWrapper;
+    private miniWavesurfer;
+    private overlay;
+    private container;
+    constructor(options: MinimapPluginOptions);
+    static create(options: MinimapPluginOptions): MinimapPlugin;
+    /** Called by wavesurfer, don't call manually */
+    onInit(): void;
+    private initMinimapWrapper;
+    private initOverlay;
+    private initMinimap;
+    /** Unmount */
+    destroy(): void;
+}
+export default MinimapPlugin;
