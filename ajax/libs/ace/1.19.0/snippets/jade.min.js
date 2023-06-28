78fe14ce438fa55564b739f9830b6adffd5f293d
robocdnjs <cdnjs-github@cloudflare.com>
2023-06-28 15:53:12 +0000

    Add ace (1.19.0)

---
+++ ajax/libs/ace/1.19.0/snippets/jade.min.js
@@ -0,0 +1,1 @@
+window.require(["ace/snippets/jade"],function(e){"object"==typeof module&&"object"==typeof exports&&module&&(module.exports=e)});
---
+++ ajax/libs/ace/1.19.0/theme-textmate.js
@@ -0,0 +1,8 @@
+define("ace/theme/textmate",["require","exports","module","ace/theme/textmate.css","ace/lib/dom"],function(e,t,n){"use strict";t.isDark=!1,t.cssClass="ace-tm",t.cssText=e("./textmate.css"),t.$id="ace/theme/textmate";var r=e("../lib/dom");r.importCssString(t.cssText,t.cssClass,!1)});                (function() {
+                    window.require(["ace/theme/textmate"], function(m) {
+                        if (typeof module == "object" && typeof exports == "object" && module) {
+                            module.exports = m;
+                        }
+                    });
+                })();
+            
