96c63f2fed80529967d4130d0d038fd42bffa19b
robocdnjs <cdnjs-github@cloudflare.com>
2023-06-29 14:51:23 +0000

    Add openlayers (7.3.1-dev.1684229361146)

---
+++ ajax/libs/openlayers/7.3.1-dev.1684229361146/events/Key.d.ts
@@ -0,0 +1,8 @@
+declare namespace _default {
+    const LEFT: string;
+    const UP: string;
+    const RIGHT: string;
+    const DOWN: string;
+}
+export default _default;
+//# sourceMappingURL=Key.d.ts.map
