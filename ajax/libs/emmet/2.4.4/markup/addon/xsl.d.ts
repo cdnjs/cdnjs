97fc86ac1920326db29700c3f984d0ca8d8b7cac
robocdnjs <cdnjs-github@cloudflare.com>
2023-06-29 07:28:38 +0000

    Add emmet (2.4.4)

---
+++ ajax/libs/emmet/2.4.4/markup/addon/xsl.d.ts
@@ -0,0 +1,6 @@
+import type { AbbreviationNode } from '@emmetio/abbreviation';
+/**
+ * XSL transformer: removes `select` attributes from certain nodes that contain
+ * children
+ */
+export default function xsl(node: AbbreviationNode): void;
