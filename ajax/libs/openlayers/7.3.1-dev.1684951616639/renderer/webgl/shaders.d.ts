112063ddf78490da2f50a82c686d539e4e35f81f
robocdnjs <cdnjs-github@cloudflare.com>
2023-06-30 05:46:14 +0000

    Add openlayers (7.3.1-dev.1684951616639)

---
+++ ajax/libs/openlayers/7.3.1-dev.1684951616639/renderer/webgl/shaders.d.ts
@@ -0,0 +1,35 @@
+/**
+ * Default polygon vertex shader.
+ * Relies on the color attribute.
+ * @type {string}
+ */
+export const FILL_VERTEX_SHADER: string;
+/**
+ * Default polygon fragment shader.
+ * @type {string}
+ */
+export const FILL_FRAGMENT_SHADER: string;
+/**
+ * Default linestring vertex shader.
+ * Relies on color and width attributes.
+ * @type {string}
+ */
+export const STROKE_VERTEX_SHADER: string;
+/**
+ * Default linestring fragment shader.
+ * @type {string}
+ */
+export const STROKE_FRAGMENT_SHADER: string;
+/**
+ * Default point vertex shader.
+ * Relies on color attribute.
+ * @type {string}
+ */
+export const POINT_VERTEX_SHADER: string;
+/**
+ * Default point fragment shader.
+ * @type {string}
+ */
+export const POINT_FRAGMENT_SHADER: string;
+export type DefaultAttributes = 'color' | 'width';
+//# sourceMappingURL=shaders.d.ts.map
