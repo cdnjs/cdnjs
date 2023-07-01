6ecbf14d3c54bb5130b6c818a7c5580660d11902
robocdnjs <cdnjs-github@cloudflare.com>
2023-07-01 19:14:55 +0000

    Add hexo-theme-next (8.17.0)

---
+++ ajax/libs/hexo-theme-next/8.17.0/third-party/tags/wavedrom.min.js
@@ -0,0 +1,1 @@
+document.addEventListener("page:loaded",()=>{NexT.utils.getScript(CONFIG.wavedrom.js,{condition:window.WaveDrom}).then(()=>{NexT.utils.getScript(CONFIG.wavedrom_skin.js,{condition:window.WaveSkin}).then(()=>{WaveDrom.ProcessAll()})})});
