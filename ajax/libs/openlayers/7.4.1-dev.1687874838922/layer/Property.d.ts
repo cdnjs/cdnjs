17cdaa3bc76ea1390d877e7c5248aafdf0dd8184
robocdnjs <cdnjs-github@cloudflare.com>
2023-07-03 20:16:23 +0000

    Add openlayers (7.4.1-dev.1687874838922)

---
+++ ajax/libs/openlayers/7.4.1-dev.1687874838922/layer/Property.d.ts
@@ -0,0 +1,14 @@
+declare namespace _default {
+    let OPACITY: string;
+    let VISIBLE: string;
+    let EXTENT: string;
+    let Z_INDEX: string;
+    let MAX_RESOLUTION: string;
+    let MIN_RESOLUTION: string;
+    let MAX_ZOOM: string;
+    let MIN_ZOOM: string;
+    let SOURCE: string;
+    let MAP: string;
+}
+export default _default;
+//# sourceMappingURL=Property.d.ts.map
