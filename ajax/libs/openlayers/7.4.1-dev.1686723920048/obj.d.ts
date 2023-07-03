7a985cb7f6256201328019e16319ae4037bb16ec
robocdnjs <cdnjs-github@cloudflare.com>
2023-07-03 01:19:56 +0000

    Add openlayers (7.4.1-dev.1686723920048)

---
+++ ajax/libs/openlayers/7.4.1-dev.1686723920048/obj.d.ts
@@ -0,0 +1,15 @@
+/**
+ * @module ol/obj
+ */
+/**
+ * Removes all properties from an object.
+ * @param {Object} object The object to clear.
+ */
+export function clear(object: any): void;
+/**
+ * Determine if an object has any properties.
+ * @param {Object} object The object to check.
+ * @return {boolean} The object is empty.
+ */
+export function isEmpty(object: any): boolean;
+//# sourceMappingURL=obj.d.ts.map
