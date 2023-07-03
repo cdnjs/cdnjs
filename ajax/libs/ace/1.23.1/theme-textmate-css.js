ca74f5caf111c19f4e7983dc410a40085caf6ef6
robocdnjs <cdnjs-github@cloudflare.com>
2023-07-03 19:48:44 +0000

    Add ace (1.23.1)

---
+++ ajax/libs/ace/1.23.1/theme-textmate-css.js
@@ -0,0 +1,8 @@
+;                (function() {
+                    window.require(["ace/theme/textmate-css"], function(m) {
+                        if (typeof module == "object" && typeof exports == "object" && module) {
+                            module.exports = m;
+                        }
+                    });
+                })();
+            
