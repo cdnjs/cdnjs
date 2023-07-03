c56818e31c7cfba07405ada218bc17e8d91b1da0
robocdnjs <cdnjs-github@cloudflare.com>
2023-07-03 18:03:45 +0000

    Add function-plot (1.23.3)

---
+++ ajax/libs/function-plot/1.23.3/graph-types/index.js
@@ -0,0 +1,13 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.scatter = exports.interval = exports.polyline = void 0;
+const polyline_1 = __importDefault(require("./polyline"));
+exports.polyline = polyline_1.default;
+const interval_1 = __importDefault(require("./interval"));
+exports.interval = interval_1.default;
+const scatter_1 = __importDefault(require("./scatter"));
+exports.scatter = scatter_1.default;
+//# sourceMappingURL=index.js.map
