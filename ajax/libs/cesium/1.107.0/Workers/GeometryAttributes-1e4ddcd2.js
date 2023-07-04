00ff372a0b000c2960cc410e246453660076bfc9
robocdnjs <cdnjs-github@cloudflare.com>
2023-07-04 07:43:20 +0000

    Add cesium (1.107.0)

---
+++ ajax/libs/cesium/1.107.0/Workers/GeometryAttributes-1e4ddcd2.js
@@ -0,0 +1,1 @@
+define(["exports","./defaultValue-f6d5e6da"],(function(t,e){"use strict";t.GeometryAttributes=function(t){t=e.defaultValue(t,e.defaultValue.EMPTY_OBJECT),this.position=t.position,this.normal=t.normal,this.st=t.st,this.bitangent=t.bitangent,this.tangent=t.tangent,this.color=t.color}}));
