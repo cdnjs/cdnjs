1df4349383a49a12d048671249096fcfa1a1c4cc
robocdnjs <cdnjs-github@cloudflare.com>
2023-07-04 22:04:43 +0000

    Add tarteaucitronjs (1.13.0)

---
+++ ajax/libs/tarteaucitronjs/1.13.0/lang/tarteaucitron.fi.js
@@ -0,0 +1,88 @@
+/*global tarteaucitron */
+tarteaucitron.lang = {
+    "middleBarHead": "☝ 🍪",
+    "adblock": "Hei! Tämä sivusto antaa sinun valita ja hallita kolmansien osapuolten asettamia evästeitä.",
+    "adblock_call": "Estä adblocker muuttaaksesi asetuksia.",
+    "reload": "Päivitä sivu",
+
+    "alertBigScroll": "Jatkamalla selailua,",
+    "alertBigClick": "Jatkamalla tämän sivuston selailua,",
+    "alertBig": "hyväksyt kolmansien osapuolien tarjoamia palveluita",
+
+    "alertBigPrivacy": "Tämä sivusto käyttää evästeitä ja antaa sinun hallita niitä.",
+    "alertSmall": "Hallinnoi palveluja",
+    "acceptAll": "OK, hyväksy kaikki",
+    "personalize": "Personoi",
+    "close": "Sulje",
+
+    "privacyUrl": "Tietosuoja",
+
+    "all": "Kaikkien palveluiden valinta",
+
+    "info": "Yksityisyyden suojaaminen",
+    "disclaimer": "Hyväksymällä kolmansien osapuolten palvelut, hyväksyt toiminnan kannalta tarpeellisten evästeiden ja seurantateknologioiden käytön.",
+    "allow": "Hyväksy",
+    "deny": "Kiellä",
+    "noCookie": "Tämä palvelu ei käytä evästeitä",
+    "useCookie": "Tämä palvelu voidaan asentaa",
+    "useCookieCurrent": "Tämä palvelu on asennettu",
+    "useNoCookie": "Tämä palvelu ei ole asentanut evästeitä",
+    "more": "Lue lisää",
+    "source": "Katso virallinen nettisivu",
+    "credit": "Evästeiden hallinta: tarteaucitron.js",
+    "noServices": "Tämä sivusto ei käytä evästeitä, jotka vaativat suostumustasi.",
+    
+    "toggleInfoBox": "Näytä/piilota tiedot evästeistä ja niiden säilytyksestä",
+    "title": "Evästeiden hallintapaneeli",
+    "cookieDetail": "Evästetiedot",
+    "ourSite": "sivustollamme",
+    "modalWindow": "(modaalinen ikkuna)",
+    "newWindow": "uusi ikkuna",
+    "allowAll": "Hyväksy kaikki evästeet",
+    "denyAll": "Kiellä kaikki evästeet",
+
+    "icon": "Cookies",
+   
+    "fallback": "hylätty.",
+    "allowed": "sallittu",
+    "disallowed": "kielletty",
+
+    "ads": {
+        "title": "Mainosverkosto",
+        "details": "Mainosverkostot saattavat saada tuloja myymällä mainostilaa sivustolla."
+    },
+    "analytic": {
+        "title": "Yleisön mittaaminen",
+        "details": "Les services de mesure d'audience permettent de générer des statistiques de fréquentation utiles à l'amélioration du site."
+    },
+    "social": {
+        "title": "Sosiaaliset verkostot",
+        "details": "Sosiaaliset verkostot voivat helpottaa sivuston käytettävyyttä ja mainontaa"
+    },
+    "video": {
+        "title": "Videot",
+        "details": "Videoiden toistopalvelut auttavat rikastamaan sivuston markkinointia ja kasvattaa sen näkyvyyttä"
+    },
+
+    "comment": {
+
+        "title": "Kommentit",
+
+        "details": "Kommentoinnin ylläpito helpottaa kommenttien arkistointia ja roskapostin hallintaa."
+    },
+    "support": {
+        "title": "Tuki",
+        "details": "Ohjelmointirajapintoja käytetään eri ohjelmistojen, kuten hakukoneiden, sijaintien tai käännösten, lataamiseen."
+    },
+    "api": {
+        "title": "Ohjelmointirajapinnat",
+        "details": "Ohjelmointirajapintoja käytetään eri ohjelmistojen, kuten hakukoneiden, sijaintien tai käännösten, lataamiseen,..."
+    },
+    "other": {
+        "title": "Muut",
+        "details": "Palvelut web-sisältöjen näyttämiseen."
+    },
+    
+    "mandatoryTitle": "Mandatory cookies",
+    "mandatoryText": "This site uses cookies necessary for its proper functioning which cannot be deactivated."
+};
---
+++ ajax/libs/tarteaucitronjs/1.13.0/lang/tarteaucitron.lb.js
@@ -0,0 +1,85 @@
+/*global tarteaucitron */
+tarteaucitron.lang = {
+    "middleBarHead": "☝ 🍪",
+    "adblock": "Gudde Moien! Dëse Site ass transparent a gëtt Iech d'Wiel wéi eng Drëtt-Partei Servicer dir aktivéiere wëllt.",
+    "adblock_call": "Deaktivéiert w.e.g. Ären Adblocker fir d'Personaliséierung unzefänken.",
+    "reload": "Säit nei lueden",
+
+    "alertBigScroll": "Andeems Dir weider scrollt,",
+    "alertBigClick": "Andeems Dir Är Navigatioun weiderféiert,",
+    "alertBig": "akzeptéier Dir d'Benotzung vun Drëtt-Partei Servicer, déi Cookien installéiere kënnen",
+
+    "alertBigPrivacy": "Dëse Site benotzt Cookien a gëtt Iech Kontroll iwwer déi Dir wëllt aktivéieren",
+    "alertSmall": "Gestioun vun de Servicer",
+    "acceptAll": "Alles akzeptéieren",
+    "personalize": "Personaliséieren",
+    "close": "Zoumaachen",
+
+    "privacyUrl": "Privatsphär Politik",
+
+    "all": "Preferenze fir all Servicer",
+
+    "info": "Schutz vun Ärer Privatsphär",
+    "disclaimer": "Andeems Dir dës Drëtt-Partei Servicer autoriséiert, akzeptéiert Dir den Depot an d'Liesen vu Cookien sou wéi d'Benotzung vun Tracking-Technologien, déi néideg sinn fir hire gudde Fonctionnement.",
+    "allow": "Erlaaben",
+    "deny": "Refuséieren",
+    "noCookie": "Dëse Service benotzt keng Cookien.",
+    "useCookie": "Dëse Service kann Cookien hannerleeën",
+    "useCookieCurrent": "Dëse Service huet Cookien hannerluecht",
+    "useNoCookie": "Dëse Service huet keng Cookien ofgespäichert.",
+    "more": "Méi liesen",
+    "source": "Kuckt déi offiziell Websäit",
+    "credit": "Cookie Management vun tarteaucitron.js",
+    "noServices": "Dëse Site benotzt keng Cookien déi Är Zoustëmmung erfuerderen.",
+
+    "toggleInfoBox": "Weisen / verstoppen d'Informatiounen iwwer de Cookie Stockage",
+    "title": "Plattform vun der Cookie Gestioun",
+    "cookieDetail": "Detailer iwwer Cookien",
+    "ourSite": "op eisem Site",
+    "modalWindow": "(modal Fënster)",
+    "newWindow": "(nei Fënster)",
+    "allowAll": "Alles akzeptéieren",
+    "denyAll": "Alles verwerfen",
+
+    "icon": "Cookies",
+
+    "fallback": "ass desaktivéiert.",
+    "allowed": "akzeptéiert",
+    "disallowed": "verworf",
+
+    "ads": {
+        "title": "Publicitéits Servicer",
+        "details": "D'Publicitéits Servicer maachen et méiglech Einnamen ze generéieren andeems d'Werbeplazen vum Site vermaart ginn."
+    },
+    "analytic": {
+        "title": "Miessung vun der Audienz",
+        "details": "Audienzmiessungsservicer maachen et méiglech Trafficstatistiken ze generéieren déi nëtzlech sinn fir de Site ze verbesseren."
+    },
+    "social": {
+        "title": "Sozial Netzwierker",
+        "details": "Sozial Netzwierker verbesseren d'Benotzerfrëndlechkeet vum Site an hëllefen duerch Deelen dësen ze promouvéieren."
+    },
+    "video": {
+        "title": "Videoen",
+        "details": "Video Sharing Servicer beräicheren de Site mat Multimedia Inhalt a vergréissere seng Visibilitéit."
+    },
+    "comment": {
+        "title": "Kommentarer",
+        "details": "De Gestionnaire vu Kommentaren erliichtert den Depot vun Äre Kommentaren a hëlleft Spam-Problemer ze vermeiden."
+    },
+    "support": {
+        "title": "Support",
+        "details": "Support-Servicer erlaben Iech mat der Ekipp vum Site a Kontakt ze trieden an ze hëllefen en ze verbesseren."
+    },
+    "api": {
+        "title": "APIen",
+        "details": "APIen erlaben Iech Skripten ze lueden wéi z.B.: Geolokalisatioun, Sichmotoren, Iwwersetzungen, …"
+    },
+    "other": {
+        "title": "Aner",
+        "details": "Servicer fir Webinhalt ze weisen."
+    },
+
+    "mandatoryTitle": "Obligatoresch Cookien",
+    "mandatoryText": "Dëse Site benotzt Cookien déi néideg sinn fir säi richtege Fonctionnement. Si kënnen net ausgeschalt ginn."
+};
