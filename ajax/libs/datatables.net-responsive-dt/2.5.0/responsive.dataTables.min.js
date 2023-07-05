c648cdcdd9193f1967813098009bfcf25ea6d962
robocdnjs <cdnjs-github@cloudflare.com>
2023-07-05 08:08:44 +0000

    Add datatables.net-responsive-dt (2.5.0)

---
+++ ajax/libs/datatables.net-responsive-dt/2.5.0/responsive.dataTables.min.js
@@ -0,0 +1,4 @@
+/*! DataTables styling wrapper for Responsive
+ * © SpryMedia Ltd - datatables.net/license
+ */
+!function(t){var o,d;"function"==typeof define&&define.amd?define(["jquery","datatables.net-dt","datatables.net-responsive"],function(e){return t(e,window,document)}):"object"==typeof exports?(o=require("jquery"),d=function(e,n){n.fn.dataTable||require("datatables.net-dt")(e,n),n.fn.dataTable.Responsive||require("datatables.net-responsive")(e,n)},"undefined"==typeof window?module.exports=function(e,n){return e=e||window,n=n||o(e),d(e,n),t(n,0,e.document)}:(d(window,o),module.exports=t(o,window,window.document))):t(jQuery,window,document)}(function(e,n,t,o){"use strict";return e.fn.dataTable});
