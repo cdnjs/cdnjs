4e39cb6d1f6a3e500f3195b831510310e3b599c1
robocdnjs <cdnjs-github@cloudflare.com>
2023-07-08 13:14:17 +0000

    Add monaco-editor (0.41.0-dev.20230708)

---
+++ ajax/libs/monaco-editor/0.41.0-dev.20230708/min/vs/base/common/worker/simpleWorker.nls.es.js
@@ -0,0 +1,8 @@
+/*!-----------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Version: 0.41.0-dev.20230708(1556feb83b4c73d0c68bc844709b76cbb0e184ce)
+ * Released under the MIT license
+ * https://github.com/microsoft/vscode/blob/main/LICENSE.txt
+ *-----------------------------------------------------------*/define("vs/base/common/worker/simpleWorker.nls.es",{"vs/base/common/platform":["_"]});
+
+//# sourceMappingURL=../../../../../min-maps/vs/base/common/worker/simpleWorker.nls.es.js.map
---
+++ ajax/libs/monaco-editor/0.41.0-dev.20230708/min/vs/base/common/worker/simpleWorker.nls.zh-tw.js
@@ -0,0 +1,8 @@
+/*!-----------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Version: 0.41.0-dev.20230708(1556feb83b4c73d0c68bc844709b76cbb0e184ce)
+ * Released under the MIT license
+ * https://github.com/microsoft/vscode/blob/main/LICENSE.txt
+ *-----------------------------------------------------------*/define("vs/base/common/worker/simpleWorker.nls.zh-tw",{"vs/base/common/platform":["_"]});
+
+//# sourceMappingURL=../../../../../min-maps/vs/base/common/worker/simpleWorker.nls.zh-tw.js.map
