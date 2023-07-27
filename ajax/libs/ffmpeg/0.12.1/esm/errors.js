3df690a4adae4b8d8ee542e31777970a4326b917
robocdnjs <cdnjs-github@cloudflare.com>
2023-07-27 08:06:50 +0000

    Add ffmpeg (0.12.1)

---
+++ ajax/libs/ffmpeg/0.12.1/esm/errors.js
@@ -0,0 +1,3 @@
+export const ERROR_UNKNOWN_MESSAGE_TYPE = new Error("unknown message type");
+export const ERROR_NOT_LOADED = new Error("ffmpeg is not loaded, call `await ffmpeg.load()` first");
+export const ERROR_TERMINATED = new Error("called FFmpeg.terminate()");
