6fa3b12aff49c624fae49e967eef85569e07ecb8
robocdnjs <cdnjs-github@cloudflare.com>
2023-07-31 00:46:34 +0000

    Add dev-timer (0.2.4)

---
+++ ajax/libs/dev-timer/0.2.4/BrowserTimer.js
@@ -0,0 +1,5 @@
+define(["require", "exports", "./Timer"], function (require, exports, Timer_1) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    exports.default = Timer_1.Timer;
+});
