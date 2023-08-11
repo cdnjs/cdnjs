4684e1ae90e58e60be1a07e7fc1f5090674dcb6e
robocdnjs <cdnjs-github@cloudflare.com>
2023-08-11 12:20:47 +0000

    Add tsparticles-shape-bubble (2.12.0)

---
+++ ajax/libs/tsparticles-shape-bubble/2.12.0/tsparticles.shape.bubble.js
@@ -0,0 +1,84 @@
+/*!
+ * Author : Matteo Bruni
+ * MIT license: https://opensource.org/licenses/MIT
+ * Demo / Generator : https://particles.js.org/
+ * GitHub : https://www.github.com/matteobruni/tsparticles
+ * How to use? : Check the GitHub README
+ * v2.12.0
+ */
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define([], factory);
+	else {
+		var a = factory();
+		for(var i in a) (typeof exports === 'object' ? exports : root)[i] = a[i];
+	}
+})(this, () => {
+return /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	// The require scope
+/******/ 	var __webpack_require__ = {};
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  loadBubbleShape: () => (/* binding */ loadBubbleShape)
+});
+
+;// CONCATENATED MODULE: ./dist/browser/BubbleDrawer.js
+class BubbleDrawer {
+  afterEffect(context, particle, radius) {
+    context.beginPath();
+    context.arc(radius / 3, -radius / 3, radius / 3, 0, Math.PI * 2, false);
+    context.closePath();
+    context.fillStyle = "#fff9";
+    context.fill();
+  }
+  draw(context, particle, radius) {
+    context.arc(0, 0, radius, 0, Math.PI * 2, false);
+  }
+}
+;// CONCATENATED MODULE: ./dist/browser/index.js
+
+async function loadBubbleShape(engine, refresh = true) {
+  await engine.addShape("bubble", new BubbleDrawer(), refresh);
+}
+/******/ 	return __webpack_exports__;
+/******/ })()
+;
+});
