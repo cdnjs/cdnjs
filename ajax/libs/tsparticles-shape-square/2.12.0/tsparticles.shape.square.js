07e8c477e5fda242a4a51b289809ff784ffc8930
robocdnjs <cdnjs-github@cloudflare.com>
2023-08-13 11:44:53 +0000

    Add tsparticles-shape-square (2.12.0)

---
+++ ajax/libs/tsparticles-shape-square/2.12.0/tsparticles.shape.square.js
@@ -0,0 +1,83 @@
+/*!
+ * Author : Matteo Bruni
+ * MIT license: https://opensource.org/licenses/MIT
+ * Demo / Generator : https://particles.js.org/
+ * GitHub : https://www.github.com/matteobruni/tsparticles
+ * How to use? : Check the GitHub README
+ * v2.12.0
+ */
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define([], factory);
+	else {
+		var a = factory();
+		for(var i in a) (typeof exports === 'object' ? exports : root)[i] = a[i];
+	}
+})(this, () => {
+return /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	// The require scope
+/******/ 	var __webpack_require__ = {};
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  loadSquareShape: () => (/* binding */ loadSquareShape)
+});
+
+;// CONCATENATED MODULE: ./dist/browser/SquareDrawer.js
+const fixFactor = Math.sqrt(2);
+class SquareDrawer {
+  draw(context, particle, radius) {
+    const fixedRadius = radius / fixFactor,
+      fixedDiameter = fixedRadius * 2;
+    context.rect(-fixedRadius, -fixedRadius, fixedDiameter, fixedDiameter);
+  }
+  getSidesCount() {
+    return 4;
+  }
+}
+;// CONCATENATED MODULE: ./dist/browser/index.js
+
+async function loadSquareShape(engine, refresh = true) {
+  await engine.addShape(["edge", "square"], new SquareDrawer(), refresh);
+}
+/******/ 	return __webpack_exports__;
+/******/ })()
+;
+});
