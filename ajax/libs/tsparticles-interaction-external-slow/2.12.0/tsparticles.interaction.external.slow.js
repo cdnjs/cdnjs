1c5d26575580c5048e060f5fb8617699d05febdb
robocdnjs <cdnjs-github@cloudflare.com>
2023-08-14 21:05:37 +0000

    Add tsparticles-interaction-external-slow (2.12.0)

---
+++ ajax/libs/tsparticles-interaction-external-slow/2.12.0/tsparticles.interaction.external.slow.js
@@ -0,0 +1,190 @@
+/*!
+ * Author : Matteo Bruni
+ * MIT license: https://opensource.org/licenses/MIT
+ * Demo / Generator : https://particles.js.org/
+ * GitHub : https://www.github.com/matteobruni/tsparticles
+ * How to use? : Check the GitHub README
+ * v2.12.0
+ */
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory(require("tsparticles-engine"));
+	else if(typeof define === 'function' && define.amd)
+		define(["tsparticles-engine"], factory);
+	else {
+		var a = typeof exports === 'object' ? factory(require("tsparticles-engine")) : factory(root["window"]);
+		for(var i in a) (typeof exports === 'object' ? exports : root)[i] = a[i];
+	}
+})(this, (__WEBPACK_EXTERNAL_MODULE__961__) => {
+return /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ 961:
+/***/ ((module) => {
+
+module.exports = __WEBPACK_EXTERNAL_MODULE__961__;
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  Slow: () => (/* reexport */ Slow),
+  loadExternalSlowInteraction: () => (/* binding */ loadExternalSlowInteraction)
+});
+
+// EXTERNAL MODULE: external {"commonjs":"tsparticles-engine","commonjs2":"tsparticles-engine","amd":"tsparticles-engine","root":"window"}
+var external_commonjs_tsparticles_engine_commonjs2_tsparticles_engine_amd_tsparticles_engine_root_window_ = __webpack_require__(961);
+;// CONCATENATED MODULE: ./dist/browser/Options/Classes/Slow.js
+class Slow {
+  constructor() {
+    this.factor = 3;
+    this.radius = 200;
+  }
+  load(data) {
+    if (!data) {
+      return;
+    }
+    if (data.factor !== undefined) {
+      this.factor = data.factor;
+    }
+    if (data.radius !== undefined) {
+      this.radius = data.radius;
+    }
+  }
+}
+;// CONCATENATED MODULE: ./dist/browser/Slower.js
+
+
+class Slower extends external_commonjs_tsparticles_engine_commonjs2_tsparticles_engine_amd_tsparticles_engine_root_window_.ExternalInteractorBase {
+  constructor(container) {
+    super(container);
+  }
+  clear(particle, delta, force) {
+    if (particle.slow.inRange && !force) {
+      return;
+    }
+    particle.slow.factor = 1;
+  }
+  init() {
+    const container = this.container,
+      slow = container.actualOptions.interactivity.modes.slow;
+    if (!slow) {
+      return;
+    }
+    container.retina.slowModeRadius = slow.radius * container.retina.pixelRatio;
+  }
+  async interact() {}
+  isEnabled(particle) {
+    const container = this.container,
+      mouse = container.interactivity.mouse,
+      events = (particle?.interactivity ?? container.actualOptions.interactivity).events;
+    return events.onHover.enable && !!mouse.position && (0,external_commonjs_tsparticles_engine_commonjs2_tsparticles_engine_amd_tsparticles_engine_root_window_.isInArray)("slow", events.onHover.mode);
+  }
+  loadModeOptions(options, ...sources) {
+    if (!options.slow) {
+      options.slow = new Slow();
+    }
+    for (const source of sources) {
+      options.slow.load(source?.slow);
+    }
+  }
+  reset(particle) {
+    particle.slow.inRange = false;
+    const container = this.container,
+      options = container.actualOptions,
+      mousePos = container.interactivity.mouse.position,
+      radius = container.retina.slowModeRadius,
+      slowOptions = options.interactivity.modes.slow;
+    if (!slowOptions || !radius || radius < 0 || !mousePos) {
+      return;
+    }
+    const particlePos = particle.getPosition(),
+      dist = (0,external_commonjs_tsparticles_engine_commonjs2_tsparticles_engine_amd_tsparticles_engine_root_window_.getDistance)(mousePos, particlePos),
+      proximityFactor = dist / radius,
+      slowFactor = slowOptions.factor,
+      {
+        slow
+      } = particle;
+    if (dist > radius) {
+      return;
+    }
+    slow.inRange = true;
+    slow.factor = proximityFactor / slowFactor;
+  }
+}
+;// CONCATENATED MODULE: ./dist/browser/index.js
+
+async function loadExternalSlowInteraction(engine, refresh = true) {
+  await engine.addInteractor("externalSlow", container => new Slower(container), refresh);
+}
+
+
+})();
+
+/******/ 	return __webpack_exports__;
+/******/ })()
+;
+});
