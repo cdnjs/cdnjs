966a523f88c06be382d34d57e7c66dd318337c28
robocdnjs <cdnjs-github@cloudflare.com>
2023-08-20 12:29:33 +0000

    Add openlayers (7.5.1-dev.1691790638276)

---
+++ ajax/libs/openlayers/7.5.1-dev.1691790638276/ViewProperty.d.ts
@@ -0,0 +1,7 @@
+declare namespace _default {
+    let CENTER: string;
+    let RESOLUTION: string;
+    let ROTATION: string;
+}
+export default _default;
+//# sourceMappingURL=ViewProperty.d.ts.map
