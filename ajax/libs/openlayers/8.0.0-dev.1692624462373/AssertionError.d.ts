13c2b0f9222e4f80f5e7fe08e6e7d6ced3f51160
robocdnjs <cdnjs-github@cloudflare.com>
2023-08-21 22:04:35 +0000

    Add openlayers (8.0.0-dev.1692624462373)

---
+++ ajax/libs/openlayers/8.0.0-dev.1692624462373/AssertionError.d.ts
@@ -0,0 +1,23 @@
+export default AssertionError;
+/**
+ * Error object thrown when an assertion failed. This is an ECMA-262 Error,
+ * extended with a `code` property.
+ * See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Error.
+ */
+declare class AssertionError extends Error {
+    /**
+     * @param {number} code Error code.
+     */
+    constructor(code: number);
+    /**
+     * Error code. The meaning of the code can be found on
+     * https://openlayers.org/en/latest/doc/errors/ (replace `latest` with
+     * the version found in the OpenLayers script's header comment if a version
+     * other than the latest is used).
+     * @type {number}
+     * @deprecated ol/AssertionError and error codes will be removed in v8.0
+     * @api
+     */
+    code: number;
+}
+//# sourceMappingURL=AssertionError.d.ts.map
