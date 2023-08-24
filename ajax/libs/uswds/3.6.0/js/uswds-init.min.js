a48596e1f6ce182f025b49001bad695c76e8be4f
robocdnjs <cdnjs-github@cloudflare.com>
2023-08-24 00:05:32 +0000

    Add uswds (3.6.0)

---
+++ ajax/libs/uswds/3.6.0/js/uswds-init.min.js
@@ -0,0 +1,2 @@
+!function(){"use strict";var n,e="usa-js-loading";function t(){document.documentElement.classList.remove(e)}document.documentElement.classList.add(e),n=setTimeout(t,8e3),window.addEventListener("load",function e(){window.uswdsPresent&&(clearTimeout(n),t(),window.removeEventListener("load",e,!0))},!0)}();
+//# sourceMappingURL=uswds-init.min.js.map
