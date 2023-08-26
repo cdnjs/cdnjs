b511f31e7433ea361163355e47c04a9798ea0e05
robocdnjs <cdnjs-github@cloudflare.com>
2023-08-26 22:05:35 +0000

    Add redux-thunk (3.0.0-beta.0)

---
+++ ajax/libs/redux-thunk/3.0.0-beta.0/redux-thunk.mjs
@@ -0,0 +1,16 @@
+// src/index.ts
+function createThunkMiddleware(extraArgument) {
+  const middleware = ({ dispatch, getState }) => (next) => (action) => {
+    if (typeof action === "function") {
+      return action(dispatch, getState, extraArgument);
+    }
+    return next(action);
+  };
+  return middleware;
+}
+var thunk = createThunkMiddleware();
+var withExtraArgument = createThunkMiddleware;
+export {
+  thunk,
+  withExtraArgument
+};
