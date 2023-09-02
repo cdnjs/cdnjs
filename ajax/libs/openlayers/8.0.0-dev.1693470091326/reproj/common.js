099fd6405d32be37d07d12a4e5d7d14ab0cb3d63
robocdnjs <cdnjs-github@cloudflare.com>
2023-09-02 19:05:56 +0000

    Add openlayers (8.0.0-dev.1693470091326)

---
+++ ajax/libs/openlayers/8.0.0-dev.1693470091326/reproj/common.js
@@ -0,0 +1,10 @@
+/**
+ * @module ol/reproj/common
+ */
+
+/**
+ * Default maximum allowed threshold  (in pixels) for reprojection
+ * triangulation.
+ * @type {number}
+ */
+export const ERROR_THRESHOLD = 0.5;
