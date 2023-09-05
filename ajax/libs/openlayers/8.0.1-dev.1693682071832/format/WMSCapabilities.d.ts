c4d09cd7372ac503fae1211b4ae749a35aa7d24e
robocdnjs <cdnjs-github@cloudflare.com>
2023-09-05 04:04:00 +0000

    Add openlayers (8.0.1-dev.1693682071832)

---
+++ ajax/libs/openlayers/8.0.1-dev.1693682071832/format/WMSCapabilities.d.ts
@@ -0,0 +1,15 @@
+export default WMSCapabilities;
+/**
+ * @classdesc
+ * Format for reading WMS capabilities data
+ *
+ * @api
+ */
+declare class WMSCapabilities extends XML {
+    /**
+     * @type {string|undefined}
+     */
+    version: string | undefined;
+}
+import XML from './XML.js';
+//# sourceMappingURL=WMSCapabilities.d.ts.map
---
+++ ajax/libs/openlayers/8.0.1-dev.1693682071832/geom/flat/reverse.js
@@ -0,0 +1,21 @@
+/**
+ * @module ol/geom/flat/reverse
+ */
+
+/**
+ * @param {Array<number>} flatCoordinates Flat coordinates.
+ * @param {number} offset Offset.
+ * @param {number} end End.
+ * @param {number} stride Stride.
+ */
+export function coordinates(flatCoordinates, offset, end, stride) {
+  while (offset < end - stride) {
+    for (let i = 0; i < stride; ++i) {
+      const tmp = flatCoordinates[offset + i];
+      flatCoordinates[offset + i] = flatCoordinates[end - stride + i];
+      flatCoordinates[end - stride + i] = tmp;
+    }
+    offset += stride;
+    end -= stride;
+  }
+}
