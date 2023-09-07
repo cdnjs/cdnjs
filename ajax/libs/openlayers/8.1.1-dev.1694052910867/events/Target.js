77c9c3d151d5799591126d3731242ed8e978c3a4
robocdnjs <cdnjs-github@cloudflare.com>
2023-09-07 22:04:07 +0000

    Add openlayers (8.1.1-dev.1694052910867)

---
+++ ajax/libs/openlayers/8.1.1-dev.1694052910867/events/Target.js
@@ -0,0 +1,188 @@
+/**
+ * @module ol/events/Target
+ */
+import Disposable from '../Disposable.js';
+import Event from './Event.js';
+import {VOID} from '../functions.js';
+import {clear} from '../obj.js';
+
+/**
+ * @typedef {EventTarget|Target} EventTargetLike
+ */
+
+/**
+ * @classdesc
+ * A simplified implementation of the W3C DOM Level 2 EventTarget interface.
+ * See https://www.w3.org/TR/2000/REC-DOM-Level-2-Events-20001113/events.html#Events-EventTarget.
+ *
+ * There are two important simplifications compared to the specification:
+ *
+ * 1. The handling of `useCapture` in `addEventListener` and
+ *    `removeEventListener`. There is no real capture model.
+ * 2. The handling of `stopPropagation` and `preventDefault` on `dispatchEvent`.
+ *    There is no event target hierarchy. When a listener calls
+ *    `stopPropagation` or `preventDefault` on an event object, it means that no
+ *    more listeners after this one will be called. Same as when the listener
+ *    returns false.
+ */
+class Target extends Disposable {
+  /**
+   * @param {*} [target] Default event target for dispatched events.
+   */
+  constructor(target) {
+    super();
+
+    /**
+     * @private
+     * @type {*}
+     */
+    this.eventTarget_ = target;
+
+    /**
+     * @private
+     * @type {Object<string, number>}
+     */
+    this.pendingRemovals_ = null;
+
+    /**
+     * @private
+     * @type {Object<string, number>}
+     */
+    this.dispatching_ = null;
+
+    /**
+     * @private
+     * @type {Object<string, Array<import("../events.js").Listener>>}
+     */
+    this.listeners_ = null;
+  }
+
+  /**
+   * @param {string} type Type.
+   * @param {import("../events.js").Listener} listener Listener.
+   */
+  addEventListener(type, listener) {
+    if (!type || !listener) {
+      return;
+    }
+    const listeners = this.listeners_ || (this.listeners_ = {});
+    const listenersForType = listeners[type] || (listeners[type] = []);
+    if (!listenersForType.includes(listener)) {
+      listenersForType.push(listener);
+    }
+  }
+
+  /**
+   * Dispatches an event and calls all listeners listening for events
+   * of this type. The event parameter can either be a string or an
+   * Object with a `type` property.
+   *
+   * @param {import("./Event.js").default|string} event Event object.
+   * @return {boolean|undefined} `false` if anyone called preventDefault on the
+   *     event object or if any of the listeners returned false.
+   * @api
+   */
+  dispatchEvent(event) {
+    const isString = typeof event === 'string';
+    const type = isString ? event : event.type;
+    const listeners = this.listeners_ && this.listeners_[type];
+    if (!listeners) {
+      return;
+    }
+
+    const evt = isString ? new Event(event) : /** @type {Event} */ (event);
+    if (!evt.target) {
+      evt.target = this.eventTarget_ || this;
+    }
+    const dispatching = this.dispatching_ || (this.dispatching_ = {});
+    const pendingRemovals =
+      this.pendingRemovals_ || (this.pendingRemovals_ = {});
+    if (!(type in dispatching)) {
+      dispatching[type] = 0;
+      pendingRemovals[type] = 0;
+    }
+    ++dispatching[type];
+    let propagate;
+    for (let i = 0, ii = listeners.length; i < ii; ++i) {
+      if ('handleEvent' in listeners[i]) {
+        propagate = /** @type {import("../events.js").ListenerObject} */ (
+          listeners[i]
+        ).handleEvent(evt);
+      } else {
+        propagate = /** @type {import("../events.js").ListenerFunction} */ (
+          listeners[i]
+        ).call(this, evt);
+      }
+      if (propagate === false || evt.propagationStopped) {
+        propagate = false;
+        break;
+      }
+    }
+    if (--dispatching[type] === 0) {
+      let pr = pendingRemovals[type];
+      delete pendingRemovals[type];
+      while (pr--) {
+        this.removeEventListener(type, VOID);
+      }
+      delete dispatching[type];
+    }
+    return propagate;
+  }
+
+  /**
+   * Clean up.
+   */
+  disposeInternal() {
+    this.listeners_ && clear(this.listeners_);
+  }
+
+  /**
+   * Get the listeners for a specified event type. Listeners are returned in the
+   * order that they will be called in.
+   *
+   * @param {string} type Type.
+   * @return {Array<import("../events.js").Listener>|undefined} Listeners.
+   */
+  getListeners(type) {
+    return (this.listeners_ && this.listeners_[type]) || undefined;
+  }
+
+  /**
+   * @param {string} [type] Type. If not provided,
+   *     `true` will be returned if this event target has any listeners.
+   * @return {boolean} Has listeners.
+   */
+  hasListener(type) {
+    if (!this.listeners_) {
+      return false;
+    }
+    return type
+      ? type in this.listeners_
+      : Object.keys(this.listeners_).length > 0;
+  }
+
+  /**
+   * @param {string} type Type.
+   * @param {import("../events.js").Listener} listener Listener.
+   */
+  removeEventListener(type, listener) {
+    const listeners = this.listeners_ && this.listeners_[type];
+    if (listeners) {
+      const index = listeners.indexOf(listener);
+      if (index !== -1) {
+        if (this.pendingRemovals_ && type in this.pendingRemovals_) {
+          // make listener a no-op, and remove later in #dispatchEvent()
+          listeners[index] = VOID;
+          ++this.pendingRemovals_[type];
+        } else {
+          listeners.splice(index, 1);
+          if (listeners.length === 0) {
+            delete this.listeners_[type];
+          }
+        }
+      }
+    }
+  }
+}
+
+export default Target;
---
+++ ajax/libs/openlayers/8.1.1-dev.1694052910867/webgl/ContextEventType.d.ts
@@ -0,0 +1,6 @@
+declare namespace _default {
+    let LOST: string;
+    let RESTORED: string;
+}
+export default _default;
+//# sourceMappingURL=ContextEventType.d.ts.map
