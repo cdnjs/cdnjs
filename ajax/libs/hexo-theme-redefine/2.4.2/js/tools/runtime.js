cd8f83fe59ea019fc1bfb87d68ce18a2a688ce47
robocdnjs <cdnjs-github@cloudflare.com>
2023-09-07 02:10:17 +0000

    Add hexo-theme-redefine (2.4.2)

---
+++ ajax/libs/hexo-theme-redefine/2.4.2/js/tools/runtime.js
@@ -0,0 +1,22 @@
+function footerRuntime() {
+    var startTime = Global.theme_config.footerStart;
+    window.setTimeout("footerRuntime()", 1000);
+    X = new Date(startTime);
+    Y = new Date();
+    T = (Y.getTime() - X.getTime());
+    M = 24 * 60 * 60 * 1000;
+    a = T / M;
+    A = Math.floor(a);
+    b = (a - A) * 24;
+    B = Math.floor(b);
+    c = (b - B) * 60;
+    C = Math.floor((b - B) * 60);
+    D = Math.floor((c - C) * 60);
+    runtime_days.innerHTML = A;
+    runtime_hours.innerHTML = B;
+    runtime_minutes.innerHTML = C;
+    runtime_seconds.innerHTML = D;
+}
+
+
+window.addEventListener('DOMContentLoaded', footerRuntime);
