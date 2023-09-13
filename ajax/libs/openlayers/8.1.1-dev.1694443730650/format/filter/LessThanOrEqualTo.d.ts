716ff1337200fd5ed7c996e03446b2630e9bf69d
robocdnjs <cdnjs-github@cloudflare.com>
2023-09-13 11:06:29 +0000

    Add openlayers (8.1.1-dev.1694443730650)

---
+++ ajax/libs/openlayers/8.1.1-dev.1694443730650/format/filter/LessThanOrEqualTo.d.ts
@@ -0,0 +1,15 @@
+export default LessThanOrEqualTo;
+/**
+ * @classdesc
+ * Represents a `<PropertyIsLessThanOrEqualTo>` comparison operator.
+ * @api
+ */
+declare class LessThanOrEqualTo extends ComparisonBinary {
+    /**
+     * @param {!string} propertyName Name of the context property to compare.
+     * @param {!number} expression The value to compare.
+     */
+    constructor(propertyName: string, expression: number);
+}
+import ComparisonBinary from './ComparisonBinary.js';
+//# sourceMappingURL=LessThanOrEqualTo.d.ts.map
