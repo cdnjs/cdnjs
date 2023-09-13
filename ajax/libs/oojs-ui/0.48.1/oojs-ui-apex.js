931e43ab1a16e518d9674fdf5dcde1ca4e1f1cd6
robocdnjs <cdnjs-github@cloudflare.com>
2023-09-13 11:08:48 +0000

    Add oojs-ui (0.48.1)

---
+++ ajax/libs/oojs-ui/0.48.1/oojs-ui-apex.js
@@ -0,0 +1,45 @@
+/*!
+ * OOUI v0.48.1
+ * https://www.mediawiki.org/wiki/OOUI
+ *
+ * Copyright 2011–2023 OOUI Team and other contributors.
+ * Released under the MIT license
+ * http://oojs.mit-license.org
+ *
+ * Date: 2023-09-12T23:28:04Z
+ */
+( function ( OO ) {
+
+'use strict';
+
+/**
+ * @class
+ * @extends OO.ui.Theme
+ *
+ * @constructor
+ */
+OO.ui.ApexTheme = function OoUiApexTheme() {
+	// Parent constructor
+	OO.ui.ApexTheme.super.call( this );
+};
+
+/* Setup */
+
+OO.inheritClass( OO.ui.ApexTheme, OO.ui.Theme );
+
+/* Methods */
+
+/**
+ * @inheritdoc
+ */
+OO.ui.ApexTheme.prototype.getDialogTransitionDuration = function () {
+	return 250;
+};
+
+/* Instantiation */
+
+OO.ui.theme = new OO.ui.ApexTheme();
+
+}( OO ) );
+
+//# sourceMappingURL=oojs-ui-apex.js.map.json
