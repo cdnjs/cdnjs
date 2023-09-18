653420eac9bd4298479bf1d411da451b6cef7409
robocdnjs <cdnjs-github@cloudflare.com>
2023-09-18 09:06:24 +0000

    Add openlayers (8.1.1-dev.1694732464464)

---
+++ ajax/libs/openlayers/8.1.1-dev.1694732464464/events/Event.d.ts
@@ -0,0 +1,60 @@
+/**
+ * @param {Event|import("./Event.js").default} evt Event
+ */
+export function stopPropagation(evt: Event | import("./Event.js").default): void;
+/**
+ * @param {Event|import("./Event.js").default} evt Event
+ */
+export function preventDefault(evt: Event | import("./Event.js").default): void;
+export default BaseEvent;
+/**
+ * @module ol/events/Event
+ */
+/**
+ * @classdesc
+ * Stripped down implementation of the W3C DOM Level 2 Event interface.
+ * See https://www.w3.org/TR/DOM-Level-2-Events/events.html#Events-interface.
+ *
+ * This implementation only provides `type` and `target` properties, and
+ * `stopPropagation` and `preventDefault` methods. It is meant as base class
+ * for higher level events defined in the library, and works with
+ * {@link module:ol/events/Target~Target}.
+ */
+declare class BaseEvent {
+    /**
+     * @param {string} type Type.
+     */
+    constructor(type: string);
+    /**
+     * @type {boolean}
+     */
+    propagationStopped: boolean;
+    /**
+     * @type {boolean}
+     */
+    defaultPrevented: boolean;
+    /**
+     * The event type.
+     * @type {string}
+     * @api
+     */
+    type: string;
+    /**
+     * The event target.
+     * @type {Object}
+     * @api
+     */
+    target: any;
+    /**
+     * Prevent default. This means that no emulated `click`, `singleclick` or `doubleclick` events
+     * will be fired.
+     * @api
+     */
+    preventDefault(): void;
+    /**
+     * Stop event propagation.
+     * @api
+     */
+    stopPropagation(): void;
+}
+//# sourceMappingURL=Event.d.ts.map
---
+++ ajax/libs/openlayers/8.1.1-dev.1694732464464/format/filter/IsLike.d.ts
@@ -0,0 +1,43 @@
+export default IsLike;
+/**
+ * @classdesc
+ * Represents a `<PropertyIsLike>` comparison operator.
+ * @api
+ */
+declare class IsLike extends Comparison {
+    /**
+     * [constructor description]
+     * @param {!string} propertyName Name of the context property to compare.
+     * @param {!string} pattern Text pattern.
+     * @param {string} [wildCard] Pattern character which matches any sequence of
+     *    zero or more string characters. Default is '*'.
+     * @param {string} [singleChar] pattern character which matches any single
+     *    string character. Default is '.'.
+     * @param {string} [escapeChar] Escape character which can be used to escape
+     *    the pattern characters. Default is '!'.
+     * @param {boolean} [matchCase] Case-sensitive?
+     */
+    constructor(propertyName: string, pattern: string, wildCard?: string | undefined, singleChar?: string | undefined, escapeChar?: string | undefined, matchCase?: boolean | undefined);
+    /**
+     * @type {!string}
+     */
+    pattern: string;
+    /**
+     * @type {!string}
+     */
+    wildCard: string;
+    /**
+     * @type {!string}
+     */
+    singleChar: string;
+    /**
+     * @type {!string}
+     */
+    escapeChar: string;
+    /**
+     * @type {boolean|undefined}
+     */
+    matchCase: boolean | undefined;
+}
+import Comparison from './Comparison.js';
+//# sourceMappingURL=IsLike.d.ts.map
