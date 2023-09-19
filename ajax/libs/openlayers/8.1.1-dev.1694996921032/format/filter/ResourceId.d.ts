cfd23f004bc0111d2f44ae349d36a6332e2d0aee
robocdnjs <cdnjs-github@cloudflare.com>
2023-09-19 13:06:24 +0000

    Add openlayers (8.1.1-dev.1694996921032)

---
+++ ajax/libs/openlayers/8.1.1-dev.1694996921032/format/filter/ResourceId.d.ts
@@ -0,0 +1,14 @@
+export default ResourceId;
+/**
+ * @classdesc
+ *
+ * @abstract
+ */
+declare class ResourceId extends Filter {
+    /**
+     * @type {!string}
+     */
+    rid: string;
+}
+import Filter from './Filter.js';
+//# sourceMappingURL=ResourceId.d.ts.map
