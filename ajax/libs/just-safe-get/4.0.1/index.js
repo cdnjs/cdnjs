e8043f7d51881a5f645b96a747b60e5615d889e6
robocdnjs <cdnjs-github@cloudflare.com>
2023-09-19 06:05:43 +0000

    Add just-safe-get (4.0.1)

---
+++ ajax/libs/just-safe-get/4.0.1/index.js
@@ -0,0 +1,61 @@
+module.exports = get;
+
+/*
+  const obj = {a: {aa: {aaa: 2}}, b: 4};
+
+  get(obj, 'a.aa.aaa'); // 2
+  get(obj, ['a', 'aa', 'aaa']); // 2
+
+  get(obj, 'b.bb.bbb'); // undefined
+  get(obj, ['b', 'bb', 'bbb']); // undefined
+
+  get(obj.a, 'aa.aaa'); // 2
+  get(obj.a, ['aa', 'aaa']); // 2
+
+  get(obj.b, 'bb.bbb'); // undefined
+  get(obj.b, ['bb', 'bbb']); // undefined
+
+  get(obj.b, 'bb.bbb', 42); // 42
+  get(obj.b, ['bb', 'bbb'], 42); // 42
+
+  get(null, 'a'); // undefined
+  get(undefined, ['a']); // undefined
+
+  get(null, 'a', 42); // 42
+  get(undefined, ['a'], 42); // 42
+
+  const obj = {a: {}};
+  const sym = Symbol();
+  obj.a[sym] = 4;
+  get(obj.a, sym); // 4
+*/
+
+function get(obj, propsArg, defaultValue) {
+  if (!obj) {
+    return defaultValue;
+  }
+  var props, prop;
+  if (Array.isArray(propsArg)) {
+    props = propsArg.slice(0);
+  }
+  if (typeof propsArg == 'string') {
+    props = propsArg.split('.');
+  }
+  if (typeof propsArg == 'symbol') {
+    props = [propsArg];
+  }
+  if (!Array.isArray(props)) {
+    throw new Error('props arg must be an array, a string or a symbol');
+  }
+  while (props.length) {
+    prop = props.shift();
+    if (!obj) {
+      return defaultValue;
+    }
+    obj = obj[prop];
+    if (obj === undefined) {
+      return defaultValue;
+    }
+  }
+  return obj;
+}
