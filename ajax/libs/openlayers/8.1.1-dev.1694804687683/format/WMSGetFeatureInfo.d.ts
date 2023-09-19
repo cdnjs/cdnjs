25ba9166728dba7c3fb2694400397dc56e0404d4
robocdnjs <cdnjs-github@cloudflare.com>
2023-09-19 04:05:04 +0000

    Add openlayers (8.1.1-dev.1694804687683)

---
+++ ajax/libs/openlayers/8.1.1-dev.1694804687683/format/WMSGetFeatureInfo.d.ts
@@ -0,0 +1,52 @@
+export default WMSGetFeatureInfo;
+export type Options = {
+    /**
+     * If set, only features of the given layers will be returned by the format when read.
+     */
+    layers?: string[] | undefined;
+};
+/**
+ * @classdesc
+ * Format for reading WMSGetFeatureInfo format. It uses
+ * {@link module:ol/format/GML2~GML2} to read features.
+ *
+ * @api
+ */
+declare class WMSGetFeatureInfo extends XMLFeature {
+    /**
+     * @param {Options} [options] Options.
+     */
+    constructor(options?: Options | undefined);
+    /**
+     * @private
+     * @type {string}
+     */
+    private featureNS_;
+    /**
+     * @private
+     * @type {GML2}
+     */
+    private gmlFormat_;
+    /**
+     * @private
+     * @type {Array<string>|null}
+     */
+    private layers_;
+    /**
+     * @return {Array<string>|null} layers
+     */
+    getLayers(): Array<string> | null;
+    /**
+     * @param {Array<string>|null} layers Layers to parse.
+     */
+    setLayers(layers: Array<string> | null): void;
+    /**
+     * @param {Element} node Node.
+     * @param {Array<*>} objectStack Object stack.
+     * @return {Array<import("../Feature.js").default>} Features.
+     * @private
+     */
+    private readFeatures_;
+}
+import XMLFeature from './XMLFeature.js';
+//# sourceMappingURL=WMSGetFeatureInfo.d.ts.map
