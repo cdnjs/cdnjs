22b2110dab791d3d63e0467e20a85a4fc91f8a32
robocdnjs <cdnjs-github@cloudflare.com>
2023-09-21 10:04:02 +0000

    Add openlayers (8.1.1-dev.1695093267486)

---
+++ ajax/libs/openlayers/8.1.1-dev.1695093267486/ImageState.d.ts
@@ -0,0 +1,9 @@
+declare namespace _default {
+    let IDLE: number;
+    let LOADING: number;
+    let LOADED: number;
+    let ERROR: number;
+    let EMPTY: number;
+}
+export default _default;
+//# sourceMappingURL=ImageState.d.ts.map
