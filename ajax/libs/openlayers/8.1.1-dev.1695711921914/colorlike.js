1b8e9f2958952b3dacd36cbde3c36dda0c3e18d5
robocdnjs <cdnjs-github@cloudflare.com>
2023-10-16 16:32:03 +0000

    Add openlayers (8.1.1-dev.1695711921914)

---
+++ ajax/libs/openlayers/8.1.1-dev.1695711921914/colorlike.js
@@ -0,0 +1,28 @@
+/**
+ * @module ol/colorlike
+ */
+import {toString} from './color.js';
+
+/**
+ * A type accepted by CanvasRenderingContext2D.fillStyle
+ * or CanvasRenderingContext2D.strokeStyle.
+ * Represents a color, pattern, or gradient. The origin for patterns and
+ * gradients as fill style is an increment of 512 css pixels from map coordinate
+ * `[0, 0]`. For seamless repeat patterns, width and height of the pattern image
+ * must be a factor of two (2, 4, 8, ..., 512).
+ *
+ * @typedef {string|CanvasPattern|CanvasGradient} ColorLike
+ * @api
+ */
+
+/**
+ * @param {import("./color.js").Color|ColorLike} color Color.
+ * @return {ColorLike} The color as an {@link ol/colorlike~ColorLike}.
+ * @api
+ */
+export function asColorLike(color) {
+  if (Array.isArray(color)) {
+    return toString(color);
+  }
+  return color;
+}
---
+++ ajax/libs/openlayers/8.1.1-dev.1695711921914/events/EventType.d.ts
@@ -0,0 +1,21 @@
+declare namespace _default {
+    let CHANGE: string;
+    let ERROR: string;
+    let BLUR: string;
+    let CLEAR: string;
+    let CONTEXTMENU: string;
+    let CLICK: string;
+    let DBLCLICK: string;
+    let DRAGENTER: string;
+    let DRAGOVER: string;
+    let DROP: string;
+    let FOCUS: string;
+    let KEYDOWN: string;
+    let KEYPRESS: string;
+    let LOAD: string;
+    let RESIZE: string;
+    let TOUCHMOVE: string;
+    let WHEEL: string;
+}
+export default _default;
+//# sourceMappingURL=EventType.d.ts.map
