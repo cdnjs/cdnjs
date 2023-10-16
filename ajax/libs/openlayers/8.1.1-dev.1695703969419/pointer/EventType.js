770e7ea508b8ffc870ed86f2d984d92826f1145c
robocdnjs <cdnjs-github@cloudflare.com>
2023-10-16 15:29:58 +0000

    Add openlayers (8.1.1-dev.1695703969419)

---
+++ ajax/libs/openlayers/8.1.1-dev.1695703969419/pointer/EventType.js
@@ -0,0 +1,18 @@
+/**
+ * @module ol/pointer/EventType
+ */
+
+/**
+ * Constants for event names.
+ * @enum {string}
+ */
+export default {
+  POINTERMOVE: 'pointermove',
+  POINTERDOWN: 'pointerdown',
+  POINTERUP: 'pointerup',
+  POINTEROVER: 'pointerover',
+  POINTEROUT: 'pointerout',
+  POINTERENTER: 'pointerenter',
+  POINTERLEAVE: 'pointerleave',
+  POINTERCANCEL: 'pointercancel',
+};
