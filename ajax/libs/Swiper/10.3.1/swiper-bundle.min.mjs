b8d5e3c4adc84e4dd1aff46b932c067bf678fc72
robocdnjs <cdnjs-github@cloudflare.com>
2023-10-16 16:23:25 +0000

    Add Swiper (10.3.1)

---
+++ ajax/libs/Swiper/10.3.1/swiper-bundle.min.mjs
@@ -0,0 +1,14 @@
+/**
+ * Swiper 10.3.1
+ * Most modern mobile touch slider and framework with hardware accelerated transitions
+ * https://swiperjs.com
+ *
+ * Copyright 2014-2023 Vladimir Kharlampidi
+ *
+ * Released under the MIT License
+ *
+ * Released on: September 28, 2023
+ */
+
+import{S as Swiper}from"./shared/swiper-core.min.mjs";import Virtual from"./modules/virtual.min.mjs";import Keyboard from"./modules/keyboard.min.mjs";import Mousewheel from"./modules/mousewheel.min.mjs";import Navigation from"./modules/navigation.min.mjs";import Pagination from"./modules/pagination.min.mjs";import Scrollbar from"./modules/scrollbar.min.mjs";import Parallax from"./modules/parallax.min.mjs";import Zoom from"./modules/zoom.min.mjs";import Controller from"./modules/controller.min.mjs";import A11y from"./modules/a11y.min.mjs";import History from"./modules/history.min.mjs";import HashNavigation from"./modules/hash-navigation.min.mjs";import Autoplay from"./modules/autoplay.min.mjs";import Thumb from"./modules/thumbs.min.mjs";import freeMode from"./modules/free-mode.min.mjs";import Grid from"./modules/grid.min.mjs";import Manipulation from"./modules/manipulation.min.mjs";import EffectFade from"./modules/effect-fade.min.mjs";import EffectCube from"./modules/effect-cube.min.mjs";import EffectFlip from"./modules/effect-flip.min.mjs";import EffectCoverflow from"./modules/effect-coverflow.min.mjs";import EffectCreative from"./modules/effect-creative.min.mjs";import EffectCards from"./modules/effect-cards.min.mjs";const modules=[Virtual,Keyboard,Mousewheel,Navigation,Pagination,Scrollbar,Parallax,Zoom,Controller,A11y,History,HashNavigation,Autoplay,Thumb,freeMode,Grid,Manipulation,EffectFade,EffectCube,EffectFlip,EffectCoverflow,EffectCreative,EffectCards];Swiper.use(modules);export{Swiper,Swiper as default};
+//# sourceMappingURL=swiper-bundle.min.mjs.map
---
+++ ajax/libs/Swiper/10.3.1/swiper-bundle.mjs
@@ -0,0 +1,42 @@
+/**
+ * Swiper 10.3.1
+ * Most modern mobile touch slider and framework with hardware accelerated transitions
+ * https://swiperjs.com
+ *
+ * Copyright 2014-2023 Vladimir Kharlampidi
+ *
+ * Released under the MIT License
+ *
+ * Released on: September 28, 2023
+ */
+
+import { S as Swiper } from './shared/swiper-core.mjs';
+import Virtual from './modules/virtual.mjs';
+import Keyboard from './modules/keyboard.mjs';
+import Mousewheel from './modules/mousewheel.mjs';
+import Navigation from './modules/navigation.mjs';
+import Pagination from './modules/pagination.mjs';
+import Scrollbar from './modules/scrollbar.mjs';
+import Parallax from './modules/parallax.mjs';
+import Zoom from './modules/zoom.mjs';
+import Controller from './modules/controller.mjs';
+import A11y from './modules/a11y.mjs';
+import History from './modules/history.mjs';
+import HashNavigation from './modules/hash-navigation.mjs';
+import Autoplay from './modules/autoplay.mjs';
+import Thumb from './modules/thumbs.mjs';
+import freeMode from './modules/free-mode.mjs';
+import Grid from './modules/grid.mjs';
+import Manipulation from './modules/manipulation.mjs';
+import EffectFade from './modules/effect-fade.mjs';
+import EffectCube from './modules/effect-cube.mjs';
+import EffectFlip from './modules/effect-flip.mjs';
+import EffectCoverflow from './modules/effect-coverflow.mjs';
+import EffectCreative from './modules/effect-creative.mjs';
+import EffectCards from './modules/effect-cards.mjs';
+
+// Swiper Class
+const modules = [Virtual, Keyboard, Mousewheel, Navigation, Pagination, Scrollbar, Parallax, Zoom, Controller, A11y, History, HashNavigation, Autoplay, Thumb, freeMode, Grid, Manipulation, EffectFade, EffectCube, EffectFlip, EffectCoverflow, EffectCreative, EffectCards];
+Swiper.use(modules);
+
+export { Swiper, Swiper as default };
