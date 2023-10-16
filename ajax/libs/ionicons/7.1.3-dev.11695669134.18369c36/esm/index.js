470f06011cd633fe97a25d9a0d8df4c792705880
robocdnjs <cdnjs-github@cloudflare.com>
2023-10-16 13:40:51 +0000

    Add ionicons (7.1.3-dev.11695669134.18369c36)

---
+++ ajax/libs/ionicons/7.1.3-dev.11695669134.18369c36/esm/index.js
@@ -0,0 +1,2 @@
+export { a as addIcons } from './utils-ccb924b9.js';
+import './index-5514a13d.js';
---
+++ ajax/libs/ionicons/7.1.3-dev.11695669134.18369c36/ionicons/p-eb3ff5ea.system.js
@@ -0,0 +1,1 @@
+System.register(["./p-cff0a9de.system.js","./p-ea7bbed1.system.js"],(function(e){"use strict";return{setters:[function(t){e("addIcons",t.a)},function(){}],execute:function(){}}}));
