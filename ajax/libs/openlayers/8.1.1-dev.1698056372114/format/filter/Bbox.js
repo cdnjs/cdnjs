88a00dcc0a5f192f813fdce6d162506d4c97b559
robocdnjs <cdnjs-github@cloudflare.com>
2023-10-23 16:08:13 +0000

    Add openlayers (8.1.1-dev.1698056372114)

---
+++ ajax/libs/openlayers/8.1.1-dev.1698056372114/format/filter/Bbox.js
@@ -0,0 +1,45 @@
+/**
+ * @module ol/format/filter/Bbox
+ */
+import Filter from './Filter.js';
+
+/**
+ * @classdesc
+ * Represents a `<BBOX>` operator to test whether a geometry-valued property
+ * intersects a fixed bounding box
+ *
+ * @api
+ */
+class Bbox extends Filter {
+  /**
+   * @param {!string} geometryName Geometry name to use.
+   * @param {!import("../../extent.js").Extent} extent Extent.
+   * @param {string} [srsName] SRS name. No srsName attribute will be set
+   * on geometries when this is not provided.
+   */
+  constructor(geometryName, extent, srsName) {
+    super('BBOX');
+
+    /**
+     * @type {!string}
+     */
+    this.geometryName = geometryName;
+
+    /**
+     * @type {import("../../extent.js").Extent}
+     */
+    this.extent = extent;
+    if (extent.length !== 4) {
+      throw new Error(
+        'Expected an extent with four values ([minX, minY, maxX, maxY])'
+      );
+    }
+
+    /**
+     * @type {string|undefined}
+     */
+    this.srsName = srsName;
+  }
+}
+
+export default Bbox;
