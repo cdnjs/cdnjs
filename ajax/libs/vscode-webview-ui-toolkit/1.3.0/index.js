689931d34e5dc2b86edd8b71ee25bdafba2c52c1
robocdnjs <cdnjs-github@cloudflare.com>
2023-11-14 06:06:17 +0000

    Add vscode-webview-ui-toolkit (1.3.0)

---
+++ ajax/libs/vscode-webview-ui-toolkit/1.3.0/index.js
@@ -0,0 +1,20 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+export * from './vscode-design-system.js';
+export * from './custom-elements.js';
+// Export components and classes
+export * from './badge/index.js';
+export * from './button/index.js';
+export * from './checkbox/index.js';
+export * from './data-grid/index.js';
+export * from './divider/index.js';
+export * from './dropdown/index.js';
+export * from './link/index.js';
+export * from './option/index.js';
+export * from './panels/index.js';
+export * from './progress-ring/index.js';
+export * from './radio-group/index.js';
+export * from './radio/index.js';
+export * from './tag/index.js';
+export * from './text-area/index.js';
+export * from './text-field/index.js';
