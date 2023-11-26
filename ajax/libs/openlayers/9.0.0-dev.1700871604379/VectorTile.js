70e8e559447452b2113eb99fdb8d62ea8f1e7df2
robocdnjs <cdnjs-github@cloudflare.com>
2023-11-26 03:05:12 +0000

    Add openlayers (9.0.0-dev.1700871604379)

---
+++ ajax/libs/openlayers/9.0.0-dev.1700871604379/VectorTile.js
@@ -0,0 +1,138 @@
+/**
+ * @module ol/VectorTile
+ */
+import Tile from './Tile.js';
+import TileState from './TileState.js';
+
+class VectorTile extends Tile {
+  /**
+   * @param {import("./tilecoord.js").TileCoord} tileCoord Tile coordinate.
+   * @param {import("./TileState.js").default} state State.
+   * @param {string} src Data source url.
+   * @param {import("./format/Feature.js").default} format Feature format.
+   * @param {import("./Tile.js").LoadFunction} tileLoadFunction Tile load function.
+   * @param {import("./Tile.js").Options} [options] Tile options.
+   */
+  constructor(tileCoord, state, src, format, tileLoadFunction, options) {
+    super(tileCoord, state, options);
+
+    /**
+     * Extent of this tile; set by the source.
+     * @type {import("./extent.js").Extent}
+     */
+    this.extent = null;
+
+    /**
+     * @private
+     * @type {import("./format/Feature.js").default}
+     */
+    this.format_ = format;
+
+    /**
+     * @private
+     * @type {Array<import("./Feature.js").FeatureLike>}
+     */
+    this.features_ = null;
+
+    /**
+     * @private
+     * @type {import("./featureloader.js").FeatureLoader}
+     */
+    this.loader_;
+
+    /**
+     * Feature projection of this tile; set by the source.
+     * @type {import("./proj/Projection.js").default}
+     */
+    this.projection = null;
+
+    /**
+     * Resolution of this tile; set by the source.
+     * @type {number}
+     */
+    this.resolution;
+
+    /**
+     * @private
+     * @type {import("./Tile.js").LoadFunction}
+     */
+    this.tileLoadFunction_ = tileLoadFunction;
+
+    /**
+     * @private
+     * @type {string}
+     */
+    this.url_ = src;
+
+    this.key = src;
+  }
+
+  /**
+   * Get the feature format assigned for reading this tile's features.
+   * @return {import("./format/Feature.js").default} Feature format.
+   * @api
+   */
+  getFormat() {
+    return this.format_;
+  }
+
+  /**
+   * Get the features for this tile. Geometries will be in the view projection.
+   * @return {Array<import("./Feature.js").FeatureLike>} Features.
+   * @api
+   */
+  getFeatures() {
+    return this.features_;
+  }
+
+  /**
+   * Load not yet loaded URI.
+   */
+  load() {
+    if (this.state == TileState.IDLE) {
+      this.setState(TileState.LOADING);
+      this.tileLoadFunction_(this, this.url_);
+      if (this.loader_) {
+        this.loader_(this.extent, this.resolution, this.projection);
+      }
+    }
+  }
+
+  /**
+   * Handler for successful tile load.
+   * @param {Array<import("./Feature.js").default>} features The loaded features.
+   * @param {import("./proj/Projection.js").default} dataProjection Data projection.
+   */
+  onLoad(features, dataProjection) {
+    this.setFeatures(features);
+  }
+
+  /**
+   * Handler for tile load errors.
+   */
+  onError() {
+    this.setState(TileState.ERROR);
+  }
+
+  /**
+   * Function for use in an {@link module:ol/source/VectorTile~VectorTile}'s `tileLoadFunction`.
+   * Sets the features for the tile.
+   * @param {Array<import("./Feature.js").FeatureLike>} features Features.
+   * @api
+   */
+  setFeatures(features) {
+    this.features_ = features;
+    this.setState(TileState.LOADED);
+  }
+
+  /**
+   * Set the feature loader for reading this tile's features.
+   * @param {import("./featureloader.js").FeatureLoader} loader Feature loader.
+   * @api
+   */
+  setLoader(loader) {
+    this.loader_ = loader;
+  }
+}
+
+export default VectorTile;
---
+++ ajax/libs/openlayers/9.0.0-dev.1700871604379/format/TopoJSON.d.ts
@@ -0,0 +1,107 @@
+export default TopoJSON;
+export type TopoJSONTopology = import("topojson-specification").Topology;
+export type TopoJSONGeometryCollection = import("topojson-specification").GeometryCollection;
+export type TopoJSONGeometry = import("topojson-specification").GeometryObject;
+export type TopoJSONPoint = import("topojson-specification").Point;
+export type TopoJSONMultiPoint = import("topojson-specification").MultiPoint;
+export type TopoJSONLineString = import("topojson-specification").LineString;
+export type TopoJSONMultiLineString = import("topojson-specification").MultiLineString;
+export type TopoJSONPolygon = import("topojson-specification").Polygon;
+export type TopoJSONMultiPolygon = import("topojson-specification").MultiPolygon;
+export type Options = {
+    /**
+     * Default data projection.
+     */
+    dataProjection?: import("../proj.js").ProjectionLike;
+    /**
+     * Set the name of the TopoJSON topology
+     * `objects`'s children as feature property with the specified name. This means
+     * that when set to `'layer'`, a topology like
+     * ```
+     * {
+     * "type": "Topology",
+     * "objects": {
+     * "example": {
+     * "type": "GeometryCollection",
+     * "geometries": []
+     * }
+     * }
+     * }
+     * ```
+     * will result in features that have a property `'layer'` set to `'example'`.
+     * When not set, no property will be added to features.
+     */
+    layerName?: string | undefined;
+    /**
+     * Names of the TopoJSON topology's
+     * `objects`'s children to read features from.  If not provided, features will
+     * be read from all children.
+     */
+    layers?: string[] | undefined;
+};
+/**
+ * @typedef {import("topojson-specification").Topology} TopoJSONTopology
+ * @typedef {import("topojson-specification").GeometryCollection} TopoJSONGeometryCollection
+ * @typedef {import("topojson-specification").GeometryObject} TopoJSONGeometry
+ * @typedef {import("topojson-specification").Point} TopoJSONPoint
+ * @typedef {import("topojson-specification").MultiPoint} TopoJSONMultiPoint
+ * @typedef {import("topojson-specification").LineString} TopoJSONLineString
+ * @typedef {import("topojson-specification").MultiLineString} TopoJSONMultiLineString
+ * @typedef {import("topojson-specification").Polygon} TopoJSONPolygon
+ * @typedef {import("topojson-specification").MultiPolygon} TopoJSONMultiPolygon
+ */
+/**
+ * @typedef {Object} Options
+ * @property {import("../proj.js").ProjectionLike} [dataProjection='EPSG:4326'] Default data projection.
+ * @property {string} [layerName] Set the name of the TopoJSON topology
+ * `objects`'s children as feature property with the specified name. This means
+ * that when set to `'layer'`, a topology like
+ * ```
+ * {
+ *   "type": "Topology",
+ *   "objects": {
+ *     "example": {
+ *       "type": "GeometryCollection",
+ *       "geometries": []
+ *     }
+ *   }
+ * }
+ * ```
+ * will result in features that have a property `'layer'` set to `'example'`.
+ * When not set, no property will be added to features.
+ * @property {Array<string>} [layers] Names of the TopoJSON topology's
+ * `objects`'s children to read features from.  If not provided, features will
+ * be read from all children.
+ */
+/**
+ * @classdesc
+ * Feature format for reading data in the TopoJSON format.
+ *
+ * @api
+ */
+declare class TopoJSON extends JSONFeature<typeof Feature> {
+    /**
+     * @param {Options} [options] Options.
+     */
+    constructor(options?: Options | undefined);
+    /**
+     * @private
+     * @type {string|undefined}
+     */
+    private layerName_;
+    /**
+     * @private
+     * @type {?Array<string>}
+     */
+    private layers_;
+    /**
+     * @param {Object} object Object.
+     * @param {import("./Feature.js").ReadOptions} [options] Read options.
+     * @protected
+     * @return {Array<Feature>} Features.
+     */
+    protected readFeaturesFromObject(object: any, options?: import("./Feature.js").ReadOptions | undefined): Array<Feature>;
+}
+import Feature from '../Feature.js';
+import JSONFeature from './JSONFeature.js';
+//# sourceMappingURL=TopoJSON.d.ts.map
