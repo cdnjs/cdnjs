505662e53ff98f58fe9126af9fc04cf2c4775b6c
robocdnjs <cdnjs-github@cloudflare.com>
2023-12-06 01:30:26 +0000

    Add primevue (3.43.0)

---
+++ ajax/libs/primevue/3.43.0/row/row.esm.js
@@ -0,0 +1,11 @@
+import BaseComponent from 'primevue/basecomponent';
+
+var script = {
+  name: 'Row',
+  "extends": BaseComponent,
+  render: function render() {
+    return null;
+  }
+};
+
+export { script as default };
