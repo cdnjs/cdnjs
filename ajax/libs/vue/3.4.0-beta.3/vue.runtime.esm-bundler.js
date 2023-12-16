d02e8cab5d8e090188dc438c01d823dbe7f9de93
robocdnjs <cdnjs-github@cloudflare.com>
2023-12-16 16:08:14 +0000

    Add vue (3.4.0-beta.3)

---
+++ ajax/libs/vue/3.4.0-beta.3/vue.runtime.esm-bundler.js
@@ -0,0 +1,22 @@
+import { initCustomFormatter, warn } from '@vue/runtime-dom';
+export * from '@vue/runtime-dom';
+
+function initDev() {
+  {
+    initCustomFormatter();
+  }
+}
+
+if (!!(process.env.NODE_ENV !== "production")) {
+  initDev();
+}
+const compile = () => {
+  if (!!(process.env.NODE_ENV !== "production")) {
+    warn(
+      `Runtime compilation is not supported in this build of Vue.` + (` Configure your bundler to alias "vue" to "vue/dist/vue.esm-bundler.js".` )
+      /* should not happen */
+    );
+  }
+};
+
+export { compile };
