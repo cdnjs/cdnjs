dde4e31a02e02ea87f7242727653d120e7665aa1
robocdnjs <cdnjs-github@cloudflare.com>
2023-12-21 16:04:37 +0000

    Add fancyapps-ui (5.0.32)

---
+++ ajax/libs/fancyapps-ui/5.0.32/carousel/l10n/cs.umd.js
@@ -0,0 +1,1 @@
+!function(e,o){"object"==typeof exports&&"undefined"!=typeof module?o(exports):"function"==typeof define&&define.amd?define(["exports"],o):o(((e="undefined"!=typeof globalThis?globalThis:e||self).Carousel=e.Carousel||{},e.Carousel.l10n=e.Carousel.l10n||{}))}(this,(function(e){"use strict";e.cs={NEXT:"Další",PREV:"Předchozí",GOTO:"Přejít na %d. snímek"}}));
