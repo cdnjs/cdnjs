41094a795d13b68993abe42552ff1932f47b98ad
robocdnjs <cdnjs-github@cloudflare.com>
2023-12-24 17:04:22 +0000

    Add blockly (11.0.0-beta.0)

---
+++ ajax/libs/blockly/11.0.0-beta.0/msg/constants.d.ts
@@ -0,0 +1,14 @@
+/**
+ * @license
+ * Copyright 2020 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * Type definitions for the Blockly constants locale.
+ */
+
+/// <reference path="msg.d.ts" />
+
+import BlocklyMsg = Blockly.Msg;
+export = BlocklyMsg;
