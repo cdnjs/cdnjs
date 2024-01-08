fd37a98d925bf3c4184fe7a3652dd4cbc0e9d414
robocdnjs <cdnjs-github@cloudflare.com>
2024-01-08 16:08:40 +0000

    Add primevue (3.46.0)

---
+++ ajax/libs/primevue/3.46.0/baseicon/baseicon.esm.js
@@ -0,0 +1,41 @@
+import BaseComponent from 'primevue/basecomponent';
+import BaseIconStyle from 'primevue/baseicon/style';
+import { ObjectUtils } from 'primevue/utils';
+
+function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
+function ownKeys(e, r) { var t = Object.keys(e); if (Object.getOwnPropertySymbols) { var o = Object.getOwnPropertySymbols(e); r && (o = o.filter(function (r) { return Object.getOwnPropertyDescriptor(e, r).enumerable; })), t.push.apply(t, o); } return t; }
+function _objectSpread(e) { for (var r = 1; r < arguments.length; r++) { var t = null != arguments[r] ? arguments[r] : {}; r % 2 ? ownKeys(Object(t), !0).forEach(function (r) { _defineProperty(e, r, t[r]); }) : Object.getOwnPropertyDescriptors ? Object.defineProperties(e, Object.getOwnPropertyDescriptors(t)) : ownKeys(Object(t)).forEach(function (r) { Object.defineProperty(e, r, Object.getOwnPropertyDescriptor(t, r)); }); } return e; }
+function _defineProperty(obj, key, value) { key = _toPropertyKey(key); if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+function _toPropertyKey(t) { var i = _toPrimitive(t, "string"); return "symbol" == _typeof(i) ? i : String(i); }
+function _toPrimitive(t, r) { if ("object" != _typeof(t) || !t) return t; var e = t[Symbol.toPrimitive]; if (void 0 !== e) { var i = e.call(t, r || "default"); if ("object" != _typeof(i)) return i; throw new TypeError("@@toPrimitive must return a primitive value."); } return ("string" === r ? String : Number)(t); }
+var script = {
+  name: 'BaseIcon',
+  "extends": BaseComponent,
+  props: {
+    label: {
+      type: String,
+      "default": undefined
+    },
+    spin: {
+      type: Boolean,
+      "default": false
+    }
+  },
+  style: BaseIconStyle,
+  methods: {
+    pti: function pti() {
+      var isLabelEmpty = ObjectUtils.isEmpty(this.label);
+      return _objectSpread(_objectSpread({}, !this.isUnstyled && {
+        "class": ['p-icon', {
+          'p-icon-spin': this.spin
+        }]
+      }), {}, {
+        role: !isLabelEmpty ? 'img' : undefined,
+        'aria-label': !isLabelEmpty ? this.label : undefined,
+        'aria-hidden': isLabelEmpty
+      });
+    }
+  }
+};
+
+export { script as default };
---
+++ ajax/libs/primevue/3.46.0/baseicon/baseicon.js
@@ -0,0 +1,48 @@
+this.primevue = this.primevue || {};
+this.primevue.baseicon = (function (BaseComponent, BaseIconStyle, utils) {
+    'use strict';
+
+    function _interopDefaultLegacy (e) { return e && typeof e === 'object' && 'default' in e ? e : { 'default': e }; }
+
+    var BaseComponent__default = /*#__PURE__*/_interopDefaultLegacy(BaseComponent);
+    var BaseIconStyle__default = /*#__PURE__*/_interopDefaultLegacy(BaseIconStyle);
+
+    function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
+    function ownKeys(e, r) { var t = Object.keys(e); if (Object.getOwnPropertySymbols) { var o = Object.getOwnPropertySymbols(e); r && (o = o.filter(function (r) { return Object.getOwnPropertyDescriptor(e, r).enumerable; })), t.push.apply(t, o); } return t; }
+    function _objectSpread(e) { for (var r = 1; r < arguments.length; r++) { var t = null != arguments[r] ? arguments[r] : {}; r % 2 ? ownKeys(Object(t), !0).forEach(function (r) { _defineProperty(e, r, t[r]); }) : Object.getOwnPropertyDescriptors ? Object.defineProperties(e, Object.getOwnPropertyDescriptors(t)) : ownKeys(Object(t)).forEach(function (r) { Object.defineProperty(e, r, Object.getOwnPropertyDescriptor(t, r)); }); } return e; }
+    function _defineProperty(obj, key, value) { key = _toPropertyKey(key); if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+    function _toPropertyKey(t) { var i = _toPrimitive(t, "string"); return "symbol" == _typeof(i) ? i : String(i); }
+    function _toPrimitive(t, r) { if ("object" != _typeof(t) || !t) return t; var e = t[Symbol.toPrimitive]; if (void 0 !== e) { var i = e.call(t, r || "default"); if ("object" != _typeof(i)) return i; throw new TypeError("@@toPrimitive must return a primitive value."); } return ("string" === r ? String : Number)(t); }
+    var script = {
+      name: 'BaseIcon',
+      "extends": BaseComponent__default["default"],
+      props: {
+        label: {
+          type: String,
+          "default": undefined
+        },
+        spin: {
+          type: Boolean,
+          "default": false
+        }
+      },
+      style: BaseIconStyle__default["default"],
+      methods: {
+        pti: function pti() {
+          var isLabelEmpty = utils.ObjectUtils.isEmpty(this.label);
+          return _objectSpread(_objectSpread({}, !this.isUnstyled && {
+            "class": ['p-icon', {
+              'p-icon-spin': this.spin
+            }]
+          }), {}, {
+            role: !isLabelEmpty ? 'img' : undefined,
+            'aria-label': !isLabelEmpty ? this.label : undefined,
+            'aria-hidden': isLabelEmpty
+          });
+        }
+      }
+    };
+
+    return script;
+
+})(primevue.basecomponent, primevue.baseicon.style, primevue.utils);
