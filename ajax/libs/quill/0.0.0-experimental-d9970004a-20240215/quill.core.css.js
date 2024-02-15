a56250184eb47a76a26999480143ed7236373a7c
robocdnjs <cdnjs-github@cloudflare.com>
2024-02-15 08:06:57 +0000

    Add quill (0.0.0-experimental-d9970004a-20240215)

---
+++ ajax/libs/quill/0.0.0-experimental-d9970004a-20240215/quill.core.css.js
@@ -0,0 +1,3 @@
+/*! For license information please see quill.core.css.js.LICENSE.txt */
+!function(e,t){"object"==typeof exports&&"object"==typeof module?module.exports=t():"function"==typeof define&&define.amd?define([],t):"object"==typeof exports?exports.Quill=t():e.Quill=t()}(self,(function(){return function(){"use strict";var e={};return e.default}()}));
+//# sourceMappingURL=quill.core.css.js.map
