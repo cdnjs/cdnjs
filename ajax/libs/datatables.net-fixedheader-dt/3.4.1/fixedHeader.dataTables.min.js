912fa725d414e057853dd198dca88bf09f7f5dab
robocdnjs <cdnjs-github@cloudflare.com>
2024-02-16 23:14:17 +0000

    Add datatables.net-fixedheader-dt (3.4.1)

---
+++ ajax/libs/datatables.net-fixedheader-dt/3.4.1/fixedHeader.dataTables.min.js
@@ -0,0 +1,4 @@
+/*! DataTables styling wrapper for FixedHeader
+ * © SpryMedia Ltd - datatables.net/license
+ */
+!function(n){var d,a;"function"==typeof define&&define.amd?define(["jquery","datatables.net-dt","datatables.net-fixedheader"],function(e){return n(e,window,document)}):"object"==typeof exports?(d=require("jquery"),a=function(e,t){t.fn.dataTable||require("datatables.net-dt")(e,t),t.fn.dataTable.FixedHeader||require("datatables.net-fixedheader")(e,t)},"undefined"==typeof window?module.exports=function(e,t){return e=e||window,t=t||d(e),a(e,t),n(t,0,e.document)}:(a(window,d),module.exports=n(d,window,window.document))):n(jQuery,window,document)}(function(e,t,n,d){"use strict";return e.fn.dataTable});
