ecdaea7c839d62c0beb046ef405a5996862e36a5
robocdnjs <cdnjs-github@cloudflare.com>
2024-02-16 00:05:34 +0000

    Add blockly (10.4.0)

---
+++ ajax/libs/blockly/10.4.0/lua.js
@@ -0,0 +1,22 @@
+/* eslint-disable */
+;(function(root, factory) {
+  if (typeof define === 'function' && define.amd) { // AMD
+    define(['./core', './lua_compressed.js'], factory);
+  } else if (typeof exports === 'object') { // Node.js
+    module.exports = factory(require('./core'), require('./lua_compressed.js'));
+  } else { // Browser
+    root.BlocklyLua = factory(root.Blockly, root.BlocklyLua);
+  }
+}(this, function(Blockly, BlocklyLua) {
+/**
+ * @license
+ * Copyright 2020 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @fileoverview Lua generator module; just a wrapper for lua_compressed.js.
+ */
+
+return BlocklyLua;
+}));
