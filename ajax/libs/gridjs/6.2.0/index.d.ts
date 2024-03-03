4d01aa2a4ba024373a36ee151739b2488e14140c
robocdnjs <cdnjs-github@cloudflare.com>
2024-03-03 20:04:27 +0000

    Add gridjs (6.2.0)

---
+++ ajax/libs/gridjs/6.2.0/index.d.ts
@@ -0,0 +1,15 @@
+import Grid from './src/grid';
+import { html } from './src/util/html';
+import { h, createElement, Component, createRef } from 'preact';
+import { useEffect, useRef, useState } from 'preact/hooks';
+import { Config } from './src/config';
+import { PluginPosition } from './src/plugin';
+import { ID } from './src/util/id';
+import { className } from './src/util/className';
+import Row from './src/row';
+import Cell from './src/cell';
+import { useConfig } from './src/hooks/useConfig';
+import { useStore } from './src/hooks/useStore';
+import useSelector from './src/hooks/useSelector';
+import { useTranslator } from './src/i18n/language';
+export { Grid, ID, Row, Cell, className, html, Config, PluginPosition, h, createElement, Component, createRef, useEffect, useRef, useStore, useConfig, useState, useSelector, useTranslator, };
