f5a34ddaee4093432b820f78309242dbfbdc4439
robocdnjs <cdnjs-github@cloudflare.com>
2024-03-09 10:04:46 +0000

    Add xsound (3.8.2)

---
+++ ajax/libs/xsound/3.8.2/types/SoundModule/Effectors/Chorus.d.ts
@@ -0,0 +1,52 @@
+import { Effector } from './Effector';
+export type ChorusParams = {
+    state?: boolean;
+    time?: number;
+    depth?: number;
+    rate?: number;
+    mix?: number;
+    tone?: number;
+    feedback?: number;
+};
+/**
+ * Effector's subclass for Chorus.
+ * @constructor
+ * @extends {Effector}
+ */
+export declare class Chorus extends Effector {
+    private delay;
+    private tone;
+    private mix;
+    private feedback;
+    private depthRate;
+    /**
+     * @param {AudioContext} context This argument is in order to use Web Audio API.
+     */
+    constructor(context: AudioContext);
+    /** @override */
+    stop(stopTime?: number, releaseTime?: number): void;
+    /** @override */
+    connect(): GainNode;
+    /**
+     * This method gets or sets parameters for chorus effector.
+     * This method is overloaded for type interface and type check.
+     * @param {keyof ChorusParams|ChorusParams} params This argument is string if getter. Otherwise, setter.
+     * @return {ChorusParams[keyof ChorusParams]|Chorus} Return value is parameter for chorus effector if getter.
+     *     Otherwise, return value is for method chain.
+     */
+    param(params: 'state'): boolean;
+    param(params: 'time'): number;
+    param(params: 'depth'): number;
+    param(params: 'rate'): number;
+    param(params: 'mix'): number;
+    param(params: 'tone'): number;
+    param(params: 'feedback'): number;
+    param(params: ChorusParams): Chorus;
+    /** @override */
+    params(): Required<ChorusParams>;
+    /** @override */
+    activate(): Chorus;
+    /** @override */
+    deactivate(): Chorus;
+}
+//# sourceMappingURL=Chorus.d.ts.map
---
+++ ajax/libs/xsound/3.8.2/types/SoundModule/Effectors/Filter.d.ts
@@ -0,0 +1,62 @@
+import { Effector } from './Effector';
+export type FilterParams = {
+    state?: boolean;
+    type?: BiquadFilterType;
+    frequency?: number;
+    Q?: number;
+    gain?: number;
+    range?: number;
+    attack?: number;
+    decay?: number;
+    sustain?: number;
+    release?: number;
+};
+/**
+ * Effector's subclass for Filter.
+ * @constructor
+ * @extends {Effector}
+ */
+export declare class Filter extends Effector {
+    private filter;
+    private maxFrequency;
+    private range;
+    private attack;
+    private decay;
+    private sustain;
+    private release;
+    /**
+     * @param {AudioContext} context This argument is in order to use Web Audio API.
+     */
+    constructor(context: AudioContext);
+    /** @override */
+    start(startTime?: number): void;
+    /** @override */
+    stop(stopTime?: number): void;
+    /** @override */
+    connect(): GainNode;
+    /**
+     * This method gets or sets parameters for filter effector.
+     * This method is overloaded for type interface and type check.
+     * @param {keyof FilterParams|FilterParams} params This argument is string if getter. Otherwise, setter.
+     * @return {FilterParams[keyof FilterParams]|Filter} Return value is parameter for filter effector if getter.
+     *     Otherwise, return value is for method chain.
+     */
+    param(params: 'state'): boolean;
+    param(params: 'type'): BiquadFilterType;
+    param(params: 'frequency'): number;
+    param(params: 'Q'): number;
+    param(params: 'gain'): number;
+    param(params: 'range'): number;
+    param(params: 'attack'): number;
+    param(params: 'decay'): number;
+    param(params: 'sustain'): number;
+    param(params: 'release'): number;
+    param(params: FilterParams): Filter;
+    /** @override */
+    params(): Required<FilterParams>;
+    /** @override */
+    activate(): Filter;
+    /** @override */
+    deactivate(): Filter;
+}
+//# sourceMappingURL=Filter.d.ts.map
