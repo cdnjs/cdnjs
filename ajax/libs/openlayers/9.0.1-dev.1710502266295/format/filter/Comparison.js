8659f14f66d5bcf625e8af5d1b4465b878729c5d
robocdnjs <cdnjs-github@cloudflare.com>
2024-03-15 13:08:30 +0000

    Add openlayers (9.0.1-dev.1710502266295)

---
+++ ajax/libs/openlayers/9.0.1-dev.1710502266295/format/filter/Comparison.js
@@ -0,0 +1,28 @@
+/**
+ * @module ol/format/filter/Comparison
+ */
+import Filter from './Filter.js';
+
+/**
+ * @classdesc
+ * Abstract class; normally only used for creating subclasses and not instantiated in apps.
+ * Base class for WFS GetFeature property comparison filters.
+ *
+ * @abstract
+ */
+class Comparison extends Filter {
+  /**
+   * @param {!string} tagName The XML tag name for this filter.
+   * @param {!string} propertyName Name of the context property to compare.
+   */
+  constructor(tagName, propertyName) {
+    super(tagName);
+
+    /**
+     * @type {!string}
+     */
+    this.propertyName = propertyName;
+  }
+}
+
+export default Comparison;
