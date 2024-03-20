e85075ab709b64bbc91ba6d286659950429227b8
robocdnjs <cdnjs-github@cloudflare.com>
2024-03-20 02:10:56 +0000

    Add shepherd.js (12.0.0-alpha.6)

---
+++ ajax/libs/shepherd.js/12.0.0-alpha.6/utils/floating-ui.js
@@ -0,0 +1,6 @@
+/*! shepherd.js 12.0.0-alpha.6 */
+
+export { d as destroyTooltip, g as getFloatingUIOptions, m as mergeTooltipConfig, s as setupTooltip } from '../floating-ui-Dy-QHw48.js';
+import './general.js';
+import './type-check.js';
+//# sourceMappingURL=floating-ui.js.map
