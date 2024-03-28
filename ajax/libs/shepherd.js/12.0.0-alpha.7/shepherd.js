2247f2e3de7ebbf134ab7ebea9e2b21da82c672a
robocdnjs <cdnjs-github@cloudflare.com>
2024-03-28 04:04:52 +0000

    Add shepherd.js (12.0.0-alpha.7)

---
+++ ajax/libs/shepherd.js/12.0.0-alpha.7/shepherd.js
@@ -0,0 +1,21 @@
+/*! shepherd.js 12.0.0-alpha.7 */
+
+import { Shepherd, Tour } from './tour.js';
+import { StepNoOp, TourNoOp } from './utils/general.js';
+import { S as Step } from './step-DQZAn_uk.js';
+import './evented.js';
+import './utils/type-check.js';
+import './utils/auto-bind.js';
+import './utils/context.js';
+import './utils/cleanup.js';
+import './utils/datarequest.js';
+import './utils/overlay-path.js';
+import './floating-ui-BI3Ffc2B.js';
+import './utils/bind.js';
+
+const isServerSide = typeof window === 'undefined';
+Shepherd.Step = isServerSide ? StepNoOp : Step;
+Shepherd.Tour = isServerSide ? TourNoOp : Tour;
+
+export { Shepherd as default };
+//# sourceMappingURL=shepherd.js.map
---
+++ ajax/libs/shepherd.js/12.0.0-alpha.7/utils/auto-bind.js
@@ -0,0 +1,23 @@
+/*! shepherd.js 12.0.0-alpha.7 */
+
+/**
+ * Binds all the methods on a JS Class to the `this` context of the class.
+ * Adapted from https://github.com/sindresorhus/auto-bind
+ * @param self The `this` context of the class
+ * @return The `this` context of the class
+ */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function autoBind(self) {
+    const keys = Object.getOwnPropertyNames(self.constructor.prototype);
+    for (let i = 0; i < keys.length; i++) {
+        const key = keys[i];
+        const val = self[key];
+        if (key !== 'constructor' && typeof val === 'function') {
+            self[key] = val.bind(self);
+        }
+    }
+    return self;
+}
+
+export { autoBind as default };
+//# sourceMappingURL=auto-bind.js.map
