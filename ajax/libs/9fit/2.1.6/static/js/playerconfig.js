125b8a67da1f099b428ffb18b78418d29cb73ee6
robocdnjs <cdnjs-github@cloudflare.com>
2024-04-02 00:13:54 +0000

    Add 9fit (2.1.6)

---
+++ ajax/libs/9fit/2.1.6/static/js/playerconfig.js
@@ -0,0 +1,7 @@
+var MacPlayerConfig={};
+//参数开始
+MacPlayerConfig={"width":"100%","height":"100%","widthmob":"100%","heightmob":"100%","widthpop":"0","heightpop":"600","second":"0.1","prestrain":"\/static\/player\/prestrain.html","buffer":"\/static\/player\/loading.html","parse":"","autofull":"1","showtop":"1","showlist":"1","flag":"0","colors":"000000,F6F6F6,F6F6F6,333333,666666,FFFFF,FF0000,2c2c2c,ffffff,a3a3a3,2c2c2c,adadad,adadad,48486c,fcfcfc"};
+//参数结束
+//缓存开始
+MacPlayerConfig.player_list={"ness":{"show":"","des":"","ps":"0","parse":""},"dplayer":{"show":"DPlayer-H5\u64ad\u653e\u5668","des":"dplayer.js.org","ps":"0","parse":""},"videojs":{"show":"videojs-H5\u64ad\u653e\u5668","des":"videojs.com","ps":"0","parse":""},"iva":{"show":"iva-H5\u64ad\u653e\u5668","des":"videojj.com","ps":"0","parse":""},"iframe":{"show":"iframe\u5916\u94fe\u6570\u636e","des":"iframe\u5916\u94fe\u6570\u636e","ps":"0","parse":""},"link":{"show":"\u5916\u94fe\u6570\u636e","des":"\u5916\u90e8\u7f51\u7ad9\u64ad\u653e\u94fe\u63a5","ps":"0","parse":""},"swf":{"show":"Flash\u6587\u4ef6","des":"swf","ps":"0","parse":""},"flv":{"show":"Flv\u6587\u4ef6","des":"flv","ps":"0","parse":""}},MacPlayerConfig.downer_list={"http":{"show":"http\u4e0b\u8f7d","des":"des\u63d0\u793a\u4fe1\u606f","ps":"0","parse":""},"xunlei":{"show":"xunlei\u4e0b\u8f7d","des":"des\u63d0\u793a\u4fe1\u606f","ps":"0","parse":""}},MacPlayerConfig.server_list={"server1":{"show":"\u6d4b\u8bd5\u670d\u52a1\u56681","des":"des\u63d0\u793a\u4fe1\u606f1"}};
+//缓存结束
