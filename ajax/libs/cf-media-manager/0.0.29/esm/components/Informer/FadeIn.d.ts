db1ebaf37960bd59b361c6013cafd10a6eb0551a
robocdnjs <cdnjs-github@cloudflare.com>
2024-04-09 17:09:29 +0000

    Add cf-media-manager (0.0.29)

---
+++ ajax/libs/cf-media-manager/0.0.29/esm/components/Informer/FadeIn.d.ts
@@ -0,0 +1,7 @@
+import { h, Component } from "preact";
+export default class FadeIn extends Component {
+    ref: import("preact").RefObject<any>;
+    componentWillEnter(callback: any): void;
+    componentWillLeave(callback: any): void;
+    render(): h.JSX.Element;
+}
---
+++ ajax/libs/cf-media-manager/0.0.29/esm/locale.d.ts
@@ -0,0 +1,71 @@
+declare const _default: {
+    strings: {
+        list_title: string;
+        add_up_to_more: {
+            0: string;
+            1: string;
+        };
+        remove: string;
+        "exit_confirm.heading": string;
+        "exit_confim.description": string;
+        cancel: string;
+        exit: string;
+        images: string;
+        upload_between_upper_and_lower: string;
+        upload_up_to: {
+            0: string;
+            1: string;
+        };
+        number_selected: {
+            0: string;
+            1: string;
+        };
+        number_errored: {
+            0: string;
+            1: string;
+        };
+        or_click: string;
+        upload_status_indicator_files_ready: {
+            0: string;
+            1: string;
+        };
+        add: string;
+        number_of_images_left: {
+            0: string;
+            1: string;
+        };
+        upload_complete: string;
+        upload_status_indicator_full_error: string;
+        upload_status_indicator_in_progres: string;
+        max_files_reached: {
+            0: string;
+            1: string;
+        };
+        upload_status_indicator_success: {
+            0: string;
+            1: string;
+        };
+        list_panel_description: string;
+        list_panel_title: string;
+        add_files: string;
+        start_panel_title: string;
+        start_panel_title_mobile: string;
+        or_browse_btn: string;
+        or_browse_btn_mobile: string;
+        select_files: string;
+        list_panel_add_files_mobile: {
+            0: string;
+            1: string;
+        };
+        min_files_not_reached: {
+            0: string;
+            1: string;
+        };
+        browse: string;
+        add_more_files: {
+            0: string;
+            1: string;
+        };
+    };
+};
+export default _default;
