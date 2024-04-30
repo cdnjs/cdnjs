1eb95f2fb7d537a1203ab9b395c8f4232e179a9d
robocdnjs <cdnjs-github@cloudflare.com>
2024-04-30 11:04:23 +0000

    Add jqgrid (5.8.6)

---
+++ ajax/libs/jqgrid/5.8.6/js/i18n/grid.locale-fi.js
@@ -0,0 +1,194 @@
+/**
+ * jqGrid (fi) Finnish Translation
+ * Jukka Inkeri  awot.fi  2010-05-19
+ * Alex Grönholm  alex.gronholm@nextday.fi  2011-05-18
+ * http://awot.fi
+ * Dual licensed under the MIT and GPL licenses:
+ * http://www.opensource.org/licenses/mit-license.php
+ * http://www.gnu.org/licenses/gpl.html
+**/
+/*global jQuery, define */
+(function( factory ) {
+	"use strict";
+	if ( typeof define === "function" && define.amd ) {
+		// AMD. Register as an anonymous module.
+		define([
+			"jquery",
+			"../grid.base"
+		], factory );
+	} else {
+		// Browser globals
+		factory( jQuery );
+	}
+}(function( $ ) {
+
+$.jgrid = $.jgrid || {};
+if(!$.jgrid.hasOwnProperty("regional")) {
+	$.jgrid.regional = [];
+}
+$.jgrid.regional["fi"] = {
+	defaults: {
+		recordtext: "Rivit {0} - {1} / {2}",
+	    emptyrecords: "Ei n&auml;ytett&auml;vi&auml;",
+		loadtext: "Haetaan...",
+		savetext: "Saving...",
+		pgtext: "Sivu {0} / {1}",		
+		pgfirst : "First Page",
+		pglast : "Last Page",
+		pgnext : "Next Page",
+		pgprev : "Previous Page",
+		pgrecs : "Records per Page",
+		showhide: "Toggle Expand Collapse Grid",
+		// mobile
+		pagerCaption : "Grid::Page Settings",
+		pageText : "Page:",
+		recordPage : "Records per Page",
+		nomorerecs : "No more records...",
+		scrollPullup: "Pull up to load more...",
+		scrollPulldown : "Pull down to refresh...",
+		scrollRefresh : "Release to refresh...",
+		valT : "checked",
+		valF : "unchecked",
+		selectLine : "Select row",
+		selectAllLines : "Select all rows"
+	},
+	search: {
+	    caption: "Etsi...",
+	    Find: "Etsi",
+	    Reset: "Tyhjenn&auml;",
+	    odata: [{ oper:'eq', text:"on"},{ oper:'ne', text:"ei ole"},{ oper:'lt', text:"pienempi"},{ oper:'le', text:"pienempi tai yht&auml;suuri"},{ oper:'gt', text:"suurempi"},{ oper:'ge', text:"suurempi tai yht&auml;suuri"},{ oper:'bw', text:"alkaa"},{ oper:'bn', text:"ei ala"},{ oper:'in', text:"joukossa"},{ oper:'ni', text:"ei joukossa"},{ oper:'ew', text:"loppuu"},{ oper:'en', text:"ei lopu"},{ oper:'cn', text:"sis&auml;lt&auml;&auml;"},{ oper:'nc', text:"ei sis&auml;ll&auml;"},{ oper:'nu', text:"on tyhj&auml;"},{ oper:'nn', text:"ei ole tyhj&auml;"},{ oper:'nu', text:'is null'},{ oper:'nn', text:'is not null'}, {oper:'bt', text:'between'}],
+	    groupOps: [	{ op: "AND", text: "kaikki" }, { op: "OR", text: "mik&auml; tahansa" }	],
+		operandTitle : "Click to select search operation.",
+		resetTitle : "Reset Search Value",
+		addsubgrup : "Add subgroup",
+		addrule : "Add rule",
+		delgroup : "Delete group",
+		delrule : "Delete rule",
+		Close : "Close",
+		Operand : "Operand : ",
+		Operation : "Oper : ",
+		filterFor : "filter for"
+	},
+	edit: {
+	    addCaption: "Uusi rivi",
+	    editCaption: "Muokkaa rivi&auml;",
+	    bSubmit: "OK",
+	    bCancel: "Peru",
+		bClose: "Sulje",
+		saveData: "Tietoja muutettu! Tallennetaanko?",
+		bYes: "Kyll&auml;",
+		bNo: "Ei",
+		bExit: "Peru",
+	    msg: {
+	        required: "pakollinen",
+	        number: "Anna kelvollinen nro",
+	        minValue: "arvon oltava suurempi tai yht&auml;suuri kuin ",
+	        maxValue: "arvon oltava pienempi tai yht&auml;suuri kuin ",
+	        email: "ei ole kelvollinen s&auml;postiosoite",
+	        integer: "Anna kelvollinen kokonaisluku",
+			date: "Anna kelvollinen pvm",
+			url: "Ei ole kelvollinen linkki(URL). Alku oltava ('http://' tai 'https://')",
+			nodefined: " ei ole m&auml;&auml;ritelty!",
+			novalue: " paluuarvo vaaditaan!",
+			customarray: "Oman funktion tulee palauttaa jono!",
+			customfcheck: "Oma funktio on m&auml;&auml;ritelt&auml;v&auml; r&auml;&auml;t&auml;l&ouml;ity&auml; tarkastusta varten!"
+		}
+	},
+	view: {
+	    caption: "N&auml;yt&auml; rivi",
+	    bClose: "Sulje"
+	},
+	del: {
+	    caption: "Poista",
+	    msg: "Poista valitut rivit?",
+	    bSubmit: "Poista",
+	    bCancel: "Peru"
+	},
+	nav: {
+		edittext: "",
+	    edittitle: "Muokkaa valittua rivi&auml;",
+		addtext: "",
+	    addtitle: "Uusi rivi",
+	    deltext: "",
+	    deltitle: "Poista valittu rivi",
+	    searchtext: "",
+	    searchtitle: "Etsi tietoja",
+	    refreshtext: "",
+	    refreshtitle: "Lataa uudelleen",
+	    alertcap: "Varoitus",
+	    alerttext: "Valitse rivi",
+		viewtext: "",
+		viewtitle: "N&auml;yta valitut rivit",
+		savetext: "",
+		savetitle: "Save row",
+		canceltext: "",
+		canceltitle : "Cancel row editing",
+		selectcaption : "Actions..."
+	},
+	col: {
+	    caption: "Valitse sarakkeet",
+	    bSubmit: "OK",
+	    bCancel: "Peru"	
+	},
+	errors : {
+		errcap: "Virhe",
+		nourl: "URL on asettamatta",
+		norecords: "Ei muokattavia tietoja",
+	    model: "Pituus colNames <> colModel!"
+	},
+	formatter: {
+		integer: {thousandsSeparator: "", defaultValue: '0'},
+		number: {decimalSeparator:",", thousandsSeparator: "", decimalPlaces: 2, defaultValue: '0,00'},
+		currency: {decimalSeparator:",", thousandsSeparator: "", decimalPlaces: 2, prefix: "", suffix:"", defaultValue: '0,00'},
+		date: {
+			dayNames:   [
+				"Su", "Ma", "Ti", "Ke", "To", "Pe", "La",
+				"Sunnuntai", "Maanantai", "Tiistai", "Keskiviikko", "Torstai", "Perjantai", "Lauantai"
+			],
+			monthNames: [
+				"Tam", "Hel", "Maa", "Huh", "Tou", "Kes", "Hei", "Elo", "Syy", "Lok", "Mar", "Jou",
+				"Tammikuu", "Helmikuu", "Maaliskuu", "Huhtikuu", "Toukokuu", "Kes&auml;kuu", "Hein&auml;kuu", "Elokuu", "Syyskuu", "Lokakuu", "Marraskuu", "Joulukuu"
+			],
+			AmPm: ["am","pm","AM","PM"],
+			S: function (j) {return j < 11 || j > 13 ? ['st', 'nd', 'rd', 'th'][Math.min((j - 1) % 10, 3)] : 'th'},
+			srcformat: 'Y-m-d',
+			newformat: 'd.m.Y',
+			parseRe : /[#%\\\/:_;.,\t\s-]/,
+			masks: {
+	            ISO8601Long:"Y-m-d H:i:s",
+	            ISO8601Short:"Y-m-d",
+	            ShortDate: "d.m.Y",
+	            LongDate: "l, F d, Y",
+	            FullDateTime: "l, F d, Y g:i:s A",
+	            MonthDay: "F d",
+	            ShortTime: "g:i A",
+	            LongTime: "g:i:s A",
+	            SortableDateTime: "Y-m-d\\TH:i:s",
+	            UniversalSortableDateTime: "Y-m-d H:i:sO",
+	            YearMonth: "F, Y"
+	        },
+	        reformatAfterEdit : false,
+			userLocalTime : false
+		},
+		baseLinkUrl: '',
+		showAction: '',
+	    target: '',
+	    checkbox: {disabled:true},
+		idName: 'id'
+	},
+	colmenu : {
+		sortasc : "Sort Ascending",
+		sortdesc : "Sort Descending",
+		columns : "Columns",
+		filter : "Filter",
+		grouping : "Group By",
+		ungrouping : "Ungroup",
+		searchTitle : "Get items with value that:",
+		freeze : "Freeze",
+		unfreeze : "Unfreeze",
+		reorder : "Move to reorder",
+		hovermenu: "Click for column quick actions"
+	}
+};
+// FI
+}));
---
+++ ajax/libs/jqgrid/5.8.6/js/i18n/grid.locale-hu.js
@@ -0,0 +1,194 @@
+/**
+ * jqGrid Hungarian Translation
+ * Őrszigety Ádám udx6bs@freemail.hu
+ * http://trirand.com/blog/ 
+ * Dual licensed under the MIT and GPL licenses:
+ * http://www.opensource.org/licenses/mit-license.php
+ * http://www.gnu.org/licenses/gpl.html
+**/
+
+/*global jQuery, define */
+(function( factory ) {
+	"use strict";
+	if ( typeof define === "function" && define.amd ) {
+		// AMD. Register as an anonymous module.
+		define([
+			"jquery",
+			"../grid.base"
+		], factory );
+	} else {
+		// Browser globals
+		factory( jQuery );
+	}
+}(function( $ ) {
+
+$.jgrid = $.jgrid || {};
+if(!$.jgrid.hasOwnProperty("regional")) {
+	$.jgrid.regional = [];
+}
+$.jgrid.regional["hu"] = {
+	defaults : {
+		recordtext: "Oldal {0} - {1} / {2}",
+		emptyrecords: "Nincs találat",
+		loadtext: "Betöltés...",
+		pgtext : "Oldal {0} / {1}",
+		savetext: "Mentés...",
+		pgfirst : "Első oldal",
+		pglast : "Utolsó oldal",
+		pgnext : "Következő oldal",
+		pgprev : "Előző oldal",
+		pgrecs : "Rekordok oldalanként",
+		showhide: "Toggle Expand Collapse Grid",
+		// mobile
+		pagerCaption : "Grid::Page Settings",
+		pageText : "Oldal:",
+		recordPage : "Rekordok oldalanként",
+		nomorerecs : "Nincs több rekord...",
+		scrollPullup: "Pull up to load more...",
+		scrollPulldown : "Pull down to refresh...",
+		scrollRefresh : "Release to refresh...",
+		valT : "checked",
+		valF : "unchecked",
+		selectLine : "Select row",
+		selectAllLines : "Select all rows"
+	},
+	search : {
+		caption: "Keresés...",
+		Find: "Keres",
+		Reset: "Alapértelmezett",
+		odata: [{ oper:'eq', text:"egyenlő"},{ oper:'ne', text:"nem egyenlő"},{ oper:'lt', text:"kevesebb"},{ oper:'le', text:"kevesebb vagy egyenlő"},{ oper:'gt', text:"nagyobb"},{ oper:'ge', text:"nagyobb vagy egyenlő"},{ oper:'bw', text:"ezzel kezdődik"},{ oper:'bn', text:"nem ezzel kezdődik"},{ oper:'in', text:"tartalmaz"},{ oper:'ni', text:"nem tartalmaz"},{ oper:'ew', text:"végződik"},{ oper:'en', text:"nem végződik"},{ oper:'cn', text:"tartalmaz"},{ oper:'nc', text:"nem tartalmaz"},{ oper:'nu', text:'is null'},{ oper:'nn', text:'is not null'}, {oper:'bt', text:'között'}],
+		groupOps: [	{ op: "AND", text: "all" },	{ op: "OR",  text: "any" }	],
+		operandTitle : "Click to select search operation.",
+		resetTitle : "Reset Search Value",
+		addsubgrup : "Add subgroup",
+		addrule : "Add rule",
+		delgroup : "Delete group",
+		delrule : "Delete rule",
+		Close : "Close",
+		Operand : "Operand : ",
+		Operation : "Oper : ",
+		filterFor : "filter for"
+	},
+	edit : {
+		addCaption: "Új tétel",
+		editCaption: "Tétel szerkesztése",
+		bSubmit: "Mentés",
+		bCancel: "Mégse",
+		bClose: "Bezárás",
+		saveData: "A tétel megváltozott! Tétel mentése?",
+		bYes : "Igen",
+		bNo : "Nem",
+		bExit : "Mégse",
+		msg: {
+			required:"Kötelező mező",
+			number:"Kérjük, adjon meg egy helyes számot",
+			minValue:"Nagyobb vagy egyenlőnek kell lenni mint ",
+			maxValue:"Kisebb vagy egyenlőnek kell lennie mint",
+			email: "Hibás email cím",
+			integer: "Kérjük adjon meg egy helyes egész számot",
+			date: "Kérjük adjon meg egy helyes dátumot",
+			url: "nem helyes cím. Előtag kötelező ('http://' vagy 'https://')",
+			nodefined : " nem definiált!",
+			novalue : " visszatérési érték kötelező!!",
+			customarray : "Custom function should return array!",
+			customfcheck : "Custom function should be present in case of custom checking!"
+			
+		}
+	},
+	view : {
+		caption: "Tétel megtekintése",
+		bClose: "Bezárás"
+	},
+	del : {
+		caption: "Törlés",
+		msg: "Kiválaztott tétel(ek) törlése?",
+		bSubmit: "Törlés",
+		bCancel: "Mégse"
+	},
+	nav : {
+		edittext: "",
+		edittitle: "Tétel szerkesztése",
+		addtext:"",
+		addtitle: "Új tétel hozzáadása",
+		deltext: "",
+		deltitle: "Tétel törlése",
+		searchtext: "",
+		searchtitle: "Keresés",
+		refreshtext: "",
+		refreshtitle: "Frissítés",
+		alertcap: "Figyelmeztetés",
+		alerttext: "Kérem válasszon tételt.",
+		viewtext: "",
+		viewtitle: "Tétel megtekintése",
+		savetext: "",
+		savetitle: "Save row",
+		canceltext: "",
+		canceltitle : "Cancel row editing",
+		selectcaption : "Actions..."
+	},
+	col : {
+		caption: "Oszlopok kiválasztása",
+		bSubmit: "Ok",
+		bCancel: "Mégse"
+	},
+	errors : {
+		errcap : "Hiba",
+		nourl : "Nincs URL beállítva",
+		norecords: "Nincs feldolgozásra váró tétel",
+		model : "colNames és colModel hossza nem egyenlő!"
+	},
+	formatter : {
+		integer : {thousandsSeparator: " ", defaultValue: '0'},
+		number : {decimalSeparator:",", thousandsSeparator: " ", decimalPlaces: 2, defaultValue: '0,00'},
+		currency : {decimalSeparator:",", thousandsSeparator: " ", decimalPlaces: 2, prefix: "", suffix:"", defaultValue: '0,00'},
+		date : {
+			dayNames:   [
+				"Va", "Hé", "Ke", "Sze", "Csü", "Pé", "Szo",
+				"Vasárnap", "Hétfő", "Kedd", "Szerda", "Csütörtök", "Péntek", "Szombat"
+			],
+			monthNames: [
+				"Jan", "Feb", "Már", "Ápr", "Máj", "Jún", "Júl", "Aug", "Szep", "Okt", "Nov", "Dec",
+				"Január", "Február", "Március", "Áprili", "Május", "Június", "Július", "Augusztus", "Szeptember", "Október", "November", "December"
+			],
+			AmPm : ["de","du","DE","DU"],
+			S: function (j) {return '.-ik';},
+			srcformat: 'Y-m-d',
+			newformat: 'Y/m/d',
+			parseRe : /[#%\\\/:_;.,\t\s-]/,
+			masks : {
+				ISO8601Long:"Y-m-d H:i:s",
+				ISO8601Short:"Y-m-d",
+				ShortDate: "Y/j/n",
+				LongDate: "Y. F hó d., l",
+				FullDateTime: "l, F d, Y g:i:s A",
+				MonthDay: "F d",
+				ShortTime: "a g:i",
+				LongTime: "a g:i:s",
+				SortableDateTime: "Y-m-d\\TH:i:s",
+				UniversalSortableDateTime: "Y-m-d H:i:sO",
+				YearMonth: "Y, F"
+			},
+			reformatAfterEdit : false,
+			userLocalTime : false
+		},
+		baseLinkUrl: '',
+		showAction: '',
+		target: '',
+		checkbox : {disabled:true},
+		idName : 'id'
+	},
+	colmenu : {
+		sortasc : "Sort Ascending",
+		sortdesc : "Sort Descending",
+		columns : "Columns",
+		filter : "Filter",
+		grouping : "Group By",
+		ungrouping : "Ungroup",
+		searchTitle : "Get items with value that:",
+		freeze : "Freeze",
+		unfreeze : "Unfreeze",
+		reorder : "Move to reorder",
+		hovermenu: "Click for column quick actions"
+	}
+};
+}));
