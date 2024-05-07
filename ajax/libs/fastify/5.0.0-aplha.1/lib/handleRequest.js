9e7d82e44eadf22629069c30923e59b87403a8d0
robocdnjs <cdnjs-github@cloudflare.com>
2024-05-07 16:04:53 +0000

    Add fastify (5.0.0-aplha.1)

---
+++ ajax/libs/fastify/5.0.0-aplha.1/lib/handleRequest.js
@@ -0,0 +1,148 @@
+'use strict'
+
+const { bodylessMethods, bodyMethods } = require('./httpMethods')
+const { validate: validateSchema } = require('./validation')
+const { preValidationHookRunner, preHandlerHookRunner } = require('./hooks')
+const wrapThenable = require('./wrapThenable')
+const {
+  kReplyIsError,
+  kRouteContext
+} = require('./symbols')
+
+function handleRequest (err, request, reply) {
+  if (reply.sent === true) return
+  if (err != null) {
+    reply[kReplyIsError] = true
+    reply.send(err)
+    return
+  }
+
+  const method = request.raw.method
+  const headers = request.headers
+  const context = request[kRouteContext]
+
+  if (bodylessMethods.has(method)) {
+    handler(request, reply)
+    return
+  }
+
+  if (bodyMethods.has(method)) {
+    const contentType = headers['content-type']
+    const contentLength = headers['content-length']
+    const transferEncoding = headers['transfer-encoding']
+
+    if (contentType === undefined) {
+      if (
+        (contentLength === undefined || contentLength === '0') &&
+        transferEncoding === undefined
+      ) {
+        // Request has no body to parse
+        handler(request, reply)
+      } else {
+        context.contentTypeParser.run('', handler, request, reply)
+      }
+    } else {
+      if (contentLength === undefined && transferEncoding === undefined && method === 'OPTIONS') {
+        // OPTIONS can have a Content-Type header without a body
+        handler(request, reply)
+        return
+      }
+      context.contentTypeParser.run(contentType, handler, request, reply)
+    }
+    return
+  }
+
+  // Return 404 instead of 405 see https://github.com/fastify/fastify/pull/862 for discussion
+  handler(request, reply)
+}
+
+function handler (request, reply) {
+  try {
+    if (request[kRouteContext].preValidation !== null) {
+      preValidationHookRunner(
+        request[kRouteContext].preValidation,
+        request,
+        reply,
+        preValidationCallback
+      )
+    } else {
+      preValidationCallback(null, request, reply)
+    }
+  } catch (err) {
+    preValidationCallback(err, request, reply)
+  }
+}
+
+function preValidationCallback (err, request, reply) {
+  if (reply.sent === true) return
+
+  if (err != null) {
+    reply[kReplyIsError] = true
+    reply.send(err)
+    return
+  }
+
+  const validationErr = validateSchema(reply[kRouteContext], request)
+  const isAsync = (validationErr && typeof validationErr.then === 'function') || false
+
+  if (isAsync) {
+    const cb = validationCompleted.bind(null, request, reply)
+    validationErr.then(cb, cb)
+  } else {
+    validationCompleted(request, reply, validationErr)
+  }
+}
+
+function validationCompleted (request, reply, validationErr) {
+  if (validationErr) {
+    if (reply[kRouteContext].attachValidation === false) {
+      reply.send(validationErr)
+      return
+    }
+
+    reply.request.validationError = validationErr
+  }
+
+  // preHandler hook
+  if (request[kRouteContext].preHandler !== null) {
+    preHandlerHookRunner(
+      request[kRouteContext].preHandler,
+      request,
+      reply,
+      preHandlerCallback
+    )
+  } else {
+    preHandlerCallback(null, request, reply)
+  }
+}
+
+function preHandlerCallback (err, request, reply) {
+  if (reply.sent) return
+
+  if (err != null) {
+    reply[kReplyIsError] = true
+    reply.send(err)
+    return
+  }
+
+  let result
+
+  try {
+    result = request[kRouteContext].handler(request, reply)
+  } catch (err) {
+    reply[kReplyIsError] = true
+    reply.send(err)
+    return
+  }
+
+  if (result !== undefined) {
+    if (result !== null && typeof result.then === 'function') {
+      wrapThenable(result, reply)
+    } else {
+      reply.send(result)
+    }
+  }
+}
+
+module.exports = handleRequest
+module.exports[Symbol.for('internals')] = { handler, preHandlerCallback }
---
+++ ajax/libs/fastify/5.0.0-aplha.1/lib/warnings.js
@@ -0,0 +1,125 @@
+'use strict'
+
+const { createDeprecation, createWarning } = require('process-warning')
+
+/**
+ * Deprecation codes:
+ *   - FSTDEP005
+ *   - FSTDEP006
+ *   - FSTDEP007
+ *   - FSTDEP008
+ *   - FSTDEP009
+ *   - FSTDEP010
+ *   - FSTDEP012
+ *   - FSTDEP013
+ *   - FSTDEP015
+ *   - FSTDEP016
+ *   - FSTDEP017
+ *   - FSTDEP018
+ *   - FSTDEP019
+ *   - FSTWRN001
+ *   - FSTSEC001
+ */
+
+const FSTDEP005 = createDeprecation({
+  code: 'FSTDEP005',
+  message: 'You are accessing the deprecated "request.connection" property. Use "request.socket" instead.'
+})
+
+const FSTDEP006 = createDeprecation({
+  code: 'FSTDEP006',
+  message: 'You are decorating Request/Reply with a reference type. This reference is shared amongst all requests. Use onRequest hook instead. Property: %s'
+})
+
+const FSTDEP007 = createDeprecation({
+  code: 'FSTDEP007',
+  message: 'You are trying to set a HEAD route using "exposeHeadRoute" route flag when a sibling route is already set. See documentation for more info.'
+})
+
+const FSTDEP008 = createDeprecation({
+  code: 'FSTDEP008',
+  message: 'You are using route constraints via the route { version: "..." } option, use { constraints: { version: "..." } } option instead.'
+})
+
+const FSTDEP009 = createDeprecation({
+  code: 'FSTDEP009',
+  message: 'You are using a custom route versioning strategy via the server { versioning: "..." } option, use { constraints: { version: "..." } } option instead.'
+})
+
+const FSTDEP010 = createDeprecation({
+  code: 'FSTDEP010',
+  message: 'Modifying the "reply.sent" property is deprecated. Use the "reply.hijack()" method instead.'
+})
+
+const FSTDEP012 = createDeprecation({
+  code: 'FSTDEP012',
+  message: 'request.context property access is deprecated. Please use "request.routeOptions.config" or "request.routeOptions.schema" instead for accessing Route settings. The "request.context" will be removed in `fastify@5`.'
+})
+
+const FSTDEP013 = createDeprecation({
+  code: 'FSTDEP013',
+  message: 'Direct return of "trailers" function is deprecated. Please use "callback" or "async-await" for return value. The support of direct return will removed in `fastify@5`.'
+})
+
+const FSTDEP015 = createDeprecation({
+  code: 'FSTDEP015',
+  message: 'You are accessing the deprecated "request.routeSchema" property. Use "request.routeOptions.schema" instead. Property "req.routeSchema" will be removed in `fastify@5`.'
+})
+
+const FSTDEP016 = createDeprecation({
+  code: 'FSTDEP016',
+  message: 'You are accessing the deprecated "request.routeConfig" property. Use "request.routeOptions.config" instead. Property "req.routeConfig" will be removed in `fastify@5`.'
+})
+
+const FSTDEP017 = createDeprecation({
+  code: 'FSTDEP017',
+  message: 'You are accessing the deprecated "request.routerPath" property. Use "request.routeOptions.url" instead. Property "req.routerPath" will be removed in `fastify@5`.'
+})
+
+const FSTDEP018 = createDeprecation({
+  code: 'FSTDEP018',
+  message: 'You are accessing the deprecated "request.routerMethod" property. Use "request.routeOptions.method" instead. Property "req.routerMethod" will be removed in `fastify@5`.'
+})
+
+const FSTDEP019 = createDeprecation({
+  code: 'FSTDEP019',
+  message: 'reply.context property access is deprecated. Please use "request.routeOptions.config" or "request.routeOptions.schema" instead for accessing Route settings. The "reply.context" will be removed in `fastify@5`.'
+})
+
+const FSTDEP020 = createDeprecation({
+  code: 'FSTDEP020',
+  message: 'You are using the deprecated "reply.getResponseTime()" method. Use the "reply.elapsedTime" property instead. Method "reply.getResponseTime()" will be removed in `fastify@5`.'
+})
+
+const FSTWRN001 = createWarning({
+  name: 'FastifyWarning',
+  code: 'FSTWRN001',
+  message: 'The %s schema for %s: %s is missing. This may indicate the schema is not well specified.',
+  unlimited: true
+})
+
+const FSTSEC001 = createWarning({
+  name: 'FastifySecurity',
+  code: 'FSTSEC001',
+  message: 'You are using /%s/ Content-Type which may be vulnerable to CORS attack. Please make sure your RegExp start with "^" or include ";?" to proper detection of the essence MIME type.',
+  unlimited: true
+})
+
+module.exports = {
+  FSTDEP005,
+  FSTDEP006,
+  FSTDEP007,
+  FSTDEP008,
+  FSTDEP009,
+  FSTDEP010,
+  FSTDEP012,
+  FSTDEP013,
+  FSTDEP015,
+  FSTDEP016,
+  FSTDEP017,
+  FSTDEP018,
+  FSTDEP019,
+  FSTDEP020,
+  FSTWRN001,
+  FSTSEC001
+}
