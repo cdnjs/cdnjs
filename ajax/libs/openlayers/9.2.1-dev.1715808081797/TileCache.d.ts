7a899464046208d49b0c906d35574899b82e731d
robocdnjs <cdnjs-github@cloudflare.com>
2024-06-18 15:48:07 +0000

    Add openlayers (9.2.1-dev.1715808081797)

---
+++ ajax/libs/openlayers/9.2.1-dev.1715808081797/TileCache.d.ts
@@ -0,0 +1,16 @@
+export default TileCache;
+declare class TileCache extends LRUCache<any> {
+    constructor(highWaterMark?: number | undefined);
+    /**
+     * @param {!Object<string, boolean>} usedTiles Used tiles.
+     */
+    expireCache(usedTiles: {
+        [x: string]: boolean;
+    }): void;
+    /**
+     * Prune all tiles from the cache that don't have the same z as the newest tile.
+     */
+    pruneExceptNewestZ(): void;
+}
+import LRUCache from './structs/LRUCache.js';
+//# sourceMappingURL=TileCache.d.ts.map
