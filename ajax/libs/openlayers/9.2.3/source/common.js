321679f7ba52ae39fa00e49936ecc02d8f3910d6
robocdnjs <cdnjs-github@cloudflare.com>
2024-06-21 15:35:09 +0000

    Add openlayers (9.2.3)

---
+++ ajax/libs/openlayers/9.2.3/source/common.js
@@ -0,0 +1,15 @@
+/**
+ * @module ol/source/common
+ */
+
+/**
+ * Default WMS version.
+ * @type {string}
+ */
+export const DEFAULT_WMS_VERSION = '1.3.0';
+
+/**
+ * Number of decimal digits to consider in integer values when rounding.
+ * @type {number}
+ */
+export const DECIMALS = 4;
