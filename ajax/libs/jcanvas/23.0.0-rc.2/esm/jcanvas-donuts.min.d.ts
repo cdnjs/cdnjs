6b95976498f1c93221333c4f9994cf7c6adbdf4b
robocdnjs <cdnjs-github@cloudflare.com>
2024-07-09 19:59:02 +0000

    Add jcanvas (23.0.0-rc.2)

---
+++ ajax/libs/jcanvas/23.0.0-rc.2/esm/jcanvas-donuts.min.d.ts
@@ -0,0 +1,9 @@
+/// <reference path="jcanvas.d.ts" />
+
+interface JCanvasDefaults {
+	holeSize: number;
+}
+
+interface JQuery {
+	drawDonut(args: Partial<JCanvasObject>): JQuery;
+}
---
+++ ajax/libs/jcanvas/23.0.0-rc.2/esm/jcanvas-hearts.min.d.ts
@@ -0,0 +1,9 @@
+/// <reference path="jcanvas.d.ts" />
+
+interface JCanvasDefaults {
+	size: number;
+}
+
+interface JQuery {
+	drawHeart(args: Partial<JCanvasObject>): JQuery;
+}
