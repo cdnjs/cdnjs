e497a03232bb8a427be511dde42b80fa9e8f2115
robocdnjs <cdnjs-github@cloudflare.com>
2024-07-09 13:09:09 +0000

    Add xsound (3.9.3)

---
+++ ajax/libs/xsound/3.9.3/types/StreamModule/StreamModuleProcessor.d.ts
@@ -0,0 +1,13 @@
+import type { Inputs, Outputs } from '../worklet';
+import { AudioWorkletProcessor } from '../worklet';
+/**
+ * This class extends `AudioWorkletProcessor`.
+ * Overrides `process` method for sound source (bypass).
+ * @extends {AudioWorkletGlobalScope.AudioWorkletProcessor}
+ */
+export declare class StreamModuleProcessor extends AudioWorkletProcessor {
+    constructor();
+    /** @override */
+    protected process(inputs: Inputs, outputs: Outputs): boolean;
+}
+//# sourceMappingURL=StreamModuleProcessor.d.ts.map
