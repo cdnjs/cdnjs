faf0e7b7cdc8014582cee176706613372f918266
robocdnjs <cdnjs-github@cloudflare.com>
2024-07-09 13:56:10 +0000

    Add openlayers (9.2.5-dev.1717915591412)

---
+++ ajax/libs/openlayers/9.2.5-dev.1717915591412/render/canvas/PolygonBuilder.d.ts
@@ -0,0 +1,18 @@
+export default CanvasPolygonBuilder;
+declare class CanvasPolygonBuilder extends CanvasBuilder {
+    /**
+     * @param {Array<number>} flatCoordinates Flat coordinates.
+     * @param {number} offset Offset.
+     * @param {Array<number>} ends Ends.
+     * @param {number} stride Stride.
+     * @private
+     * @return {number} End.
+     */
+    private drawFlatCoordinatess_;
+    /**
+     * @private
+     */
+    private setFillStrokeStyles_;
+}
+import CanvasBuilder from './Builder.js';
+//# sourceMappingURL=PolygonBuilder.d.ts.map
