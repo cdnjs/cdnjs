b314ed049508733124ecc0110404bd904fed2a49
robocdnjs <cdnjs-github@cloudflare.com>
2024-07-10 11:23:37 +0000

    Add xsound (3.9.4)

---
+++ ajax/libs/xsound/3.9.4/types/MML/TreeConstructor.d.ts
@@ -0,0 +1,25 @@
+import { Tokenizer } from './Tokenizer';
+import { Tree, MMLSyntaxError } from './Tree';
+/**
+ * This class is Tree Construction (syntax tree) for MML (Music Macro Language).
+ * @constructor
+ */
+export declare class TreeConstructor {
+    static id: number;
+    private tokenizer;
+    private syntaxTree;
+    /**
+     * @param {Tokenizer} tokenizer This argument is instance of `Tokenizer`.
+     */
+    constructor(tokenizer: Tokenizer);
+    /**
+     * This method executes tree construction (parsing) from tokens.
+     * @return {Array<Tree>|MMLSyntaxError} Return value is syntax tree.
+     */
+    parse(): Tree[] | MMLSyntaxError;
+    /**
+     * This method releases memory that stack has.
+     */
+    free(): void;
+}
+//# sourceMappingURL=TreeConstructor.d.ts.map
