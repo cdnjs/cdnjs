2b59c28103a2865b8ec270cb840d59f9bafe48f5
robocdnjs <cdnjs-github@cloudflare.com>
2024-07-10 20:04:28 +0000

    Add fusioncharts (4.0.1)

---
+++ ajax/libs/fusioncharts/4.0.1/fusioncharts.vml.d.ts
@@ -0,0 +1,8 @@
+
+import { FusionChartStatic } from 'fusioncharts';
+
+declare namespace Vml {}
+declare var Vml: (H: FusionChartStatic) => FusionChartStatic;
+export = Vml;
+export as namespace Vml;
+
