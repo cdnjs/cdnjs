ae82b5479f1284bad452972f0e37ed6d0e6a29d3
robocdnjs <cdnjs-github@cloudflare.com>
2024-07-10 20:51:57 +0000

    Add jose (5.6.1)

---
+++ ajax/libs/jose/5.6.1/runtime/normalize_key.js
@@ -0,0 +1,30 @@
+import { decode } from './base64url.js';
+import importJWK from './jwk_to_key.js';
+const normalizeSecretKey = (k) => decode(k);
+const normalizePublicKey = async (key, alg) => {
+    if (key?.[Symbol.toStringTag] === 'KeyObject') {
+        let jwk = key.export({ format: 'jwk' });
+        delete jwk.d;
+        delete jwk.dp;
+        delete jwk.dq;
+        delete jwk.p;
+        delete jwk.q;
+        delete jwk.qi;
+        if (jwk.k) {
+            return normalizeSecretKey(jwk.k);
+        }
+        return importJWK({ ...jwk, alg });
+    }
+    return key;
+};
+const normalizePrivateKey = async (key, alg) => {
+    if (key?.[Symbol.toStringTag] === 'KeyObject') {
+        let jwk = key.export({ format: 'jwk' });
+        if (jwk.k) {
+            return normalizeSecretKey(jwk.k);
+        }
+        return importJWK({ ...jwk, alg });
+    }
+    return key;
+};
+export default { normalizePublicKey, normalizePrivateKey };
