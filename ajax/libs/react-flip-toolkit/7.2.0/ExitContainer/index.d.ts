3c554f03c489f3e8e1f25e231dd13faf80c506d3
robocdnjs <cdnjs-github@cloudflare.com>
2024-07-11 05:19:19 +0000

    Add react-flip-toolkit (7.2.0)

---
+++ ajax/libs/react-flip-toolkit/7.2.0/ExitContainer/index.d.ts
@@ -0,0 +1,5 @@
+import { ReactElement, ReactNode } from 'react';
+declare const ExitContainer: ({ children }: {
+    children: ReactNode;
+}) => ReactElement<any, string | import("react").JSXElementConstructor<any>>;
+export default ExitContainer;
