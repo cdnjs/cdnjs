4d18735ee30cd6d33114769e43a1d18fef0c5088
robocdnjs <cdnjs-github@cloudflare.com>
2024-07-11 07:46:44 +0000

    Add react-flip-toolkit (7.2.4)

---
+++ ajax/libs/react-flip-toolkit/7.2.4/Flipper/context.d.ts
@@ -0,0 +1,3 @@
+import { FlipCallbacks } from 'flip-toolkit/lib/types';
+export declare const FlipContext: import("react").Context<FlipCallbacks>;
+export declare const PortalContext: import("react").Context<string>;
---
+++ ajax/libs/react-flip-toolkit/7.2.4/index.d.ts
@@ -0,0 +1,5 @@
+export { disableFlip, enableFlip, isFlipEnabled } from 'flip-toolkit';
+export { default as Flipper } from './Flipper';
+export { default as Flipped } from './Flipped';
+export { default as ExitContainer } from './ExitContainer';
+export { default as spring } from './Spring';
