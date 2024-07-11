93f241b1bb98ce99fae03c7ee8fd8023a6ff77c1
robocdnjs <cdnjs-github@cloudflare.com>
2024-07-11 08:32:14 +0000

    Add openlayers (9.2.5-dev.1720344310382)

---
+++ ajax/libs/openlayers/9.2.5-dev.1720344310382/geom/LinearRing.d.ts
@@ -0,0 +1,59 @@
+export default LinearRing;
+/**
+ * @classdesc
+ * Linear ring geometry. Only used as part of polygon; cannot be rendered
+ * on its own.
+ *
+ * @api
+ */
+declare class LinearRing extends SimpleGeometry {
+    /**
+     * @param {Array<import("../coordinate.js").Coordinate>|Array<number>} coordinates Coordinates.
+     *     For internal use, flat coordinates in combination with `layout` are also accepted.
+     * @param {import("./Geometry.js").GeometryLayout} [layout] Layout.
+     */
+    constructor(coordinates: Array<import("../coordinate.js").Coordinate> | Array<number>, layout?: import("./Geometry.js").GeometryLayout | undefined);
+    /**
+     * @private
+     * @type {number}
+     */
+    private maxDelta_;
+    /**
+     * @private
+     * @type {number}
+     */
+    private maxDeltaRevision_;
+    /**
+     * Make a complete copy of the geometry.
+     * @return {!LinearRing} Clone.
+     * @api
+     */
+    clone(): LinearRing;
+    /**
+     * Return the area of the linear ring on projected plane.
+     * @return {number} Area (on projected plane).
+     * @api
+     */
+    getArea(): number;
+    /**
+     * Return the coordinates of the linear ring.
+     * @return {Array<import("../coordinate.js").Coordinate>} Coordinates.
+     * @api
+     */
+    getCoordinates(): Array<import("../coordinate.js").Coordinate>;
+    /**
+     * @param {number} squaredTolerance Squared tolerance.
+     * @return {LinearRing} Simplified LinearRing.
+     * @protected
+     */
+    protected getSimplifiedGeometryInternal(squaredTolerance: number): LinearRing;
+    /**
+     * Set the coordinates of the linear ring.
+     * @param {!Array<import("../coordinate.js").Coordinate>} coordinates Coordinates.
+     * @param {import("./Geometry.js").GeometryLayout} [layout] Layout.
+     * @api
+     */
+    setCoordinates(coordinates: Array<import("../coordinate.js").Coordinate>, layout?: import("./Geometry.js").GeometryLayout | undefined): void;
+}
+import SimpleGeometry from './SimpleGeometry.js';
+//# sourceMappingURL=LinearRing.d.ts.map
