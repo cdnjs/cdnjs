33c0a73e634d982e5181fe4b90338338e2c92344
robocdnjs <cdnjs-github@cloudflare.com>
2024-08-14 10:12:07 +0000

    Add openlayers (10.0.1-dev.1723626173667)

---
+++ ajax/libs/openlayers/10.0.1-dev.1723626173667/source/VectorEventType.d.ts
@@ -0,0 +1,12 @@
+declare namespace _default {
+    let ADDFEATURE: string;
+    let CHANGEFEATURE: string;
+    let CLEAR: string;
+    let REMOVEFEATURE: string;
+    let FEATURESLOADSTART: string;
+    let FEATURESLOADEND: string;
+    let FEATURESLOADERROR: string;
+}
+export default _default;
+export type VectorSourceEventTypes = "addfeature" | "changefeature" | "clear" | "removefeature" | "featuresloadstart" | "featuresloadend" | "featuresloaderror";
+//# sourceMappingURL=VectorEventType.d.ts.map
