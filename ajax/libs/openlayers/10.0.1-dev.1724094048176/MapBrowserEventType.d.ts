e8afe1d67341ed4190432256e46bc04afb8bcf3b
robocdnjs <cdnjs-github@cloudflare.com>
2024-08-19 21:07:40 +0000

    Add openlayers (10.0.1-dev.1724094048176)

---
+++ ajax/libs/openlayers/10.0.1-dev.1724094048176/MapBrowserEventType.d.ts
@@ -0,0 +1,20 @@
+declare namespace _default {
+    let SINGLECLICK: string;
+    let CLICK: string;
+    let DBLCLICK: string;
+    let POINTERDRAG: string;
+    let POINTERMOVE: string;
+    let POINTERDOWN: string;
+    let POINTERUP: string;
+    let POINTEROVER: string;
+    let POINTEROUT: string;
+    let POINTERENTER: string;
+    let POINTERLEAVE: string;
+    let POINTERCANCEL: string;
+}
+export default _default;
+/**
+ * *
+ */
+export type Types = "singleclick" | "click" | "dblclick" | "pointerdrag" | "pointermove";
+//# sourceMappingURL=MapBrowserEventType.d.ts.map
