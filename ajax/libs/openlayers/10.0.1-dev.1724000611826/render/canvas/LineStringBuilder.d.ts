b9749030b30136d4224d5aafd762093421e7e788
robocdnjs <cdnjs-github@cloudflare.com>
2024-08-19 08:34:08 +0000

    Add openlayers (10.0.1-dev.1724000611826)

---
+++ ajax/libs/openlayers/10.0.1-dev.1724000611826/render/canvas/LineStringBuilder.d.ts
@@ -0,0 +1,14 @@
+export default CanvasLineStringBuilder;
+declare class CanvasLineStringBuilder extends CanvasBuilder {
+    /**
+     * @param {Array<number>} flatCoordinates Flat coordinates.
+     * @param {number} offset Offset.
+     * @param {number} end End.
+     * @param {number} stride Stride.
+     * @private
+     * @return {number} end.
+     */
+    private drawFlatCoordinates_;
+}
+import CanvasBuilder from './Builder.js';
+//# sourceMappingURL=LineStringBuilder.d.ts.map
