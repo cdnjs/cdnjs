2b11a184f320fe16717e606e0fc05896aa7f8f99
robocdnjs <cdnjs-github@cloudflare.com>
2024-08-19 08:32:37 +0000

    Add openlayers (10.0.1-dev.1723996571401)

---
+++ ajax/libs/openlayers/10.0.1-dev.1723996571401/control/Control.js
@@ -0,0 +1,159 @@
+/**
+ * @module ol/control/Control
+ */
+import BaseObject from '../Object.js';
+import MapEventType from '../MapEventType.js';
+import {VOID} from '../functions.js';
+import {listen, unlistenByKey} from '../events.js';
+
+/**
+ * @typedef {Object} Options
+ * @property {HTMLElement} [element] The element is the control's
+ * container element. This only needs to be specified if you're developing
+ * a custom control.
+ * @property {function(import("../MapEvent.js").default):void} [render] Function called when
+ * the control should be re-rendered. This is called in a `requestAnimationFrame`
+ * callback.
+ * @property {HTMLElement|string} [target] Specify a target if you want
+ * the control to be rendered outside of the map's viewport.
+ */
+
+/**
+ * @classdesc
+ * A control is a visible widget with a DOM element in a fixed position on the
+ * screen. They can involve user input (buttons), or be informational only;
+ * the position is determined using CSS. By default these are placed in the
+ * container with CSS class name `ol-overlaycontainer-stopevent`, but can use
+ * any outside DOM element.
+ *
+ * This is the base class for controls. You can use it for simple custom
+ * controls by creating the element with listeners, creating an instance:
+ * ```js
+ * const myControl = new Control({element: myElement});
+ * ```
+ * and then adding this to the map.
+ *
+ * The main advantage of having this as a control rather than a simple separate
+ * DOM element is that preventing propagation is handled for you. Controls
+ * will also be objects in a {@link module:ol/Collection~Collection}, so you can use their methods.
+ *
+ * You can also extend this base for your own control class. See
+ * examples/custom-controls for an example of how to do this.
+ *
+ * @api
+ */
+class Control extends BaseObject {
+  /**
+   * @param {Options} options Control options.
+   */
+  constructor(options) {
+    super();
+
+    const element = options.element;
+    if (element && !options.target && !element.style.pointerEvents) {
+      element.style.pointerEvents = 'auto';
+    }
+
+    /**
+     * @protected
+     * @type {HTMLElement}
+     */
+    this.element = element ? element : null;
+
+    /**
+     * @private
+     * @type {HTMLElement}
+     */
+    this.target_ = null;
+
+    /**
+     * @private
+     * @type {import("../Map.js").default|null}
+     */
+    this.map_ = null;
+
+    /**
+     * @protected
+     * @type {!Array<import("../events.js").EventsKey>}
+     */
+    this.listenerKeys = [];
+
+    if (options.render) {
+      this.render = options.render;
+    }
+
+    if (options.target) {
+      this.setTarget(options.target);
+    }
+  }
+
+  /**
+   * Clean up.
+   * @override
+   */
+  disposeInternal() {
+    this.element?.remove();
+    super.disposeInternal();
+  }
+
+  /**
+   * Get the map associated with this control.
+   * @return {import("../Map.js").default|null} Map.
+   * @api
+   */
+  getMap() {
+    return this.map_;
+  }
+
+  /**
+   * Remove the control from its current map and attach it to the new map.
+   * Pass `null` to just remove the control from the current map.
+   * Subclasses may set up event handlers to get notified about changes to
+   * the map here.
+   * @param {import("../Map.js").default|null} map Map.
+   * @api
+   */
+  setMap(map) {
+    if (this.map_) {
+      this.element?.remove();
+    }
+    for (let i = 0, ii = this.listenerKeys.length; i < ii; ++i) {
+      unlistenByKey(this.listenerKeys[i]);
+    }
+    this.listenerKeys.length = 0;
+    this.map_ = map;
+    if (map) {
+      const target = this.target_ ?? map.getOverlayContainerStopEvent();
+      target.appendChild(this.element);
+      if (this.render !== VOID) {
+        this.listenerKeys.push(
+          listen(map, MapEventType.POSTRENDER, this.render, this),
+        );
+      }
+      map.render();
+    }
+  }
+
+  /**
+   * Renders the control.
+   * @param {import("../MapEvent.js").default} mapEvent Map event.
+   * @api
+   */
+  render(mapEvent) {}
+
+  /**
+   * This function is used to set a target element for the control. It has no
+   * effect if it is called after the control has been added to the map (i.e.
+   * after `setMap` is called on the control). If no `target` is set in the
+   * options passed to the control constructor and if `setTarget` is not called
+   * then the control is added to the map's overlay container.
+   * @param {HTMLElement|string} target Target.
+   * @api
+   */
+  setTarget(target) {
+    this.target_ =
+      typeof target === 'string' ? document.getElementById(target) : target;
+  }
+}
+
+export default Control;
---
+++ ajax/libs/openlayers/10.0.1-dev.1723996571401/format/GML32.js
@@ -0,0 +1,357 @@
+/**
+ * @module ol/format/GML32
+ */
+import GML2 from './GML2.js';
+import GML3 from './GML3.js';
+import GMLBase from './GMLBase.js';
+import {
+  makeArrayExtender,
+  makeArrayPusher,
+  makeChildAppender,
+  makeReplacer,
+} from '../xml.js';
+import {writeStringTextNode} from '../format/xsd.js';
+
+/**
+ * @classdesc Feature format for reading and writing data in the GML format
+ *            version 3.2.1.
+ * @api
+ */
+class GML32 extends GML3 {
+  /**
+   * @param {import("./GMLBase.js").Options} [options] Optional configuration object.
+   */
+  constructor(options) {
+    options = options ? options : {};
+
+    super(options);
+
+    /**
+     * @type {string}
+     */
+    this.schemaLocation = options.schemaLocation
+      ? options.schemaLocation
+      : this.namespace + ' http://schemas.opengis.net/gml/3.2.1/gml.xsd';
+  }
+
+  /**
+   * @param {Node} node Node.
+   * @param {import("../geom/Geometry.js").default|import("../extent.js").Extent} geometry Geometry.
+   * @param {Array<*>} objectStack Node stack.
+   * @override
+   */
+  writeGeometryElement(node, geometry, objectStack) {
+    const context = objectStack[objectStack.length - 1];
+    objectStack[objectStack.length - 1] = Object.assign(
+      {multiCurve: true, multiSurface: true},
+      context,
+    );
+    super.writeGeometryElement(node, geometry, objectStack);
+  }
+}
+
+GML32.prototype.namespace = 'http://www.opengis.net/gml/3.2';
+
+/**
+ * @const
+ * @type {Object<string, Object<string, import("../xml.js").Parser>>}
+ */
+GML32.prototype.GEOMETRY_FLAT_COORDINATES_PARSERS = {
+  'http://www.opengis.net/gml/3.2': {
+    'pos': makeReplacer(GML3.prototype.readFlatPos),
+    'posList': makeReplacer(GML3.prototype.readFlatPosList),
+    'coordinates': makeReplacer(GML2.prototype.readFlatCoordinates),
+  },
+};
+
+/**
+ * @const
+ * @type {Object<string, Object<string, import("../xml.js").Parser>>}
+ */
+GML32.prototype.FLAT_LINEAR_RINGS_PARSERS = {
+  'http://www.opengis.net/gml/3.2': {
+    'interior': GML3.prototype.interiorParser,
+    'exterior': GML3.prototype.exteriorParser,
+  },
+};
+
+/**
+ * @const
+ * @type {Object<string, Object<string, import("../xml.js").Parser>>}
+ */
+GML32.prototype.GEOMETRY_PARSERS = {
+  'http://www.opengis.net/gml/3.2': {
+    'Point': makeReplacer(GMLBase.prototype.readPoint),
+    'MultiPoint': makeReplacer(GMLBase.prototype.readMultiPoint),
+    'LineString': makeReplacer(GMLBase.prototype.readLineString),
+    'MultiLineString': makeReplacer(GMLBase.prototype.readMultiLineString),
+    'LinearRing': makeReplacer(GMLBase.prototype.readLinearRing),
+    'Polygon': makeReplacer(GMLBase.prototype.readPolygon),
+    'MultiPolygon': makeReplacer(GMLBase.prototype.readMultiPolygon),
+    'Surface': makeReplacer(GML32.prototype.readSurface),
+    'MultiSurface': makeReplacer(GML3.prototype.readMultiSurface),
+    'Curve': makeReplacer(GML32.prototype.readCurve),
+    'MultiCurve': makeReplacer(GML3.prototype.readMultiCurve),
+    'Envelope': makeReplacer(GML32.prototype.readEnvelope),
+  },
+};
+
+/**
+ * @const
+ * @type {Object<string, Object<string, import("../xml.js").Parser>>}
+ */
+GML32.prototype.MULTICURVE_PARSERS = {
+  'http://www.opengis.net/gml/3.2': {
+    'curveMember': makeArrayPusher(GML3.prototype.curveMemberParser),
+    'curveMembers': makeArrayPusher(GML3.prototype.curveMemberParser),
+  },
+};
+
+/**
+ * @const
+ * @type {Object<string, Object<string, import("../xml.js").Parser>>}
+ */
+GML32.prototype.MULTISURFACE_PARSERS = {
+  'http://www.opengis.net/gml/3.2': {
+    'surfaceMember': makeArrayPusher(GML3.prototype.surfaceMemberParser),
+    'surfaceMembers': makeArrayPusher(GML3.prototype.surfaceMemberParser),
+  },
+};
+
+/**
+ * @const
+ * @type {Object<string, Object<string, import("../xml.js").Parser>>}
+ */
+GML32.prototype.CURVEMEMBER_PARSERS = {
+  'http://www.opengis.net/gml/3.2': {
+    'LineString': makeArrayPusher(GMLBase.prototype.readLineString),
+    'Curve': makeArrayPusher(GML3.prototype.readCurve),
+  },
+};
+
+/**
+ * @const
+ * @type {Object<string, Object<string, import("../xml.js").Parser>>}
+ */
+GML32.prototype.SURFACEMEMBER_PARSERS = {
+  'http://www.opengis.net/gml/3.2': {
+    'Polygon': makeArrayPusher(GMLBase.prototype.readPolygon),
+    'Surface': makeArrayPusher(GML3.prototype.readSurface),
+  },
+};
+
+/**
+ * @const
+ * @type {Object<string, Object<string, import("../xml.js").Parser>>}
+ */
+GML32.prototype.SURFACE_PARSERS = {
+  'http://www.opengis.net/gml/3.2': {
+    'patches': makeReplacer(GML3.prototype.readPatch),
+  },
+};
+
+/**
+ * @const
+ * @type {Object<string, Object<string, import("../xml.js").Parser>>}
+ */
+GML32.prototype.CURVE_PARSERS = {
+  'http://www.opengis.net/gml/3.2': {
+    'segments': makeReplacer(GML3.prototype.readSegment),
+  },
+};
+
+/**
+ * @const
+ * @type {Object<string, Object<string, import("../xml.js").Parser>>}
+ */
+GML32.prototype.ENVELOPE_PARSERS = {
+  'http://www.opengis.net/gml/3.2': {
+    'lowerCorner': makeArrayPusher(GML3.prototype.readFlatPosList),
+    'upperCorner': makeArrayPusher(GML3.prototype.readFlatPosList),
+  },
+};
+
+/**
+ * @const
+ * @type {Object<string, Object<string, import("../xml.js").Parser>>}
+ */
+GML32.prototype.PATCHES_PARSERS = {
+  'http://www.opengis.net/gml/3.2': {
+    'PolygonPatch': makeReplacer(GML3.prototype.readPolygonPatch),
+  },
+};
+
+/**
+ * @const
+ * @type {Object<string, Object<string, import("../xml.js").Parser>>}
+ */
+GML32.prototype.SEGMENTS_PARSERS = {
+  'http://www.opengis.net/gml/3.2': {
+    'LineStringSegment': makeArrayExtender(
+      GML3.prototype.readLineStringSegment,
+    ),
+  },
+};
+
+/**
+ * @const
+ * @type {Object<string, Object<string, import("../xml.js").Parser>>}
+ */
+GML32.prototype.MULTIPOINT_PARSERS = {
+  'http://www.opengis.net/gml/3.2': {
+    'pointMember': makeArrayPusher(GMLBase.prototype.pointMemberParser),
+    'pointMembers': makeArrayPusher(GMLBase.prototype.pointMemberParser),
+  },
+};
+
+/**
+ * @const
+ * @type {Object<string, Object<string, import("../xml.js").Parser>>}
+ */
+GML32.prototype.MULTILINESTRING_PARSERS = {
+  'http://www.opengis.net/gml/3.2': {
+    'lineStringMember': makeArrayPusher(
+      GMLBase.prototype.lineStringMemberParser,
+    ),
+    'lineStringMembers': makeArrayPusher(
+      GMLBase.prototype.lineStringMemberParser,
+    ),
+  },
+};
+
+/**
+ * @const
+ * @type {Object<string, Object<string, import("../xml.js").Parser>>}
+ */
+GML32.prototype.MULTIPOLYGON_PARSERS = {
+  'http://www.opengis.net/gml/3.2': {
+    'polygonMember': makeArrayPusher(GMLBase.prototype.polygonMemberParser),
+    'polygonMembers': makeArrayPusher(GMLBase.prototype.polygonMemberParser),
+  },
+};
+
+/**
+ * @const
+ * @type {Object<string, Object<string, import("../xml.js").Parser>>}
+ */
+GML32.prototype.POINTMEMBER_PARSERS = {
+  'http://www.opengis.net/gml/3.2': {
+    'Point': makeArrayPusher(GMLBase.prototype.readFlatCoordinatesFromNode),
+  },
+};
+
+/**
+ * @const
+ * @type {Object<string, Object<string, import("../xml.js").Parser>>}
+ */
+GML32.prototype.LINESTRINGMEMBER_PARSERS = {
+  'http://www.opengis.net/gml/3.2': {
+    'LineString': makeArrayPusher(GMLBase.prototype.readLineString),
+  },
+};
+
+/**
+ * @const
+ * @type {Object<string, Object<string, import("../xml.js").Parser>>}
+ */
+GML32.prototype.POLYGONMEMBER_PARSERS = {
+  'http://www.opengis.net/gml/3.2': {
+    'Polygon': makeArrayPusher(GMLBase.prototype.readPolygon),
+  },
+};
+
+/**
+ * @const
+ * @type {Object<string, Object<string, import("../xml.js").Parser>>}
+ */
+GML32.prototype.RING_PARSERS = {
+  'http://www.opengis.net/gml/3.2': {
+    'LinearRing': makeReplacer(GMLBase.prototype.readFlatLinearRing),
+    'Ring': makeReplacer(GML32.prototype.readFlatCurveRing),
+  },
+};
+
+/**
+ * @type {Object<string, Object<string, import("../xml.js").Serializer>>}
+ */
+GML32.prototype.RING_SERIALIZERS = {
+  'http://www.opengis.net/gml/3.2': {
+    'exterior': makeChildAppender(GML3.prototype.writeRing),
+    'interior': makeChildAppender(GML3.prototype.writeRing),
+  },
+};
+
+/**
+ * @type {Object<string, Object<string, import("../xml.js").Serializer>>}
+ */
+GML32.prototype.ENVELOPE_SERIALIZERS = {
+  'http://www.opengis.net/gml/3.2': {
+    'lowerCorner': makeChildAppender(writeStringTextNode),
+    'upperCorner': makeChildAppender(writeStringTextNode),
+  },
+};
+
+/**
+ * @type {Object<string, Object<string, import("../xml.js").Serializer>>}
+ */
+GML32.prototype.SURFACEORPOLYGONMEMBER_SERIALIZERS = {
+  'http://www.opengis.net/gml/3.2': {
+    'surfaceMember': makeChildAppender(
+      GML3.prototype.writeSurfaceOrPolygonMember,
+    ),
+    'polygonMember': makeChildAppender(
+      GML3.prototype.writeSurfaceOrPolygonMember,
+    ),
+  },
+};
+
+/**
+ * @type {Object<string, Object<string, import("../xml.js").Serializer>>}
+ */
+GML32.prototype.POINTMEMBER_SERIALIZERS = {
+  'http://www.opengis.net/gml/3.2': {
+    'pointMember': makeChildAppender(GML3.prototype.writePointMember),
+  },
+};
+
+/**
+ * @type {Object<string, Object<string, import("../xml.js").Serializer>>}
+ */
+GML32.prototype.LINESTRINGORCURVEMEMBER_SERIALIZERS = {
+  'http://www.opengis.net/gml/3.2': {
+    'lineStringMember': makeChildAppender(
+      GML3.prototype.writeLineStringOrCurveMember,
+    ),
+    'curveMember': makeChildAppender(
+      GML3.prototype.writeLineStringOrCurveMember,
+    ),
+  },
+};
+
+/**
+ * @type {Object<string, Object<string, import("../xml.js").Serializer>>}
+ */
+GML32.prototype.GEOMETRY_SERIALIZERS = {
+  'http://www.opengis.net/gml/3.2': {
+    'Curve': makeChildAppender(GML3.prototype.writeCurveOrLineString),
+    'MultiCurve': makeChildAppender(GML3.prototype.writeMultiCurveOrLineString),
+    'Point': makeChildAppender(GML32.prototype.writePoint),
+    'MultiPoint': makeChildAppender(GML3.prototype.writeMultiPoint),
+    'LineString': makeChildAppender(GML3.prototype.writeCurveOrLineString),
+    'MultiLineString': makeChildAppender(
+      GML3.prototype.writeMultiCurveOrLineString,
+    ),
+    'LinearRing': makeChildAppender(GML3.prototype.writeLinearRing),
+    'Polygon': makeChildAppender(GML3.prototype.writeSurfaceOrPolygon),
+    'MultiPolygon': makeChildAppender(
+      GML3.prototype.writeMultiSurfaceOrPolygon,
+    ),
+    'Surface': makeChildAppender(GML3.prototype.writeSurfaceOrPolygon),
+    'MultiSurface': makeChildAppender(
+      GML3.prototype.writeMultiSurfaceOrPolygon,
+    ),
+    'Envelope': makeChildAppender(GML3.prototype.writeEnvelope),
+  },
+};
+
+export default GML32;
