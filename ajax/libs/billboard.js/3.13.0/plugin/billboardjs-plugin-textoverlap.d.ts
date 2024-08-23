19fe7e52731e6aa28af40a81e328cb3e9529dfc3
robocdnjs <cdnjs-github@cloudflare.com>
2024-08-23 09:04:32 +0000

    Add billboard.js (3.13.0)

---
+++ ajax/libs/billboard.js/3.13.0/plugin/billboardjs-plugin-textoverlap.d.ts
@@ -0,0 +1,7 @@
+/**
+ * Copyright (c) 2017 ~ present NAVER Corp.
+ * billboard.js project is licensed under the MIT license
+ */
+import PluginClass from "../../types/plugin/textoverlap";
+
+export default PluginClass;
