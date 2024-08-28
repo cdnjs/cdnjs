780c818b08f1de170b6caab9c414cb11b42eb075
robocdnjs <cdnjs-github@cloudflare.com>
2024-08-28 00:08:41 +0000

    Add openlayers (10.1.1-dev.1724790516521)

---
+++ ajax/libs/openlayers/10.1.1-dev.1724790516521/geom/flat/segments.d.ts
@@ -0,0 +1,18 @@
+/**
+ * @module ol/geom/flat/segments
+ */
+/**
+ * This function calls `callback` for each segment of the flat coordinates
+ * array. If the callback returns a truthy value the function returns that
+ * value immediately. Otherwise the function returns `false`.
+ * @param {Array<number>} flatCoordinates Flat coordinates.
+ * @param {number} offset Offset.
+ * @param {number} end End.
+ * @param {number} stride Stride.
+ * @param {function(import("../../coordinate.js").Coordinate, import("../../coordinate.js").Coordinate): T} callback Function
+ *     called for each segment.
+ * @return {T|boolean} Value.
+ * @template T
+ */
+export function forEach<T>(flatCoordinates: Array<number>, offset: number, end: number, stride: number, callback: (arg0: import("../../coordinate.js").Coordinate, arg1: import("../../coordinate.js").Coordinate) => T): T | boolean;
+//# sourceMappingURL=segments.d.ts.map
