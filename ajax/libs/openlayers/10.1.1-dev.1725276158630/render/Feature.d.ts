8ad3b2ada4d95084d1fb132d8422f4a69292eccd
robocdnjs <cdnjs-github@cloudflare.com>
2024-09-02 14:06:07 +0000

    Add openlayers (10.1.1-dev.1725276158630)

---
+++ ajax/libs/openlayers/10.1.1-dev.1725276158630/render/Feature.d.ts
@@ -0,0 +1,231 @@
+/**
+ * Create a geometry from an `ol/render/Feature`
+ * @param {RenderFeature} renderFeature
+ * Render Feature
+ * @return {Point|MultiPoint|LineString|MultiLineString|Polygon|MultiPolygon}
+ * New geometry instance.
+ * @api
+ */
+export function toGeometry(renderFeature: RenderFeature): Point | MultiPoint | LineString | MultiLineString | Polygon | MultiPolygon;
+/**
+ * Create an `ol/Feature` from an `ol/render/Feature`
+ * @param {RenderFeature} renderFeature RenderFeature
+ * @param {string} [geometryName='geometry'] Geometry name to use
+ * when creating the Feature.
+ * @return {Feature} Newly constructed `ol/Feature` with properties,
+ * geometry, and id copied over.
+ * @api
+ */
+export function toFeature(renderFeature: RenderFeature, geometryName?: string | undefined): Feature;
+export default RenderFeature;
+/**
+ * The geometry type.  One of `'Point'`, `'LineString'`, `'LinearRing'`,
+ * `'Polygon'`, `'MultiPoint'` or 'MultiLineString'`.
+ */
+export type Type = "Point" | "LineString" | "LinearRing" | "Polygon" | "MultiPoint" | "MultiLineString";
+/**
+ * Lightweight, read-only, {@link module:ol/Feature~Feature} and {@link module:ol/geom/Geometry~Geometry} like
+ * structure, optimized for vector tile rendering and styling. Geometry access
+ * through the API is limited to getting the type and extent of the geometry.
+ */
+declare class RenderFeature {
+    /**
+     * @param {Type} type Geometry type.
+     * @param {Array<number>} flatCoordinates Flat coordinates. These always need
+     *     to be right-handed for polygons.
+     * @param {Array<number>} ends Ends.
+     * @param {number} stride Stride.
+     * @param {Object<string, *>} properties Properties.
+     * @param {number|string|undefined} id Feature id.
+     */
+    constructor(type: Type, flatCoordinates: Array<number>, ends: Array<number>, stride: number, properties: {
+        [x: string]: any;
+    }, id: number | string | undefined);
+    /**
+     * @type {import("../style/Style.js").StyleFunction|undefined}
+     */
+    styleFunction: import("../style/Style.js").StyleFunction | undefined;
+    /**
+     * @private
+     * @type {import("../extent.js").Extent|undefined}
+     */
+    private extent_;
+    /**
+     * @private
+     * @type {number|string|undefined}
+     */
+    private id_;
+    /**
+     * @private
+     * @type {Type}
+     */
+    private type_;
+    /**
+     * @private
+     * @type {Array<number>}
+     */
+    private flatCoordinates_;
+    /**
+     * @private
+     * @type {Array<number>}
+     */
+    private flatInteriorPoints_;
+    /**
+     * @private
+     * @type {Array<number>}
+     */
+    private flatMidpoints_;
+    /**
+     * @private
+     * @type {Array<number>|null}
+     */
+    private ends_;
+    /**
+     * @private
+     * @type {Object<string, *>}
+     */
+    private properties_;
+    /**
+     * @private
+     * @type {number}
+     */
+    private squaredTolerance_;
+    /**
+     * @private
+     * @type {number}
+     */
+    private stride_;
+    /**
+     * @private
+     * @type {RenderFeature}
+     */
+    private simplifiedGeometry_;
+    /**
+     * Get a feature property by its key.
+     * @param {string} key Key
+     * @return {*} Value for the requested key.
+     * @api
+     */
+    get(key: string): any;
+    /**
+     * Get the extent of this feature's geometry.
+     * @return {import("../extent.js").Extent} Extent.
+     * @api
+     */
+    getExtent(): import("../extent.js").Extent;
+    /**
+     * @return {Array<number>} Flat interior points.
+     */
+    getFlatInteriorPoint(): Array<number>;
+    /**
+     * @return {Array<number>} Flat interior points.
+     */
+    getFlatInteriorPoints(): Array<number>;
+    /**
+     * @return {Array<number>} Flat midpoint.
+     */
+    getFlatMidpoint(): Array<number>;
+    /**
+     * @return {Array<number>} Flat midpoints.
+     */
+    getFlatMidpoints(): Array<number>;
+    /**
+     * Get the feature identifier.  This is a stable identifier for the feature and
+     * is set when reading data from a remote source.
+     * @return {number|string|undefined} Id.
+     * @api
+     */
+    getId(): number | string | undefined;
+    /**
+     * @return {Array<number>} Flat coordinates.
+     */
+    getOrientedFlatCoordinates(): Array<number>;
+    /**
+     * For API compatibility with {@link module:ol/Feature~Feature}, this method is useful when
+     * determining the geometry type in style function (see {@link #getType}).
+     * @return {RenderFeature} Feature.
+     * @api
+     */
+    getGeometry(): RenderFeature;
+    /**
+     * @param {number} squaredTolerance Squared tolerance.
+     * @return {RenderFeature} Simplified geometry.
+     */
+    getSimplifiedGeometry(squaredTolerance: number): RenderFeature;
+    /**
+     * Get a transformed and simplified version of the geometry.
+     * @param {number} squaredTolerance Squared tolerance.
+     * @param {import("../proj.js").TransformFunction} [transform] Optional transform function.
+     * @return {RenderFeature} Simplified geometry.
+     */
+    simplifyTransformed(squaredTolerance: number, transform?: import("../proj.js").TransformFunction | undefined): RenderFeature;
+    /**
+     * Get the feature properties.
+     * @return {Object<string, *>} Feature properties.
+     * @api
+     */
+    getProperties(): {
+        [x: string]: any;
+    };
+    /**
+     * Get an object of all property names and values.  This has the same behavior as getProperties,
+     * but is here to conform with the {@link module:ol/Feature~Feature} interface.
+     * @return {Object<string, *>?} Object.
+     */
+    getPropertiesInternal(): {
+        [x: string]: any;
+    } | null;
+    /**
+     * @return {number} Stride.
+     */
+    getStride(): number;
+    /**
+     * @return {import('../style/Style.js').StyleFunction|undefined} Style
+     */
+    getStyleFunction(): import("../style/Style.js").StyleFunction | undefined;
+    /**
+     * Get the type of this feature's geometry.
+     * @return {Type} Geometry type.
+     * @api
+     */
+    getType(): Type;
+    /**
+     * Transform geometry coordinates from tile pixel space to projected.
+     *
+     * @param {import("../proj.js").ProjectionLike} projection The data projection
+     */
+    transform(projection: import("../proj.js").ProjectionLike): void;
+    /**
+     * Apply a transform function to the coordinates of the geometry.
+     * The geometry is modified in place.
+     * If you do not want the geometry modified in place, first `clone()` it and
+     * then use this function on the clone.
+     * @param {import("../proj.js").TransformFunction} transformFn Transform function.
+     */
+    applyTransform(transformFn: import("../proj.js").TransformFunction): void;
+    /**
+     * @return {RenderFeature} A cloned render feature.
+     */
+    clone(): RenderFeature;
+    /**
+     * @return {Array<number>|null} Ends.
+     */
+    getEnds(): Array<number> | null;
+    /**
+     * Add transform and resolution based geometry simplification to this instance.
+     * @return {RenderFeature} This render feature.
+     */
+    enableSimplifyTransformed(): RenderFeature;
+    /**
+     * @return {Array<number>} Flat coordinates.
+     */
+    getFlatCoordinates: () => Array<number>;
+}
+import { Point } from '../geom.js';
+import { MultiPoint } from '../geom.js';
+import { LineString } from '../geom.js';
+import { MultiLineString } from '../geom.js';
+import { Polygon } from '../geom.js';
+import { MultiPolygon } from '../geom.js';
+import Feature from '../Feature.js';
+//# sourceMappingURL=Feature.d.ts.map
---
+++ ajax/libs/openlayers/10.1.1-dev.1725276158630/webgl/Buffer.js
@@ -0,0 +1,159 @@
+/**
+ * @module ol/webgl/Buffer
+ */
+import {
+  ARRAY_BUFFER,
+  DYNAMIC_DRAW,
+  ELEMENT_ARRAY_BUFFER,
+  STATIC_DRAW,
+  STREAM_DRAW,
+} from '../webgl.js';
+import {assert} from '../asserts.js';
+
+/**
+ * Used to describe the intended usage for the data: `STATIC_DRAW`, `STREAM_DRAW`
+ * or `DYNAMIC_DRAW`.
+ * @enum {number}
+ */
+export const BufferUsage = {
+  STATIC_DRAW: STATIC_DRAW,
+  STREAM_DRAW: STREAM_DRAW,
+  DYNAMIC_DRAW: DYNAMIC_DRAW,
+};
+
+/**
+ * @classdesc
+ * Object used to store an array of data as well as usage information for that data.
+ * Stores typed arrays internally, either Float32Array or Uint16/32Array depending on
+ * the buffer type (ARRAY_BUFFER or ELEMENT_ARRAY_BUFFER) and available extensions.
+ *
+ * To populate the array, you can either use:
+ * * A size using `#ofSize(buffer)`
+ * * An `ArrayBuffer` object using `#fromArrayBuffer(buffer)`
+ * * A plain array using `#fromArray(array)`
+ *
+ * Note:
+ * See the documentation of [WebGLRenderingContext.bufferData](https://developer.mozilla.org/en-US/docs/Web/API/WebGLRenderingContext/bufferData)
+ * for more info on buffer usage.
+ */
+class WebGLArrayBuffer {
+  /**
+   * @param {number} type Buffer type, either ARRAY_BUFFER or ELEMENT_ARRAY_BUFFER.
+   * @param {number} [usage] Intended usage, either `STATIC_DRAW`, `STREAM_DRAW` or `DYNAMIC_DRAW`.
+   * Default is `STATIC_DRAW`.
+   */
+  constructor(type, usage) {
+    /**
+     * @private
+     * @type {Float32Array|Uint32Array|null}
+     */
+    this.array_ = null;
+
+    /**
+     * @private
+     * @type {number}
+     */
+    this.type_ = type;
+
+    assert(
+      type === ARRAY_BUFFER || type === ELEMENT_ARRAY_BUFFER,
+      'A `WebGLArrayBuffer` must either be of type `ELEMENT_ARRAY_BUFFER` or `ARRAY_BUFFER`',
+    );
+
+    /**
+     * @private
+     * @type {number}
+     */
+    this.usage_ = usage !== undefined ? usage : BufferUsage.STATIC_DRAW;
+  }
+
+  /**
+   * Populates the buffer with an array of the given size (all values will be zeroes).
+   * @param {number} size Array size
+   * @return {WebGLArrayBuffer} This
+   */
+  ofSize(size) {
+    this.array_ = new (getArrayClassForType(this.type_))(size);
+    return this;
+  }
+
+  /**
+   * Populates the buffer with an array of the given size.
+   * @param {Array<number>} array Numerical array
+   * @return {WebGLArrayBuffer} This
+   */
+  fromArray(array) {
+    this.array_ = getArrayClassForType(this.type_).from(array);
+    return this;
+  }
+
+  /**
+   * Populates the buffer with a raw binary array buffer.
+   * @param {ArrayBuffer} buffer Raw binary buffer to populate the array with. Note that this buffer must have been
+   * initialized for the same typed array class.
+   * @return {WebGLArrayBuffer} This
+   */
+  fromArrayBuffer(buffer) {
+    this.array_ = new (getArrayClassForType(this.type_))(buffer);
+    return this;
+  }
+
+  /**
+   * @return {number} Buffer type.
+   */
+  getType() {
+    return this.type_;
+  }
+
+  /**
+   * Will return null if the buffer was not initialized
+   * @return {Float32Array|Uint32Array|null} Array.
+   */
+  getArray() {
+    return this.array_;
+  }
+
+  /**
+   * @param {Float32Array|Uint32Array} array Array.
+   */
+  setArray(array) {
+    const ArrayType = getArrayClassForType(this.type_);
+    if (!(array instanceof ArrayType)) {
+      throw new Error(`Expected ${ArrayType}`);
+    }
+    this.array_ = array;
+  }
+
+  /**
+   * @return {number} Usage.
+   */
+  getUsage() {
+    return this.usage_;
+  }
+
+  /**
+   * Will return 0 if the buffer is not initialized
+   * @return {number} Array size
+   */
+  getSize() {
+    return this.array_ ? this.array_.length : 0;
+  }
+}
+
+/**
+ * Returns a typed array constructor based on the given buffer type
+ * @param {number} type Buffer type, either ARRAY_BUFFER or ELEMENT_ARRAY_BUFFER.
+ * @return {Float32ArrayConstructor|Uint32ArrayConstructor} The typed array class to use for this buffer.
+ */
+export function getArrayClassForType(type) {
+  switch (type) {
+    case ARRAY_BUFFER:
+      return Float32Array;
+    case ELEMENT_ARRAY_BUFFER:
+      return Uint32Array;
+    default:
+      return Float32Array;
+  }
+}
+
+export default WebGLArrayBuffer;
