f81aca4923674820622ee9fca795f01748dc9abf
robocdnjs <cdnjs-github@cloudflare.com>
2024-09-03 22:05:31 +0000

    Add intl-tel-input (24.3.7)

---
+++ ajax/libs/intl-tel-input/24.3.7/build/js/i18n/el/interface.js
@@ -0,0 +1,13 @@
+const interfaceTranslations = {
+  selectedCountryAriaLabel: "Επιλεγμένη χώρα",
+  noCountrySelected: "Δεν έχει επιλεγεί χώρα",
+  countryListAriaLabel: "Κατάλογος χωρών",
+  searchPlaceholder: "Αναζήτηση",
+  zeroSearchResults: "Δεν βρέθηκαν αποτελέσματα",
+  oneSearchResult: "Βρέθηκε 1 αποτέλεσμα",
+  multipleSearchResults: "Βρέθηκαν ${count} αποτελέσματα",
+  // additional countries (not supported by country-list library)
+  ac: "Νησί της Ανάληψης",
+  xk: "Κοσσυφοπέδιο"
+};
+export default interfaceTranslations;
---
+++ ajax/libs/intl-tel-input/24.3.7/build/js/i18n/en/interface.js
@@ -0,0 +1,13 @@
+const interfaceTranslations = {
+  selectedCountryAriaLabel: "Selected country",
+  noCountrySelected: "No country selected",
+  countryListAriaLabel: "List of countries",
+  searchPlaceholder: "Search",
+  zeroSearchResults: "No results found",
+  oneSearchResult: "1 result found",
+  multipleSearchResults: "${count} results found",
+  // additional countries (not supported by country-list library)
+  ac: "Ascension Island",
+  xk: "Kosovo"
+};
+export default interfaceTranslations;
