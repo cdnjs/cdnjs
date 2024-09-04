d4b228864a7560179700681b6667d2c650c9f51c
robocdnjs <cdnjs-github@cloudflare.com>
2024-09-04 17:07:29 +0000

    Add intl-tel-input (24.4.0)

---
+++ ajax/libs/intl-tel-input/24.4.0/build/js/i18n/ca/interface.js
@@ -0,0 +1,13 @@
+const interfaceTranslations = {
+  selectedCountryAriaLabel: "País seleccionat",
+  noCountrySelected: "No s'ha seleccionat cap país",
+  countryListAriaLabel: "Llista de països",
+  searchPlaceholder: "Cerca",
+  zeroSearchResults: "Sense resultats",
+  oneSearchResult: "1 resultat trobat",
+  multipleSearchResults: "${count} resultats trobats",
+  // additional countries (not supported by country-list library)
+  ac: "Illa de l'Ascensió",
+  xk: "Kosovo"
+};
+export default interfaceTranslations;
