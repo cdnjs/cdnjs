1490c95e6f4288987030632f8966b690be5d9e63
robocdnjs <cdnjs-github@cloudflare.com>
2024-09-05 10:15:52 +0000

    Add aui (9.12.1)

---
+++ ajax/libs/aui/9.12.1/aui/aui-prototyping-design-tokens-base-themes-css.js
@@ -0,0 +1,9 @@
+/*!!
+ * @atlassian/aui - Atlassian User Interface library
+ * @version v9.12.1
+ * @link https://aui.atlassian.com
+ * @license Apache-2.0
+ * @author Atlassian Pty Ltd.
+ */
+!function(e,o){"object"==typeof exports&&"object"==typeof module?module.exports=o():"function"==typeof define&&define.amd?define("@atlassian/aui",[],o):"object"==typeof exports?exports["@atlassian/aui"]=o():(e.AJS=e.AJS||{},e.AJS.DesignTokens=o())}(window,(()=>(()=>{"use strict";var e={r:e=>{"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})}},o={};return e.r(o),o})()));
+//# sourceMappingURL=aui-prototyping-design-tokens-base-themes-css.js.map
