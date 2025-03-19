c483dd66c7eee886c4da2b225c99a499cbdb1af8
robocdnjs <cdnjs-github@cloudflare.com>
2025-03-19 23:55:42 +0000

    Add crayons (4.3.0-beta.16)

---
+++ ajax/libs/crayons/4.3.0-beta.16/p-d7293421.js
@@ -0,0 +1,1 @@
+function o(t){return(o="function"==typeof Symbol&&"symbol"==typeof Symbol.iterator?function(o){return typeof o}:function(o){return o&&"function"==typeof Symbol&&o.constructor===Symbol&&o!==Symbol.prototype?"symbol":typeof o})(t)}export{o as _}
