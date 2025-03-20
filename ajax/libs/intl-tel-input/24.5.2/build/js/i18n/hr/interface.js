a70926f8f3683dad7f3bf90617e6da44a157f920
robocdnjs <cdnjs-github@cloudflare.com>
2025-03-20 14:06:08 +0000

    Add intl-tel-input (24.5.2)

---
+++ ajax/libs/intl-tel-input/24.5.2/build/js/i18n/hr/interface.js
@@ -0,0 +1,13 @@
+const interfaceTranslations = {
+  selectedCountryAriaLabel: "Odabrana zemlja",
+  noCountrySelected: "Zemlja nije odabrana",
+  countryListAriaLabel: "Lista zemalja",
+  searchPlaceholder: "Pretraži",
+  zeroSearchResults: "Nema pronađenih rezultata",
+  oneSearchResult: "Pronađen 1 rezultat",
+  multipleSearchResults: "${count} rezultata pronađeno",
+  // additional countries (not supported by country-list library)
+  ac: "Ascension",
+  xk: "Kosovo"
+};
+export default interfaceTranslations;
---
+++ ajax/libs/intl-tel-input/24.5.2/build/js/i18n/id/interface.js
@@ -0,0 +1,13 @@
+const interfaceTranslations = {
+  selectedCountryAriaLabel: "Negara yang dipilih",
+  noCountrySelected: "Tidak ada negara yang dipilih",
+  countryListAriaLabel: "Daftar negara",
+  searchPlaceholder: "Mencari",
+  zeroSearchResults: "Tidak ada hasil yang ditemukan",
+  oneSearchResult: "1 hasil ditemukan",
+  multipleSearchResults: "${count} hasil ditemukan",
+  // additional countries (not supported by country-list library)
+  ac: "Pulau Kenaikan",
+  xk: "Kosovo"
+};
+export default interfaceTranslations;
