ab60c8f4d6836c099a80636529d6b093e5c91892
robocdnjs <cdnjs-github@cloudflare.com>
2025-03-21 07:55:06 +0000

    Add fusioncharts (4.1.0-beta.4)

---
+++ ajax/libs/fusioncharts/4.1.0-beta.4/fusioncharts.gantt.d.ts
@@ -0,0 +1,8 @@
+
+import { FusionChartStatic } from 'fusioncharts';
+
+declare namespace Gantt {}
+declare var Gantt: (H: FusionChartStatic) => FusionChartStatic;
+export = Gantt;
+export as namespace Gantt;
+
---
+++ ajax/libs/fusioncharts/4.1.0-beta.4/fusioncharts.overlappedbar2d.d.ts
@@ -0,0 +1,8 @@
+
+import { FusionChartStatic } from 'fusioncharts';
+
+declare namespace Overlappedbar2d {}
+declare var Overlappedbar2d: (H: FusionChartStatic) => FusionChartStatic;
+export = Overlappedbar2d;
+export as namespace Overlappedbar2d;
+
