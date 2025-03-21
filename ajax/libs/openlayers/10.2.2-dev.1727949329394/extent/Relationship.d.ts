5a9c073d1e35b8f767ad47d339666f2efab9d6a6
robocdnjs <cdnjs-github@cloudflare.com>
2025-03-21 09:27:36 +0000

    Add openlayers (10.2.2-dev.1727949329394)

---
+++ ajax/libs/openlayers/10.2.2-dev.1727949329394/extent/Relationship.d.ts
@@ -0,0 +1,10 @@
+declare namespace _default {
+    let UNKNOWN: number;
+    let INTERSECTING: number;
+    let ABOVE: number;
+    let RIGHT: number;
+    let BELOW: number;
+    let LEFT: number;
+}
+export default _default;
+//# sourceMappingURL=Relationship.d.ts.map
