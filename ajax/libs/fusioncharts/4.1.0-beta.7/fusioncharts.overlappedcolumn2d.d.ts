c7460d4460b40282b3ed572af6a94ddabc2c42e1
robocdnjs <cdnjs-github@cloudflare.com>
2025-03-21 10:23:37 +0000

    Add fusioncharts (4.1.0-beta.7)

---
+++ ajax/libs/fusioncharts/4.1.0-beta.7/fusioncharts.overlappedcolumn2d.d.ts
@@ -0,0 +1,8 @@
+
+import { FusionChartStatic } from 'fusioncharts';
+
+declare namespace Overlappedcolumn2d {}
+declare var Overlappedcolumn2d: (H: FusionChartStatic) => FusionChartStatic;
+export = Overlappedcolumn2d;
+export as namespace Overlappedcolumn2d;
+
