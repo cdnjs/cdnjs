a2a84424331a541dda7ec8a9d6a6e074df893b58
robocdnjs <cdnjs-github@cloudflare.com>
2025-03-23 01:46:21 +0000

    Add react-router-dom (0.0.0-experimental-7c6c0664d)

---
+++ ajax/libs/react-router-dom/0.0.0-experimental-7c6c0664d/react-router-dom.development.js
@@ -0,0 +1,35 @@
+/**
+ * React Router DOM v0.0.0-experimental-7c6c0664d
+ *
+ * Copyright (c) Remix Software Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE.md file in the root directory of this source tree.
+ *
+ * @license MIT
+ */
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('react-router/dom'), require('react-router')) :
+	typeof define === 'function' && define.amd ? define(['exports', 'react-router/dom', 'react-router'], factory) :
+	(global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global.ReactRouterDOM = {}, global.dom, global.ReactRouter));
+})(this, (function (exports, dom, reactRouter) { 'use strict';
+
+	Object.defineProperty(exports, 'HydratedRouter', {
+		enumerable: true,
+		get: function () { return dom.HydratedRouter; }
+	});
+	Object.defineProperty(exports, 'RouterProvider', {
+		enumerable: true,
+		get: function () { return dom.RouterProvider; }
+	});
+	Object.keys(reactRouter).forEach(function (k) {
+		if (k !== 'default' && !exports.hasOwnProperty(k)) Object.defineProperty(exports, k, {
+			enumerable: true,
+			get: function () { return reactRouter[k]; }
+		});
+	});
+
+	Object.defineProperty(exports, '__esModule', { value: true });
+
+}));
+//# sourceMappingURL=react-router-dom.development.js.map
