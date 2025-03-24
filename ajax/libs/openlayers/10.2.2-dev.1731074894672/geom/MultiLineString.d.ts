dcc718bf3da1b0b5ce9a4c7d88735001ef75ec4e
robocdnjs <cdnjs-github@cloudflare.com>
2025-03-24 17:35:51 +0000

    Add openlayers (10.2.2-dev.1731074894672)

---
+++ ajax/libs/openlayers/10.2.2-dev.1731074894672/geom/MultiLineString.d.ts
@@ -0,0 +1,114 @@
+export default MultiLineString;
+/**
+ * @classdesc
+ * Multi-linestring geometry.
+ *
+ * @api
+ */
+declare class MultiLineString extends SimpleGeometry {
+    /**
+     * @param {Array<Array<import("../coordinate.js").Coordinate>|LineString>|Array<number>} coordinates
+     *     Coordinates or LineString geometries. (For internal use, flat coordinates in
+     *     combination with `layout` and `ends` are also accepted.)
+     * @param {import("./Geometry.js").GeometryLayout} [layout] Layout.
+     * @param {Array<number>} [ends] Flat coordinate ends for internal use.
+     */
+    constructor(coordinates: Array<Array<import("../coordinate.js").Coordinate> | LineString> | Array<number>, layout?: import("./Geometry.js").GeometryLayout | undefined, ends?: number[] | undefined);
+    /**
+     * @type {Array<number>}
+     * @private
+     */
+    private ends_;
+    /**
+     * @private
+     * @type {number}
+     */
+    private maxDelta_;
+    /**
+     * @private
+     * @type {number}
+     */
+    private maxDeltaRevision_;
+    /**
+     * Append the passed linestring to the multilinestring.
+     * @param {LineString} lineString LineString.
+     * @api
+     */
+    appendLineString(lineString: LineString): void;
+    /**
+     * Make a complete copy of the geometry.
+     * @return {!MultiLineString} Clone.
+     * @api
+     * @override
+     */
+    override clone(): MultiLineString;
+    /**
+     * Returns the coordinate at `m` using linear interpolation, or `null` if no
+     * such coordinate exists.
+     *
+     * `extrapolate` controls extrapolation beyond the range of Ms in the
+     * MultiLineString. If `extrapolate` is `true` then Ms less than the first
+     * M will return the first coordinate and Ms greater than the last M will
+     * return the last coordinate.
+     *
+     * `interpolate` controls interpolation between consecutive LineStrings
+     * within the MultiLineString. If `interpolate` is `true` the coordinates
+     * will be linearly interpolated between the last coordinate of one LineString
+     * and the first coordinate of the next LineString.  If `interpolate` is
+     * `false` then the function will return `null` for Ms falling between
+     * LineStrings.
+     *
+     * @param {number} m M.
+     * @param {boolean} [extrapolate] Extrapolate. Default is `false`.
+     * @param {boolean} [interpolate] Interpolate. Default is `false`.
+     * @return {import("../coordinate.js").Coordinate|null} Coordinate.
+     * @api
+     */
+    getCoordinateAtM(m: number, extrapolate?: boolean | undefined, interpolate?: boolean | undefined): import("../coordinate.js").Coordinate | null;
+    /**
+     * Return the coordinates of the multilinestring.
+     * @return {Array<Array<import("../coordinate.js").Coordinate>>} Coordinates.
+     * @api
+     * @override
+     */
+    override getCoordinates(): Array<Array<import("../coordinate.js").Coordinate>>;
+    /**
+     * @return {Array<number>} Ends.
+     */
+    getEnds(): Array<number>;
+    /**
+     * Return the linestring at the specified index.
+     * @param {number} index Index.
+     * @return {LineString} LineString.
+     * @api
+     */
+    getLineString(index: number): LineString;
+    /**
+     * Return the linestrings of this multilinestring.
+     * @return {Array<LineString>} LineStrings.
+     * @api
+     */
+    getLineStrings(): Array<LineString>;
+    /**
+     * @return {Array<number>} Flat midpoints.
+     */
+    getFlatMidpoints(): Array<number>;
+    /**
+     * @param {number} squaredTolerance Squared tolerance.
+     * @return {MultiLineString} Simplified MultiLineString.
+     * @protected
+     * @override
+     */
+    protected override getSimplifiedGeometryInternal(squaredTolerance: number): MultiLineString;
+    /**
+     * Set the coordinates of the multilinestring.
+     * @param {!Array<Array<import("../coordinate.js").Coordinate>>} coordinates Coordinates.
+     * @param {import("./Geometry.js").GeometryLayout} [layout] Layout.
+     * @api
+     * @override
+     */
+    override setCoordinates(coordinates: Array<Array<import("../coordinate.js").Coordinate>>, layout?: import("./Geometry.js").GeometryLayout | undefined): void;
+}
+import SimpleGeometry from './SimpleGeometry.js';
+import LineString from './LineString.js';
+//# sourceMappingURL=MultiLineString.d.ts.map
---
+++ ajax/libs/openlayers/10.2.2-dev.1731074894672/source/TileJSON.d.ts
@@ -0,0 +1,217 @@
+export default TileJSON;
+export type Config = {
+    /**
+     * The name.
+     */
+    name?: string | undefined;
+    /**
+     * The description.
+     */
+    description?: string | undefined;
+    /**
+     * The version.
+     */
+    version?: string | undefined;
+    /**
+     * The attribution.
+     */
+    attribution?: string | undefined;
+    /**
+     * The template.
+     */
+    template?: string | undefined;
+    /**
+     * The legend.
+     */
+    legend?: string | undefined;
+    /**
+     * The scheme.
+     */
+    scheme?: string | undefined;
+    /**
+     * The tile URL templates.
+     */
+    tiles: Array<string>;
+    /**
+     * Optional grids.
+     */
+    grids?: string[] | undefined;
+    /**
+     * Minimum zoom level.
+     */
+    minzoom?: number | undefined;
+    /**
+     * Maximum zoom level.
+     */
+    maxzoom?: number | undefined;
+    /**
+     * Optional bounds.
+     */
+    bounds?: number[] | undefined;
+    /**
+     * Optional center.
+     */
+    center?: number[] | undefined;
+};
+export type Options = {
+    /**
+     * Attributions.
+     */
+    attributions?: import("./Source.js").AttributionLike | undefined;
+    /**
+     * Deprecated.  Use the cacheSize option on the layer instead.
+     */
+    cacheSize?: number | undefined;
+    /**
+     * The `crossOrigin` attribute for loaded images.  Note that
+     * you must provide a `crossOrigin` value if you want to access pixel data with the Canvas renderer.
+     * See https://developer.mozilla.org/en-US/docs/Web/HTML/CORS_enabled_image for more detail.
+     */
+    crossOrigin?: string | null | undefined;
+    /**
+     * Use interpolated values when resampling.  By default,
+     * linear interpolation is used when resampling.  Set to false to use the nearest neighbor instead.
+     */
+    interpolate?: boolean | undefined;
+    /**
+     * Use JSONP with callback to load the TileJSON.
+     * Useful when the server does not support CORS..
+     */
+    jsonp?: boolean | undefined;
+    /**
+     * Maximum allowed reprojection error (in pixels).
+     * Higher values can increase reprojection performance, but decrease precision.
+     */
+    reprojectionErrorThreshold?: number | undefined;
+    /**
+     * TileJSON configuration for this source.
+     * If not provided, `url` must be configured.
+     */
+    tileJSON?: Config | undefined;
+    /**
+     * Optional function to load a tile given a URL. The default is
+     * ```js
+     * function(imageTile, src) {
+     * imageTile.getImage().src = src;
+     * };
+     * ```
+     */
+    tileLoadFunction?: import("../Tile.js").LoadFunction | undefined;
+    /**
+     * The tile size used by the tile service.
+     * Note: `tileSize` and other non-standard TileJSON properties are currently ignored.
+     */
+    tileSize?: number | import("../size.js").Size | undefined;
+    /**
+     * URL to the TileJSON file. If not provided, `tileJSON` must be configured.
+     */
+    url?: string | undefined;
+    /**
+     * Whether to wrap the world horizontally.
+     */
+    wrapX?: boolean | undefined;
+    /**
+     * Duration of the opacity transition for rendering.
+     * To disable the opacity transition, pass `transition: 0`.
+     */
+    transition?: number | undefined;
+    /**
+     * Choose whether to use tiles with a higher or lower zoom level when between integer
+     * zoom levels. See {@link module :ol/tilegrid/TileGrid~TileGrid#getZForResolution}.
+     */
+    zDirection?: number | import("../array.js").NearestDirectionFunction | undefined;
+};
+/**
+ * @typedef {Object} Config
+ * @property {string} [name] The name.
+ * @property {string} [description] The description.
+ * @property {string} [version] The version.
+ * @property {string} [attribution] The attribution.
+ * @property {string} [template] The template.
+ * @property {string} [legend] The legend.
+ * @property {string} [scheme] The scheme.
+ * @property {Array<string>} tiles The tile URL templates.
+ * @property {Array<string>} [grids] Optional grids.
+ * @property {number} [minzoom] Minimum zoom level.
+ * @property {number} [maxzoom] Maximum zoom level.
+ * @property {Array<number>} [bounds] Optional bounds.
+ * @property {Array<number>} [center] Optional center.
+ */
+/**
+ * @typedef {Object} Options
+ * @property {import("./Source.js").AttributionLike} [attributions] Attributions.
+ * @property {number} [cacheSize] Deprecated.  Use the cacheSize option on the layer instead.
+ * @property {null|string} [crossOrigin] The `crossOrigin` attribute for loaded images.  Note that
+ * you must provide a `crossOrigin` value if you want to access pixel data with the Canvas renderer.
+ * See https://developer.mozilla.org/en-US/docs/Web/HTML/CORS_enabled_image for more detail.
+ * @property {boolean} [interpolate=true] Use interpolated values when resampling.  By default,
+ * linear interpolation is used when resampling.  Set to false to use the nearest neighbor instead.
+ * @property {boolean} [jsonp=false] Use JSONP with callback to load the TileJSON.
+ * Useful when the server does not support CORS..
+ * @property {number} [reprojectionErrorThreshold=0.5] Maximum allowed reprojection error (in pixels).
+ * Higher values can increase reprojection performance, but decrease precision.
+ * @property {Config} [tileJSON] TileJSON configuration for this source.
+ * If not provided, `url` must be configured.
+ * @property {import("../Tile.js").LoadFunction} [tileLoadFunction] Optional function to load a tile given a URL. The default is
+ * ```js
+ * function(imageTile, src) {
+ *   imageTile.getImage().src = src;
+ * };
+ * ```
+ * @property {number|import("../size.js").Size} [tileSize=[256, 256]] The tile size used by the tile service.
+ * Note: `tileSize` and other non-standard TileJSON properties are currently ignored.
+ * @property {string} [url] URL to the TileJSON file. If not provided, `tileJSON` must be configured.
+ * @property {boolean} [wrapX=true] Whether to wrap the world horizontally.
+ * @property {number} [transition] Duration of the opacity transition for rendering.
+ * To disable the opacity transition, pass `transition: 0`.
+ * @property {number|import("../array.js").NearestDirectionFunction} [zDirection=0]
+ * Choose whether to use tiles with a higher or lower zoom level when between integer
+ * zoom levels. See {@link module:ol/tilegrid/TileGrid~TileGrid#getZForResolution}.
+ */
+/**
+ * @classdesc
+ * Layer source for tile data in TileJSON format.
+ * @api
+ */
+declare class TileJSON extends TileImage {
+    /**
+     * @param {Options} options TileJSON options.
+     */
+    constructor(options: Options);
+    /**
+     * @type {Config}
+     * @private
+     */
+    private tileJSON_;
+    /**
+     * @type {number|import("../size.js").Size}
+     * @private
+     */
+    private tileSize_;
+    /**
+     * @private
+     * @param {Event} event The load event.
+     */
+    private onXHRLoad_;
+    /**
+     * @private
+     * @param {Event} event The error event.
+     */
+    private onXHRError_;
+    /**
+     * @return {Config} The tilejson object.
+     * @api
+     */
+    getTileJSON(): Config;
+    /**
+     * @protected
+     * @param {Config} tileJSON Tile JSON.
+     */
+    protected handleTileJSONResponse(tileJSON: Config): void;
+    /**
+     * @protected
+     */
+    protected handleTileJSONError(): void;
+}
+import TileImage from './TileImage.js';
+//# sourceMappingURL=TileJSON.d.ts.map
