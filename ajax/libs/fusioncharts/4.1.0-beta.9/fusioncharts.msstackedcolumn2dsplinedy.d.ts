5b7b9f6f583d3d7804ae3bd7106825564d87d7f4
robocdnjs <cdnjs-github@cloudflare.com>
2025-03-24 08:56:30 +0000

    Add fusioncharts (4.1.0-beta.9)

---
+++ ajax/libs/fusioncharts/4.1.0-beta.9/fusioncharts.msstackedcolumn2dsplinedy.d.ts
@@ -0,0 +1,8 @@
+
+import { FusionChartStatic } from 'fusioncharts';
+
+declare namespace Msstackedcolumn2dsplinedy {}
+declare var Msstackedcolumn2dsplinedy: (H: FusionChartStatic) => FusionChartStatic;
+export = Msstackedcolumn2dsplinedy;
+export as namespace Msstackedcolumn2dsplinedy;
+
