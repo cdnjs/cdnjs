19360cb572a1acd57e5bffdf2138dc614af9fb63
robocdnjs <cdnjs-github@cloudflare.com>
2025-03-25 08:15:31 +0000

    Add openlayers (10.2.2-dev.1731986043112)

---
+++ ajax/libs/openlayers/10.2.2-dev.1731986043112/proj/projections.d.ts
@@ -0,0 +1,17 @@
+/**
+ * Clear the projections cache.
+ */
+export function clear(): void;
+/**
+ * Get a cached projection by code.
+ * @param {string} code The code for the projection.
+ * @return {import("./Projection.js").default|null} The projection (if cached).
+ */
+export function get(code: string): import("./Projection.js").default | null;
+/**
+ * Add a projection to the cache.
+ * @param {string} code The projection code.
+ * @param {import("./Projection.js").default} projection The projection to cache.
+ */
+export function add(code: string, projection: import("./Projection.js").default): void;
+//# sourceMappingURL=projections.d.ts.map
