228e09714f7c3589019435b293d4e1529507ad45
robocdnjs <cdnjs-github@cloudflare.com>
2025-03-25 09:57:15 +0000

    Add fusioncharts (4.1.2)

---
+++ ajax/libs/fusioncharts/4.1.2/fusioncharts.maps.d.ts
@@ -0,0 +1,8 @@
+
+import { FusionChartStatic } from 'fusioncharts';
+
+declare namespace Maps {}
+declare var Maps: (H: FusionChartStatic) => FusionChartStatic;
+export = Maps;
+export as namespace Maps;
+
---
+++ ajax/libs/fusioncharts/4.1.2/fusioncharts.timeseries.d.ts
@@ -0,0 +1,8 @@
+
+import { FusionChartStatic } from 'fusioncharts';
+
+declare namespace Timeseries {}
+declare var Timeseries: (H: FusionChartStatic) => FusionChartStatic;
+export = Timeseries;
+export as namespace Timeseries;
+
