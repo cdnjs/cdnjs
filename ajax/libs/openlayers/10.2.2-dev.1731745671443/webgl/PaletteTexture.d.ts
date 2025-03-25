07674df537199415e3c33adf7f529a1439bc1aea
robocdnjs <cdnjs-github@cloudflare.com>
2025-03-25 05:48:02 +0000

    Add openlayers (10.2.2-dev.1731745671443)

---
+++ ajax/libs/openlayers/10.2.2-dev.1731745671443/webgl/PaletteTexture.d.ts
@@ -0,0 +1,28 @@
+export default PaletteTexture;
+/**
+ * @module ol/webgl/PaletteTexture
+ */
+declare class PaletteTexture {
+    /**
+     * @param {string} name The name of the texture.
+     * @param {Uint8Array} data The texture data.
+     */
+    constructor(name: string, data: Uint8Array);
+    name: string;
+    data: Uint8Array;
+    /**
+     * @type {WebGLTexture|null}
+     * @private
+     */
+    private texture_;
+    /**
+     * @param {WebGLRenderingContext} gl Rendering context.
+     * @return {WebGLTexture} The texture.
+     */
+    getTexture(gl: WebGLRenderingContext): WebGLTexture;
+    /**
+     * @param {WebGLRenderingContext} gl Rendering context.
+     */
+    delete(gl: WebGLRenderingContext): void;
+}
+//# sourceMappingURL=PaletteTexture.d.ts.map
