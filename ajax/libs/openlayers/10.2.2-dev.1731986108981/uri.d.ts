f6c97b44ddc90b76290d3bd99f5e99972df5f137
robocdnjs <cdnjs-github@cloudflare.com>
2025-03-25 08:20:12 +0000

    Add openlayers (10.2.2-dev.1731986108981)

---
+++ ajax/libs/openlayers/10.2.2-dev.1731986108981/uri.d.ts
@@ -0,0 +1,33 @@
+/**
+ * Appends query parameters to a URI.
+ *
+ * @param {string} uri The original URI, which may already have query data.
+ * @param {!Object} params An object where keys are URI-encoded parameter keys,
+ *     and the values are arbitrary types or arrays.
+ * @return {string} The new URI.
+ */
+export function appendParams(uri: string, params: any): string;
+/**
+ * @param {string} template The URL template.  Should have `{x}`, `{y}`, and `{z}` placeholders.  If
+ * the template has a `{-y}` placeholder, the `maxY` parameter must be supplied.
+ * @param {number} z The tile z coordinate.
+ * @param {number} x The tile x coordinate.
+ * @param {number} y The tile y coordinate.
+ * @param {number} [maxY] The maximum y coordinate at the given z level.
+ * @return {string} The URL.
+ */
+export function renderXYZTemplate(template: string, z: number, x: number, y: number, maxY?: number | undefined): string;
+/**
+ * @param {Array<string>} urls List of URLs.
+ * @param {number} z The tile z coordinate.
+ * @param {number} x The tile x coordinate.
+ * @param {number} y The tile y coordinate.
+ * @return {string} The chosen URL.
+ */
+export function pickUrl(urls: Array<string>, z: number, x: number, y: number): string;
+/**
+ * @param {string} url URL.
+ * @return {Array<string>} Array of urls.
+ */
+export function expandUrl(url: string): Array<string>;
+//# sourceMappingURL=uri.d.ts.map
