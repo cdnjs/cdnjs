9a08cdcb982c072f1f2a40eb658a75e78522f3ec
robocdnjs <cdnjs-github@cloudflare.com>
2025-03-25 08:21:37 +0000

    Add hexo-theme-redefine (2.8.0)

---
+++ ajax/libs/hexo-theme-redefine/2.8.0/js/plugins/pangu.min.js
@@ -0,0 +1,1 @@
+function initPanguJS(){pangu.spacingElementByClassName("markdown-body"),pangu.autoSpacingPage()}document.addEventListener("DOMContentLoaded",initPanguJS);try{swup.hooks.on("page:view",initPanguJS)}catch(n){}
