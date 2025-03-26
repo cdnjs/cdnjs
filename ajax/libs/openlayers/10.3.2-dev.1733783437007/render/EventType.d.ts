51d17caa4d917abee5f26de5d66f26fa4c47e4b7
robocdnjs <cdnjs-github@cloudflare.com>
2025-03-26 22:42:30 +0000

    Add openlayers (10.3.2-dev.1733783437007)

---
+++ ajax/libs/openlayers/10.3.2-dev.1733783437007/render/EventType.d.ts
@@ -0,0 +1,11 @@
+declare namespace _default {
+    let PRERENDER: string;
+    let POSTRENDER: string;
+    let PRECOMPOSE: string;
+    let POSTCOMPOSE: string;
+    let RENDERCOMPLETE: string;
+}
+export default _default;
+export type MapRenderEventTypes = "postrender" | "precompose" | "postcompose" | "rendercomplete";
+export type LayerRenderEventTypes = "postrender" | "prerender";
+//# sourceMappingURL=EventType.d.ts.map
