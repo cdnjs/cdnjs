f786031fa027fa926d80e68a2dd6f78bc6b08e85
robocdnjs <cdnjs-github@cloudflare.com>
2025-03-26 12:50:00 +0000

    Add simplebar (6.3.0)

---
+++ ajax/libs/simplebar/6.3.0/index.d.ts
@@ -0,0 +1,12 @@
+import SimpleBarCore from 'simplebar-core';
+export default class SimpleBar extends SimpleBarCore {
+    static globalObserver: MutationObserver;
+    static instances: WeakMap<Node, SimpleBar>;
+    constructor(...args: ConstructorParameters<typeof SimpleBarCore>);
+    static initDOMLoadedElements(): void;
+    static removeObserver(): void;
+    initDOM(): void;
+    unMount(): void;
+    static initHtmlApi(): void;
+    static handleMutations(mutations: MutationRecord[]): void;
+}
