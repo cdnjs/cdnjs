83ea441f160be41320f80a4be51aa43462a9d479
robocdnjs <cdnjs-github@cloudflare.com>
2025-03-27 12:31:49 +0000

    Add react-dom (18.3.0-canary-1dba980e1f-20241220)

---
+++ ajax/libs/react-dom/18.3.0-canary-1dba980e1f-20241220/cjs/react-dom-server-rendering-stub.production.min.js
@@ -0,0 +1,16 @@
+/**
+ * @license React
+ * react-dom-server-rendering-stub.production.min.js
+ *
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+'use strict';var e={usingClientEntryPoint:!1,Events:null,Dispatcher:{current:null}};function f(b){for(var a="https://reactjs.org/docs/error-decoder.html?invariant="+b,c=1;c<arguments.length;c++)a+="&args[]="+encodeURIComponent(arguments[c]);return"Minified React error #"+b+"; visit "+a+" for the full message or use the non-minified dev environment for full errors and additional helpful warnings."}function g(b,a){if("font"===b)return"";if("string"===typeof a)return"use-credentials"===a?a:""}
+var k=e.Dispatcher;exports.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED=e;exports.createPortal=function(){throw Error(f(448));};exports.experimental_useFormState=function(){throw Error(f(248));};exports.experimental_useFormStatus=function(){throw Error(f(248));};exports.flushSync=function(){throw Error(f(449));};exports.preconnect=function(b,a){var c=k.current;c&&"string"===typeof b&&(a?(a=a.crossOrigin,a="string"===typeof a?"use-credentials"===a?a:"":void 0):a=null,c.preconnect(b,a))};
+exports.prefetchDNS=function(b){var a=k.current;a&&"string"===typeof b&&a.prefetchDNS(b)};
+exports.preinit=function(b,a){var c=k.current;if(c&&"string"===typeof b&&a&&"string"===typeof a.as){var d=a.as,h=g(d,a.crossOrigin),l="string"===typeof a.integrity?a.integrity:void 0,m="string"===typeof a.fetchPriority?a.fetchPriority:void 0;"style"===d?c.preinitStyle(b,"string"===typeof a.precedence?a.precedence:void 0,{crossOrigin:h,integrity:l,fetchPriority:m}):"script"===d&&c.preinitScript(b,{crossOrigin:h,integrity:l,fetchPriority:m,nonce:"string"===typeof a.nonce?a.nonce:void 0})}};
+exports.preinitModule=function(b,a){var c=k.current;if(c&&"string"===typeof b)if("object"===typeof a&&null!==a){if(null==a.as||"script"===a.as){var d=g(a.as,a.crossOrigin);c.preinitModuleScript(b,{crossOrigin:d,integrity:"string"===typeof a.integrity?a.integrity:void 0,nonce:"string"===typeof a.nonce?a.nonce:void 0})}}else null==a&&c.preinitModuleScript(b)};
+exports.preload=function(b,a){var c=k.current;if(c&&"string"===typeof b&&"object"===typeof a&&null!==a&&"string"===typeof a.as){var d=a.as,h=g(d,a.crossOrigin);c.preload(b,d,{crossOrigin:h,integrity:"string"===typeof a.integrity?a.integrity:void 0,nonce:"string"===typeof a.nonce?a.nonce:void 0,type:"string"===typeof a.type?a.type:void 0,fetchPriority:"string"===typeof a.fetchPriority?a.fetchPriority:void 0,referrerPolicy:"string"===typeof a.referrerPolicy?a.referrerPolicy:void 0,imageSrcSet:"string"===
+typeof a.imageSrcSet?a.imageSrcSet:void 0,imageSizes:"string"===typeof a.imageSizes?a.imageSizes:void 0})}};exports.preloadModule=function(b,a){var c=k.current;if(c&&"string"===typeof b)if(a){var d=g(a.as,a.crossOrigin);c.preloadModule(b,{as:"string"===typeof a.as&&"script"!==a.as?a.as:void 0,crossOrigin:d,integrity:"string"===typeof a.integrity?a.integrity:void 0})}else c.preloadModule(b)};exports.unstable_batchedUpdates=function(b,a){return b(a)};exports.version="18.3.0-canary-1dba980e1f-20241220";
