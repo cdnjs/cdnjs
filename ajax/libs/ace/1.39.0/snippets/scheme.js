643693b514f782e3d1c85b548226ee2fed7b5d1e
robocdnjs <cdnjs-github@cloudflare.com>
2025-04-05 15:08:38 +0000

    Add ace (1.39.0)

---
+++ ajax/libs/ace/1.39.0/snippets/scheme.js
@@ -0,0 +1,8 @@
+;                (function() {
+                    window.require(["ace/snippets/scheme"], function(m) {
+                        if (typeof module == "object" && typeof exports == "object" && module) {
+                            module.exports = m;
+                        }
+                    });
+                })();
+            
