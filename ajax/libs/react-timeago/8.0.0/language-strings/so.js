13e19f3111b740972e87b20c468a3e395556c570
robocdnjs <cdnjs-github@cloudflare.com>
2025-04-05 12:21:06 +0000

    Add react-timeago (8.0.0)

---
+++ ajax/libs/react-timeago/8.0.0/language-strings/so.js
@@ -0,0 +1,25 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+var strings = {
+  prefixAgo: null,
+  prefixFromNow: null,
+  suffixAgo: 'kahor',
+  suffixFromNow: 'kadib',
+  seconds: 'sikino',
+  minute: 'daqiiqad',
+  minutes: '%d daqiiqo',
+  hour: 'saac',
+  hours: '%d saacadood',
+  day: 'maalin',
+  days: '%d maalmood',
+  month: 'bil',
+  months: '%d bilood',
+  year: 'sano',
+  years: '%d sano',
+  wordSeparator: ' '
+};
+var _default = exports["default"] = strings;
