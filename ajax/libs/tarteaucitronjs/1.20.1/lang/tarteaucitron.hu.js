ea9c513b5317e1680ae793ba4b8f37a060fad36a
robocdnjs <cdnjs-github@cloudflare.com>
2025-06-17 03:03:56 +0000

    Add tarteaucitronjs (1.20.1)

---
+++ ajax/libs/tarteaucitronjs/1.20.1/lang/tarteaucitron.hu.js
@@ -0,0 +1,95 @@
+/*global tarteaucitron */
+/* min ready */
+tarteaucitron.lang = {
+    "middleBarHead": "☝ 🍪",
+    "adblock": "Szia! Ez a webhely átlátható, és lehetővé teszi a kívánt harmadik fél szolgáltatásainak kiválasztását",
+    "adblock_call": "A testreszabás megkezdéséhez állítsd le az adblockert, kérlek.",
+    "reload": "Oldal frissítése",
+    
+    "alertBigScroll": "A görgetés folytatásával,",
+    "alertBigClick": "Ha folytatod a böngészést ezen oldalon,",
+    "alertBig": "engedélyezed a harmadik fél összes szolgáltatását.",
+    
+    "alertBigPrivacy": "A webhely tartalmának megjelenítéséhez és a felhasználói élmény javításához cookie-kat használunk",
+    "alertSmall": "Szolgáltatások kezelése",
+    "personalize": "Beállítások",
+    "acceptAll": "OK, elfogadom",
+    "close": "Bezár",
+    "closeBanner": "Cookie-szalag elrejtése",
+
+    "privacyUrl": "Adatvédelmi irányelvek",
+    
+    "all": "Összes szolgáltatás előnyben részesítése",
+
+    "info": "Személyi adataid védelme",
+    "disclaimer": "A harmadik fél szolgáltatásainak engedélyezésével elfogadja a sütiket és a megfelelő működésükhöz szükséges nyomkövetési technológiák használatát.",
+    "allow": "Elfogadom",
+    "deny": "Elutasítom",
+    "noCookie": "Ez a szolgáltatás nem használ sütit.",
+    "useCookie": "Ez a szolgáltatás telepíthető",
+    "useCookieCurrent": "Ez a szolgáltatás telepített",
+    "useNoCookie": "Ez a szolgáltatás nem telepített sütiket",
+    "more": "Olvass többet",
+    "source": "Tekintsd meg a hivatalos weboldalt",
+    "credit": "Cookie-kezelő: tarteaucitron.js",
+    "noServices": "Ez a weboldal nem használ olyan sütiket, amelyekhez a beleegyezésed szükséges.",
+
+    "toggleInfoBox": "Információk megjelenítése / elrejtése a süti-tárolással kapcsolatban",
+    "title": "Süti preferenciák",
+    "cookieDetail": "Süti adatok a következőhöz:",
+    "ourSite": "weboldalunkon",
+    "modalWindow": "(modális ablak)",
+    "newWindow": "(új ablak)",
+    "allowAll": "Elfogadom az öszeset",
+    "denyAll": "Elutasítom",
+
+    "icon": "Cookies",
+    
+    "fallback": "letiltott.",
+    "allowed": "megengedett",
+    "disallowed": "nem engedélyezett",
+
+    "ads": {
+        "title": "Reklámhálózat",
+        "details": "A hirdetési hálózatok bevételt teremthetnek azáltal, hogy értékesítik a webhelyen található hirdetési felületet"
+    },
+    "analytic": {
+        "title": "Közönségmérés",
+        "details": "A közönségmérési szolgáltatások hasznos statisztikai adatokat generáltak a webhely fejlesztése érdekében."
+    },
+    "social": {
+        "title": "Közösségi hálózatok",
+        "details": "A közösségi hálózatok javíthatják a webhely használhatóságát, és elősegíthetik annak promoválását a megosztások révén."
+    },
+    "video": {
+        "title": "Videók",
+        "details": "A videomegosztó szolgáltatások hozzájárulnak hasznos multimédiához a webhelyen és növelik annak láthatóságát."
+    },
+    "comment": {
+        "title": "Kommentek",
+        "details": "A megjegyzésfigyelők megkönnyítik a megjegyzések kitöltését és a spam elleni küzdelmet."
+    },
+    "support": {
+        "title": "Támogatás",
+        "details": "A támogatási szolgáltatások lehetővé teszik, hogy kapcsolatba lépjen a webhely csapatával, és segítsen annak fejlesztésében."
+    },
+    "api": {
+        "title": "APIk",
+        "details": "Az API-kat a szkriptek betöltésére használják: földrajzi helymeghatározás, keresőmotorok, fordítások..."
+    },
+    "other": {
+        "title": "Más",
+        "details": "Szolgáltatások webtartalom megjelenítésére."
+    },
+
+    "google": {
+        "title": "Speciális hozzájárulás a Google szolgáltatásaihoz",
+        "details": "A Google használhatja az adatait közönségmérésre, reklámhatékonyságra, vagy személyre szabott hirdetések megjelenítésére."
+    },
+    
+    "mandatoryTitle": "Kötelező sütik",
+    "mandatoryText": "A webhely tartalmának megjelenítéséhez és a felhasználói bejelentkezéshez sütiket használunk amiket nem lehet kikapcsolni.",
+
+    "save": "Mentés",
+    "ourpartners": "Partnereink"
+};
