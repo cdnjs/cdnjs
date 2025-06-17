d5b8b7813b1b033c3ced258b9c064f6ff1ed0edf
robocdnjs <cdnjs-github@cloudflare.com>
2025-06-17 12:19:54 +0000

    Add xsound (4.0.3)

---
+++ ajax/libs/xsound/4.0.3/types/SoundModule/Effectors/AudioWorkletProcessors/VocalCancelerProcessor.d.ts
@@ -0,0 +1,15 @@
+import type { Inputs, Outputs } from '../../../worklet';
+import { AudioWorkletProcessor } from '../../../worklet';
+/**
+ * This class extends `AudioWorkletProcessor`.
+ * Override `process` method for vocal canceler and Update parameters on message event.
+ */
+export declare class VocalCancelerProcessor extends AudioWorkletProcessor {
+    private instance;
+    private depth;
+    private isActive;
+    constructor();
+    /** @override */
+    protected process(inputs: Inputs, outputs: Outputs): boolean;
+}
+//# sourceMappingURL=VocalCancelerProcessor.d.ts.map
