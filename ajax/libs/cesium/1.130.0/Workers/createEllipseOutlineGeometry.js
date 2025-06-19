eaa53b868134a094310155102c71a1d710801e4b
robocdnjs <cdnjs-github@cloudflare.com>
2025-06-19 21:29:33 +0000

    Add cesium (1.130.0)

---
+++ ajax/libs/cesium/1.130.0/Workers/createEllipseOutlineGeometry.js
@@ -0,0 +1,26 @@
+/**
+ * @license
+ * Cesium - https://github.com/CesiumGS/cesium
+ * Version 1.130
+ *
+ * Copyright 2011-2022 Cesium Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Columbus View (Pat. Pend.)
+ *
+ * Portions licensed separately.
+ * See https://github.com/CesiumGS/cesium/blob/main/LICENSE.md for full licensing details.
+ */
+
+import{a as i}from"./chunk-WJ2PDGYW.js";import"./chunk-ESSNY5GC.js";import"./chunk-64C3WG2L.js";import"./chunk-SFW65PGL.js";import"./chunk-BTGP76WP.js";import"./chunk-YQL7OPIW.js";import"./chunk-DPQZPDIL.js";import"./chunk-YJEV67VS.js";import"./chunk-4BTDTSQG.js";import{a as t,d as c}from"./chunk-XFVXIGTK.js";import"./chunk-MPRZ6V46.js";import"./chunk-FO77LMQ7.js";import"./chunk-TYEGOJMY.js";import"./chunk-VZMWSUG7.js";import{e as o}from"./chunk-362ETHZ6.js";function a(r,n){return o(n)&&(r=i.unpack(r,n)),r._center=t.clone(r._center),r._ellipsoid=c.clone(r._ellipsoid),i.createGeometry(r)}var p=a;export{p as default};
---
+++ ajax/libs/cesium/1.130.0/Workers/createEllipsoidOutlineGeometry.js
@@ -0,0 +1,26 @@
+/**
+ * @license
+ * Cesium - https://github.com/CesiumGS/cesium
+ * Version 1.130
+ *
+ * Copyright 2011-2022 Cesium Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Columbus View (Pat. Pend.)
+ *
+ * Portions licensed separately.
+ * See https://github.com/CesiumGS/cesium/blob/main/LICENSE.md for full licensing details.
+ */
+
+import{a as t}from"./chunk-FCEUFRFD.js";import"./chunk-64C3WG2L.js";import"./chunk-SFW65PGL.js";import"./chunk-BTGP76WP.js";import"./chunk-YQL7OPIW.js";import"./chunk-DPQZPDIL.js";import"./chunk-YJEV67VS.js";import"./chunk-4BTDTSQG.js";import"./chunk-XFVXIGTK.js";import"./chunk-MPRZ6V46.js";import"./chunk-FO77LMQ7.js";import"./chunk-TYEGOJMY.js";import"./chunk-VZMWSUG7.js";import{e as n}from"./chunk-362ETHZ6.js";function u(r,e){return n(r.buffer,e)&&(r=t.unpack(r,e)),t.createGeometry(r)}var a=u;export{a as default};
