8b7fd209bc4c10010c1d6902e28f3ec33bc71a0a
robocdnjs <cdnjs-github@cloudflare.com>
2025-06-20 00:07:55 +0000

    Add temporal-polyfill (0.2.0)

---
+++ ajax/libs/temporal-polyfill/0.2.0/global.esm.js
@@ -0,0 +1,9 @@
+import { c as t, T as a, D as e, t as s } from "./chunk-33169ef4.esm.js";
+
+Object.defineProperties(globalThis, t({
+  Temporal: a
+})), Object.defineProperties(Intl, t({
+  DateTimeFormat: e
+})), Object.defineProperties(Date.prototype, t({
+  toTemporalInstant: s
+}));
