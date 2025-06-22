b0850611f3f1f85efd609b078f177ac4d17c7ad1
robocdnjs <cdnjs-github@cloudflare.com>
2025-06-22 02:14:14 +0000

    Add fusioncharts (4.1.3-beta.2)

---
+++ ajax/libs/fusioncharts/4.1.3-beta.2/fusioncharts.zoomline.d.ts
@@ -0,0 +1,8 @@
+
+import { FusionChartStatic } from 'fusioncharts';
+
+declare namespace Zoomline {}
+declare var Zoomline: (H: FusionChartStatic) => FusionChartStatic;
+export = Zoomline;
+export as namespace Zoomline;
+
