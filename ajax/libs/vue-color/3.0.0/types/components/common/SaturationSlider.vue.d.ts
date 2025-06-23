a49de151b4acc67354b075c3e49627c663fa4b1c
robocdnjs <cdnjs-github@cloudflare.com>
2025-06-23 21:25:11 +0000

    Add vue-color (3.0.0)

---
+++ ajax/libs/vue-color/3.0.0/types/components/common/SaturationSlider.vue.d.ts
@@ -0,0 +1,12 @@
+import { type useTinyColorModelProps } from '../../composable/colorModel.ts';
+type Props = {
+    /** Use this hue value to render background first.
+     * Second priority is the hue value from `v-model` or `v-model:tineColor`.
+     * */
+    hue?: number;
+};
+type __VLS_Props = Props & useTinyColorModelProps;
+declare const _default: import("vue").DefineComponent<__VLS_Props, {}, {}, {}, {}, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, {}, string, import("vue").PublicProps, Readonly<__VLS_Props> & Readonly<{}>, {}, {}, {}, {}, string, import("vue").ComponentProvideOptions, false, {
+    container: HTMLDivElement;
+}, HTMLDivElement>;
+export default _default;
