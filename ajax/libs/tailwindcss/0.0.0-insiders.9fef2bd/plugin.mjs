619fbbee9b448c58c9cc8edc004ae207d6463eff
robocdnjs <cdnjs-github@cloudflare.com>
2025-06-26 17:08:33 +0000

    Add tailwindcss (0.0.0-insiders.9fef2bd)

---
+++ ajax/libs/tailwindcss/0.0.0-insiders.9fef2bd/plugin.mjs
@@ -0,0 +1,1 @@
+function u(n,i){return{handler:n,config:i}}u.withOptions=function(n,i=()=>({})){function t(o){return{handler:n(o),config:i(o)}}return t.__isOptionsFunction=!0,t};var g=u;export{g as default};
