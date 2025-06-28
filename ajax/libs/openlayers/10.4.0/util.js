8ecbc90001faa1464c90390b924674de64edd0fe
robocdnjs <cdnjs-github@cloudflare.com>
2025-06-28 09:30:34 +0000

    Add openlayers (10.4.0)

---
+++ ajax/libs/openlayers/10.4.0/util.js
@@ -0,0 +1,36 @@
+/**
+ * @module ol/util
+ */
+
+/**
+ * @return {never} Any return.
+ */
+export function abstract() {
+  throw new Error('Unimplemented abstract method.');
+}
+
+/**
+ * Counter for getUid.
+ * @type {number}
+ * @private
+ */
+let uidCounter_ = 0;
+
+/**
+ * Gets a unique ID for an object. This mutates the object so that further calls
+ * with the same object as a parameter returns the same value. Unique IDs are generated
+ * as a strictly increasing sequence. Adapted from goog.getUid.
+ *
+ * @param {Object} obj The object to get the unique ID for.
+ * @return {string} The unique ID for the object.
+ * @api
+ */
+export function getUid(obj) {
+  return obj.ol_uid || (obj.ol_uid = String(++uidCounter_));
+}
+
+/**
+ * OpenLayers version.
+ * @type {string}
+ */
+export const VERSION = '10.4.0';
