4682e706380052c98be39bc77d4caa436e6792fe
robocdnjs <cdnjs-github@cloudflare.com>
2025-06-30 01:22:07 +0000

    Add openlayers (10.4.1-dev.1738579243620)

---
+++ ajax/libs/openlayers/10.4.1-dev.1738579243620/layer.d.ts
@@ -0,0 +1,13 @@
+export { default as Graticule } from "./layer/Graticule.js";
+export { default as Group } from "./layer/Group.js";
+export { default as Heatmap } from "./layer/Heatmap.js";
+export { default as Image } from "./layer/Image.js";
+export { default as Layer } from "./layer/Layer.js";
+export { default as Tile } from "./layer/Tile.js";
+export { default as Vector } from "./layer/Vector.js";
+export { default as VectorImage } from "./layer/VectorImage.js";
+export { default as VectorTile } from "./layer/VectorTile.js";
+export { default as WebGLPoints } from "./layer/WebGLPoints.js";
+export { default as WebGLTile } from "./layer/WebGLTile.js";
+export { default as WebGLVector } from "./layer/WebGLVector.js";
+//# sourceMappingURL=layer.d.ts.map
