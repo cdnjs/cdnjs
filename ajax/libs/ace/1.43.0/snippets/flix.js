3109aa787bcc49713330cc7e4617a223d693c0ad
robocdnjs <cdnjs-github@cloudflare.com>
2025-06-30 23:32:44 +0000

    Add ace (1.43.0)

---
+++ ajax/libs/ace/1.43.0/snippets/flix.js
@@ -0,0 +1,8 @@
+;                (function() {
+                    window.require(["ace/snippets/flix"], function(m) {
+                        if (typeof module == "object" && typeof exports == "object" && module) {
+                            module.exports = m;
+                        }
+                    });
+                })();
+            
---
+++ ajax/libs/ace/1.43.0/snippets/html_ruby.js
@@ -0,0 +1,8 @@
+;                (function() {
+                    window.require(["ace/snippets/html_ruby"], function(m) {
+                        if (typeof module == "object" && typeof exports == "object" && module) {
+                            module.exports = m;
+                        }
+                    });
+                })();
+            
