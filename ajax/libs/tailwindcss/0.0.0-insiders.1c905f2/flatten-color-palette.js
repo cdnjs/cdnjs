b050d82cc0ab5e40a5dcd2483f0d99c89ee691c4
robocdnjs <cdnjs-github@cloudflare.com>
2025-07-01 05:38:16 +0000

    Add tailwindcss (0.0.0-insiders.1c905f2)

---
+++ ajax/libs/tailwindcss/0.0.0-insiders.1c905f2/flatten-color-palette.js
@@ -0,0 +1,1 @@
+"use strict";function s(r){let n={};for(let[e,t]of Object.entries(r??{}))if(e!=="__CSS_VALUES__")if(typeof t=="object"&&t!==null)for(let[i,l]of Object.entries(s(t)))n[`${e}${i==="DEFAULT"?"":`-${i}`}`]=l;else n[e]=t;if("__CSS_VALUES__"in r)for(let[e,t]of Object.entries(r.__CSS_VALUES__))Number(t)&4||(n[e]=r[e]);return n}module.exports=s;
