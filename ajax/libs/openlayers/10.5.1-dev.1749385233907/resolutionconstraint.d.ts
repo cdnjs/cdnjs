5a8be5d9a03f416adb33a6c92d6378684f964c0a
robocdnjs <cdnjs-github@cloudflare.com>
2025-07-02 14:49:02 +0000

    Add openlayers (10.5.1-dev.1749385233907)

---
+++ ajax/libs/openlayers/10.5.1-dev.1749385233907/resolutionconstraint.d.ts
@@ -0,0 +1,29 @@
+/**
+ * @param {Array<number>} resolutions Resolutions.
+ * @param {boolean} [smooth] If true, the view will be able to slightly exceed resolution limits. Default: true.
+ * @param {import("./extent.js").Extent} [maxExtent] Maximum allowed extent.
+ * @param {boolean} [showFullExtent] If true, allows us to show the full extent. Default: false.
+ * @return {Type} Zoom function.
+ */
+export function createSnapToResolutions(resolutions: Array<number>, smooth?: boolean, maxExtent?: import("./extent.js").Extent, showFullExtent?: boolean): Type;
+/**
+ * @param {number} power Power.
+ * @param {number} maxResolution Maximum resolution.
+ * @param {number} [minResolution] Minimum resolution.
+ * @param {boolean} [smooth] If true, the view will be able to slightly exceed resolution limits. Default: true.
+ * @param {import("./extent.js").Extent} [maxExtent] Maximum allowed extent.
+ * @param {boolean} [showFullExtent] If true, allows us to show the full extent. Default: false.
+ * @return {Type} Zoom function.
+ */
+export function createSnapToPower(power: number, maxResolution: number, minResolution?: number, smooth?: boolean, maxExtent?: import("./extent.js").Extent, showFullExtent?: boolean): Type;
+/**
+ * @param {number} maxResolution Max resolution.
+ * @param {number} minResolution Min resolution.
+ * @param {boolean} [smooth] If true, the view will be able to slightly exceed resolution limits. Default: true.
+ * @param {import("./extent.js").Extent} [maxExtent] Maximum allowed extent.
+ * @param {boolean} [showFullExtent] If true, allows us to show the full extent. Default: false.
+ * @return {Type} Zoom function.
+ */
+export function createMinMaxResolution(maxResolution: number, minResolution: number, smooth?: boolean, maxExtent?: import("./extent.js").Extent, showFullExtent?: boolean): Type;
+export type Type = (arg0: (number | undefined), arg1: number, arg2: import("./size.js").Size, arg3: boolean | undefined) => (number | undefined);
+//# sourceMappingURL=resolutionconstraint.d.ts.map
