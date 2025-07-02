e43849f62608328b5597d1fe750cbadf6744a463
robocdnjs <cdnjs-github@cloudflare.com>
2025-07-02 09:20:54 +0000

    Add openlayers (10.5.1-dev.1747056116877)

---
+++ ajax/libs/openlayers/10.5.1-dev.1747056116877/MapProperty.d.ts
@@ -0,0 +1,8 @@
+declare namespace _default {
+    let LAYERGROUP: string;
+    let SIZE: string;
+    let TARGET: string;
+    let VIEW: string;
+}
+export default _default;
+//# sourceMappingURL=MapProperty.d.ts.map
