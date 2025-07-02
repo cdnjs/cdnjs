0f74adbc087f7c8a5eafaac4f72f0027a7115c38
robocdnjs <cdnjs-github@cloudflare.com>
2025-07-02 04:37:33 +0000

    Add openlayers (10.5.1-dev.1745329995795)

---
+++ ajax/libs/openlayers/10.5.1-dev.1745329995795/format/filter/IsNull.d.ts
@@ -0,0 +1,14 @@
+export default IsNull;
+/**
+ * @classdesc
+ * Represents a `<PropertyIsNull>` comparison operator.
+ * @api
+ */
+declare class IsNull extends Comparison {
+    /**
+     * @param {!string} propertyName Name of the context property to compare.
+     */
+    constructor(propertyName: string);
+}
+import Comparison from './Comparison.js';
+//# sourceMappingURL=IsNull.d.ts.map
