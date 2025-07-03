12ba9da75ced47d17f628d117aa1283cbcf5db98
robocdnjs <cdnjs-github@cloudflare.com>
2025-07-03 18:53:42 +0000

    Add ace (1.43.1)

---
+++ ajax/libs/ace/1.43.1/snippets/latte.js
@@ -0,0 +1,8 @@
+;                (function() {
+                    window.require(["ace/snippets/latte"], function(m) {
+                        if (typeof module == "object" && typeof exports == "object" && module) {
+                            module.exports = m;
+                        }
+                    });
+                })();
+            
