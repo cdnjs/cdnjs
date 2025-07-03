80acfbd50db547baa4ef0cce97515e4108d23680
robocdnjs <cdnjs-github@cloudflare.com>
2025-07-03 23:33:11 +0000

    Add tarteaucitronjs (1.22.0)

---
+++ ajax/libs/tarteaucitronjs/1.22.0/lang/tarteaucitron.sq.js
@@ -0,0 +1,95 @@
+/*global tarteaucitron */
+ /* min ready */
+ tarteaucitron.lang = {
+    "middleBarHead": "☝️ 🍪",
+    "adblock": "Përshëndetje! Kjo faqe është transparente dhe ju lejon të zgjidhni shërbimet e palëve të treta që dëshironi të lejoni.",
+    "adblock_call": "Ju lutemi çaktivizoni bllokuesin tuaj të reklamave për të filluar personalizimin.",
+    "reload": "Rifresko faqen",
+    
+    "alertBigScroll": "Duke vazhduar të lëvizni poshtë,",
+    "alertBigClick": "Nëse vazhdoni të shfletoni këtë faqe interneti,",
+    "alertBig": "ju po lejoni të gjitha shërbimet e palëve të treta",
+    
+    "alertBigPrivacy": "Kjo faqe përdor cookies dhe ju jep kontroll mbi atë që dëshironi të aktivizoni",
+    "alertSmall": "Menaxho shërbimet",
+    "personalize": "Personalizo",
+    "acceptAll": "OK, prano të gjitha",
+    "close": "Mbyll",
+    "closeBanner": "Fshih banerin e cookies",
+
+    "privacyUrl": "Politika e privatësisë",
+    
+    "all": "Preferencat për të gjitha shërbimet",
+
+    "info": "Mbrojtja e privatësisë suaj",
+    "disclaimer": "Duke lejuar këto shërbime të palëve të treta, ju pranoni cookies e tyre dhe përdorimin e teknologjive të gjurmimit të nevojshme për funksionimin e tyre të duhur.",
+    "allow": "Lejo",
+    "deny": "Refuzo",
+    "noCookie": "Ky shërbim nuk përdor cookie.",
+    "useCookie": "Ky shërbim mund të instalojë",
+    "useCookieCurrent": "Ky shërbim ka instaluar",
+    "useNoCookie": "Ky shërbim nuk ka instaluar asnjë cookie.",
+    "more": "Lexo më shumë",
+    "source": "Shiko faqen zyrtare",
+    "credit": "Menaxher i cookies nga tarteaucitron.js",
+    "noServices": "Kjo faqe nuk përdor asnjë cookie që kërkon pranimin tuaj.",
+
+    "toggleInfoBox": "Shfaq/fshih informacionet në lidhje me ruajtjen e cookies",
+    "title": "Paneli i menaxhimit të cookies",
+    "cookieDetail": "Detajet e cookies për",
+    "ourSite": "në faqen tonë",
+    "modalWindow": "(dritare modale)",
+    "newWindow": "(dritare e re)",
+    "allowAll": "Lejo të gjitha cookies",
+    "denyAll": "Refuzo të gjitha cookies",
+
+    "icon": "Cookies",
+    
+    "fallback": "është çaktivizuar.",
+    "allowed": "lejuar",
+    "disallowed": "nuk lejohet",
+
+    "ads": {
+        "title": "Rrjeti i reklamave",
+        "details": "Rrjetet e reklamave mund të gjenerojnë të ardhura duke shitur hapësirë reklamimi në faqe."
+    },
+    "analytic": {
+        "title": "Matja e audiencës",
+        "details": "Shërbimet e matjes së audiencës përdoren për të gjeneruar statistika të dobishme për të përmirësuar faqen."
+    },
+    "social": {
+        "title": "Rrjetet sociale",
+        "details": "Rrjetet sociale mund të përmirësojnë përdorshmërinë e faqes dhe të ndihmojnë në promovimin e saj përmes shpërndarjeve."
+    },
+    "video": {
+        "title": "Videot",
+        "details": "Shërbimet e ndarjes së videove ndihmojnë në shtimin e mediave të pasura në faqe dhe rrisin dukshmërinë e saj."
+    },
+    "comment": {
+        "title": "Komentet",
+        "details": "Menaxherët e komenteve lehtësojnë paraqitjen e komenteve dhe luftojnë kundër spam-it."
+    },
+    "support": {
+        "title": "Mbështetja",
+        "details": "Shërbimet e mbështetjes ju lejojnë të kontaktoni me ekipin e faqes dhe të ndihmoni në përmirësimin e saj."
+    },
+    "api": {
+        "title": "APIs",
+        "details": "APIs përdoren për të ngarkuar skripte: gjeolokalizim, motorë kërkimi, përkthime, ..."
+    },
+    "other": {
+        "title": "Të tjera",
+        "details": "Shërbime për të shfaqur përmbajtje web."
+    },
+
+    "google": {
+        "title": "Pëlqim specifik për shërbimet Google",
+        "details": "Google mund të përdorë të dhënat tuaja për matjen e audiencës, performancën e reklamave, ose për t'ju ofruar reklama të personalizuara."
+    },
+    
+    "mandatoryTitle": "Cookies të detyrueshme",
+    "mandatoryText": "Kjo faqe përdor cookies të nevojshme për funksionimin e saj të duhur të cilat nuk mund të çaktivizohen.",
+
+    "save": "Ruaj",
+    "ourpartners": "Partnerët tanë"
+};
