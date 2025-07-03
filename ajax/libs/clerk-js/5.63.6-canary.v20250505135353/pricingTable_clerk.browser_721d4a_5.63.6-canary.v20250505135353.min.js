f52d472b9dffb24ff14b355724d6ee80186a5ed3
robocdnjs <cdnjs-github@cloudflare.com>
2025-07-03 13:16:39 +0000

    Add clerk-js (5.63.6-canary.v20250505135353)

---
+++ ajax/libs/clerk-js/5.63.6-canary.v20250505135353/pricingTable_clerk.browser_721d4a_5.63.6-canary.v20250505135353.min.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunk_clerk_clerk_js=globalThis.webpackChunk_clerk_clerk_js||[]).push([["219"],{9828:function(e,l,c){c.r(l),c.d(l,{__experimental_PricingTable:()=>r.C});var r=c(5006)}}]);
