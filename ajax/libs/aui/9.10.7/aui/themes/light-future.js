bf28d502de88245112e3c1f406dea49fb47c453b
robocdnjs <cdnjs-github@cloudflare.com>
2025-07-08 15:09:09 +0000

    Add aui (9.10.7)

---
+++ ajax/libs/aui/9.10.7/aui/themes/light-future.js
@@ -0,0 +1,2 @@
+"use strict";(window.webpackChunk_auiPrototyping=window.webpackChunk_auiPrototyping||[]).push([[630],{732:(t,a,d)=>{d.r(a),d.d(a,{default:()=>o});const o='\nhtml[data-color-mode="light"][data-theme~="light:light"],\nhtml[data-color-mode="dark"][data-theme~="dark:light"] {\n  color-scheme: light;\n  --ds-background-disabled: #28311B;\n}\n'}}]);
+//# sourceMappingURL=light-future.js.map
