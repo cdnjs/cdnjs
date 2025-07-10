a7fc7b52dc410a1a7985ae7e67c706fa5a3924a2
robocdnjs <cdnjs-github@cloudflare.com>
2025-07-10 17:13:19 +0000

    Add fancyapps-ui (6.0.10)

---
+++ ajax/libs/fancyapps-ui/6.0.10/utils/isPlainObject.js
@@ -0,0 +1,2 @@
+/*! License details at fancyapps.com/license */
+const t=t=>"object"==typeof t&&null!==t&&t.constructor===Object&&"[object Object]"===Object.prototype.toString.call(t);export{t as isPlainObject};
