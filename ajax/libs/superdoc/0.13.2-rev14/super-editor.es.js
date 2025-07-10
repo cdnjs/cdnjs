a493afdf014a6a093f6274273c9cff4591aff578
robocdnjs <cdnjs-github@cloudflare.com>
2025-07-10 16:05:19 +0000

    Add superdoc (0.13.2-rev14)

---
+++ ajax/libs/superdoc/0.13.2-rev14/super-editor.es.js
@@ -0,0 +1,26 @@
+import { A, a, _, C, D, E, b, S, c, d, e, T, f, g, i, h, j, k, l, m, n, o } from "./chunks/super-editor.es-D1LXSpud.es.js";
+import "./chunks/vue-BBJVqxjL.es.js";
+export {
+  A as AIWriter,
+  a as AnnotatorHelpers,
+  _ as BasicUpload,
+  C as CommentsPluginKey,
+  D as DocxZipper,
+  E as Editor,
+  b as Extensions,
+  S as SuperConverter,
+  c as SuperEditor,
+  d as SuperInput,
+  e as SuperToolbar,
+  T as Toolbar,
+  f as TrackChangesBasePluginKey,
+  g as createZip,
+  i as fieldAnnotationHelpers,
+  h as getActiveFormatting,
+  j as getAllowedImageDimensions,
+  k as getMarksFromSelection,
+  l as getRichTextExtensions,
+  m as getStarterExtensions,
+  n as helpers,
+  o as trackChangesHelpers
+};
