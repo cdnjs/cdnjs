6239c4bb6620db2ff359ea2ad064d3f003056fb6
robocdnjs <cdnjs-github@cloudflare.com>
2025-07-13 19:05:00 +0000

    Add fusioncharts (4.2.0-beta.1)

---
+++ ajax/libs/fusioncharts/4.2.0-beta.1/fusioncharts.charts.d.ts
@@ -0,0 +1,8 @@
+
+import { FusionChartStatic } from 'fusioncharts';
+
+declare namespace Charts {}
+declare var Charts: (H: FusionChartStatic) => FusionChartStatic;
+export = Charts;
+export as namespace Charts;
+
