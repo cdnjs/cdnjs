4d7f9883ff84a5e8c66e4528b23612c832f31910
robocdnjs <cdnjs-github@cloudflare.com>
2025-07-21 09:08:17 +0000

    Add fancyapps-ui (6.0.16)

---
+++ ajax/libs/fancyapps-ui/6.0.16/fancybox/l10n/de_DE.js
@@ -0,0 +1,2 @@
+/*! License details at fancyapps.com/license */
+import{de_DE as e}from"../../carousel/l10n/de_DE.js";const n=Object.assign(Object.assign({},e),{CLOSE:"Schließen",NEXT:"Weiter",PREV:"Zurück",MODAL:"Sie können diesen modalen Inhalt mit der ESC-Taste schließen",ELEMENT_NOT_FOUND:"HTML-Element nicht gefunden",IFRAME_ERROR:"Fehler beim Laden der Seite"});export{n as de_DE};
---
+++ ajax/libs/fancyapps-ui/6.0.16/utils/removeClass.js
@@ -0,0 +1,2 @@
+/*! License details at fancyapps.com/license */
+const s=(s,t="")=>{s&&s.classList&&t.split(" ").forEach((t=>{t&&s.classList.remove(t)}))};export{s as removeClass};
