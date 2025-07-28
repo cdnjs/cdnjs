a52b7acf132a23c7fe1858f3e97226ca2667dbf2
robocdnjs <cdnjs-github@cloudflare.com>
2025-07-28 15:05:56 +0000

    Add openlayers (10.6.2-dev.1753710813544)

---
+++ ajax/libs/openlayers/10.6.2-dev.1753710813544/tilegrid/common.d.ts
@@ -0,0 +1,14 @@
+/**
+ * @module ol/tilegrid/common
+ */
+/**
+ * Default maximum zoom for default tile grids.
+ * @type {number}
+ */
+export const DEFAULT_MAX_ZOOM: number;
+/**
+ * Default tile size.
+ * @type {number}
+ */
+export const DEFAULT_TILE_SIZE: number;
+//# sourceMappingURL=common.d.ts.map
