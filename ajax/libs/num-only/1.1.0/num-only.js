d398db1639ca5d2713d60376727850c41272d542
robocdnjs <cdnjs-github@cloudflare.com>
2025-08-10 17:04:30 +0000

    Add num-only (1.1.0)

---
+++ ajax/libs/num-only/1.1.0/num-only.js
@@ -0,0 +1,21 @@
+/**
+ * num-only v1.1.0
+ *
+ * @author Julio L. Muller.
+ * @license MIT - 2020-2021
+ */
+
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+  typeof define === 'function' && define.amd ? define(factory) :
+  (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global.numOnly = factory());
+}(this, (function () { 'use strict';
+
+  function numOnly(target) {
+      return String(target).replace(/\D/g, '');
+  }
+
+  return numOnly;
+
+})));
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibnVtLW9ubHkuanMiLCJzb3VyY2VzIjpbXSwic291cmNlc0NvbnRlbnQiOltdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7OzsifQ==
