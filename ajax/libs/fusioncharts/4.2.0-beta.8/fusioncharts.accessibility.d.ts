24eba3c65b6d3529e3d70668555c99fb4075e1ff
robocdnjs <cdnjs-github@cloudflare.com>
2025-08-21 17:16:07 +0000

    Add fusioncharts (4.2.0-beta.8)

---
+++ ajax/libs/fusioncharts/4.2.0-beta.8/fusioncharts.accessibility.d.ts
@@ -0,0 +1,8 @@
+
+import { FusionChartStatic } from 'fusioncharts';
+
+declare namespace Accessibility {}
+declare var Accessibility: (H: FusionChartStatic) => FusionChartStatic;
+export = Accessibility;
+export as namespace Accessibility;
+
