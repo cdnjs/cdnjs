78e4a9897638ef409ab8f65a8b5112955fcda408
robocdnjs <cdnjs-github@cloudflare.com>
2025-08-21 13:20:39 +0000

    Add intl-tel-input (25.4.1)

---
+++ ajax/libs/intl-tel-input/25.4.1/build/js/i18n/uz/interface.js
@@ -0,0 +1,13 @@
+const interfaceTranslations = {
+  selectedCountryAriaLabel: "Tanlangan davlat",
+  noCountrySelected: "Davlat tanlanmagan",
+  countryListAriaLabel: "Davlatlar roʻyxati",
+  searchPlaceholder: "Davlatni qidiring",
+  zeroSearchResults: "Natija topilmadi",
+  oneSearchResult: "1-ta natija topildi",
+  multipleSearchResults: "${count}-ta natija topildi",
+  // qo‘shimcha davlatlar (country-list kutubxonasi tomonidan qo‘llab-quvvatlanmaydi)
+  ac: "Ascension oroli",
+  xk: "Kosovo"
+};
+export default interfaceTranslations;
