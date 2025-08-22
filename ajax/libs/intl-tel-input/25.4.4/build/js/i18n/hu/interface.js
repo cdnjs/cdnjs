97b0b973df3d825f6470f43c32a8f48be85c826e
robocdnjs <cdnjs-github@cloudflare.com>
2025-08-22 11:06:38 +0000

    Add intl-tel-input (25.4.4)

---
+++ ajax/libs/intl-tel-input/25.4.4/build/js/i18n/hu/interface.js
@@ -0,0 +1,13 @@
+const interfaceTranslations = {
+  selectedCountryAriaLabel: "Kiválasztott ország",
+  noCountrySelected: "Nincs ország kiválasztva",
+  countryListAriaLabel: "Országok listája",
+  searchPlaceholder: "Keresés",
+  zeroSearchResults: "Nincs találat",
+  oneSearchResult: "1 találat",
+  multipleSearchResults: "${count} találat",
+  // additional countries (not supported by country-list library)
+  ac: "Ascension-sziget",
+  xk: "Koszovó"
+};
+export default interfaceTranslations;
