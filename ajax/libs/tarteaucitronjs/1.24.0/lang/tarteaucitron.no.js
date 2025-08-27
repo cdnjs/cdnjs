7b837bae63e0a0a22245377495258c2a95d651fc
robocdnjs <cdnjs-github@cloudflare.com>
2025-08-27 17:17:08 +0000

    Add tarteaucitronjs (1.24.0)

---
+++ ajax/libs/tarteaucitronjs/1.24.0/lang/tarteaucitron.no.js
@@ -0,0 +1,94 @@
+/*global tarteaucitron */
+/* min ready */
+tarteaucitron.lang = {
+	"middleBarHead" : "☝️ 🍪",
+	"adblock"       : "Hei! Dette nettstedet er gjennomsiktig og lar deg kontrollere hvilke tredjeparts tjenester du vil tillate.",
+	"adblock_call"  : "For å gjøre endringer, vær så snill å deaktivere annonse-blokkering.",
+	"reload"        : "Oppdater side",
+
+	"alertBigScroll" : "Ved å fortsette å scrolle,",
+	"alertBigClick"  : "Dersom du fortsetter å bruke dette nettstedet,",
+	"alertBig"       : "tillater du alle tredjeparts tjenester",
+
+	"alertBigPrivacy" : "Dette nettstedet bruker informasjonskapsler og gir deg kontroll over hva du vil aktivere",
+	"alertSmall"      : "Administrer tjenester",
+	"personalize"     : "Personaliser",
+	"acceptAll"       : "OK, aksepter alt",
+	"close"           : "Steng",
+    "closeBanner"     : "Skjul informasjonskapselbanner",
+
+	"privacyUrl" : "Personvernregler",
+
+	"all" : "Preferanse for alle tjenester",
+
+	"info"             : "Beskytt ditt personvern",
+	"disclaimer"       : "Ved å tillate disse tredjepartstjenestene godtar du informasjonskapslene deres og bruken av sporingsteknologier som er nødvendige for at de skal fungere korrekt.",
+	"allow"            : "Tillat",
+	"deny"             : "Ikke tillat",
+	"noCookie"         : "Denne tjenesten bruker ikke informasjonskapsel.",
+	"useCookie"        : "Denne tjenesten kan installeres",
+	"useCookieCurrent" : "Denne tjenesten er installert",
+	"useNoCookie"      : "TDenne tjenesten har ikke installert noen informasjonskapsel.",
+	"more"             : "Les mer",
+	"source"           : "Se den offisielle nettsiden",
+	"credit"           : "Informasjonskapsler styres av tarteaucitron.js",
+	"noServices"       : "Dette nettstedet bruker ingen informasjonskapsler som krever ditt samtykke.",
+
+	"toggleInfoBox" : "Vis / skjul informasjon om lagring av informasjonskapsler",
+	"title"         : "Panel for informasjonskapsler",
+	"cookieDetail"  : "Informasjon om informasjonskapsler for",
+	"ourSite"       : "på nettstedet vårt",
+	"newWindow"     : "(nytt vindu)",
+	"allowAll"      : "Tillat alle informasjonskapsler",
+	"denyAll"       : "Nekt alle informasjonskapsler",
+
+    "icon": "Cookies",
+
+	"fallback" : "er skrudd av.",
+	"allowed": "tillatt",
+    "disallowed": "ikke tillatt",
+
+	"ads"      : {
+		"title"   : "Annonsenettverk",
+		"details" : "Annonsenettverket kan generere inntekter ved å selge reklameplass på nettstedet."
+	},
+	"analytic" : {
+		"title"   : "Målgruppe målinger",
+		"details" : "Målgruppens målingstjenester ble brukt til å generere nyttig informasjon for å forbedre nettstedet."
+	},
+	"social"   : {
+		"title"   : "Sosiale nettverk",
+		"details" : "Sosiale nettverk kan forbedre brukervennligheten til nettstedet og bidra til å markedsføre det."
+	},
+	"video"    : {
+		"title"   : "Video",
+		"details" : "Videodelingstjenester hjelper til med å legge til rik media på nettstedet og øke synligheten."
+	},
+	"comment"  : {
+		"title"   : "Kommentarer",
+		"details" : "Kommentaradministratorer legger til rette for arkivering av kommentarer og bekjemper spam."
+	},
+	"support"  : {
+		"title"   : "Brukerstøtte",
+		"details" : "Brukerstøtte lar deg komme i kontakt med nettstedsteamet og bidra til å forbedre nettstedet."
+	},
+	"api"      : {
+		"title"   : "API-er",
+		"details" : "API-er brukes til å laste inn skript: geolokalisering, søkemotorer, oversettelser, ..."
+	},
+	"other"    : {
+		"title"   : "Annet",
+		"details" : "Tjenester for å vise innhold på nettet."
+	},
+
+	"google": {
+		"title": "Spesifikt samtykke for Google-tjenester",
+		"details": "Google kan bruke dataene dine til måling av publikum, reklameprestasjoner eller til å tilby deg personlig tilpassede annonser."
+	},
+
+	"mandatoryTitle" : "Obligatoriske informasjonskapsler",
+	"mandatoryText"  : "Dette nettstedet bruker obligatoriske informasjonskapsler som er nødvendige for at nettstedet skal fungere som det skal. Disse kan ikke deaktiveres.",
+
+	"save": "Lagre",
+	"ourpartners": "Våre partnere"
+};
---
+++ ajax/libs/tarteaucitronjs/1.24.0/lang/tarteaucitron.sk.js
@@ -0,0 +1,95 @@
+/*global tarteaucitron */
+/* min ready */
+tarteaucitron.lang = {
+    "middleBarHead": "☝️ 🍪",
+    "adblock": "Ahoj! Táto stránka je transparentná a umožňuje vám vybrať služby tretích strán, ktoré chcete povoliť.",
+    "adblock_call": "Prosím, vypnite blokovanie reklám k začatiu prispôsobovania",
+    "reload": "Obnovte stránku",
+    
+    "alertBigScroll": "Pokračovaním v posúvaní,",
+    "alertBigClick": "Ak budete pokračovať v prehliadaní tejto webovej stránky,",
+    "alertBig": "povoľujete všetky služby tretích strán",
+    
+    "alertBigPrivacy": "Táto stránka používa cookies a dáva vám kontrolu nad tým, čo chcete aktivovať",
+    "alertSmall": "Spravovať služby",
+    "personalize": "Prispôsobiť",
+    "acceptAll": "OK, prijať všetko",
+    "close": "Zatvoriť",
+    "closeBanner": "Skryť banner so súbormi cookie",
+
+    "privacyUrl": "Zásady ochrany osobných údajov",
+    
+    "all": "Prednosť pre všetky služby",
+
+    "info": "Ochrana vášho súkromia",
+    "disclaimer": "Povolením týchto služieb tretích strán, prijímate ich cookies a používanie sledovacích technológií potrebných pre ich správne fungovanie.",
+    "allow": "Povoliť",
+    "deny": "Odmietnúť",
+    "noCookie": "Táto služba nepoužíva cookies.",
+    "useCookie": "Túto službu je možné nainštalovať",
+    "useCookieCurrent": "Táto služba je nainštalovaná",
+    "useNoCookie": "Táto služba nenainštalovala žiadny súbor cookie.",
+    "more": "Čítaj viac",
+    "source": "Pozrite si oficiálnu webovú stránku",
+    "credit": "Správca súborov cookie od tarteaucitron.js",
+    "noServices": "Táto webová stránka nepoužíva žiadny súbor cookie, ktorý vyžaduje váš súhlas.",
+
+    "toggleInfoBox": "Zobraziť/skryť informácie o ukladaní súborov cookie",
+    "title": "Panel riadenia súborov cookie",
+    "cookieDetail": "Podrobnosti súboru cookie pre",
+    "ourSite": "na našich stránkach",
+    "modalWindow": "(modálne okno)",
+    "newWindow": "(nové okno)",
+    "allowAll": "Povoľte všetky súbory cookie",
+    "denyAll": "Odmietnuť všetky súbory cookie",
+
+    "icon": "Cookies",
+    
+    "fallback": "je zakázané.",
+    "allowed": "povolený",
+    "disallowed": "nepovolený",
+
+    "ads": {
+        "title": "Reklamná sieť",
+        "details": "Reklamné siete môžu generovať príjmy predajom reklamného priestoru na webe."
+    },
+    "analytic": {
+        "title": "Meranie publika",
+        "details": "Služby merania publika používané na generovanie užitočnej štatistickej účasti na zlepšenie stránky."
+    },
+    "social": {
+        "title": "Sociálne siete",
+        "details": "Sociálne siete môžu zlepšiť použiteľnosť stránky a pomôcť ju propagovať prostredníctvom akcií."
+    },
+    "video": {
+        "title": "Videá",
+        "details": "Služby zdieľania videa pomáhajú pridať na web bohatý obsah a zvýšiť jeho viditeľnosť."
+    },
+    "comment": {
+        "title": "Komentáre",
+        "details": "Manažéri komentárov uľahčujú zadávanie komentárov a bojujú proti spamu."
+    },
+    "support": {
+        "title": "Podpora",
+        "details": "Podporné služby vám umožňujú skontaktovať sa s tímom stránok a pomôcť vám ich vylepšiť."
+    },
+    "api": {
+        "title": "APIs",
+        "details": "Rozhrania API sa používajú na načítanie skriptov: geolokácia, vyhľadávače, preklady, ..."
+    },
+    "other": {
+        "title": "Ostatné",
+        "details": "Služby na zobrazovanie webového obsahu."
+    },
+
+    "google": {
+        "title": "Špecifický súhlas so službami Google",
+        "details": "Google môže použiť vaše údaje na meranie publika, reklamnú efektivitu alebo na vám ponúkanie personalizovaných reklám."
+    },
+
+    "mandatoryTitle": "Povinné súbory cookie",
+    "mandatoryText": "Táto stránka používa súbory cookie, ktoré sú nevyhnutné pre jej správne fungovanie a nemôžu byť deaktivované.",
+
+    "save": "Uložiť",
+    "ourpartners": "Naši partneri"
+};
