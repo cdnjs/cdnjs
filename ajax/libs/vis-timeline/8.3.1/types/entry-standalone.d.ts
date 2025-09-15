43a33bba47f16b9c3e1ee69076c4fc58ba5ccf62
robocdnjs <cdnjs-github@cloudflare.com>
2025-09-15 20:04:56 +0000

    Add vis-timeline (8.3.1)

---
+++ ajax/libs/vis-timeline/8.3.1/types/entry-standalone.d.ts
@@ -0,0 +1,14 @@
+import { DataInterface, DataSet, DataView, Queue } from "vis-data";
+export { DataInterface, DataSet, DataView, Queue };
+
+import * as util from "vis-util/esnext";
+export { util };
+
+import moment from "moment";
+export { moment };
+import Hammer from "hammerjs";
+export { Hammer };
+import keycharm from "keycharm";
+export { keycharm };
+
+export * from "./entry-esnext";
