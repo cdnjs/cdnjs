62c7d0b7cf979da3b20e67468b2838231e5e5716
robocdnjs <cdnjs-github@cloudflare.com>
2025-09-19 12:01:37 +0000

    Add openlayers (10.6.2-dev.1758267478938)

---
+++ ajax/libs/openlayers/10.6.2-dev.1758267478938/geom/flat/length.d.ts
@@ -0,0 +1,20 @@
+/**
+ * @module ol/geom/flat/length
+ */
+/**
+ * @param {Array<number>} flatCoordinates Flat coordinates.
+ * @param {number} offset Offset.
+ * @param {number} end End.
+ * @param {number} stride Stride.
+ * @return {number} Length.
+ */
+export function lineStringLength(flatCoordinates: Array<number>, offset: number, end: number, stride: number): number;
+/**
+ * @param {Array<number>} flatCoordinates Flat coordinates.
+ * @param {number} offset Offset.
+ * @param {number} end End.
+ * @param {number} stride Stride.
+ * @return {number} Perimeter.
+ */
+export function linearRingLength(flatCoordinates: Array<number>, offset: number, end: number, stride: number): number;
+//# sourceMappingURL=length.d.ts.map
