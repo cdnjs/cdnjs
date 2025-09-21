1d5ea63eaf111d82d9e06ef3cf85455475ab1dc0
robocdnjs <cdnjs-github@cloudflare.com>
2025-09-21 16:06:23 +0000

    Add view-ui-plus (1.3.21)

---
+++ ajax/libs/view-ui-plus/1.3.21/locale/lang.js
@@ -0,0 +1,1 @@
+const e=typeof window!="undefined";function n(i){e&&typeof window.viewuiplus!="undefined"&&("langs"in viewuiplus||(viewuiplus.langs={}),viewuiplus.langs[i.i.locale]=i)}export{n as s};
