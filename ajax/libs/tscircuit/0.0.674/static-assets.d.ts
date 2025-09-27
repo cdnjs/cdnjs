746bfdf769cc7d6bfb76b2debaf0619b8c6a4163
robocdnjs <cdnjs-github@cloudflare.com>
2025-09-27 13:08:37 +0000

    Add tscircuit (0.0.674)

---
+++ ajax/libs/tscircuit/0.0.674/static-assets.d.ts
@@ -0,0 +1,29 @@
+declare module "*.obj" {
+  const src: string
+  export default src
+}
+
+declare module "*.stl" {
+  const src: string
+  export default src
+}
+
+declare module "*.kicad_mod" {
+  const src: string
+  export default src
+}
+
+declare module "*.glb" {
+  const src: string
+  export default src
+}
+
+declare module "*.gltf" {
+  const src: string
+  export default src
+}
+
+declare module "*.step" {
+  const src: string
+  export default src
+}
