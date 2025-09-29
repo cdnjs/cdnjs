bfa6022a6d0ca3ad5c69496e7bbe3aeab1902ba1
robocdnjs <cdnjs-github@cloudflare.com>
2025-09-29 13:13:45 +0000

    Add descope-web-component (3.47.9)

---
+++ ajax/libs/descope-web-component/3.47.9/esm/descope-wc/index.js
@@ -0,0 +1,2 @@
+import e from"./DescopeWc.js";customElements.get("descope-wc")?console.log("descope-wc is already defined"):customElements.define("descope-wc",e);export{e as default};
+//# sourceMappingURL=index.js.map
